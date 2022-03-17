08bb6fbb9c73eecffefe4f621ae00ad02e9915fe
Tom Vaughan <tom.vaughan@madetech.com>
2022-03-17 11:31:07 +0000

    WIP - Playing around with adding trigger configurations to allow specific forces/courts to ignore triggers

--- src/use-cases/generateTriggers.test.ts
+++ src/use-cases/generateTriggers.test.ts
@@ -22,4 +22,32 @@ describe("generateTriggers", () => {
       { code: TriggerCode.TRPR0012 }
     ])
   })
+
+  it("should generate multipe triggers including TRPR0015", () => {
+    const data = parseFile("input-message-003.xml") //TODO: Use correct testdata
+    const result = generateTriggers(data, true)
+    expect(result).toHaveLength(4)
+    expect(result).toStrictEqual([
+      { code: TriggerCode.TRPR0001, offenceSequenceNumber: 1 },
+      { code: TriggerCode.TRPR0001, offenceSequenceNumber: 3 },
+      { code: TriggerCode.TRPR0006 },
+      { code: TriggerCode.TRPR0015 }
+    ])
+  })
+
+  // it("should generate triggers when allowed by a force" () => {})
+
+  // it("should generate triggers when allowed by a court" () => {})
+
+  // it("shouldn't generate triggers when prohibited by a force" () => {})
+
+  // it("shouldn't generate triggers when prohibited by a court" () => {})
+
+  // it("should generate triggers when allowed by both a force and a court" () => {})
+
+  // it("shouldn't generate triggers when prohibited by both a force and a court" () => {})
+
+  // it("should generate triggers when allowed by a force and prohibited by a court" () => {})
+
+  // it("should generate triggers when allowed by a court and prohibited by a force" () => {})
 })
--- src/use-cases/generateTriggers.ts
+++ src/use-cases/generateTriggers.ts
@@ -3,6 +3,21 @@ import type { AnnotatedHearingOutcome } from "../types/AnnotatedHearingOutcome"
 import type { Trigger } from "../types/Trigger"
 import type { TriggerGenerator } from "../types/TriggerGenerator"
 
+//TODO: Replace with actual trigger.properties and
+// const excludedTriggers = {
+//   "01": ["TRPR0001", "TRPR0002"],
+//   "02": ["TRPR0001", "TRPR0005"],
+//   B01DU: ["TRPR0001", "TRPR0002"]
+// }
+
+const generateConfiguredTriggers = (courtResult: AnnotatedHearingOutcome, recordable: boolean): Trigger[] => {
+  const t: Trigger[] = Object.values(triggers).reduce((acc: Trigger[], trigger: TriggerGenerator) => {
+    console.log(`${courtResult}${trigger}`) //TODO: Replace with call to specific triggers
+    return acc.concat(trigger.generate(courtResult, recordable))
+  }, [])
+  return t
+}
+
 export default (courtResult: AnnotatedHearingOutcome, recordable: boolean): Trigger[] => {
   const independentTriggers = Object.values(triggers)
     .filter((t) => t.independent)
@@ -10,11 +25,15 @@ export default (courtResult: AnnotatedHearingOutcome, recordable: boolean): Trig
       return acc.concat(trigger.generate(courtResult, recordable))
     }, [])
 
-  const dependentTriggers = Object.values(triggers)
-    .filter((t) => !t.independent)
-    .reduce((acc: Trigger[], trigger: TriggerGenerator) => {
-      return acc.concat(trigger.generate(courtResult, recordable, independentTriggers))
-    }, [])
+  const trigger15 = triggers.TRPR0015.generate(courtResult, recordable)
 
-  return independentTriggers.concat(dependentTriggers)
+  const configuredTriggers = generateConfiguredTriggers(courtResult, recordable)
+
+  // const dependentTriggers = Object.values(triggers)
+  //   .filter((t) => !t.independent)
+  //   .reduce((acc: Trigger[], trigger: TriggerGenerator) => {
+  //     return acc.concat(trigger.generate(courtResult, recordable, independentTriggers))
+  //   }, [])
+
+  return independentTriggers.concat(trigger15, configuredTriggers)
 }
