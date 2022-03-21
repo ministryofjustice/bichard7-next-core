82a075f21aaad31c3974b26243accdeeca41c9c6
Ben Pirt <ben@pirt.co.uk>
2022-03-21 10:02:01 +0000

    Add skeleton TRPR0027

---
+++ src/triggers/TRPR0027.ts
@@ -0,0 +1,8 @@
+import type { TriggerGenerator } from "../types/TriggerGenerator"
+
+const generator: TriggerGenerator = {
+  independent: false,
+  generate: () => []
+}
+
+export default generator
