a1ed9a0cbed14f4d943fee06a5b2ba293204fd00
csabagyorfi <csaba@madetech.com>
2022-03-24 10:53:51 +0000

    Call enrichCourt function in the enrich hearing outcome function

--- src/use-cases/enrichHearingOutcome/index.ts
+++ src/use-cases/enrichHearingOutcome/index.ts
@@ -1,7 +1,7 @@
 import type { AnnotatedHearingOutcome } from "src/types/AnnotatedHearingOutcome"
 import type { EnrichAhoFunction } from "src/types/EnrichAhoFunction"
 import type PncGateway from "src/types/PncGateway"
-import { enrichDefendant, enrichWithPncQuery } from "./enrichFunctions"
+import { enrichCourt, enrichDefendant, enrichWithPncQuery } from "./enrichFunctions"
 
 const enrichHearingOutcome = (
   hearingOutcome: AnnotatedHearingOutcome,
@@ -9,7 +9,7 @@ const enrichHearingOutcome = (
 ): AnnotatedHearingOutcome => {
   const pncEnrich: EnrichAhoFunction = (aho) => enrichWithPncQuery(aho, pncGateway)
 
-  const enrichSteps: EnrichAhoFunction[] = [enrichDefendant, pncEnrich]
+  const enrichSteps: EnrichAhoFunction[] = [enrichCourt, enrichDefendant, pncEnrich]
 
   return enrichSteps.reduce((aho, fn) => fn(aho), hearingOutcome)
 }
