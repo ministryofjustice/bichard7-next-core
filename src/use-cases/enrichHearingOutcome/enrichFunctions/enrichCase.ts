305781a0e01acef646a6bd91b3a2cc62620bbdd3
csabagyorfi <csaba@madetech.com>
2022-03-24 16:52:44 +0000

    Enrich Case.CourtReference

--- src/use-cases/enrichHearingOutcome/enrichFunctions/enrichCase.ts
+++ src/use-cases/enrichHearingOutcome/enrichFunctions/enrichCase.ts
@@ -10,16 +10,24 @@ const enrichCase: EnrichAhoFunction = (hearingOutcome) => {
   const ahoCase = hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case
   if (ahoCase) {
     ahoCase.RecordableOnPNCindicator = ahoCase.HearingDefendant?.Offence.some((offence) => recordable(offence))
+
     const mostUrgentResult = Math.min(
       ahoCase.HearingDefendant.Offence.map((offence) => offence.Result.map((result) => result.Urgent?.urgency))
         .flat()
         .filter((x) => x)
         .push(Number.MAX_SAFE_INTEGER)
     )
+
     if (mostUrgentResult < HALF_LIFE_HOURS_URGENT_THRESHOLD) {
       ahoCase.Urgent = {
         urgency: mostUrgentResult,
         urgent: "Y"
+      }
+    }
+
+    if (ahoCase.CourtReference?.MagistratesCourtReference?.toUpperCase() === "Truncated".toUpperCase()) {
+      if (ahoCase.PTIURN) {
+        ahoCase.CourtReference.MagistratesCourtReference = ahoCase.PTIURN
       }
     }
   }
