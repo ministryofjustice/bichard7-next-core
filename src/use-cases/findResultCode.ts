c41fe33fbe975f766c2d304d2d7b0aa218ab5012
csabagyorfi <csaba@madetech.com>
2022-05-11 09:52:40 +0100

    Use ResultCode type from bichard7-next-data package

--- src/use-cases/findResultCode.ts
+++ src/use-cases/findResultCode.ts
@@ -1,5 +1,5 @@
 import data from "@moj-bichard7-developers/bichard7-next-data"
-import type ResultCode from "src/types/ResultCode"
+import type { ResultCode } from "@moj-bichard7-developers/bichard7-next-data/types/types"
 
 export default (code: number): ResultCode => {
   const resultCode = (data.resultCode as ResultCode[]).find(
