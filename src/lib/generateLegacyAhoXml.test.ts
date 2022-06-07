a5f46f7387ae4b8639f59c5b322099610de87e6b
csabagyorfi <csaba@madetech.com>
2022-06-07 15:06:48 +0100

    Fix generateLegacyAhoXml unit test
    The generated result has a dependency on current date

--- src/lib/generateLegacyAhoXml.test.ts
+++ src/lib/generateLegacyAhoXml.test.ts
@@ -2,9 +2,12 @@ import fs from "fs"
 import "jest-xml-matcher"
 import generateMessage from "tests/helpers/generateMessage"
 import processMessage from "tests/helpers/processMessage"
+import MockDate from "mockdate"
 
 describe("generateLegacyAhoXml", () => {
   it.ifNewBichard("should generate legacy xml from aho", async () => {
+    MockDate.set(new Date("2022-06-06").getTime())
+
     const xml = fs.readFileSync("test-data/generated-aho.xml").toString()
 
     const inputMessage = generateMessage({
@@ -13,5 +16,7 @@ describe("generateLegacyAhoXml", () => {
     const { ahoXml } = await processMessage(inputMessage)
 
     expect(ahoXml).toEqualXML(xml)
+
+    MockDate.reset()
   })
 })
