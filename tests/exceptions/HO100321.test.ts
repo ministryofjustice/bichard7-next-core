e5a910995c46283dceefbd95020b49341f2bdbee
csabagyorfi <csaba@madetech.com>
2022-06-13 22:11:58 +0100

    Only add HO100321 when offence is recordable offence

--- tests/exceptions/HO100321.test.ts
+++ tests/exceptions/HO100321.test.ts
@@ -4,14 +4,14 @@ import generateMessage from "tests/helpers/generateMessage"
 import PostgresHelper from "tests/helpers/PostgresHelper"
 import processMessage from "tests/helpers/processMessage"
 
+const dummyASN = "0807NRPR00000038482H"
+
 describe("HO100321", () => {
   afterAll(() => {
     PostgresHelper.closeConnection()
   })
 
   it("should create an exception when there is a recordable offence but the Arrest Summons Number is a dummy", async () => {
-    const dummyASN = "0807NRPR00000038482H"
-
     // Generate a mock message
     const inputMessage = generateMessage({
       ASN: dummyASN,
@@ -31,5 +31,27 @@ describe("HO100321", () => {
         path: ["AnnotatedHearingOutcome", "HearingOutcome", "Case", "HearingDefendant", "ArrestSummonsNumber"]
       }
     ])
+  })
+
+  it("should not create an exception when there is no recordable offence", async () => {
+    const nonRecordableOffenceCode = "BA76004"
+
+    const inputMessage = generateMessage({
+      ASN: dummyASN,
+      offences: [
+        {
+          code: nonRecordableOffenceCode,
+          results: [{ code: 4592 }],
+          recordable: false
+        }
+      ]
+    })
+
+    const { exceptions } = await processMessage(inputMessage, {
+      expectTriggers: false,
+      recordable: false
+    })
+
+    expect(exceptions).toHaveLength(0)
   })
 })
