197fbee68fd7ab031feecb20322348bc0fd86e1c
csabagyorfi <csaba@madetech.com>
2022-06-13 15:17:56 +0100

    Update validateASN to catch dummyASN patterns

--- src/use-cases/validations.ts
+++ src/use-cases/validations.ts
@@ -21,11 +21,43 @@ import {
   lookupYesNoByCjsCode
 } from "./dataLookup"
 
+const dummyASNPatterns = [
+  "0800(N|P)P01[0-9]{11}[A-HJ-NP-RT-Z]{1}",
+  "[0-9]{4}NRPR[0-9A-Z]{12}",
+  "[0-9]{2}12LN00[0-9]{11}[A-HJ-NP-RT-Z]{1}",
+  "[0-9]{2}00NP00[0-9]{11}[A-HJ-NP-RT-Z]{1}",
+  "[0-9]{2}6300[0-9]{13}[A-HJ-NP-RT-Z]{1}",
+  "[0-9]{2}06SS[0-9A-Z]{2}[0-9]{11}[A-HJ-NP-RT-Z]{1}",
+  "[0-9]{2}00XX[0-9A-Z]{2}[0-9]{11}[A-HJ-NP-RT-Z]{1}",
+  "[0-9]{2}50(11|12|21|41|42|43|OF)[0-9A-Z]{2}[0-9]{11}[A-HJ-NP-RT-Z]{1}",
+  "[0-9]{2}50(11|12|21|41|42|43|OF|SJ)[0-9A-Z]{2}[0-9]{11}[A-HJ-NP-RT-Z]{1}"
+]
+
 const validateRemandStatus = (data: string): boolean => remandStatus.some((el) => el.cjsCode === data)
 
-const validateASN = (data: string): boolean => {
+const validateASNFormat = (data: string): boolean => {
   const asn = new ASN(data)
   return !!data.match(/[0-9]{2}[A-Z0-9]{6,7}[0-9]{11}[A-HJ-NP-RT-Z]{1}/) && asn.checkCharacter() === data.slice(-1)
+}
+
+const dummyASN = (data: string): boolean => {
+  let result = false
+
+  dummyASNPatterns.forEach((p) => {
+    if (data.match(p)) {
+      result = true
+    }
+  })
+
+  return result
+}
+
+const validateASN = (data: string, ctx: z.RefinementCtx): void => {
+  if (dummyASN(data)) {
+    ctx.addIssue({ code: z.ZodIssueCode.custom, message: ExceptionCode.HO100321 })
+  } else if (!validateASNFormat(data)) {
+    ctx.addIssue({ code: z.ZodIssueCode.custom, message: ExceptionCode.HO100206 })
+  }
 }
 
 const validateResultCode = (data: number, ctx: z.RefinementCtx): void => {
