97683a1da3706d93d6f49d6dc1deeb8fe2721d3e
Ben Pirt <ben@pirt.co.uk>
2022-06-24 09:14:06 +0100

    Fix the excluded triggers test config

--- src/lib/excludedTriggerConfig.ts
+++ src/lib/excludedTriggerConfig.ts
@@ -1,7 +1,9 @@
 import { excludedTriggerConfig } from "@moj-bichard7-developers/bichard7-next-data"
+import { TriggerCode } from "src/types/TriggerCode"
 
 if (process.env.NODE_ENV === "test" && "01" in excludedTriggerConfig) {
   delete excludedTriggerConfig["01"]
+  excludedTriggerConfig["42"] = [TriggerCode.TRPR0001]
 }
 
 export default excludedTriggerConfig
