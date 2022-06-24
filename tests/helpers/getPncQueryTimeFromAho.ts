1c9ef6ed3a777359bac4bd97b1b611a1d91fe7f6
Ben Pirt <ben@pirt.co.uk>
2022-06-24 10:39:58 +0100

    Fix incorrectly set pncQueryTime

--- tests/helpers/getPncQueryTimeFromAho.ts
+++ tests/helpers/getPncQueryTimeFromAho.ts
@@ -1,7 +1,7 @@
 import { XMLParser } from "fast-xml-parser"
 import type { RawAho } from "src/types/RawAho"
 
-const getPncQueryTimeFromAho = (ahoXml: string): Date => {
+const getPncQueryTimeFromAho = (ahoXml: string): Date | undefined => {
   const parser = new XMLParser({
     ignoreAttributes: false,
     alwaysCreateTextNode: true
@@ -9,7 +9,7 @@ const getPncQueryTimeFromAho = (ahoXml: string): Date => {
   const rawParsedObj = parser.parse(ahoXml) as RawAho
   const queryTime = rawParsedObj["br7:AnnotatedHearingOutcome"]?.["br7:PNCQueryDate"]
   if (!queryTime) {
-    return new Date()
+    return
   }
   return new Date(queryTime["#text"])
 }
