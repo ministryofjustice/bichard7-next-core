ab64342ac44bffb27f2f0be3023896432b1b6722
Emad Karamad <emad.karamad@madetech.com>
2022-06-28 08:53:23 +0100

    Add ExceptionPath type

--- src/types/Exception.ts
+++ src/types/Exception.ts
@@ -1,11 +1,14 @@
 import { z } from "zod"
 import { ExceptionCode } from "./ExceptionCode"
 
+const exceptionPathSchema = z.array(z.number().or(z.string()))
+
 export const exceptionSchema = z.object({
   code: z.nativeEnum(ExceptionCode),
-  path: z.array(z.number().or(z.string()))
+  path: exceptionPathSchema
 })
 
-export type Exception = z.infer<typeof exceptionSchema>
+export type ExceptionPath = z.infer<typeof exceptionPathSchema>
+type Exception = z.infer<typeof exceptionSchema>
 
 export default Exception
