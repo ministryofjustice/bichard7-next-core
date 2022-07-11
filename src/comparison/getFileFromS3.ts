a01dc543aea465ef6b8c74e03ba9762db80998e3
Jamie Davies <jamie.davies@madetech.com>
2022-07-11 11:21:08 +0100

    Retry downloading a file from S3 up to 5 times before failing

--- src/comparison/getFileFromS3.ts
+++ src/comparison/getFileFromS3.ts
@@ -1,6 +1,7 @@
 import type { S3ClientConfig } from "@aws-sdk/client-s3"
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3"
 import type { Readable } from "stream"
+import { isError } from "./Types"
 
 const streamToBuffer = (stream: Readable) =>
   new Promise<Buffer>((resolve, reject) => {
@@ -13,15 +14,17 @@ const streamToBuffer = (stream: Readable) =>
 const getFileFromS3 = async (fileName: string, bucket: string, config: S3ClientConfig): Promise<string | Error> => {
   const client = new S3Client(config)
   const command = new GetObjectCommand({ Bucket: bucket, Key: fileName })
-  const response = await client.send(command).catch((err: Error) => err)
 
-  if ("Body" in response) {
-    const stream = response.Body as Readable
-    const buffer = await streamToBuffer(stream)
-    return buffer.toString()
+  for (let retries = 0; retries < 5; retries++) {
+    const response = await client.send(command).catch((err: Error) => err)
+
+    if (response && !isError(response) && "Body" in response) {
+      const stream = response.Body as Readable
+      const buffer = await streamToBuffer(stream)
+      return buffer.toString()
+    }
   }
 
-  console.error(response)
   return new Error("Couldn't retrieve file from S3")
 }
 
