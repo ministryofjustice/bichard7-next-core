b0e4e1f8083af87d3e717a6f9ab57d5b6ae86846
Emad Karamad <emad.karamad@madetech.com>
2022-07-22 13:41:18 +0100

    Add script to install standing data versions

---
+++ scripts/install-standing-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSIONS=$(npm view @moj-bichard7-developers/bichard7-next-data versions | tr "'" '"')
+
+# Set minimum package version required
+declare -i MAJOR=2 MINOR=0 PATCH=20
+declare -i FROM_VERSION=MAJOR*10000+MINOR*100+PATCH
+
+ALL_VERSIONS=$(echo $VERSIONS | jq -r ".[] | { version: ., parsedVersion: (. | split(\".\")) } | .parsedVersion[] |= tonumber | select((.parsedVersion[0] * 10000) + (.parsedVersion[1] * 100) + .parsedVersion[2] >= $FROM_VERSION) | { c: (\" bichard7-next-data-\" + .version + \"@npm:@moj-bichard7-developers/bichard7-next-data@\" + .version) } | .c")
+LATEST_VERSION=$(echo $VERSIONS | jq -rj ". | last | { c: (\" bichard7-next-data-latest@npm:@moj-bichard7-developers/bichard7-next-data@\" + .) } | .c")
+
+npm i $ALL_VERSIONS$LATEST_VERSION
