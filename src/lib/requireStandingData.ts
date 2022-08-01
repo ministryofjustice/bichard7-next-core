f507c78076547d6549a15132902e09e06b401688
Tom Vaughan <tomvaughan77@gmail.com>
2022-08-01 12:35:56 +0100

    Slightly ugly fix to support importing standing data for both webpack and npm package usecases

--- src/lib/requireStandingData.ts
+++ src/lib/requireStandingData.ts
@@ -6,12 +6,29 @@ import logger from "./logging"
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 type StandingData = typeof import("bichard7-next-data-latest").default
 
+const importDataRepoVersion = (version: string, acc: KeyValue<StandingData>): KeyValue<StandingData> => {
+  try {
+    // Webpack friendly format for import
+    acc[version] = require(`node_modules/bichard7-next-data-${version}/dist`)
+  } catch (e) {
+    logger.debug(`Webpack import of bichard7-next-data-${version} failed, retrying in npm format: ${e}`)
+    try {
+      // Npm friendly format for import
+      acc[version] = require(`bichard7-next-data-${version}/dist`)
+    } catch (e1) {
+      logger.warn(`Npm and Webpack formatted import of bichard7-next-data-${version} failed: ${e1}`)
+    } finally {
+      return acc
+    }
+  } finally {
+    return acc
+  }
+}
+
 const versions = Object.keys(packageInfo.dependencies)
   .filter((d) => d.startsWith("bichard7-next-data-"))
   .reduce((acc: KeyValue<StandingData>, v) => {
-    const version = v.replace("bichard7-next-data-", "")
-    acc[version] = require(`node_modules/bichard7-next-data-${version}/dist`)
-    return acc
+    return importDataRepoVersion(v.replace("bichard7-next-data-", ""), acc)
   }, {})
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
