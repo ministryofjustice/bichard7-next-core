fb143f0cf7152073353ae7f10a1d7f538711f140
csabagyorfi <csaba@madetech.com>
2022-08-31 15:03:02 +0100

    Change eventSourceQueueName and eventSourceArn to optional

--- src/types/AuditLogEvent.ts
+++ src/types/AuditLogEvent.ts
@@ -4,8 +4,8 @@ export default interface AuditLogEvent {
   timestamp: Date
   eventType: string
   eventXml?: string
-  eventSourceArn: string
-  eventSourceQueueName: string
+  eventSourceArn?: string
+  eventSourceQueueName?: string
   eventSource: string
   category: EventCategory
 }
