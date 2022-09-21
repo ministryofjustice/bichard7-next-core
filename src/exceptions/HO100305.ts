bc00d046abbb28fc2c438dad09233fd1403205b1
Jamie Davies <jamie.davies@madetech.com>
2022-09-21 16:03:40 +0100

    Ensure HO100305 exceptions aren't raised for non-recordable cases

--- src/exceptions/HO100305.ts
+++ src/exceptions/HO100305.ts
@@ -1,5 +1,6 @@
 import isNotGuiltyVerdict from "src/enrichAho/enrichFunctions/enrichOffenceResultsPostPncEnrichment/isNotGuiltyVerdict"
 import isResultClassCode from "src/enrichAho/enrichFunctions/enrichOffenceResultsPostPncEnrichment/isResultClassCode"
+import isCaseRecordable from "src/lib/isCaseRecordable"
 import errorPaths from "../lib/errorPaths"
 import resultCodeIsOnStopList from "../lib/result/resultCodeIsOnStopList"
 import type Exception from "../types/Exception"
@@ -9,6 +10,10 @@ import ResultClass from "../types/ResultClass"
 
 const HO100305: ExceptionGenerator = (hearingOutcome) => {
   const generatedExceptions: Exception[] = []
+
+  if (!isCaseRecordable(hearingOutcome)) {
+    return generatedExceptions
+  }
 
   hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence.forEach((offence, offenceIndex) =>
     offence.Result.forEach((result, resultIndex) => {
