6f2bb9c2f9aed784148f807fa96db57883f839d3
Jamie Davies <jamie.davies@madetech.com>
2022-12-01 15:31:00 +0000

    Add integration test for running against an actual AHO

--- src/steps/phase1/processPhase1.integration.test.ts
+++ src/steps/phase1/processPhase1.integration.test.ts
@@ -1,7 +1,10 @@
 import "tests/helpers/setEnvironmentVariables"
 
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import fs from "fs"
+import type { ImportedComparison } from "src/comparison/types/ImportedComparison"
 import createS3Config from "src/lib/createS3Config"
+import type { Phase1SuccessResult } from "src/types/Phase1Result"
 import MockS3 from "tests/helpers/MockS3"
 import processPhase1 from "./processPhase1"
 
@@ -32,5 +35,23 @@ describe("processPhase1", () => {
     const result = await processPhase1(s3Path)
 
     expect(result).toHaveProperty("failure", true)
+    expect(result).toHaveProperty("auditLogEvents")
+    expect(result.auditLogEvents).toHaveLength(3)
+  })
+
+  it("should correctly process an input message that doesn't raise any triggers or exceptions", async () => {
+    const comparisonFile = fs.readFileSync("test-data/e2e-comparison/test-019.json")
+    const comparison = JSON.parse(comparisonFile.toString()) as ImportedComparison
+
+    const s3Path = "no-triggers-exceptions.xml"
+    const command = new PutObjectCommand({ Bucket: bucket, Key: s3Path, Body: comparison.incomingMessage })
+    await client.send(command)
+
+    const result = (await processPhase1(s3Path)) as Phase1SuccessResult
+
+    expect(result).not.toHaveProperty("failure")
+    expect(result.triggers).toHaveLength(0)
+    expect(result.hearingOutcome.Exceptions).toHaveLength(0)
+    console.log(JSON.stringify(result, null, 2))
   })
 })
