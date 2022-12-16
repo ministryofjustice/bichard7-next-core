e7003b8028000a16a97e403b9ba8c5e94d09b221
Emad Karamad <emad.karamad@madetech.com>
2022-12-16 09:48:08 +0000

    Update compare single lambda to get function name from env vars (#248)
    * Update compare single lambda to get function name from env vars
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- airflow/dags/compare_single_dag.py
+++ airflow/dags/compare_single_dag.py
@@ -1,9 +1,11 @@
 from datetime import datetime
-from typing import Any, Dict
 from airflow.providers.amazon.aws.operators.lambda_function import AwsLambdaInvokeFunctionOperator
-from airflow.decorators import dag, task
+from airflow.decorators import dag
 import json
 import os
+
+COMPARE_SINGLE_LAMBDA_NAME = os.getenv(
+    "AIRFLOW__CORE__VAR_COMPARE_SINGLE_LAMBDA_NAME")
 
 
 @dag(schedule_interval=None, start_date=datetime(2021, 1, 1), catchup=False, tags=['example'])
@@ -13,7 +15,7 @@ def compare_single():
     # """
     AwsLambdaInvokeFunctionOperator(
         task_id='invoke_lambda',
-        function_name="bichard-7-emadk6-compare-single",
+        function_name=COMPARE_SINGLE_LAMBDA_NAME,
         payload=json.dumps({
             "detail": {
                 "object": {
