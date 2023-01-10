413bb0f33bb035e91356e5ed6c9648bedf66eda0
Jamie Davies <jamie.davies@madetech.com>
2023-01-10 12:02:51 +0000

    Add rerun-failures workflow, task and worker

--- src/conductor/index.ts
+++ src/conductor/index.ts
@@ -1,21 +1,31 @@
 import type { ConductorWorker } from "@io-orkes/conductor-typescript"
 import { ConductorClient, TaskManager } from "@io-orkes/conductor-typescript"
+import https from "https"
+import createDynamoDbConfig from "src/comparison/lib/createDynamoDbConfig"
+import DynamoGateway from "src/comparison/lib/DynamoGateway"
+import { isError } from "src/comparison/types"
 import type { Task } from "./Task"
 
 const conductorLog = (log: string): { log: string; createdTime: number } => ({ log, createdTime: new Date().getTime() })
 
 const allS3Paths = [...Array(50).keys()].map((i) => `path/to/file/${i}.json`)
 
+const dynamoConfig = createDynamoDbConfig()
+const gateway = new DynamoGateway(dynamoConfig)
+
 const client = new ConductorClient({
-  serverUrl: "http://localhost:8080/api"
+  serverUrl: "https://localhost:5001/api",
+  AGENT: new https.Agent({
+    rejectUnauthorized: false
+  })
 })
 
 const compareBatchWorker: ConductorWorker = {
   taskDefName: "compare_batch",
   execute: async (task: Task) => {
     const s3Paths = task.inputData?.s3Paths ?? []
-    await new Promise((resolve) => setTimeout(resolve, 10_000))
     console.log("working on compare_batch")
+    await new Promise((resolve) => setTimeout(resolve, 10_000))
     return {
       logs: [conductorLog(`working on ${s3Paths.join(",")}`)],
       status: "COMPLETED"
@@ -40,7 +50,32 @@ const rerunAllWorker: ConductorWorker = {
   }
 }
 
-const workers = [compareBatchWorker, rerunAllWorker]
+const rerunFailuresWorker: ConductorWorker = {
+  taskDefName: "rerun_failures",
+  execute: async (task: Task) => {
+    console.log("working on rerun_all")
+    const startKey = task.inputData?.startKey
+    const resultPage = await gateway.getAllFailuresPage(1000, false, startKey)
+
+    if (isError(resultPage)) {
+      return {
+        logs: [conductorLog(resultPage.message)],
+        status: "FAILED"
+      }
+    }
+
+    return {
+      logs: [conductorLog(`starting at ${startKey}`)],
+      outputData: {
+        nextKey: resultPage.lastEvaluatedKey,
+        s3Paths: allS3Paths.slice(startKey, startKey + 5)
+      },
+      status: "COMPLETED"
+    }
+  }
+}
+
+const workers = [compareBatchWorker, rerunAllWorker, rerunFailuresWorker]
 const taskManager = new TaskManager(client, workers)
 
 console.log("Starting polling...")
