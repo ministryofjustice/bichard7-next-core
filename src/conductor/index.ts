3a055c22116f763b54513ebda78f9b706a2f8adb
Jamie Davies <jamie.davies@madetech.com>
2023-01-12 13:28:20 +0000

    Start actually pulling files from S3 and comparing them inside the fork

--- src/conductor/index.ts
+++ src/conductor/index.ts
@@ -1,5 +1,15 @@
 import type { ConductorWorker } from "@io-orkes/conductor-typescript"
 import { ConductorClient, TaskManager } from "@io-orkes/conductor-typescript"
+import getStandingDataVersionByDate from "src/comparison/cli/getStandingDataVersionByDate"
+import type { ComparisonResult } from "src/comparison/lib/compareMessage"
+import compareMessage from "src/comparison/lib/compareMessage"
+import createDynamoDbConfig from "src/comparison/lib/createDynamoDbConfig"
+import DynamoGateway from "src/comparison/lib/DynamoGateway"
+import getDateFromComparisonFilePath from "src/comparison/lib/getDateFromComparisonFilePath"
+import recordResultsInDynamo from "src/comparison/lib/recordResultsInDynamo"
+import { isError } from "src/comparison/types/Result"
+import createS3Config from "src/lib/createS3Config"
+import getFileFromS3 from "src/lib/getFileFromS3"
 import type { Task } from "./Task"
 
 type ConductorLog = {
@@ -16,12 +26,16 @@ type Range = {
 const conductorLog = (log: string): ConductorLog => ({ log, createdTime: new Date().getTime() })
 const logWorkingMessage = (task: Task) => console.log(`working on ${task.taskDefName} (${task.taskId})`)
 
-// const dynamoConfig = createDynamoDbConfig()
-// const gateway = new DynamoGateway(dynamoConfig)
+const dynamoConfig = createDynamoDbConfig()
+const s3Config = createS3Config()
+const gateway = new DynamoGateway(dynamoConfig)
 
 const client = new ConductorClient({
   serverUrl: "http://localhost:5002/api"
 })
+
+const isPass = (result: ComparisonResult): boolean =>
+  result.triggersMatch && result.exceptionsMatch && result.xmlOutputMatches && result.xmlParsingMatches
 
 const generateRerunFailuresTasks: ConductorWorker = {
   taskDefName: "generate_rerun_failures_tasks",
@@ -41,9 +55,6 @@ const generateRerunFailuresTasks: ConductorWorker = {
     const logs: ConductorLog[] = []
     const ranges: Range[] = []
 
-    logs.push(conductorLog(`start date: ${startDate}`))
-    logs.push(conductorLog(`end date: ${endDate}`))
-
     for (const d = new Date(startDate); d <= new Date(endDate); d.setDate(d.getDate() + 1)) {
       const start = d.toISOString()
       const end = new Date(d)
@@ -51,7 +62,7 @@ const generateRerunFailuresTasks: ConductorWorker = {
       ranges.push({ start, end: end.toISOString() })
     }
 
-    logs.push(conductorLog(`generated ${ranges.length} intervals`))
+    logs.push(conductorLog(`Generated ${ranges.length} day intervals`))
 
     return Promise.resolve({
       logs,
@@ -69,12 +80,95 @@ const generateRerunFailuresTasks: ConductorWorker = {
 
 const rerunFailureDay: ConductorWorker = {
   taskDefName: "rerun_failure_day",
-  execute: (task: Task) => {
+  execute: async (task: Task) => {
     logWorkingMessage(task)
-    return Promise.resolve({
-      logs: [conductorLog(`Processing from ${task.inputData?.start} to ${task.inputData?.end}`)],
+
+    const start = task.inputData?.start
+    const end = task.inputData?.end
+
+    if (!start || !end) {
+      return {
+        logs: [conductorLog("taskName must be specified")],
+        status: "FAILED_WITH_TERMINAL_ERROR"
+      }
+    }
+
+    const logs: ConductorLog[] = []
+    const count = { pass: 0, fail: 0 }
+
+    for await (const batch of gateway.getRange(start, end, false, 1000)) {
+      if (!batch || batch instanceof Error) {
+        return {
+          logs: [conductorLog("Failed to get batch from Dynamo")],
+          status: "FAILED"
+        }
+      }
+
+      logs.push(conductorLog(`Processing ${batch.length} comparison tests...`))
+
+      const resultPromises = batch.map(async (test) => {
+        const testLogs: ConductorLog[] = []
+        const bucket = "bichard-7-production-processing-validation"
+        const s3Path = test.s3Path
+
+        const content = await getFileFromS3(s3Path, bucket, s3Config)
+        if (content instanceof Error) {
+          throw content
+        }
+
+        let comparisonResult: ComparisonResult
+        let error: Error | undefined
+        const date = getDateFromComparisonFilePath(s3Path)
+        try {
+          comparisonResult = await compareMessage(content, false, {
+            defaultStandingDataVersion: getStandingDataVersionByDate(date)
+          })
+        } catch (e) {
+          error = e as Error
+          testLogs.push(conductorLog(error.message))
+          comparisonResult = {
+            triggersMatch: false,
+            exceptionsMatch: false,
+            xmlOutputMatches: false,
+            xmlParsingMatches: false
+          }
+        }
+
+        if (isPass(comparisonResult)) {
+          count.pass += 1
+        } else {
+          count.fail += 1
+          testLogs.push(conductorLog(`Comparison failed: ${s3Path}`))
+        }
+
+        if (error) {
+          testLogs.push(conductorLog(error.message))
+        }
+
+        return { s3Path, comparisonResult, logs: testLogs }
+      })
+
+      const allTestResults = await Promise.all(resultPromises)
+
+      const allTestLogs = allTestResults.reduce((accumulatedLogs: ConductorLog[], testResult) => {
+        return accumulatedLogs.concat(testResult.logs)
+      }, [])
+
+      logs.push(...allTestLogs)
+
+      const recordResultsInDynamoResult = await recordResultsInDynamo(allTestResults, gateway)
+      if (isError(recordResultsInDynamoResult)) {
+        throw recordResultsInDynamoResult
+      }
+
+      logs.push(conductorLog(`Results of processing: ${count.pass} passed. ${count.fail} failed`))
+    }
+
+    return {
+      logs,
+      outputData: { ...count },
       status: "COMPLETED"
-    })
+    }
   }
 }
 
