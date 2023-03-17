557ec6a49bea80176d940dedfbc5a10ac84cf3c6
Ian King <ian.antking@gmail.com>
2023-03-17 16:22:48 +0000

    add simple status check endpoint

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,10 @@
 import * as express from "express"
 import * as cors from "cors"
+import statusRouter from "./routes/status"
 
 const app: express.Application = express()
 app.use(cors())
+
+app.use("/status", statusRouter)
 
 export default app
