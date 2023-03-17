3f2bb357a72f929e3986de1098254b412dced159
Ian King <ian.antking@gmail.com>
2023-03-17 17:06:53 +0000

    add integration test for GET /status

---
+++ api/tests/status.integration.test.ts
@@ -0,0 +1,12 @@
+import * as request from "supertest"
+import app from "../src/app"
+
+describe("/status", () => {
+  describe("GET", () => {
+    it("returns a 200 status code", async () => {
+      const response = await request(app).get("/status")
+
+      expect(response.statusCode).toBe(200)
+    })
+  })
+})
