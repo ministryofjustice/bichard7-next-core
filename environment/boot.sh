e85625273e2722dbcc7c27265fd327e4505224fb
Ben Pirt <ben@pirt.co.uk>
2023-03-24 17:12:47 +0000

    Remove the line removing conductor image from resolving caching issues

--- environment/boot.sh
+++ environment/boot.sh
@@ -4,15 +4,13 @@ set -e
 IMAGES=(beanconnect pncemulator)
 SERVICES=$@
 
-docker image rm conductor
-
 PLATFORM=$(uname -m)
 if [ $PLATFORM != "arm64" ]; then
     IMAGES+=(bichard7-liberty conductor e2etests nginx-auth-proxy ui user-service)
 fi
 
 for image in "${IMAGES[@]}"; do
-    if [[ "$(docker images -q $image 2> /dev/null)" == "" ]]; then
+    if [[ "$CI" == "true" || "$(docker images -q $image 2> /dev/null)" == "" ]]; then
         echo "Fetching $image..."
         scripts/fetch-docker-image.sh $image
     fi
