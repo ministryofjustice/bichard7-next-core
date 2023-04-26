9679cbe8f01e899ffc19163611d37f2d24f2cb89
Donny <donny.hyon@madetech.com>
2023-04-26 17:01:12 +0100

    WIP

--- api/src/middleware/validate.ts
+++ api/src/middleware/validate.ts
@@ -1,14 +1,30 @@
 import type { Request, Response, NextFunction } from "express"
 import { z, ZodError } from "zod"
 
-const caseListQuerySchema: z.Schema = z.object({
-  forces: z.array(z.string()),
-  maxPageItems: z.string()
-})
+const caseListQuerySchema: z.Schema = z
+  .object({
+    forces: z.array(z.string()),
+    maxPageItems: z.string(),
+    orderBy: z.string().optional(),
+    order: z.string().optional(),
+    reasons: z.string().optional(),
+    defendantName: z.string().optional(),
+    courtName: z.string().optional(),
+    ptiurn: z.string().optional(),
+    urgent: z.string().optional(),
+    pageNum: z.string().optional(),
+    courtDateRange: z.array(z.object({ from: z.date(), to: z.date() }).optional()).optional(),
+    locked: z.boolean().optional(),
+    caseState: z.string().optional(),
+    allocatedToUserName: z.string().optional(),
+    reasonCode: z.string().optional(),
+    resolvedByUsername: z.string().optional()
+  })
+  .strict()
 
 export const validateCaseListQueryParams = (req: Request, res: Response, next: NextFunction) => {
   try {
-    caseListQuerySchema.parse(req.query)
+    req.query = caseListQuerySchema.parse(req.query)
 
     next()
   } catch (err) {
--- api/tests/middleware/validate.unit.test.ts
+++ api/tests/middleware/validate.unit.test.ts
@@ -36,6 +36,7 @@ describe("validateCourtCaseListQueryParams", () => {
       ]
     })
   })
+
   it("returns 400 status code if maxPageItems are absent", () => {
     const req = { query: { forces: ["01"] } } as unknown as Request
     const res = {} as Response
@@ -58,4 +59,41 @@ describe("validateCourtCaseListQueryParams", () => {
       ]
     })
   })
+
+  it("calls the next function if query has all optional fields", () => {
+    const req = {
+      query: {
+        forces: ["01"],
+        maxPageItems: "10",
+        orderBy: "a column",
+        order: "desc",
+        reasons: ["Triggers"],
+        defendantName: "WAYNE Bruce",
+        courtName: "A court",
+        ptiurn: "PTIURN",
+        urgent: "Urgent",
+        pageNum: "1",
+        courtDateRange: [
+          { from: new Date(), to: new Date() },
+          { from: new Date(), to: new Date() }
+        ],
+        locked: true,
+        caseState: "Resolved",
+        allocatedToUserName: "a user",
+        reasonCode: "reasonCode",
+        resolvedByUsername: "resolvedBy"
+      }
+    } as unknown as Request
+    const res = {} as Response
+    res.status = jest.fn().mockReturnValue(res)
+    res.json = jest.fn().mockReturnValue(res)
+    const next = jest.fn() as NextFunction
+
+    validateCaseListQueryParams(req, res, next)
+
+    expect(next).toHaveBeenCalled()
+  })
 })
+
+// all optional fields then next will get called
+// send something not expecting, send 400
