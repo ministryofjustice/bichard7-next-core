d6eb40ba1d02f317f9bb6f44a7f4fe2dfd6c59d2
Ben Pirt <ben@pirt.co.uk>
2023-04-28 15:54:08 +0100

    Update the filter for non matching manual sequence number

--- src/comparison/lib/isIntentionalMatchingDifference/nonMatchingManualSequenceNumber.ts
+++ src/comparison/lib/isIntentionalMatchingDifference/nonMatchingManualSequenceNumber.ts
@@ -3,15 +3,11 @@ import type { AnnotatedHearingOutcome } from "src/types/AnnotatedHearingOutcome"
 import { ExceptionCode } from "src/types/ExceptionCode"
 
 const nonMatchingManualSequenceNumber = (
-  expected: CourtResultMatchingSummary,
+  _: CourtResultMatchingSummary,
   actual: CourtResultMatchingSummary,
-  _: AnnotatedHearingOutcome,
-  __: AnnotatedHearingOutcome
+  __: AnnotatedHearingOutcome,
+  ___: AnnotatedHearingOutcome
 ): boolean => {
-  if ("exceptions" in expected) {
-    return false
-  }
-
   const coreRaisesHo100320 =
     "exceptions" in actual && actual.exceptions.some((exception) => exception.code === ExceptionCode.HO100320)
 
