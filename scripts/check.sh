b06374732c6f2e36c6bc0d4edc829b8830388870
Ben Pirt <ben@pirt.co.uk>
2023-05-09 13:52:18 +0100

    Add a script to wrap a lot of the other comparison scripts

---
+++ scripts/check.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+S3_PATH=$1
+LOCAL_PATH="${S3_PATH/s3:\/\/bichard-7-production-processing-validation//tmp/comparison}"
+AUDIT_LOG_FILE="${LOCAL_PATH/.json/.auditlog.json}"
+PNC_LOG_FILE="${LOCAL_PATH/.json/.pnclog.json}"
+echo $AUDIT_LOG_FILE
+CORRELATION_ID=$(jq -r .correlationId $LOCAL_PATH)
+API_KEY=$(aws ssm get-parameter --name /cjse-production-bichard-7/audit-logging/api-key --with-decryption --query "Parameter.Value" --output text)
+
+npm run compare:summarise $S3_PATH
+
+curl -s -H "X-Api-Key: ${API_KEY}" https://xylflxxbue.execute-api.eu-west-2.amazonaws.com/production/messages/${CORRELATION_ID} | jq .[0] > $AUDIT_LOG_FILE
+code $AUDIT_LOG_FILE
+
+jq -r '.events[].attributes."PNC Request Message" | select(. != null)' $AUDIT_LOG_FILE > $PNC_LOG_FILE
+code $PNC_LOG_FILE
+
+USE_NEW_MATCHER=true npm run compare -- -c -x -f $S3_PATH
