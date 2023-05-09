c32ff40471ca29d80f4e10058103f08b527f80af
Ben Pirt <ben@pirt.co.uk>
2023-05-09 09:34:11 +0100

    Change the summarise script to show the same result groups across offences with different times

--- src/comparison/lib/hoOffencesAreEqual.ts
+++ src/comparison/lib/hoOffencesAreEqual.ts
@@ -3,8 +3,6 @@ import getOffenceCode from "src/lib/offence/getOffenceCode"
 import type { Offence } from "src/types/AnnotatedHearingOutcome"
 
 const hoOffencesAreEqual = (offence1: Offence, offence2: Offence): boolean =>
-  getOffenceCode(offence1) === getOffenceCode(offence2) &&
-  offence1.ActualOffenceStartDate.StartDate.getTime() === offence2.ActualOffenceStartDate.StartDate.getTime() &&
-  offencesHaveEqualResults([offence1, offence2])
+  getOffenceCode(offence1) === getOffenceCode(offence2) && offencesHaveEqualResults([offence1, offence2])
 
 export default hoOffencesAreEqual
