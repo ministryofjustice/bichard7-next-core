860fb0f0725d6403a2ea5174ac438a46fbfb552d
Donny <donny.hyon@madetech.com>
2023-05-10 14:46:49 +0100

    WIP trying to parse date through urlparams

--- api/tests/court-cases.integration.test.ts
+++ api/tests/court-cases.integration.test.ts
@@ -65,5 +65,45 @@ describe("/court-cases", () => {
         )
       expect(response.body.result).toHaveLength(1)
     })
+    it("can filter on ptirun2", async () => {
+      const orgCode = "01"
+      const firstCase = "00001"
+      const secondCase = "00002"
+      const thirdCase = "00003"
+
+      await insertCourtCasesWithFields([
+        { ptiurn: firstCase, orgForPoliceFilter: orgCode },
+        { ptiurn: secondCase, orgForPoliceFilter: orgCode },
+        { ptiurn: thirdCase, orgForPoliceFilter: orgCode }
+      ])
+      const response = await request(app).get("/court-cases?forces[]=01&maxPageItems=10&ptiurn=00001")
+      console.log(response.body)
+
+      expect(response.body.result).toHaveLength(1)
+    })
+
+    it("can filter on courtDate", async () => {
+      const orgCode = "01"
+      const firstDate = new Date("2001-09-26")
+      const secondDate = new Date("2008-01-26")
+      const thirdDate = new Date("2008-03-26")
+      const fourthDate = new Date("2013-10-16")
+
+      await insertCourtCasesWithFields([
+        { courtDate: firstDate, orgForPoliceFilter: orgCode },
+        { courtDate: secondDate, orgForPoliceFilter: orgCode },
+        { courtDate: thirdDate, orgForPoliceFilter: orgCode },
+        { courtDate: fourthDate, orgForPoliceFilter: orgCode }
+      ])
+      const encodedUrl = encodeURIComponent(
+        JSON.stringify({ from: "2008-01-01T00:00:00Z", to: "2008-12-30T00:00:00Z" })
+      )
+      const url = "/court-cases?forces[]=01&maxPageItems=10courtDateRange[]=" + encodedUrl
+      const response = await request(app).get(url)
+      // .query(JSON.stringify({ from: "2008-01-01T00:00:00Z", to: "2008-12-30T00:00:00Z" }))
+      console.log(url)
+
+      expect(response.body.result).toHaveLength(2)
+    })
   })
 })
