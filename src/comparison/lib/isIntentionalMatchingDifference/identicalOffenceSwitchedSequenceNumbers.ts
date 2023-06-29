e641a3d38f2c33eddbab4651b3825cf667dada78
Ben Pirt <ben@pirt.co.uk>
2023-06-29 09:29:38 +0100

    Improve the filter for flipped offence matches for identical offences

--- src/comparison/lib/isIntentionalMatchingDifference/identicalOffenceSwitchedSequenceNumbers.ts
+++ src/comparison/lib/isIntentionalMatchingDifference/identicalOffenceSwitchedSequenceNumbers.ts
@@ -21,13 +21,13 @@ const groupIdenticalOffences = (offences: Offence[]): Offence[][] => {
   return output
 }
 
-const groupOffences = (offences: Offence[], matches: OffenceMatchingSummary[]): (number | undefined)[][] => {
+const groupOffences = (offences: Offence[], matches: OffenceMatchingSummary[]): string[][] => {
   const identicalOffenceGroups = groupIdenticalOffences(offences)
   return identicalOffenceGroups.map((group) =>
-    group.map(
-      (hoOffence) =>
-        matches.find((match) => match.hoSequenceNumber === hoOffence.CourtOffenceSequenceNumber)?.pncSequenceNumber
-    )
+    group.map((hoOffence) => {
+      const matchedOffence = matches.find((match) => match.hoSequenceNumber === hoOffence.CourtOffenceSequenceNumber)
+      return `${matchedOffence?.courtCaseReference}/${matchedOffence?.pncSequenceNumber}`
+    })
   )
 }
 
@@ -50,7 +50,11 @@ const identicalOffenceSwitchedSequenceNumbers = (
   expectedOffenceGroups.forEach((group) => group.sort())
   actualOffenceGroups.forEach((group) => group.sort())
 
-  return differenceBeforeSort && JSON.stringify(expectedOffenceGroups) === JSON.stringify(actualOffenceGroups)
+  const result = differenceBeforeSort && JSON.stringify(expectedOffenceGroups) === JSON.stringify(actualOffenceGroups)
+  if (result) {
+    debugger
+  }
+  return result
 }
 
 export default identicalOffenceSwitchedSequenceNumbers
