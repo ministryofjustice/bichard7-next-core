56bfe9ef2ffc086e11ba18fbe425686b1ce0da98
Jamie Davies <jamie.davies@madetech.com>
2023-06-29 12:44:44 +0100

    Add intentional difference for Bichard 332s on offences with the same results

---
+++ src/comparison/lib/isIntentionalMatchingDifference/ho100332WithSameResults.ts
@@ -0,0 +1,35 @@
+import type { CourtResultMatchingSummary } from "src/comparison/types/MatchingComparisonOutput"
+import { offencesHaveEqualResults } from "src/enrichAho/enrichFunctions/enrichCourtCases/offenceMatcher/resultsAreEqual"
+import type { AnnotatedHearingOutcome } from "src/types/AnnotatedHearingOutcome"
+import { ExceptionCode } from "src/types/ExceptionCode"
+
+// Bichard sometimes raises a HO100332 for offences that have identical results. Core detects this and
+// assigns a match anyway (as it doesn't matter!)
+
+const ho100332WithSameResults = (
+  expected: CourtResultMatchingSummary,
+  actual: CourtResultMatchingSummary,
+  expectedAho: AnnotatedHearingOutcome,
+  __: AnnotatedHearingOutcome
+): boolean => {
+  if (!("exceptions" in expected) || "exceptions" in actual) {
+    return false
+  }
+
+  const ho100332s = expected.exceptions.filter((exception) => exception.code === ExceptionCode.HO100332)
+  const bichardRaisesHo100332 = ho100332s.length > 0
+
+  const coreMatches = !("exceptions" in actual)
+
+  const offenceIndices = ho100332s.map((e) => e.path[5])
+
+  const exceptionOffences = expectedAho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence.filter(
+    (_, index) => offenceIndices.includes(index)
+  )
+
+  const ho100332OffencesHaveSameResults = offencesHaveEqualResults(exceptionOffences)
+
+  return bichardRaisesHo100332 && coreMatches && ho100332OffencesHaveSameResults
+}
+
+export default ho100332WithSameResults
--- src/comparison/lib/isIntentionalMatchingDifference/index.ts
+++ src/comparison/lib/isIntentionalMatchingDifference/index.ts
@@ -8,6 +8,7 @@ import ho100304WithExistingFinalOffence from "./ho100304WithExistingFinalOffence
 import ho100310AndHo100332Equivalent from "./ho100310AndHo100332Equivalent"
 import ho100332NotHo100304 from "./ho100332NotHo100304"
 import ho100332WithConvictionDate from "./ho100332WithConvictionDate"
+import ho100332WithSameResults from "./ho100332WithSameResults"
 import ho100333AndCCRHasLeadingZero from "./ho100333AndCCRHasLeadingZero"
 import identicalOffenceSwitchedSequenceNumbers from "./identicalOffenceSwitchedSequenceNumbers"
 import invalidManualSequenceNumber from "./invalidManualSequenceNumber"
@@ -23,6 +24,7 @@ const filters = [
   ho100310AndHo100332Equivalent,
   ho100332NotHo100304,
   ho100332WithConvictionDate,
+  ho100332WithSameResults,
   ho100333AndCCRHasLeadingZero,
   identicalOffenceSwitchedSequenceNumbers,
   invalidManualSequenceNumber,
