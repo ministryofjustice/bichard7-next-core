c2e605641045090f16566ff9d1d17f57de70f466
Ben Pirt <ben@pirt.co.uk>
2023-07-06 14:35:04 +0100

    Enable forces 67 and 91

--- src/enrichAho/enrichFunctions/enrichForceOwner.ts
+++ src/enrichAho/enrichFunctions/enrichForceOwner.ts
@@ -4,13 +4,11 @@ import logger from "../../lib/logging"
 import type { AnnotatedHearingOutcome, OrganisationUnitCodes } from "../../types/AnnotatedHearingOutcome"
 import type { EnrichAhoFunction } from "../../types/EnrichAhoFunction"
 
+// prettier-ignore
 const validForceCodes = [
-  // eslint-disable-next-line prettier/prettier
-  "01","02","03","04","05","06","07","10","11","12","13","14","16","17","20","21",
-  // eslint-disable-next-line prettier/prettier
-  "22","23","24","30","31","32","33","34","35","36","37","40","41","42","43","44",
-  // eslint-disable-next-line prettier/prettier
-  "45","46","47","48","50","52","53","54","55","60","61","62","63","88","89","93"
+  "01","02","03","04","05","06","07","10","11","12","13","14","16","17","20","21","22",
+  "23","24","30","31","32","33","34","35","36","37","40","41","42","43","44","45","46",
+  "47","48","50","52","53","54","55","60","61","62","63","67","88","89","91","93"
 ]
 
 const isValidForceCode = (code: string): boolean => validForceCodes.includes(code)
