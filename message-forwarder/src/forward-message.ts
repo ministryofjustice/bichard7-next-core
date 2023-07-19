84c4297e37f06a692586177816646af9c4083a2f
Jamie Davies <jamie.davies@madetech.com>
2023-07-19 09:55:30 +0100

    Upload file to S3 in message-forwarder if DESTINATION_TYPE is s3

--- message-forwarder/src/forward-message.ts
+++ message-forwarder/src/forward-message.ts
@@ -1,13 +1,27 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import { v4 as uuid } from "uuid"
 import type MqGateway from "./MqGateway/MqGateway"
+import type { PromiseResult } from "./Result"
 
 const destinationType = process.env.DESTINATION_TYPE ?? "mq"
 const destination = process.env.DESTINATION ?? "HEARING_OUTCOME_INPUT_QUEUE"
 
-const forwardMessage = async (message: string, mqGateway: MqGateway) => {
+const forwardMessage = async (message: string, mqGateway: MqGateway): PromiseResult<void> => {
   if (destinationType === "mq") {
-    await mqGateway.sendMessage(message, destination)
+    return mqGateway.sendMessage(message, destination)
   } else if (destinationType === "s3") {
-    //
+    const client = new S3Client({ region: "eu-west-2" })
+    const command = new PutObjectCommand({
+      Body: message,
+      Bucket: destination,
+      Key: `${uuid()}.xml`
+    })
+
+    try {
+      await client.send(command)
+    } catch (e) {
+      return e as Error
+    }
   }
 }
 
