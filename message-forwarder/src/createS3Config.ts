f8aa242655b9ab01c101c261f2b7bf45619ea705
Jamie Davies <jamie.davies@madetech.com>
2023-07-20 11:22:43 +0100

    Add Dockerfile for building the message-forwarder

---
+++ message-forwarder/src/createS3Config.ts
@@ -0,0 +1,20 @@
+import type { S3ClientConfig } from "@aws-sdk/client-s3"
+
+const createS3Config = () => {
+  const s3Config: S3ClientConfig = {
+    endpoint: process.env.S3_ENDPOINT ?? "https://s3.eu-west-2.amazonaws.com",
+    region: process.env.S3_REGION ?? "eu-west-2",
+    forcePathStyle: true
+  }
+
+  if (process.env.S3_AWS_ACCESS_KEY_ID && process.env.S3_AWS_SECRET_ACCESS_KEY) {
+    s3Config.credentials = {
+      accessKeyId: process.env.S3_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_AWS_SECRET_ACCESS_KEY
+    }
+  }
+
+  return s3Config
+}
+
+export default createS3Config
