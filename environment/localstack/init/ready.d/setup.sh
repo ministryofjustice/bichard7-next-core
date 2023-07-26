066912d73b2c515d7a062a19f18bd45f42191bce
Ben Pirt <ben@pirt.co.uk>
2023-07-26 13:53:38 +0100

    Enable the audit log api image to be run as part of the Core environment

--- environment/localstack/init/ready.d/setup.sh
+++ environment/localstack/init/ready.d/setup.sh
@@ -90,3 +90,134 @@ awslocal dynamodb create-table --region $AWS_REGION --table-name $TABLE \
     }
   ]'
 done
+
+awslocal dynamodb create-table --region $AWS_REGION --table-name auditLogTable \
+  --attribute-definitions '[{ "AttributeName": "messageId", "AttributeType": "S" },
+    { "AttributeName": "_", "AttributeType": "S" },
+    { "AttributeName": "receivedDate", "AttributeType": "S" },
+    { "AttributeName": "externalCorrelationId", "AttributeType": "S" },
+    { "AttributeName": "messageHash", "AttributeType": "S" },
+    { "AttributeName": "status", "AttributeType": "S" },
+    { "AttributeName": "caseId", "AttributeType": "S" },
+    { "AttributeName": "isSanitised", "AttributeType": "N" },
+    { "AttributeName": "nextSanitiseCheck", "AttributeType": "S" }]' \
+  --key-schema '[{ "AttributeName": "messageId", "KeyType": "HASH" }]' \
+  --billing-mode PAY_PER_REQUEST \
+  --global-secondary-indexes '[
+    {
+      "IndexName": "receivedDateIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "_", "KeyType": "HASH"},
+        {"AttributeName": "receivedDate", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "externalCorrelationIdIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "externalCorrelationId", "KeyType": "HASH"}
+      ]
+    },
+    {
+      "IndexName": "messageHashIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "messageHash", "KeyType": "HASH"}
+      ]
+    },
+    {
+      "IndexName": "statusIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "status", "KeyType": "HASH"},
+        {"AttributeName": "receivedDate", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "caseIdIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "caseId", "KeyType": "HASH"}
+      ]
+    },
+    {
+      "IndexName": "isSanitisedIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "isSanitised", "KeyType": "HASH"},
+        {"AttributeName": "nextSanitiseCheck", "KeyType": "RANGE"}
+      ]
+    }
+  ]'
+
+awslocal dynamodb create-table --region $AWS_REGION --table-name auditLogEventsTable \
+  --attribute-definitions '[{ "AttributeName": "_", "AttributeType": "S" },
+    { "AttributeName": "_id", "AttributeType": "S" },
+    { "AttributeName": "_messageId", "AttributeType": "S" },
+    { "AttributeName": "user", "AttributeType": "S" },
+    { "AttributeName": "timestamp", "AttributeType": "S" },
+    { "AttributeName": "_automationReport", "AttributeType": "N" },
+    { "AttributeName": "_topExceptionsReport", "AttributeType": "N" },
+    { "AttributeName": "eventCode", "AttributeType": "S" },
+    { "AttributeName": "category", "AttributeType": "S" }]' \
+  --key-schema '[{ "AttributeName": "_id", "KeyType": "HASH" }]' \
+  --billing-mode PAY_PER_REQUEST \
+  --global-secondary-indexes '[
+    {
+      "IndexName": "timestampIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "_", "KeyType": "HASH"},
+        {"AttributeName": "timestamp", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "messageIdIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "_messageId", "KeyType": "HASH"},
+        {"AttributeName": "timestamp", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "userIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "user", "KeyType": "HASH"},
+        {"AttributeName": "timestamp", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "eventCodeIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "eventCode", "KeyType": "HASH"},
+        {"AttributeName": "timestamp", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "categoryIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "category", "KeyType": "HASH"},
+        {"AttributeName": "timestamp", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "automationReportIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "_messageId", "KeyType": "HASH"},
+        {"AttributeName": "_automationReport", "KeyType": "RANGE"}
+      ]
+    },
+    {
+      "IndexName": "topExceptionsReportIndex",
+      "Projection": { "ProjectionType": "ALL" },
+      "KeySchema": [
+        {"AttributeName": "_messageId", "KeyType": "HASH"},
+        {"AttributeName": "_topExceptionsReport", "KeyType": "RANGE"}
+      ]
+    }
+  ]'
