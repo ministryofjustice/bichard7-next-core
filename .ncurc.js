4424556ecfb65a5ccd9cf6c6e1cddc85fc6601cf
Richard Cane <richard.cane@madetech.com>
2023-07-31 18:24:27 +0100

    Make ncu ignore the comparison data packages

---
+++ .ncurc.js
@@ -0,0 +1,25 @@
+// ncu updates all the bichard7-next-data packages
+// to the latest version, which is decidedly unhelpful
+const semver = new RegExp(
+  /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/
+)
+const pinned = []
+const ignored = [`bichard7-next-data-(${semver.source})`]
+
+module.exports = {
+  target: (package) => {
+    if (pinned.some((p) => new RegExp(`^${p}$`).test(package))) {
+      const res = "minor"
+      console.log(` ${package} is pinned to ${res} upgrades only (.ncurc.js)`)
+      return res
+    }
+    return "latest"
+  },
+
+  filterResults: (package) => {
+    if (ignored.some((p) => new RegExp(`^${p}$`).test(package))) {
+      return
+    }
+    return true
+  }
+}
