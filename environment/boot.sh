cedfd893579e1b9667ee9421fff10f6948bdf373
Richard Cane <richard.cane@madetech.com>
2023-08-02 17:23:44 +0100

    Modify boot.sh to run the legacy stack (#458)
    * Modify boot.sh to run the legacy stack as well
    
    * Set defaults for variables

--- environment/boot.sh
+++ environment/boot.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+LEGACY=${LEGACY:-"false"}
+NOWORKER=${NOWORKER:-"false"}
+
 IMAGES=(beanconnect pncemulator)
 SERVICES=$@
 
@@ -16,19 +19,20 @@ for image in "${IMAGES[@]}"; do
     fi
 done
 
-if [ "$NOWORKER" == "true" ]; then
-    DOCKER_COMPOSE="docker compose --project-name bichard -f environment/docker-compose.yml"
-else
-    DOCKER_COMPOSE="docker compose --project-name bichard -f environment/docker-compose.yml -f environment/docker-compose-worker.yml"
+
+DOCKER_COMPOSE="docker compose --project-name bichard -f environment/docker-compose.yml"
+if [ "$LEGACY" == "true" ]; then
+    DOCKER_COMPOSE="${DOCKER_COMPOSE} -f environment/docker-compose-bichard.yml"
+fi
+
+# should run by default
+if [ "$LEGACY" == "false" ] && [ "$NOWORKER" == "false" ]; then
+    DOCKER_COMPOSE="${DOCKER_COMPOSE} -f environment/docker-compose-worker.yml"
     eval "$DOCKER_COMPOSE build worker"
 fi
 
-if [ "$CI" == "true" ]; then
-    ATTEMPTS=5
-else
-    ATTEMPTS=1
-fi
 
+[ "$CI" == "true" ] && ATTEMPTS=5 || ATTEMPTS=1
 for i in $(seq 1 $ATTEMPTS); do
     echo "Setting up infrastructure"
     eval "$DOCKER_COMPOSE up -d --wait $SERVICES" && break
@@ -37,7 +41,9 @@ for i in $(seq 1 $ATTEMPTS); do
     fi
 done;
 
-for i in $(seq 1 $ATTEMPTS); do
-    echo "Setting up conductor"
-    npm run conductor-setup && break || sleep 5
-done;
+if [ "$LEGACY" == "false" ]; then
+    for i in $(seq 1 $ATTEMPTS); do
+        echo "Setting up conductor"
+        npm run conductor-setup && break || sleep 5
+    done;
+fi
