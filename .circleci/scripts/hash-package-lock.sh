e80a9a7f6aada61de29a3b65285c2cdd931fbdd3
Ben Pirt <ben@pirt.co.uk>
2023-08-21 13:47:52 +0100

    Make the package hashing use all package lock files now we're using workspaces

--- .circleci/scripts/hash-package-lock.sh
+++ .circleci/scripts/hash-package-lock.sh
@@ -6,5 +6,4 @@ then
     md5="md5sum"
 fi
 
-$md5 package-lock.json > package-lock.json.md5
-$md5 api/package-lock.json > api/package-lock.json.md5
+$md5 package-lock.json packages/*/package-lock.json > package-lock.json.md5
