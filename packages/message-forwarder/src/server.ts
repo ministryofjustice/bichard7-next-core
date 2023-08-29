bda1926eb2bb8fe17bc91be0c59954e98d5366ef
Ben Pirt <ben@pirt.co.uk>
2023-08-29 12:33:45 +0100

    Handle failover URLs in the stomp library

--- packages/message-forwarder/src/server.ts
+++ packages/message-forwarder/src/server.ts
@@ -1,22 +1,13 @@
 import type { Message } from "@stomp/stompjs"
-import { Client } from "@stomp/stompjs"
 import { WebSocket } from "ws"
-import createMqConfig from "./createMqConfig"
+import createStompClient from "./createStompClient"
 import forwardMessage from "./forward-message"
 Object.assign(global, { WebSocket })
 
-const mqConfig = createMqConfig()
 const sourceQueue = process.env.SOURCE_QUEUE ?? "PHASE_1_RESUBMIT_QUEUE"
 
-const client = new Client({
-  brokerURL: mqConfig.url,
-  connectHeaders: {
-    login: mqConfig.username,
-    passcode: mqConfig.password
-  }
-})
+const client = createStompClient()
 
-console.log(`Connecting to ${mqConfig.url}`)
 client.onConnect = () => {
   console.log("Connected")
   client.subscribe(sourceQueue, async (message: Message) => {
