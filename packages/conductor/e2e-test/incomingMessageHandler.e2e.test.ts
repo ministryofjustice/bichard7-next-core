1917460521416965f8faa5326d9a3dab03edad30
Ben Pirt <ben@pirt.co.uk>
2023-09-11 12:13:25 +0100

    Give the tests longer to wait for Conductor to be ready

--- packages/conductor/e2e-test/incomingMessageHandler.e2e.test.ts
+++ packages/conductor/e2e-test/incomingMessageHandler.e2e.test.ts
@@ -52,7 +52,7 @@ const searchWorkflows = async (params: WorkflowSearchParams) => {
 const waitForWorkflows = (query: WorkflowSearchParams) =>
   promisePoller({
     taskFn: () => searchWorkflows(query),
-    retries: 100,
+    retries: 300,
     interval: 100 // milliseconds
   }).catch(() => {
     throw new Error("Could not find workflow")
