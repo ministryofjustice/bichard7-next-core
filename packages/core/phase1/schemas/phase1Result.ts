4cd95f90660d8172a5602932add8d56cb9e7efc8
Richard Cane <richard.cane@madetech.com>
2023-09-12 13:53:16 +0100

    Add resultType to result schema

--- packages/core/phase1/schemas/phase1Result.ts
+++ packages/core/phase1/schemas/phase1Result.ts
@@ -4,9 +4,12 @@ import { annotatedHearingOutcomeSchema } from "./annotatedHearingOutcome"
 import { auditLogEventSchema } from "./auditLogEvent"
 import { triggerSchema } from "./trigger"
 
+export const phase1ResultTypeSchema = z.nativeEnum(Phase1ResultType)
+
 export const phase1ResultSchema = z.object({
   correlationId: z.string(),
-  auditLogEvents: z.array(auditLogEventSchema)
+  auditLogEvents: z.array(auditLogEventSchema),
+  resultType: phase1ResultTypeSchema
 })
 
 export const phase1SuccessResultSchema = phase1ResultSchema.extend({
