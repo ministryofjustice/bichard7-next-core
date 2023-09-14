f8a15ca7b6ad9774d9cf3360c596ec6deb00218c
Ben Pirt <ben@pirt.co.uk>
2023-09-14 16:29:37 +0100

    Fix the unit tests

--- packages/core/phase1/phase1.test.ts
+++ packages/core/phase1/phase1.test.ts
@@ -27,7 +27,7 @@ describe("Bichard Core processing logic", () => {
     expect(result).toHaveProperty("triggers")
   })
 
-  it("should create audit logs when a message received", async () => {
+  it("should create audit logs when a message is received", async () => {
     await phase1Handler(inputAho, mockPncGateway, auditLogger)
     expect(auditLogger.getEvents()).toEqual(
       expect.arrayContaining([
@@ -51,7 +51,7 @@ describe("Bichard Core processing logic", () => {
           eventCode: "hearing-outcome.details",
           eventSource: "CoreHandler",
           category: "information",
-          timestamp: mockedDate.toISOString()
+          timestamp: mockedDate
         })
       ])
     )
