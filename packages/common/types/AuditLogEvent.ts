a47f0c4af8929b61e83f6c1129ba9fa34e606f7c
Ben Pirt <ben@pirt.co.uk>
2023-09-18 11:31:41 +0100

    WIP - Refoactor audit logging for Core

--- packages/common/types/AuditLogEvent.ts
+++ packages/common/types/AuditLogEvent.ts
@@ -8,138 +8,42 @@ export enum AuditLogEventSource {
   EnrichWithPncQuery = "EnrichWithPncQuery"
 }
 
-export type AuditLogEventOption = {
-  code: EventCode
-  type: string
-}
-
-export const AuditLogEventOptions: Record<string, AuditLogEventOption> = {
-  hearingOutcomeReceivedPhase1: {
-    code: EventCode.HearingOutcomeReceivedPhase1,
-    type: "Hearing outcome received by phase 1"
-  },
-  hearingOutcomeReceivedPhase2: {
-    code: EventCode.HearingOutcomeReceivedPhase2,
-    type: "Hearing outcome received by phase 2"
-  },
-  hearingOutcomeReceivedPhase3: {
-    code: EventCode.HearingOutcomeReceivedPhase3,
-    type: "Hearing outcome received by phase 3"
-  },
-  exceptionResolved: {
-    code: EventCode.ExceptionsResolved,
-    type: "Exception marked as resolved by user"
-  },
-  triggerResolved: {
-    code: EventCode.TriggersResolved,
-    type: "Trigger marked as resolved by user"
-  },
-  allTriggersResolved: {
-    code: EventCode.AllTriggersResolved,
-    type: "All triggers marked as resolved"
-  },
-  hearingOutcomeReallocated: {
-    code: EventCode.HearingOutcomeReallocated,
-    type: "Hearing outcome reallocated by user"
-  },
-  resubmittedToPhase1: {
-    code: EventCode.HearingOutcomeResubmittedPhase1,
-    type: "Hearing outcome resubmitted to phase 1"
-  },
-  submittedToPhase2: {
-    code: EventCode.HearingOutcomeSubmittedPhase2,
-    type: "Hearing outcome submitted to phase 2"
-  },
-  resubmittedToPhase2: {
-    code: EventCode.HearingOutcomeResubmittedPhase2,
-    type: "Hearing outcome resubmitted to phase 2"
-  },
-  submittedToPhase3: {
-    code: EventCode.HearingOutcomeSubmittedPhase3,
-    type: "Hearing outcome submitted to phase 3"
-  },
-  exceptionLocked: {
-    code: EventCode.ExceptionsLocked,
-    type: "Exception locked"
-  },
-  exceptionUnlocked: {
-    code: EventCode.ExceptionsUnlocked,
-    type: "Exception unlocked"
-  },
-  triggerLocked: {
-    code: EventCode.TriggersLocked,
-    type: "Trigger locked"
-  },
-  triggerUnlocked: {
-    code: EventCode.TriggersUnlocked,
-    type: "Trigger unlocked"
-  },
-  exceptionsGenerated: {
-    code: EventCode.ExceptionsGenerated,
-    type: "Exceptions generated"
-  },
-  triggerGenerated: {
-    code: EventCode.TriggersGenerated,
-    type: "Triggers generated"
-  },
-  triggerDeleted: {
-    code: EventCode.TriggersDeleted,
-    type: "Triggers deleted"
-  },
-  hearingOutcomeIgnoredNoOffences: {
-    code: EventCode.IgnoredNoOffences,
-    type: "Hearing Outcome ignored as it contains no offences"
-  },
-  hearingOutcomeIgnoredReopened: {
-    code: EventCode.IgnoredReopened,
-    type: "Re-opened / Statutory Declaration case ignored"
-  },
-  hearingOutcomeIgnoredCourtDisabled: {
-    code: EventCode.IgnoredDisabled,
-    type: "Hearing outcome ignored - PNC update is not enabled for this court"
-  },
-  hearingOutcomeIgnoredAppeal: {
-    code: EventCode.IgnoredAppeal,
-    type: "Hearing outcome ignored - Appeal result did not amend disposal"
-  },
-  hearingOutcomeIgnoredNonrecordable: {
-    code: EventCode.IgnoredNonrecordable,
-    type: "Hearing Outcome ignored as no offences are recordable"
-  },
-  hearingOutcomeIgnoredAncillary: {
-    code: EventCode.IgnoredAncillary,
-    type: "Interim hearing with ancillary only court results. PNC not updated"
-  },
-  hearingOutcomeDetails: {
-    code: EventCode.HearingOutcomeDetails,
-    type: "Hearing outcome details"
-  },
-  messageRejectedByCoreHandler: {
-    code: EventCode.MessageRejected,
-    type: "Message Rejected by CoreHandler"
-  },
-  resubmittedHearingOutcomeReceived: {
-    code: EventCode.ReceivedResubmittedHearingOutcome,
-    type: "Resubmitted hearing outcome received"
-  },
-  resultsAlreadyOnPNC: {
-    code: EventCode.IgnoredAlreadyOnPNC,
-    type: "Results already on PNC"
-  },
-  pncUpdated: {
-    code: EventCode.PncUpdated,
-    type: "PNC Update applied successfully"
-  },
-  reportRun: {
-    code: EventCode.ReportRun,
-    type: "Report run"
-  },
-  pncResponseReceived: {
-    code: EventCode.PncResponseReceived,
-    type: "PNC Response received"
-  },
-  pncResponseNotReceived: {
-    code: EventCode.PncResponseNotReceived,
-    type: "PNC Response not received"
-  }
+export const auditLogEventLookup: Record<EventCode, string> = {
+  [EventCode.AllTriggersResolved]: "All triggers marked as resolved",
+  [EventCode.DuplicateMessage]: "Duplicate message body received",
+  [EventCode.ErrorRecordArchived]: "Error record archived",
+  [EventCode.ExceptionsGenerated]: "Exceptions generated",
+  [EventCode.ExceptionsLocked]: "Exception locked",
+  [EventCode.ExceptionsResolved]: "Exception marked as resolved by user",
+  [EventCode.ExceptionsUnlocked]: "Exception unlocked",
+  [EventCode.HearingOutcomeDetails]: "Hearing outcome details",
+  [EventCode.HearingOutcomeReallocated]: "Hearing outcome reallocated by user",
+  [EventCode.HearingOutcomeReceivedPhase1]: "Hearing outcome received by phase 1",
+  [EventCode.HearingOutcomeReceivedPhase2]: "Hearing outcome received by phase 2",
+  [EventCode.HearingOutcomeReceivedPhase3]: "Hearing outcome received by phase 3",
+  [EventCode.HearingOutcomeResubmittedPhase1]: "Hearing outcome resubmitted to phase 1",
+  [EventCode.HearingOutcomeResubmittedPhase2]: "Hearing outcome resubmitted to phase 2",
+  [EventCode.HearingOutcomeSubmittedPhase2]: "Hearing outcome submitted to phase 2",
+  [EventCode.HearingOutcomeSubmittedPhase3]: "Hearing outcome submitted to phase 3",
+  [EventCode.IgnoredAlreadyOnPNC]: "Results already on PNC",
+  [EventCode.IgnoredAncillary]: "Interim hearing with ancillary only court results. PNC not updated",
+  [EventCode.IgnoredAppeal]: "Hearing outcome ignored - Appeal result did not amend disposal",
+  [EventCode.IgnoredDisabled]: "Hearing outcome ignored - PNC update is not enabled for this court",
+  [EventCode.IgnoredNonrecordable]: "Hearing Outcome ignored as no offences are recordable",
+  [EventCode.IgnoredNoOffences]: "Hearing Outcome ignored as it contains no offences",
+  [EventCode.IgnoredReopened]: "Re-opened / Statutory Declaration case ignored",
+  [EventCode.MessageRejected]: "Message Rejected by CoreHandler",
+  [EventCode.PncResponseNotReceived]: "PNC Response not received",
+  [EventCode.PncResponseReceived]: "PNC Response received",
+  [EventCode.PncUpdated]: "PNC Update applied successfully",
+  [EventCode.ReceivedIncomingHearingOutcome]: "Incoming hearing outcome received",
+  [EventCode.ReceivedResubmittedHearingOutcome]: "Resubmitted hearing outcome received",
+  [EventCode.ReportRun]: "Report run",
+  [EventCode.RetryingMessage]: "Retrying message",
+  [EventCode.Sanitised]: "Message record sanitised",
+  [EventCode.TriggersDeleted]: "Triggers deleted",
+  [EventCode.TriggersGenerated]: "Triggers generated",
+  [EventCode.TriggersLocked]: "Trigger locked",
+  [EventCode.TriggersResolved]: "Trigger marked as resolved by user",
+  [EventCode.TriggersUnlocked]: "Trigger unlocked"
 }
--- packages/core/lib/CoreAuditLogger.ts
+++ packages/core/lib/CoreAuditLogger.ts
@@ -1,4 +1,5 @@
 import type { AuditLogEvent } from "@moj-bichard7/common/types/AuditLogEvent"
+import getAuditLogEvent from "../phase1/lib/auditLog/getAuditLogEvent"
 import type AuditLogger from "../phase1/types/AuditLogger"
 
 export default class CoreAuditLogger implements AuditLogger {
@@ -7,6 +8,11 @@ export default class CoreAuditLogger implements AuditLogger {
   logEvent = (auditLogEvent: AuditLogEvent): AuditLogger => {
     this.events.push(auditLogEvent)
 
+    return this
+  }
+
+  debug = (code: EventCode): AuditLogger => {
+    const event = getAuditLogEvent(eventCode)
     return this
   }
 
