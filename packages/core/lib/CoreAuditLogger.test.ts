b26430246cfcfe5db63c3bf7df2469ff74178dd0
Richard Cane <richard.cane@madetech.com>
2023-09-18 15:05:29 +0100

    Update core audit logger tests

--- packages/core/lib/CoreAuditLogger.test.ts
+++ packages/core/lib/CoreAuditLogger.test.ts
@@ -12,14 +12,60 @@ describe("CoreAuditLogger", () => {
     expect(auditLogger.getEvents()).toHaveLength(0)
   })
 
-  it("should log an info level log", () => {
-    const attributes = { foo: "bar" }
+  it("should log info", () => {
+    const attributes = { foo: "info" }
     auditLogger.info(EventCode.ExceptionsGenerated, attributes)
+
     expect(auditLogger.getEvents()).toHaveLength(1)
     expect(auditLogger.getEvents()[0]).toStrictEqual({
       eventCode: EventCode.ExceptionsGenerated,
       eventType: auditLogEventLookup[EventCode.ExceptionsGenerated],
       category: EventCategory.information,
+      eventSource: AuditLogEventSource.CorePhase1,
+      attributes,
+      timestamp: expect.any(Date)
+    })
+  })
+
+  it("should log errors", () => {
+    const attributes = { foo: "error" }
+    auditLogger.error(EventCode.MessageRejected, attributes)
+
+    expect(auditLogger.getEvents()).toHaveLength(1)
+    expect(auditLogger.getEvents()[0]).toStrictEqual({
+      eventCode: EventCode.MessageRejected,
+      eventType: auditLogEventLookup[EventCode.MessageRejected],
+      category: EventCategory.error,
+      eventSource: AuditLogEventSource.CorePhase1,
+      attributes,
+      timestamp: expect.any(Date)
+    })
+  })
+
+  it("should log warnings", () => {
+    const attributes = { foo: "warn" }
+    auditLogger.warn(EventCode.DuplicateMessage, attributes)
+
+    expect(auditLogger.getEvents()).toHaveLength(1)
+    expect(auditLogger.getEvents()[0]).toStrictEqual({
+      eventCode: EventCode.DuplicateMessage,
+      eventType: auditLogEventLookup[EventCode.DuplicateMessage],
+      category: EventCategory.warning,
+      eventSource: AuditLogEventSource.CorePhase1,
+      attributes,
+      timestamp: expect.any(Date)
+    })
+  })
+
+  it("should log debug info", () => {
+    const attributes = { foo: "debug" }
+    auditLogger.debug(EventCode.ExceptionsGenerated, attributes)
+
+    expect(auditLogger.getEvents()).toHaveLength(1)
+    expect(auditLogger.getEvents()[0]).toStrictEqual({
+      eventCode: EventCode.ExceptionsGenerated,
+      eventType: auditLogEventLookup[EventCode.ExceptionsGenerated],
+      category: EventCategory.debug,
       eventSource: AuditLogEventSource.CorePhase1,
       attributes,
       timestamp: expect.any(Date)
