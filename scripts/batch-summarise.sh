c8801081f17623fc39fb1c120769d1913a18bf9e
Ben Pirt <ben@pirt.co.uk>
2023-11-03 09:48:36 +0000

    Rework the rerun workflows to take an arbitrary duration for the period (#576)

--- scripts/batch-summarise.sh
+++ scripts/batch-summarise.sh
@@ -12,8 +12,8 @@ while read line; do
   summary=${line/json/summary.txt}
   summary=${summary/s3:\/\/bichard-7-production-processing-validation\//}
   outfile=${summary//\//-}
-  NO_OPEN=true npm run compare:summarise $line
+  NO_OPEN=true npm run -w packages/core compare:summarise $line
   echo $'\n\nCore Comparison\n======================================================' >> $cache_dir/$summary
-  npm run --silent compare -- -c -x -f $line >> $cache_dir/$summary
+  npm run -w packages/core --silent compare -- -c -x -f $line >> $cache_dir/$summary
   cp $cache_dir/$summary $outdir/$outfile
 done < "$INPUT_FILE"
