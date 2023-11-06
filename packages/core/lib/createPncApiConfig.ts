a92369225957a9bdd9704573f4148dcd2a801723
Richard Cane <richard.cane@madetech.com>
2023-11-06 11:25:24 +0000

    Set working default PNC API KEY

--- packages/core/lib/createPncApiConfig.ts
+++ packages/core/lib/createPncApiConfig.ts
@@ -2,7 +2,7 @@ import type PncApiConfig from "../types/PncApiConfig"
 
 const createPncApiConfig = (): PncApiConfig => ({
   url: process.env.PNC_API_URL ?? "https://localhost:9443/bichard-api/pnc",
-  key: process.env.PNC_API_KEY ?? "apikey"
+  key: process.env.PNC_API_KEY ?? "super_secret_key"
 })
 
 export default createPncApiConfig
