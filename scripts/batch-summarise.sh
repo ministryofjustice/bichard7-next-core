8346116fa2069fff6f450823522827361e5a3169
Ben Pirt <ben@pirt.co.uk>
2023-11-07 14:50:36 +0000

    Update the batch summarise script

--- scripts/batch-summarise.sh
+++ scripts/batch-summarise.sh
@@ -13,7 +13,16 @@ while read line; do
   summary=${summary/s3:\/\/bichard-7-production-processing-validation\//}
   outfile=${summary//\//-}
   NO_OPEN=true npm run -w packages/core compare:summarise $line
-  echo $'\n\nCore Comparison\n======================================================' >> $cache_dir/$summary
-  npm run -w packages/core --silent compare -- -c -x -f $line >> $cache_dir/$summary
+  echo $'\n\nCore Comparison\n======================================================\n' >> $cache_dir/$summary
+  echo $line >> $cache_dir/$summary
+  npm run -w packages/core --silent compare -- -c -f $line > /dev/null
+  COMPARISON_RESULT=$?
+  if [ $COMPARISON_RESULT == 0 ]
+  then
+    echo "AHO XML Comparison matches" >>  $cache_dir/$summary
+  else
+    echo $'AHO XML Comparison does not match\n' >>  $cache_dir/$summary
+    npm run -w packages/core --silent compare -- -c -x -f $line >> $cache_dir/$summary
+  fi
   cp $cache_dir/$summary $outdir/$outfile
 done < "$INPUT_FILE"
