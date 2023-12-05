c3e56cea049ecfef2951fa174fbfbc72e65f259f
Richard Cane <richard.cane@madetech.com>
2023-12-05 15:59:25 +0000

    Add error output for s3 path parsing failure

--- packages/core/phase1/lib/parseS3Path.ts
+++ packages/core/phase1/lib/parseS3Path.ts
@@ -1,3 +1,4 @@
+import { isError, type Result } from "@moj-bichard7/common/types/Result"
 import path from "path"
 
 export type IncomingMessageMetadata = {
@@ -5,20 +6,24 @@ export type IncomingMessageMetadata = {
   receivedDate: Date
 }
 
-const readReceivedDateFromS3ObjectKey = (key: string): Date => {
+const readReceivedDateFromS3ObjectKey = (key: string): Result<Date> => {
   const segments = key.split("/")
 
   if (segments.length < 6) {
-    throw new Error(`The object key "${key}" is in an invalid format`)
+    return new Error(`The object key "${key}" is in an invalid format`)
   }
 
   const iso = `${segments[0]}-${segments[1]}-${segments[2]}T${segments[3]}:${segments[4]}:00Z`
   return new Date(iso)
 }
 
-const parseS3Path = (s3Path: string): IncomingMessageMetadata => {
+const parseS3Path = (s3Path: string): Result<IncomingMessageMetadata> => {
   const externalId = path.basename(s3Path).replace(".xml", "")
   const receivedDate = readReceivedDateFromS3ObjectKey(s3Path)
+  if (isError(receivedDate)) {
+    return receivedDate
+  }
+
   return { externalId, receivedDate }
 }
 
