326feff7c9231d2edd79b3836d6526a8009a579d
Richard Cane <richard.cane@madetech.com>
2023-12-14 15:19:29 +0000

    Unit tests for forwardMessage and sendToResubmissionQueue

---
+++ packages/message-forwarder/src/forwardMessage/forwardMessage.unit.test.ts
@@ -0,0 +1,36 @@
+import "../test/setup/setEnvironmentVariables"
+process.env.DESTINATION_TYPE = "mq"
+
+import { Client } from "@stomp/stompjs"
+import { randomUUID } from "crypto"
+import fs from "fs"
+
+import * as conductor from "@moj-bichard7/common/conductor/conductorApi"
+import createMqConfig from "@moj-bichard7/common/mq/createMqConfig"
+
+import createStompClient from "../createStompClient"
+import forwardMessage from "./forwardMessage"
+
+const mq = createMqConfig()
+const stomp = createStompClient()
+
+describe("forwardMessage", () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it("throws an exception if aho is invalid", async () => {
+    await expect(forwardMessage("<>", expect.any(Client))).rejects.toThrow("Could not parse AHO XML")
+  })
+
+  it("throws an exception if getWorkflowByCorrelationId returns an error", async () => {
+    jest.spyOn(conductor, "getWorkflowByCorrelationId").mockReturnValue(Promise.resolve(new Error("Mock error")))
+
+    const incomingMessage = String(fs.readFileSync("src/test/fixtures/success-exceptions-aho-resubmitted.xml")).replace(
+      "CORRELATION_ID",
+      randomUUID()
+    )
+
+    await expect(forwardMessage(incomingMessage, stomp)).rejects.toThrow("Mock error")
+  })
+})
---
+++ packages/message-forwarder/src/forwardMessage/sendToResubmissionQueue/sendToResubmissionQueue.unit.test.ts
@@ -0,0 +1,32 @@
+import "../../test/setup/setEnvironmentVariables"
+
+import { type Client } from "@stomp/stompjs"
+import { randomUUID } from "crypto"
+
+import logger from "@moj-bichard7/common/utils/logger"
+
+import { sendToResubmissionQueue } from "./sendToResubmissionQueue"
+
+describe("sendToResubmissionQueue", () => {
+  const stomp: { publish: jest.Func } = jest.createMockFromModule("../../createStompClient")
+  stomp.publish = jest.fn()
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it("calls client.publish", () => {
+    sendToResubmissionQueue(stomp as Client, "", "")
+
+    expect(stomp.publish).toHaveBeenCalled()
+  })
+
+  it("calls logger.info with the correlationId parameter", () => {
+    jest.spyOn(logger, "info")
+
+    const correlationId = randomUUID()
+    sendToResubmissionQueue(stomp as Client, "", correlationId)
+
+    expect(logger.info).toHaveBeenCalledWith(`Sent message to MQ (${correlationId})`)
+  })
+})
