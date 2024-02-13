a3574f1467fa31bf58110cfa814ddcca03a949f4
Ben Pirt <ben@pirt.co.uk>
2024-02-13 13:42:51 +0000

    Switch back to using stompit for the mq connection

--- packages/core/lib/mq/connectAndSendMessage.ts
+++ packages/core/lib/mq/connectAndSendMessage.ts
@@ -1,60 +1,46 @@
-import { Client } from "@stomp/stompjs"
-import { WebSocket } from "ws"
+import logger from "@moj-bichard7/common/utils/logger"
+import type { Client } from "stompit"
+import { ConnectFailover } from "stompit"
+import createMqConfig from "./createMqConfig"
+import deconstructServers from "./deconstructServers"
 
-Object.assign(global, { WebSocket })
+const config = createMqConfig()
 
-const createStompClient = (): Client => {
-  const { MQ_URL, MQ_AUTH } = process.env
-
-  if (!MQ_URL || !MQ_AUTH) {
-    throw Error("MQ environment variables must all have value.")
-  }
-
-  const { username, password } = JSON.parse(MQ_AUTH)
-  if (!username || !password) {
-    throw Error("MQ_AUTH environment variable set incorrectly")
-  }
-
-  const brokerURL = MQ_URL.replace(/\s/g, "")
-  const match = brokerURL.match(/failover:\((.+)\)/i)
-  const brokerUrls = match && match[1] ? match[1].split(",") : [brokerURL]
-
-  let activeBrokerIndex = 0
-
-  const client = new Client({
-    brokerURL: brokerUrls[0],
-    connectHeaders: {
-      login: username,
-      passcode: password
-    },
-    beforeConnect: () => {
-      client.brokerURL = brokerUrls[activeBrokerIndex]
-      activeBrokerIndex += 1
-      if (!brokerUrls[activeBrokerIndex]) {
-        activeBrokerIndex = 0
-      }
-    }
-  })
-  return client
+const connectionOptions = deconstructServers(config)
+const reconnectOptions: ConnectFailover.ConnectFailoverOptions = {
+  initialReconnectDelay: 1000,
+  maxReconnectDelay: 3000,
+  maxReconnects: 2,
+  useExponentialBackOff: false
 }
 
-const connectAndSendMessage = (destination: string, body: string, timeoutMs = 5000): Promise<void> => {
+const connectAndSendMessage = (destination: string, body: string): Promise<void> => {
   return new Promise((resolve, reject) => {
-    const client = createStompClient()
-    const timeout = setTimeout(() => {
-      client.deactivate()
-      reject(new Error("Timed out sending message"))
-    }, timeoutMs)
-    client.onConnect = () => {
-      client.publish({ destination, body, skipContentLengthHeader: true })
-      client.deactivate()
-      clearTimeout(timeout)
-      resolve()
-    }
-    client.onStompError = (frame) => {
-      reject(new Error(frame.headers.message ?? "Error sending message"))
-    }
-    client.activate()
+    const connectionManager = new ConnectFailover(connectionOptions, reconnectOptions)
+    connectionManager.connect((error: Error | null, client: Client) => {
+      if (error) {
+        reject(error)
+        return
+      }
+
+      const cleanup = () => {
+        client.disconnect((e: Error | null) => {
+          if (e) {
+            logger.error(e)
+          }
+          resolve()
+        })
+      }
+
+      const options: Client.SendOptions = {
+        onReceipt: cleanup,
+        onError: reject
+      }
+
+      const writable = client.send({ destination: `/queue/${destination}` }, options)
+      writable.write(body)
+      writable.end()
+    })
   })
 }
 
