b0fac08710b8fb7658e3ea4b06d8213160a20b49
Ben Pirt <ben@pirt.co.uk>
2024-03-11 15:02:17 +0000

    Add test for PopulateOffences with Adjournment Sine Die results

--- packages/core/phase1/parse/transformSpiToAho/PopulateOffences.test.ts
+++ packages/core/phase1/parse/transformSpiToAho/PopulateOffences.test.ts
@@ -3,13 +3,23 @@ import parseSpiResult from "../parseSpiResult"
 import PopulateOffences from "./PopulateOffences"
 
 describe("PopulateOffences", () => {
-  const message = readFileSync("phase1/tests/fixtures/input-message-001.xml", "utf-8")
-  const courtResult = parseSpiResult(message).DeliverRequest.Message.ResultedCaseMessage
-
   it("should transform SPI Offences to Hearing Outcome Offences", () => {
+    const message = readFileSync("phase1/tests/fixtures/input-message-001.xml", "utf-8")
+    const courtResult = parseSpiResult(message).DeliverRequest.Message.ResultedCaseMessage
     const result = new PopulateOffences(courtResult).execute()
 
     expect(result).toBeDefined()
     expect(result).toMatchSnapshot()
+  })
+
+  it("should handle adjournment sine die results by setting the conviction date on offences", () => {
+    const message = readFileSync("phase1/tests/fixtures/input-message-001-sine-die.xml", "utf-8")
+    const courtResult = parseSpiResult(message).DeliverRequest.Message.ResultedCaseMessage
+    const result = new PopulateOffences(courtResult).execute()
+
+    expect(result).toBeDefined()
+    expect(result.offences[0].ConvictionDate).toBeUndefined()
+    expect(result.offences[1].ConvictionDate).toStrictEqual(new Date("2011-09-26"))
+    expect(result.offences[2].ConvictionDate).toStrictEqual(new Date("2011-09-26"))
   })
 })
