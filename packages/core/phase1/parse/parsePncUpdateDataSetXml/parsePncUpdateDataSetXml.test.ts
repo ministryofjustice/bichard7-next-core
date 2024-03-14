3fdd0e8156078fef217579d0ae797bf79e654ae8
Kate Sugden <kate.sugden@madetech.com>
2024-03-14 13:56:20 +0000

    Refactor: corrects naming of variable

--- packages/core/phase1/parse/parsePncUpdateDataSetXml/parsePncUpdateDataSetXml.test.ts
+++ packages/core/phase1/parse/parsePncUpdateDataSetXml/parsePncUpdateDataSetXml.test.ts
@@ -5,22 +5,22 @@ import parsePncUpdateDataSetXml from "./parsePncUpdateDataSetXml"
 describe("parsePncUpdateDataSetXml", () => {
   it("converts XML to PncUpdateDataSet when operations exist", () => {
     const inputXml = fs.readFileSync("phase1/tests/fixtures/PncUpdateDataSet-with-operations.xml").toString()
-    const parsedAho = parsePncUpdateDataSetXml(inputXml)
+    const parsedPncUpdateDatasetAho = parsePncUpdateDataSetXml(inputXml)
 
-    expect(parsedAho).toMatchSnapshot()
+    expect(parsedPncUpdateDatasetAho).toMatchSnapshot()
   })
 
   it("converts XML to PncUpdateDataSet when no operations exist", () => {
     const inputXml = fs.readFileSync("phase1/tests/fixtures/PncUpdateDataSet-no-operations.xml").toString()
-    const parsedAho = parsePncUpdateDataSetXml(inputXml)
+    const parsedPncUpdateDatasetAho = parsePncUpdateDataSetXml(inputXml)
 
-    expect(parsedAho).toMatchSnapshot()
+    expect(parsedPncUpdateDatasetAho).toMatchSnapshot()
   })
 
   it("converts XML to PncUpdateDataSet when an empty operation element exists", () => {
     const inputXml = fs.readFileSync("phase1/tests/fixtures/PncUpdateDataSet-with-empty-operations.xml").toString()
-    const parsedAho = parsePncUpdateDataSetXml(inputXml)
+    const parsedPncUpdateDatasetAho = parsePncUpdateDataSetXml(inputXml)
 
-    expect(parsedAho).toMatchSnapshot()
+    expect(parsedPncUpdateDatasetAho).toMatchSnapshot()
   })
 })
