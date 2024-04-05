ec714a50f680072809a0ff4be601320ef3eb50ef
Richard Race <1273965+richrace@users.noreply.github.com>
2024-04-05 15:34:59 +0100

    Set locale on server

---
+++ .circleci/scripts/set-locale.sh
@@ -0,0 +1,6 @@
+export LANG=en_GB.UTF-8
+
+sudo apt-get install -y locales && \
+  sudo sed -i -e "s/# $LANG.*/$LANG UTF-8/" /etc/locale.gen && \
+  sudo dpkg-reconfigure --frontend=noninteractive locales && \
+  sudo update-locale LANG=$LANG
