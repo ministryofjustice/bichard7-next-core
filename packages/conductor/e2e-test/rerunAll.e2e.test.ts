89d0824d5a9ab14d7fb6988403a853f19fb8a60f
Emad Karamad <emad.karamad@madetech.com>
2024-04-05 11:42:34 +0100

    Add rerun_all e2e test

--- packages/conductor/e2e-test/rerunAll.e2e.test.ts
+++ packages/conductor/e2e-test/rerunAll.e2e.test.ts
@@ -2,7 +2,8 @@ jest.setTimeout(30_000)
 jest.retryTimes(10)
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
-import { DynamoDBDocumentClient, GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb"
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs"
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb"
 import createS3Config from "@moj-bichard7/common/s3/createS3Config"
 import fs from "fs"
 import waitForExpect from "wait-for-expect"
@@ -14,6 +15,12 @@ const secretAccessKey = (process.env.S3_AWS_SECRET_ACCESS_KEY = "FAKE")
 const s3Config = createS3Config()
 
 const db = new DynamoDBClient({
+  endpoint,
+  region: "eu-west-2",
+  credentials: { accessKeyId, secretAccessKey }
+})
+
+const client = new SQSClient({
   endpoint,
   region: "eu-west-2",
   credentials: { accessKeyId, secretAccessKey }
@@ -33,31 +40,6 @@ const getDynamoRecord = async (s3Path: string, tableName: string): Promise<undef
   return result.Item
 }
 
-const insertRecordInDynamo = async (s3Path: string, tableName: string) => {
-  const putCommand = new PutCommand({
-    TableName: tableName,
-    Item: {
-      s3Path,
-      latestRunAt: "2024-04-03T13:15:47.011Z",
-      latestResult: 0,
-      correlationId: "5b515377-e65b-4c6c-a69b-5ba835cabb87",
-      initialRunAt: "2024-04-03T13:15:47.011Z",
-      history: [
-        {
-          result: 0,
-          details: { triggersMatch: 0, exceptionsMatch: 0, xmlParsingMatches: 0, xmlOutputMatches: 0 },
-          runAt: "2024-04-03T13:15:47.011Z"
-        }
-      ],
-      version: 1,
-      _: "_",
-      initialResult: 0
-    }
-  })
-  const docClient = DynamoDBDocumentClient.from(db)
-  await docClient.send(putCommand)
-}
-
 const getPhaseTableName = (phase: number): string => {
   switch (phase) {
     case 1:
@@ -72,24 +54,44 @@ const getPhaseTableName = (phase: number): string => {
 }
 
 describe("Rerun all workflow", () => {
-  const phases = [1, 2]
-  it.each(phases)("should rerun and update dynamo record", async (phase) => {
+  it("should rerun phase 2 comparisons and update dynamo record", async () => {
+    const phase = 2
     const fixturePath = `../core/phase${phase}/tests/fixtures/e2e-comparison/test-001.json`
     const tableName = getPhaseTableName(phase)
     //write file to s3 with unique id
     const s3Path = `${new Date().toISOString().replace(/:/g, "_")}.json`
     await sendFileToS3(fixturePath, s3Path, "comparisons")
-    await insertRecordInDynamo(s3Path, tableName)
-
-    // TODO: Push a message to SQS to trigger the workflow
 
     //wait for dynamo to be updated
-    let record: Record<string, any> | undefined
+    let originalRecord: Record<string, any> | undefined
     await waitForExpect(async () => {
-      record = await getDynamoRecord(s3Path, tableName)
-      expect(record).toBeDefined()
+      originalRecord = await getDynamoRecord(s3Path, tableName)
+      expect(originalRecord?.["history"]).toHaveLength(1)
+      expect(originalRecord).toBeDefined()
     })
 
-    expect(record).toHaveProperty("s3Path", s3Path)
+    expect(originalRecord).toHaveProperty("s3Path", s3Path)
+
+    const startDate = new Date()
+    startDate.setHours(startDate.getHours() - 1)
+
+    const command = new SendMessageCommand({
+      QueueUrl: "rerunAll",
+      DelaySeconds: 3,
+      MessageBody: JSON.stringify({ startDate: startDate.toISOString() })
+    })
+
+    await client.send(command)
+
+    await waitForExpect(
+      async () => {
+        const updatedRecord = await getDynamoRecord(s3Path, tableName)
+        expect(updatedRecord).toBeDefined()
+        expect(updatedRecord?.["history"]).toHaveLength(2)
+        expect(originalRecord?.["latestRunAt"]).not.toBe(updatedRecord?.latestRunAt)
+      },
+      60_000,
+      5_000
+    )
   })
 })
