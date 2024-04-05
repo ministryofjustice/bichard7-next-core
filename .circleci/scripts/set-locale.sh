fd915859bb0f294c593d27d2141ee120f746c05a
Richard Race <1273965+richrace@users.noreply.github.com>
2024-04-05 16:06:00 +0100

    Update scirpt to set the locale by default

--- .circleci/scripts/set-locale.sh
+++ .circleci/scripts/set-locale.sh
@@ -1,6 +1,8 @@
-export LANG=en_GB.UTF-8
+LANG="en_GB.UTF-8"
 
 sudo apt-get install -y locales && \
   sudo sed -i -e "s/# $LANG.*/$LANG UTF-8/" /etc/locale.gen && \
   sudo dpkg-reconfigure --frontend=noninteractive locales && \
   sudo update-locale LANG=$LANG
+
+echo "export LANG=$LANG" >> ~/.bash_profile
