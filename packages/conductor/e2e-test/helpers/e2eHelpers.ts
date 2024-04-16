5233c03c9129b0276c1dbd7d642af692b87d1858
Kate Sugden <kate.sugden@madetech.com>
2024-04-16 11:56:40 +0100

    Helper takes dynamoDbClient argument

--- packages/conductor/e2e-test/helpers/e2eHelpers.ts
+++ packages/conductor/e2e-test/helpers/e2eHelpers.ts
@@ -2,6 +2,7 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
 import { GetCommand, DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb"
 import createS3Config from "@moj-bichard7/common/s3/createS3Config"
 import fs from "fs"
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 
 const sendFileToS3 = async (srcFilename: string, destFilename: string, bucket: string) => {
     const client = new S3Client(createS3Config())
@@ -10,21 +11,21 @@ const sendFileToS3 = async (srcFilename: string, destFilename: string, bucket: s
     return client.send(command)
   }
   
-const getDynamoRecord = async (s3Path: string, tableName: string): Promise<undefined | Record<string, any>> => {
+const getDynamoRecord = async (dynamoDbClient: DynamoDBClient, s3Path: string, tableName: string): Promise<undefined | Record<string, any>> => {
   const getCommand = new GetCommand({ TableName: tableName, Key: { s3Path } })
-  const docClient = DynamoDBDocumentClient.from(db)
+  const docClient = DynamoDBDocumentClient.from(dynamoDbClient)
   const result = await docClient.send(getCommand)
   return result.Item
 }
 
-const setDynamoRecordToFailedStatus = async (record: Record<string, unknown>, tableName: string) => {
+const setDynamoRecordToFailedStatus = async (dynamoDbClient: DynamoDBClient, record: Record<string, unknown>, tableName: string) => {
   const updatedRecord = {
     ...record,
     latestResult: 0
   }
 
   const putCommand = new PutCommand({ TableName: tableName, Item: updatedRecord })
-  const docClient = DynamoDBDocumentClient.from(db)
+  const docClient = DynamoDBDocumentClient.from(dynamoDbClient)
   await docClient.send(putCommand)
 }
   
