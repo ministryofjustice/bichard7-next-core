9060ea1b8eb5b8da02ad5aa342cd406e885a6a38
Kate Sugden <kate.sugden@madetech.com>
2024-04-17 17:08:31 +0100

    Adds a unit test for comparePhase2

---
+++ packages/core/comparison/lib/comparePhase2.test.ts
@@ -0,0 +1,20 @@
+import comparePhase2 from "./comparePhase2"
+import type { Phase2Comparison } from "../types/ComparisonFile"
+import processTestFile from "./processTestFile"
+
+describe("compare phase 2", () => {
+  describe("success", () => {
+    it("should correctly compare xml parsing", async () => {
+      const comparison = processTestFile("phase2/tests/fixtures/e2e-comparison/test-001.json") as Phase2Comparison
+      const result = await comparePhase2(comparison)
+      expect(result.xmlParsingMatches).toBe(true)
+    })
+  }),
+  describe("failure", () => {
+    it("should not correctly parse phase1 message", async () => {
+      const comparison = processTestFile("phase1/tests/fixtures/comparison/failing.json") as Phase2Comparison
+      const result = await comparePhase2(comparison)
+      expect(result.xmlParsingMatches).toBe(false)
+    })
+  })
+})
