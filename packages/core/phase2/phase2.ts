50f156f156834e8e3024027e77d68b0e0ab2f869
Emad Karamad <emad.karamad@madetech.com>
2024-04-18 15:50:44 +0100

    Fix lint errors

--- packages/core/phase2/phase2.ts
+++ packages/core/phase2/phase2.ts
@@ -2,13 +2,12 @@ import type AuditLogger from "../phase1/types/AuditLogger"
 import type Phase2Result from "./types/Phase2Result"
 import { Phase2ResultType } from "./types/Phase2Result"
 import type { AnnotatedHearingOutcome } from "../types/AnnotatedHearingOutcome"
-import { PncUpdateDataset } from "../types/PncUpdateDataset"
+import type { PncUpdateDataset } from "../types/PncUpdateDataset"
 
 const phase2 = (
   incomingMessage: AnnotatedHearingOutcome | PncUpdateDataset,
   _auditLogger: AuditLogger
 ): Phase2Result => {
-
   const outputMessage = structuredClone(incomingMessage) as PncUpdateDataset
   outputMessage.HasError = false
   outputMessage.PncOperations = outputMessage.PncOperations || []
