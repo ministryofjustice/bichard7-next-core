dc5becd339d30670dd13b738c07fbc5a3658d2ad
Kate Sugden <kate.sugden@madetech.com>
2024-04-19 10:17:45 +0100

    Removes async from synchronous test function

--- packages/core/comparison/lib/comparePhase2.test.ts
+++ packages/core/comparison/lib/comparePhase2.test.ts
@@ -4,14 +4,14 @@ import processTestFile from "./processTestFile"
 
 describe("compare phase 2", () => {
   describe("success", () => {
-    it("should correctly compare xml parsing", async () => {
+    it("should correctly compare xml parsing", () => {
       const comparison = processTestFile("phase2/tests/fixtures/e2e-comparison/test-001.json") as Phase2Comparison
       const result = comparePhase2(comparison)
       expect(result.xmlParsingMatches).toBe(true)
     })
   })
   describe("failure", () => {
-    it("should not correctly parse phase1 message", async () => {
+    it("should not correctly parse phase1 message", () => {
       const comparison = processTestFile("phase1/tests/fixtures/comparison/failing.json") as Phase2Comparison
       const result = comparePhase2(comparison)
       expect(result.xmlParsingMatches).toBe(false)
