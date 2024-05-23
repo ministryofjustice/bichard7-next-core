0f41e140fb5d495adce1e3ffd37ff117bb96bc6c
Kate Sugden <kate.sugden@madetech.com>
2024-05-23 17:18:06 +0100

    Fixes: can extract location with missing closing clause in resultVariableText

--- packages/core/phase2/exclusionRequirementsDisposalText.ts
+++ packages/core/phase2/exclusionRequirementsDisposalText.ts
@@ -1,24 +1,38 @@
-// regex matchs "NOT ENTER X THIS EXCLUSION LASTS" or "NOT TO ENTER X THIS EXCLUSION LASTS" and ignores line breaks
+// regex matchs "NOT ENTER X THIS EXCLUSION LASTS FOR" or "NOT TO ENTER X THIS EXCLUSION LASTS FOR" and ignores line breaks
+
 const REGEX =
-  /NOT\s+TO\s+ENTER(?<location1>[\s\S]*?)THIS\s+EXCLUSION\s+REQUIREMENT\s+LASTS\s+FOR|NOT\s+ENTER(?<location2>[\s\S]*?)THIS\s+EXCLUSION\s+REQUIREMENT\s+LASTS\s+FOR/g
+  /NOT\s+TO\s+ENTER(?<location1>[\s\S]*?)THIS\s+EXCLUSION\s+REQUIREMENT\s+LASTS\s+FOR|NOT\s+ENTER(?<location2>[\s\S]*?)THIS\s+EXCLUSION\s+REQUIREMENT\s+LASTS\s+FOR/gs
+
+const REGEX2 = /NOT\s+TO\s+ENTER\s(?<location3>[\s\S]*)/g
 
 const exclusionRequirementsDisposalText = (resultVariableText: string): string => {
-  let match: RegExpExecArray | null
 
   const locations: string[] = []
 
-  while ((match = REGEX.exec(resultVariableText)) !== null) {
-    if (match.groups) {
-      Object.values(match.groups).forEach(
-        (location) => location && locations.push(location.replace(/\s+/g, " ").trim())
-      )
+  const matches = [...resultVariableText.matchAll(REGEX)]
+  
+  const capturedValues = matches
+  .flatMap((match) => Object.values(match.groups || {}).filter(x=>x).map((value) => value.replace(/\s+/g, " ").trim()))
+  
+  capturedValues.forEach( (value) => {
+    const nestedMatches = [...value.matchAll(REGEX2)]
+    if (nestedMatches.length > 0) {
+      nestedMatches.forEach((match) => {
+        if (match.groups) {
+          Object.values(match.groups).forEach((group) => {
+            locations.push(group)
+          })
+        }
+      })
+    } else {
+      locations.push(value)
     }
-  }
+  })
 
   if (locations.length) {
-    const longestLoaction = locations.reduce((a, b) => (a.length > b.length ? a : b))
+    const longestLocation = locations.reduce((a, b) => (a.length > b.length ? a : b))
 
-    return `EXCLUDED FROM ${longestLoaction}`
+    return `EXCLUDED FROM ${longestLocation}`
   }
 
   return ""
