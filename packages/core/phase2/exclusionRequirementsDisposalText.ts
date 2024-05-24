8d958a536cfcc5258df7d6f4f1b714e87840710f
Kate Sugden <kate.sugden@madetech.com>
2024-05-24 12:58:05 +0100

    Makes regex processing easier to read

--- packages/core/phase2/exclusionRequirementsDisposalText.ts
+++ packages/core/phase2/exclusionRequirementsDisposalText.ts
@@ -1,34 +1,32 @@
 // regex matchs "NOT ENTER X THIS EXCLUSION LASTS FOR" or "NOT TO ENTER X THIS EXCLUSION LASTS FOR" and ignores line breaks
 
 const REGEX =
-  /NOT\s+(TO\s+)?ENTER(?<location1>.*?)THIS\s+EXCLUSION\s+REQUIREMENT\s+LASTS\s+FOR/gs
+  /NOT\s+(TO\s+)?ENTER(?<location>.*?)THIS\s+EXCLUSION\s+REQUIREMENT\s+LASTS\s+FOR/gs
 
-const nestedREGEX = /NOT\s+(TO\s+)?ENTER\s(?<location3>[\s\S]*)/g
+const nestedREGEX = /NOT\s+(TO\s+)?ENTER\s(?<nestedLocation>[\s\S]*)/g
 
 const exclusionRequirementsDisposalText = (resultVariableText: string): string => {
 
   const locations: string[] = []
 
-  const matches = [...resultVariableText.matchAll(REGEX)]
-  
-  const capturedValues = matches
-  .flatMap((match) => Object.values(match.groups || {}).filter(x=>x).map((value) => value.replace(/\s+/g, " ").trim()))
-  
-  capturedValues.forEach( (value) => {
-    const nestedMatches = [...value.matchAll(nestedREGEX)]
-    if (nestedMatches.length > 0) {
-      nestedMatches.forEach((match) => {
-        if (match.groups) {
-          Object.values(match.groups).forEach((group) => {
-            locations.push(group)
-          })
-        }
-      })
-    } else {
-      locations.push(value)
-    }
-  })
+  const matches = resultVariableText.matchAll(REGEX)
 
+  for (const match of matches) {
+    if (match?.groups?.location) {
+      let nestedMatchFound = false
+      const nestedMatches = match.groups.location.matchAll(nestedREGEX)
+      for (const nestedMatch of nestedMatches) {
+        if (nestedMatch?.groups?.nestedLocation) {
+          locations.push(nestedMatch.groups.nestedLocation.replace(/\s+/g, " ").trim())
+          nestedMatchFound = true
+        }
+      }
+      if (!nestedMatchFound){
+        locations.push(match.groups.location.replace(/\s+/g, " ").trim())
+      }
+      }
+  }
+  
   if (locations.length) {
     const longestLocation = locations.reduce((a, b) => (a.length > b.length ? a : b))
 
