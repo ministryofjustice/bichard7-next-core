2325501843285e82afec7b2f06dbdb516cc98595
Kate Sugden <kate.sugden@madetech.com>
2024-05-30 09:30:11 +0100

    Uncommments log messages

--- packages/core/phase2/putPncUpdateError.ts
+++ packages/core/phase2/putPncUpdateError.ts
@@ -1,9 +1,9 @@
 import type { AnnotatedPNCUpdateDataset } from "../types/AnnotatedPNCUpdateDataset"
 
 const putPncUpdateError = (pud: AnnotatedPNCUpdateDataset) => {
-  // console.log("pud has exceptions:", pud.AnnotatedPNCUpdateDataset.PNCUpdateDataset.Exceptions)
-  // console.log("Inserts or updates an error list record derived from the specified annotated PNC update dataset")
-  // console.log("To be implemented: ErrorListServiceImpl.java:440")
+  console.log("pud has exceptions:", pud.AnnotatedPNCUpdateDataset.PNCUpdateDataset.Exceptions)
+  console.log("Inserts or updates an error list record derived from the specified annotated PNC update dataset")
+  console.log("To be implemented: ErrorListServiceImpl.java:440")
 }
 
 export default putPncUpdateError
