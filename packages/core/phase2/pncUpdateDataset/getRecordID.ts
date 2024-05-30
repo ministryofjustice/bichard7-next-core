7724540b411fe5639ac0aed76dc839e1c2f8d45c
Ian King <ian.antking@gmail.com>
2024-05-30 11:25:10 +0100

    add skeleton functions for putPncUpdateError

---
+++ packages/core/phase2/pncUpdateDataset/getRecordID.ts
@@ -0,0 +1,6 @@
+const getRecordId = (messageId: string): number => {
+  console.log("To be implemented: ErrorListServicesImpl.java:1076", messageId)
+  return 1
+}
+
+export default getRecordId
--- packages/core/phase2/pncUpdateDataset/putPncUpdateError.ts
+++ packages/core/phase2/pncUpdateDataset/putPncUpdateError.ts
@@ -1,5 +1,35 @@
 import type { AnnotatedPNCUpdateDataset } from "../../types/AnnotatedPNCUpdateDataset"
+import getRecordId from "./getRecordID"
+import insertPncUpdateError from "./insertPncUpdateError"
+import revisePncUpdateError from "./revisePncUpdateError"
 
-const putPncUpdateError = (_: AnnotatedPNCUpdateDataset) => {}
+const METHOD_NAME = "putPNCUpdateError"
+const MESSAGE_ID_SIZE = 70
+const RECORD_NOT_FOUND = -1
+
+const putPncUpdateError = (annotatedDataset: AnnotatedPNCUpdateDataset) => {
+  const messageId =
+    annotatedDataset.AnnotatedPNCUpdateDataset.PNCUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Hearing.SourceReference.UniqueID.slice(
+      0,
+      MESSAGE_ID_SIZE
+    )
+
+  const errorId: number = getRecordId(messageId)
+
+  const message = `${METHOD_NAME}: Looked up error ID for message Id '${messageId}', the result is ${errorId}`
+  if (errorId === RECORD_NOT_FOUND) {
+    message.concat(" (not found)")
+  }
+
+  try {
+    if (errorId === RECORD_NOT_FOUND) {
+      insertPncUpdateError(annotatedDataset)
+    } else {
+      revisePncUpdateError(errorId, annotatedDataset)
+    }
+  } catch (err) {
+    console.log("to be implemented: ErrorListServicesImpl.java:471")
+  }
+}
 
 export default putPncUpdateError
