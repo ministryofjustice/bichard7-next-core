2a86c55ad4f4004ff54e553744947441cfadf0f2
Sen <65850511+Senny10@users.noreply.github.com>
2024-05-30 11:44:51 +0100

    Rewrite PNCUpdateChoreographyDS.java:159 (#740)
    * wip
    
    * wip
    
    * Rewrite PNCUpdateChoreographyDS.java:159

---
+++ packages/core/phase2/pncUpdateDataset/combineTriggerLists.ts
@@ -0,0 +1,8 @@
+import type { Trigger } from "../../phase1/types/Trigger"
+
+const combineTriggerLists = (_preUpdateTriggersArray: Trigger[], _postUpdateTriggersArray: Trigger[]): Trigger[] => {
+  console.log("To be implemented: TriggerBuilder.java:459")
+  return []
+}
+
+export default combineTriggerLists
