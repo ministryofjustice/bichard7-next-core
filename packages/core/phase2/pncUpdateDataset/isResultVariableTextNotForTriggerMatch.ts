273ba016762a5658365f1e2a286d3fd32811d090
Emad Karamad <emad.karamad@madetech.com>
2024-06-07 15:23:46 +0100

    Add skeleton for post-update triggers

---
+++ packages/core/phase2/pncUpdateDataset/isResultVariableTextNotForTriggerMatch.ts
@@ -0,0 +1,7 @@
+const isResultVariableTextNotForTriggerMatch = (_triggerCode: string, _resultVariableText: string): boolean => {
+  console.log("To be implemented: TriggerBuilderUtilsImpl.java:628")
+
+  return false
+}
+
+export default isResultVariableTextNotForTriggerMatch
