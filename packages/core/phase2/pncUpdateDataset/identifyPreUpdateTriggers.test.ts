dc566a6634ecd7248ec45ef10937fd41f3e61cdc
Ian King <ian.antking@gmail.com>
2024-06-10 15:40:22 +0100

    move AHO fields into helper function

--- packages/core/phase2/pncUpdateDataset/identifyPreUpdateTriggers.test.ts
+++ packages/core/phase2/pncUpdateDataset/identifyPreUpdateTriggers.test.ts
@@ -1,5 +1,4 @@
 import type { Trigger } from "../../phase1/types/Trigger"
-import type { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
 import type { PncUpdateDataset } from "../../types/PncUpdateDataset"
 import { TriggerCode } from "../../types/TriggerCode"
 import generatePncUpdateDatasetFromOffenceList from "../tests/fixtures/helpers/generatePncUpdateDatasetFromOffenceList"
@@ -7,25 +6,7 @@ import identifyPreUpdateTriggers from "./identifyPreUpdateTriggers"
 
 describe("identifyPreUpdateTriggers", () => {
   it("returns an array of pre-update Triggers", () => {
-    const pncUpdateDataset = generatePncUpdateDatasetFromOffenceList([])
-    pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.BailConditions = []
-    pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Hearing = {
-      CourtHearingLocation: {
-        TopLevelCode: "",
-        SecondLevelCode: "",
-        ThirdLevelCode: "",
-        BottomLevelCode: "",
-        OrganisationUnitCode: ""
-      }
-    } as Extract<AnnotatedHearingOutcome, "AnnotatedHearingOutcome.HearingOutcome.Hearing">
-    pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.ForceOwner = {
-      TopLevelCode: "",
-      SecondLevelCode: "second-level-code",
-      ThirdLevelCode: "",
-      BottomLevelCode: "",
-      OrganisationUnitCode: ""
-    }
-    pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence = [
+    const pncUpdateDataset = generatePncUpdateDatasetFromOffenceList([
       {
         Result: [
           {
@@ -49,9 +30,7 @@ describe("identifyPreUpdateTriggers", () => {
         CommittedOnBail: "",
         CourtOffenceSequenceNumber: 0
       }
-    ]
-
-    pncUpdateDataset
+    ])
 
     expect(identifyPreUpdateTriggers(pncUpdateDataset as PncUpdateDataset)).toEqual<Trigger[]>([
       { code: TriggerCode.TRPR0005 }
