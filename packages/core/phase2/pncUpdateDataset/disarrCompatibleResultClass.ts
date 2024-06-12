3ef2b5f7a59701fdec6355dea3998ea01abe55f7
Kate Sugden <kate.sugden@madetech.com>
2024-06-12 16:00:45 +0100

    Add skeleton function disarrCompatibleResultClass

---
+++ packages/core/phase2/pncUpdateDataset/disarrCompatibleResultClass.ts
@@ -0,0 +1,8 @@
+import type { Offence } from "../../types/AnnotatedHearingOutcome"
+
+const disarrCompatibleResultClass = (offence: Offence): boolean => {
+  console.log("To be implemented: TriggerBuilderUtilsImpl.java:998", !!offence)
+  return true
+}
+
+export default disarrCompatibleResultClass
