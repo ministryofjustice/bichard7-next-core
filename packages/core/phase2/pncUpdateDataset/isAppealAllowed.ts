fe6b367b07f767f275356c0389d84361f1f8e678
Kate Sugden <kate.sugden@madetech.com>
2024-06-13 15:59:41 +0100

    Linting

--- packages/core/phase2/pncUpdateDataset/isAppealAllowed.ts
+++ packages/core/phase2/pncUpdateDataset/isAppealAllowed.ts
@@ -2,7 +2,7 @@ import type { Offence } from "../../types/AnnotatedHearingOutcome"
 
 const isAppealAllowed = (_offence: Offence): boolean => {
   // Assume this is always false as we don't currently handle appeals
-  // @TODO clean up once this is confirmed through comparison tests 
+  // @TODO clean up once this is confirmed through comparison tests
   // B7 ref: TriggerBuilder.java:1046-1053
   return false
 }
