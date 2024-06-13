bad0e88556cdda1697e4fbe33ad7ad58d32e64db
Kate Sugden <kate.sugden@madetech.com>
2024-06-13 12:57:01 +0100

    Add stubs for phase2 triggers with generators

---
+++ packages/core/phase1/triggers/TRPS0010.ts
@@ -0,0 +1,11 @@
+import { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
+import { TriggerCode } from "../../types/TriggerCode"
+
+const triggerCode = TriggerCode.TRPS0010
+
+const generator: TriggerGenerator = (_hearingOutcome) => {
+  const shouldRaiseTrigger = false
+  return shouldRaiseTrigger ? [{ code: triggerCode }] : []
+}
+
+export default generator
