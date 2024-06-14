2df0d307d63be104f1c55ff986003d66c0d209c6
Kate Sugden <kate.sugden@madetech.com>
2024-06-14 14:36:01 +0100

    Add TriggerRules to define rules in one place

--- packages/core/phase2/pncUpdateDataset/mostSpecificCourtRuleAllowsTrigger.ts
+++ packages/core/phase2/pncUpdateDataset/mostSpecificCourtRuleAllowsTrigger.ts
@@ -1,40 +1,14 @@
 import type TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { PncUpdateDataset } from "../../types/PncUpdateDataset"
+import { TRIGGER_RULE_LIST } from "./TriggerRules"
 
 const mostSpecificCourtRuleAllowsTrigger = (
   _pncUpdateDataset: PncUpdateDataset,
   _triggerCode: TriggerCode
 ): boolean | undefined => {
-  console.log("To be implemented: TriggerBuilder.java:339")
+  const ruleList = TRIGGER_RULE_LIST
+  console.log("To be implemented: TriggerBuilder.java:339", !!ruleList)
   return true
 }
-
-// From old Bichard, triggers.properties:
-// trigger.rule.23.TRPR9993=exclude
-// trigger.rule.53=exclude
-// trigger.rule.86.TRPS0002=Exclude
-// trigger.rule.53.TRPR9233=include
-// trigger.rule.47=include
-// # Following rules added for testing br700002562
-// trigger.rule.B77=include
-// trigger.rule.B772=exclude
-// trigger.rule.B7724=include
-// trigger.rule.B77249=exclude
-// trigger.rule.B772490=include
-// trigger.rule.B77.TRPR0014=exclude
-// trigger.rule.B772.TRPR0014=include
-// trigger.rule.B7724.TRPR0014=exclude
-// trigger.rule.B77249.TRPR0014=include
-// trigger.rule.B772490.TRPR0014=exclude
-// trigger.rule.B78=exclude
-// trigger.rule.B782=include
-// trigger.rule.B7824=exclude
-// trigger.rule.B78249=include
-// trigger.rule.B782490=exclude
-// trigger.rule.B78.TRPR0014=include
-// trigger.rule.B782.TRPR0014=exclude
-// trigger.rule.B7824.TRPR0014=include
-// trigger.rule.B78249.TRPR0014=exclude
-// trigger.rule.B782490.TRPR0014=include
 
 export default mostSpecificCourtRuleAllowsTrigger
