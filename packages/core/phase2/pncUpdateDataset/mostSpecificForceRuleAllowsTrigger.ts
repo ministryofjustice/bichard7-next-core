eb1d1a6fbb11fb539fdff9178e71ea2d340ffc0a
Kate Sugden <kate.sugden@madetech.com>
2024-06-14 09:35:29 +0100

    Add commented pointer to relevant old Bichard code

--- packages/core/phase2/pncUpdateDataset/mostSpecificForceRuleAllowsTrigger.ts
+++ packages/core/phase2/pncUpdateDataset/mostSpecificForceRuleAllowsTrigger.ts
@@ -9,4 +9,32 @@ const mostSpecificForceRuleAllowsTrigger = (
   return true
 }
 
+// From old Bichard, triggers.properties:
+// trigger.rule.23.TRPR9993=exclude 
+// trigger.rule.53=exclude
+// trigger.rule.86.TRPS0002=Exclude
+// trigger.rule.53.TRPR9233=include
+// trigger.rule.47=include
+// # Following rules added for testing br700002562
+// trigger.rule.B77=include
+// trigger.rule.B772=exclude
+// trigger.rule.B7724=include
+// trigger.rule.B77249=exclude
+// trigger.rule.B772490=include
+// trigger.rule.B77.TRPR0014=exclude
+// trigger.rule.B772.TRPR0014=include
+// trigger.rule.B7724.TRPR0014=exclude
+// trigger.rule.B77249.TRPR0014=include
+// trigger.rule.B772490.TRPR0014=exclude
+// trigger.rule.B78=exclude
+// trigger.rule.B782=include
+// trigger.rule.B7824=exclude
+// trigger.rule.B78249=include
+// trigger.rule.B782490=exclude
+// trigger.rule.B78.TRPR0014=include
+// trigger.rule.B782.TRPR0014=exclude
+// trigger.rule.B7824.TRPR0014=include
+// trigger.rule.B78249.TRPR0014=exclude
+// trigger.rule.B782490.TRPR0014=include
+
 export default mostSpecificForceRuleAllowsTrigger
