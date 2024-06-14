8e64c92e1bff6c7dd99c54bb1e2bed345d43d9be
Kate Sugden <kate.sugden@madetech.com>
2024-06-14 16:08:07 +0100

    Add test for include rule

--- packages/core/phase2/pncUpdateDataset/mostSpecificForceRuleAllowsTrigger.test.ts
+++ packages/core/phase2/pncUpdateDataset/mostSpecificForceRuleAllowsTrigger.test.ts
@@ -4,12 +4,20 @@ import generateFakePncUpdateDataset from "../tests/fixtures/helpers/generateFake
 import mostSpecificForceRuleAllowsTrigger from "./mostSpecificForceRuleAllowsTrigger"
 
 describe("mostSpecificForceRuleAllowsTrigger", () => {
-  it("should return true", () => {
+  it("should return false when force code matches an 'exclude' rule", () => {
     const pncUpdateDataset = generateFakePncUpdateDataset()
     pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.ForceOwner = {
       SecondLevelCode: "53"
     } as OrganisationUnitCodes
     const result = mostSpecificForceRuleAllowsTrigger(pncUpdateDataset, TriggerCode.TRPR0014)
     expect(result).toBe(false)
+  })
+  it("should return true when force code does match an 'include' rule", () => {
+    const pncUpdateDataset = generateFakePncUpdateDataset()
+    pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.ForceOwner = {
+      SecondLevelCode: "47"
+    } as OrganisationUnitCodes
+    const result = mostSpecificForceRuleAllowsTrigger(pncUpdateDataset, TriggerCode.TRPR0014)
+    expect(result).toBe(true)
   })
 })
