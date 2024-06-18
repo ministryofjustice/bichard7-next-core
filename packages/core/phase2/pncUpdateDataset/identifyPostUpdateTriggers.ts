e1e517b1594e36e5868d8dbde8c24698b29fb894
Sen <65850511+Senny10@users.noreply.github.com>
2024-06-18 12:07:39 +0100

    Remove unused TriggerRule.ts and related files
    This commit removes the unused file `TriggerRule.ts` and its related files `mostSpecificCourtRuleAllowsTrigger.ts`, `mostSpecificForceRuleAllowsTrigger.ts`, `mostSpecificCourtRuleAllowsTrigger.test.ts`, and `mostSpecificForceRuleAllowsTrigger.test.ts`. These files are no longer needed and can be safely deleted.

--- packages/core/phase2/pncUpdateDataset/identifyPostUpdateTriggers.ts
+++ packages/core/phase2/pncUpdateDataset/identifyPostUpdateTriggers.ts
@@ -2,15 +2,12 @@ import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import errorPaths from "../../phase1/lib/errorPaths"
 import getOffenceCode from "../../phase1/lib/offence/getOffenceCode"
 import type { Trigger } from "../../phase1/types/Trigger"
-import { CjsVerdict } from "../../phase1/types/Verdict"
 import type { PncUpdateDataset } from "../../types/PncUpdateDataset"
 import isRecordableOffence from "../isRecordableOffence"
 import disarrCompatibleResultClass from "../lib/deriveOperationSequence/disarrCompatibleResultClass"
-import createTriggerIfNecessary from "./createTriggerIfNecessary"
 import getGenericTriggerCaseOrOffenceLevelIndicator from "./getGenericTriggerCaseOrOffenceLevelIndicator"
 import getResultCodeValuesForTriggerCode from "./getResultCodeValuesForTriggerCode"
 import getUpdateTriggersMap from "./getUpdateTriggersMap"
-import isAppealAllowed from "./isAppealAllowed"
 import isResultVariableTextForTriggerMatch from "./isResultVariableTextForTriggerMatch"
 import isResultVariableTextNotForTriggerMatch from "./isResultVariableTextNotForTriggerMatch"
 
@@ -25,9 +22,6 @@ const identifyPostUpdateTriggers = (pncUpdateDataset: PncUpdateDataset): Trigger
   for (let offenceIndex = -1; offenceIndex < offences.length; offenceIndex++) {
     const offence = offences[offenceIndex]
     const offenceCode = offence ? getOffenceCode(offence) : undefined
-
-    const appealAllowed = isAppealAllowed(offence)
-
     let sentDISARR = false
     let addedByCourtTriggerRaised = false
     let addedAtCourtAddToPNCTriggerRaised = false
@@ -47,15 +41,9 @@ const identifyPostUpdateTriggers = (pncUpdateDataset: PncUpdateDataset): Trigger
 
     offenceTriggerCodes?.forEach((offenceTriggerCode) => {
       if (getGenericTriggerCaseOrOffenceLevelIndicator(offenceTriggerCode) === offenceLevelTrigger) {
-        createTriggerIfNecessary(
-          triggers,
-          offenceTriggerCode,
-          offence.CourtOffenceSequenceNumber,
-          pncUpdateDataset,
-          false
-        )
+        triggers.push({ code: offenceTriggerCode, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
       } else {
-        createTriggerIfNecessary(triggers, offenceTriggerCode, undefined, pncUpdateDataset, false)
+        triggers.push({ code: offenceTriggerCode })
       }
     })
 
@@ -66,7 +54,6 @@ const identifyPostUpdateTriggers = (pncUpdateDataset: PncUpdateDataset): Trigger
       : undefined
     results?.forEach((result, resultIndex) => {
       const ticsInResult = !!result.NumberOfOffencesTIC
-      const acquittedOnAppeal = appealAllowed && result.Verdict === CjsVerdict.NotGuilty
       if (restrainingOrderCJSResultCodes.includes(result.CJSresultCode)) {
         if (
           pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Hearing.CourtType === "CC" ||
@@ -74,13 +61,7 @@ const identifyPostUpdateTriggers = (pncUpdateDataset: PncUpdateDataset): Trigger
             isResultVariableTextForTriggerMatch(TriggerCode.TRPS0001, result.ResultVariableText) &&
             !isResultVariableTextNotForTriggerMatch(TriggerCode.TRPS0001, result.ResultVariableText))
         ) {
-          createTriggerIfNecessary(
-            triggers,
-            TriggerCode.TRPS0001,
-            offence.CourtOffenceSequenceNumber,
-            pncUpdateDataset,
-            acquittedOnAppeal
-          )
+          triggers.push({ code: TriggerCode.TRPS0001, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
         }
       }
 
@@ -91,20 +72,14 @@ const identifyPostUpdateTriggers = (pncUpdateDataset: PncUpdateDataset): Trigger
             getGenericTriggerCaseOrOffenceLevelIndicator(triggerCode) === offenceLevelTrigger
               ? offence.CourtOffenceSequenceNumber
               : undefined
-          createTriggerIfNecessary(triggers, triggerCode, offenceSeqNr, pncUpdateDataset, acquittedOnAppeal)
+          triggers.push({ code: triggerCode, offenceSequenceNumber: offenceSeqNr })
         })
       }
 
       const errorPath = errorPaths.offence(offenceIndex).result(resultIndex).resultVariableText
       const disposalTextError = pncUpdateDataset.Exceptions.find((e) => e.code === "HO200200" && e.path === errorPath)
       if (disposalTextError) {
-        createTriggerIfNecessary(
-          triggers,
-          TriggerCode.TRPS0003,
-          offence.CourtOffenceSequenceNumber,
-          pncUpdateDataset,
-          acquittedOnAppeal
-        )
+        triggers.push({ code: TriggerCode.TRPS0003, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
       }
 
       if (offence.AddedByTheCourt || (ticsInResult && isRecordableOffence(offence))) {
@@ -118,35 +93,17 @@ const identifyPostUpdateTriggers = (pncUpdateDataset: PncUpdateDataset): Trigger
         })
         if (sentDISARR && offence.AddedByTheCourt && disarrCompatibleResultClass(offence)) {
           if (!addedByCourtTriggerRaised) {
-            createTriggerIfNecessary(
-              triggers,
-              TriggerCode.TRPS0010,
-              offence.CourtOffenceSequenceNumber,
-              pncUpdateDataset,
-              acquittedOnAppeal
-            )
+            triggers.push({ code: TriggerCode.TRPS0010, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
             addedByCourtTriggerRaised = true
           }
         } else {
           if (!addedAtCourtAddToPNCTriggerRaised && offence.AddedByTheCourt) {
-            createTriggerIfNecessary(
-              triggers,
-              TriggerCode.TRPS0011,
-              offence.CourtOffenceSequenceNumber,
-              pncUpdateDataset,
-              acquittedOnAppeal
-            )
+            triggers.push({ code: TriggerCode.TRPS0011, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
             addedAtCourtAddToPNCTriggerRaised = true
           }
 
           if (!addTICSToOffenceTriggerRaised && ticsInResult && !sentDISARR) {
-            createTriggerIfNecessary(
-              triggers,
-              TriggerCode.TRPS0013,
-              offence.CourtOffenceSequenceNumber,
-              pncUpdateDataset,
-              false
-            )
+            triggers.push({ code: TriggerCode.TRPS0013, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
             addTICSToOffenceTriggerRaised = true
           }
         }
@@ -156,7 +113,7 @@ const identifyPostUpdateTriggers = (pncUpdateDataset: PncUpdateDataset): Trigger
 
   const newremCount = pncUpdateDataset.PncOperations.filter((op) => op.code === "NEWREM").length
   if (newremCount > 1) {
-    createTriggerIfNecessary(triggers, TriggerCode.TRPS0004, undefined, pncUpdateDataset, false)
+    triggers.push({ code: TriggerCode.TRPS0004 })
   }
 
   return triggers
