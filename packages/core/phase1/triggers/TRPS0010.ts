a840f9ade74562c7e1d0e4c2843db88110e7739c
Emad Karamad <emad.karamad@madetech.com>
2024-06-20 17:56:09 +0100

    Add logic for TRPS0010

--- packages/core/phase1/triggers/TRPS0010.ts
+++ packages/core/phase1/triggers/TRPS0010.ts
@@ -1,15 +1,34 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
+import disarrCompatibleResultClass from "../../phase2/lib/deriveOperationSequence/disarrCompatibleResultClass"
+import { Offence } from "../../types/AnnotatedHearingOutcome"
+import { isPncUpdateDataset, PncUpdateDataset } from "../../types/PncUpdateDataset"
 
 const triggerCode = TriggerCode.TRPS0010
 
-const generator: TriggerGenerator = (_hearingOutcome, options) => {
-  if (options?.phase !== 2) {
+const hasCompletedDisarr = (pncUpdateDataset: PncUpdateDataset, offence: Offence) =>
+  pncUpdateDataset.PncOperations.some(
+    ({ code, status, data }) =>
+      code === "DISARR" &&
+      status === "Completed" &&
+      (!data?.courtCaseReference || data.courtCaseReference === offence.CourtCaseReferenceNumber)
+  )
+
+const generator: TriggerGenerator = (hearingOutcome, options) => {
+  if (options?.phase !== 2 || !isPncUpdateDataset(hearingOutcome)) {
     return []
   }
 
-  const shouldRaiseTrigger = false
-  return shouldRaiseTrigger ? [{ code: triggerCode }] : []
+  const offences = hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
+
+  const triggers = offences
+    .filter(
+      (offence) =>
+        offence.AddedByTheCourt && hasCompletedDisarr(hearingOutcome, offence) && disarrCompatibleResultClass(offence)
+    )
+    .map((offence) => ({ code: triggerCode, offenceSequenceNumber: offence.CourtOffenceSequenceNumber }))
+
+  return triggers
 }
 
 export default generator
