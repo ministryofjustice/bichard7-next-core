c49c76e36d8429b7049c29cc7cb6977601c56153
Emad Karamad <emad.karamad@madetech.com>
2024-06-20 16:14:55 +0100

    Add logic for TRPS0008

--- packages/core/phase1/triggers/TRPS0008.ts
+++ packages/core/phase1/triggers/TRPS0008.ts
@@ -1,15 +1,40 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
+import getOffenceCode from "../lib/offence/getOffenceCode"
+import type { Trigger } from "../types/Trigger"
 
 const triggerCode = TriggerCode.TRPS0008
+const offenceOrResultCodeForTrigger = "3105"
 
-const generator: TriggerGenerator = (_hearingOutcome, options) => {
+const generator: TriggerGenerator = (hearingOutcome, options) => {
   if (options?.phase !== 2) {
     return []
   }
 
-  const shouldRaiseTrigger = false
-  return shouldRaiseTrigger ? [{ code: triggerCode }] : []
+  const offences = hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
+  const triggers: Trigger[] = []
+
+  for (let offenceIndex = -1; offenceIndex < offences.length; offenceIndex++) {
+    const offence = offences[offenceIndex]
+    const offenceCode = offence ? getOffenceCode(offence) : undefined
+
+    if (offenceCode === offenceOrResultCodeForTrigger) {
+      triggers.push({ code: triggerCode, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
+    }
+
+    const results = offence
+      ? offence.Result
+      : hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result
+      ? [hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result]
+      : undefined
+    results?.forEach((result) => {
+      if (result.CJSresultCode === Number(offenceOrResultCodeForTrigger)) {
+        triggers.push({ code: triggerCode, offenceSequenceNumber: offence.CourtOffenceSequenceNumber })
+      }
+    })
+  }
+
+  return triggers
 }
 
 export default generator
