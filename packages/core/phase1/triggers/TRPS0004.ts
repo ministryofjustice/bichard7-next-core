8b18c1cb2884fe5b264a667e1f9485145236fb09
Emad Karamad <emad.karamad@madetech.com>
2024-06-20 16:13:40 +0100

    Remove hearing outcome type casting from TRPS triggers

--- packages/core/phase1/triggers/TRPS0004.ts
+++ packages/core/phase1/triggers/TRPS0004.ts
@@ -1,16 +1,15 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
-import type { PncUpdateDataset } from "../../types/PncUpdateDataset"
+import { isPncUpdateDataset } from "../../types/PncUpdateDataset"
 
 const triggerCode = TriggerCode.TRPS0004
 
 const generator: TriggerGenerator = (hearingOutcome, options) => {
-  if (options?.phase !== 2) {
+  if (options?.phase !== 2 || !isPncUpdateDataset(hearingOutcome)) {
     return []
   }
 
-  const pncUpdateDataset = hearingOutcome as PncUpdateDataset
-  const newremCount = pncUpdateDataset.PncOperations.filter((op) => op.code === "NEWREM").length
+  const newremCount = hearingOutcome.PncOperations.filter((op) => op.code === "NEWREM").length
   if (newremCount > 1) {
     return [{ code: triggerCode }]
   }
