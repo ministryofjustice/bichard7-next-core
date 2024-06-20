f99183632d3a22c555ee2e1f7fe8fe578ca9e2ce
Emad Karamad <emad.karamad@madetech.com>
2024-06-20 15:28:18 +0100

    Add logic for TRPS0002

--- packages/core/phase1/triggers/TRPS0002.ts
+++ packages/core/phase1/triggers/TRPS0002.ts
@@ -1,15 +1,40 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
+import { PncUpdateDataset } from "../../types/PncUpdateDataset"
+import getOffenceCode from "../lib/offence/getOffenceCode"
 
 const triggerCode = TriggerCode.TRPS0002
+const offenceOrResultCodeForTrigger = "3107"
 
-const generator: TriggerGenerator = (_hearingOutcome, options) => {
+const generator: TriggerGenerator = (hearingOutcome, options) => {
   if (options?.phase !== 2) {
     return []
   }
 
-  const shouldRaiseTrigger = false
-  return shouldRaiseTrigger ? [{ code: triggerCode }] : []
+  const pncUpdateDataset = hearingOutcome as PncUpdateDataset
+  const offences = pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
+
+  for (let offenceIndex = -1; offenceIndex < offences.length; offenceIndex++) {
+    const offence = offences[offenceIndex]
+    const offenceCode = offence ? getOffenceCode(offence) : undefined
+
+    if (offenceCode === offenceOrResultCodeForTrigger) {
+      return [{ code: triggerCode }]
+    }
+
+    const results = offence
+      ? offence.Result
+      : pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result
+      ? [pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result]
+      : undefined
+    results?.forEach((result) => {
+      if (result.CJSresultCode === Number(offenceOrResultCodeForTrigger)) {
+        return [{ code: triggerCode }]
+      }
+    })
+  }
+
+  return []
 }
 
 export default generator
