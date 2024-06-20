7342606beaf2552e028f5bb99aef637a35528cb1
Emad Karamad <emad.karamad@madetech.com>
2024-06-20 18:34:46 +0100

    Refactor TRPS functions

--- packages/core/phase1/triggers/TRPS0002.ts
+++ packages/core/phase1/triggers/TRPS0002.ts
@@ -2,6 +2,7 @@ import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
 import Phase from "../../types/Phase"
 import getOffenceCode from "../lib/offence/getOffenceCode"
+import getResults from "./getResults"
 
 const triggerCode = TriggerCode.TRPS0002
 const offenceOrResultCodeForTrigger = "3107"
@@ -16,21 +17,14 @@ const generator: TriggerGenerator = (hearingOutcome, options) => {
   for (let offenceIndex = -1; offenceIndex < offences.length; offenceIndex++) {
     const offence = offenceIndex > -1 ? offences[offenceIndex] : undefined
     const offenceCode = offence ? getOffenceCode(offence) : undefined
-
     if (offenceCode === offenceOrResultCodeForTrigger) {
       return [{ code: triggerCode }]
     }
 
-    const results = offence
-      ? offence.Result
-      : hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result
-      ? [hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result]
-      : undefined
-    results?.forEach((result) => {
-      if (result.CJSresultCode === Number(offenceOrResultCodeForTrigger)) {
-        return [{ code: triggerCode }]
-      }
-    })
+    const results = getResults(hearingOutcome, offence)
+    if (results.some((result) => result.CJSresultCode === Number(offenceOrResultCodeForTrigger))) {
+      return [{ code: triggerCode }]
+    }
   }
 
   return []
--- packages/core/phase1/triggers/TRPS0003.ts
+++ packages/core/phase1/triggers/TRPS0003.ts
@@ -1,11 +1,18 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import isEqual from "lodash.isequal"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
+import { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
 import Phase from "../../types/Phase"
 import errorPaths from "../lib/errorPaths"
 import type { Trigger } from "../types/Trigger"
+import getResults from "./getResults"
 
 const triggerCode = TriggerCode.TRPS0003
+
+const hasException200200 = (hearingOutcome: AnnotatedHearingOutcome, offenceIndex: number, resultIndex: number) => {
+  const errorPath = errorPaths.offence(offenceIndex).result(resultIndex).resultVariableText
+  return hearingOutcome.Exceptions.some(({ code, path }) => code === "HO200200" && isEqual(path, errorPath))
+}
 
 const generator: TriggerGenerator = (hearingOutcome, options) => {
   if (options?.phase !== Phase.PNC_UPDATE) {
@@ -17,21 +24,13 @@ const generator: TriggerGenerator = (hearingOutcome, options) => {
 
   for (let offenceIndex = -1; offenceIndex < offences.length; offenceIndex++) {
     const offence = offenceIndex > -1 ? offences[offenceIndex] : undefined
-
-    const results = offence
-      ? offence.Result
-      : hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result
-      ? [hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Result]
-      : undefined
-    results?.forEach((_, resultIndex) => {
-      const errorPath = errorPaths.offence(offenceIndex).result(resultIndex).resultVariableText
-      const disposalTextError = hearingOutcome.Exceptions.find(
-        (e) => e.code === "HO200200" && isEqual(e.path, errorPath)
-      )
-      if (disposalTextError) {
-        triggers.push({ code: triggerCode, offenceSequenceNumber: offence?.CourtOffenceSequenceNumber })
-      }
-    })
+    const results = getResults(hearingOutcome, offence)
+    const shouldGenerateTrigger = results.some((_, resultIndex) =>
+      hasException200200(hearingOutcome, offenceIndex, resultIndex)
+    )
+    if (shouldGenerateTrigger) {
+      triggers.push({ code: triggerCode, offenceSequenceNumber: offence?.CourtOffenceSequenceNumber })
+    }
   }
 
   return triggers
