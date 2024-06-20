7b711ac268ef15660e135cad702518fdff936aa4
Emad Karamad <emad.karamad@madetech.com>
2024-06-20 15:46:50 +0100

    Add logic for TRPS0004

--- packages/core/phase1/triggers/TRPS0004.ts
+++ packages/core/phase1/triggers/TRPS0004.ts
@@ -1,15 +1,21 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
+import type { PncUpdateDataset } from "../../types/PncUpdateDataset"
 
 const triggerCode = TriggerCode.TRPS0004
 
-const generator: TriggerGenerator = (_hearingOutcome, options) => {
+const generator: TriggerGenerator = (hearingOutcome, options) => {
   if (options?.phase !== 2) {
     return []
   }
 
-  const shouldRaiseTrigger = false
-  return shouldRaiseTrigger ? [{ code: triggerCode }] : []
+  const pncUpdateDataset = hearingOutcome as PncUpdateDataset
+  const newremCount = pncUpdateDataset.PncOperations.filter((op) => op.code === "NEWREM").length
+  if (newremCount > 1) {
+    return [{ code: triggerCode }]
+  }
+
+  return []
 }
 
 export default generator
