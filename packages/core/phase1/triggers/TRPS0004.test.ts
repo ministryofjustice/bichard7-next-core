8d275e743009e06645ea701f5bdf63e8f3914a41
Sen <65850511+Senny10@users.noreply.github.com>
2024-06-21 17:09:39 +0100

    Add TRPS0004 trigger test

---
+++ packages/core/phase1/triggers/TRPS0004.test.ts
@@ -0,0 +1,103 @@
+import generateAhoFromOffenceList from "../../phase2/tests/fixtures/helpers/generateAhoFromOffenceList"
+import generatePncUpdateDatasetFromOffenceList from "../../phase2/tests/fixtures/helpers/generatePncUpdateDatasetFromOffenceList"
+import type { Offence } from "../../types/AnnotatedHearingOutcome"
+import Phase from "../../types/Phase"
+import TRPS0004 from "./TRPS0004"
+/*
+  return empty when PncOperations is not in hearingOutcome
+  return empty when PncOperations is empty
+  return empty when all PncOperations are non NEWREM
+  return 1 trigger when PncOperations has one NEWREM
+  return 1 trigger when PncOperations has multiple NEWREMs
+
+*/
+describe("TRPS0004", () => {
+  it("should return an empty array if PncOperations is not in hearingOutcome", () => {
+    const pncUpdateDataset = generateAhoFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        }
+      }
+    ] as Offence[])
+
+    const options = { phase: Phase.HEARING_OUTCOME }
+    const result = TRPS0004(pncUpdateDataset, options)
+    expect(result).toEqual([])
+  })
+  it("should generate an empty array if the phase is not PNC_UPDATE", () => {
+    const pncUpdateDataset = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        }
+      }
+    ] as Offence[])
+    pncUpdateDataset.PncOperations = []
+
+    const options = { phase: Phase.HEARING_OUTCOME }
+    const result = TRPS0004(pncUpdateDataset, options)
+    expect(result).toEqual([])
+  })
+
+  it('should return an empty array if PncOperations does not contain an operation with code "NEWREM"', () => {
+    const pncUpdateDataset = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        }
+      }
+    ] as Offence[])
+
+    const options = { phase: Phase.PNC_UPDATE }
+    const result = TRPS0004(pncUpdateDataset, options)
+    expect(result).toEqual([])
+  })
+
+  it('should return trigger TRPS0004 if PncOperations contains an operation with code "NEWREM"', () => {
+    const pncUpdateDataset = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        }
+      }
+    ] as Offence[])
+    pncUpdateDataset.PncOperations = [
+      {
+        code: "NEWREM",
+        status: "Completed"
+      }
+    ]
+    const options = { phase: Phase.PNC_UPDATE }
+    const result = TRPS0004(pncUpdateDataset, options)
+    expect(result).toEqual([{ code: "TRPS0004" }])
+  })
+})
