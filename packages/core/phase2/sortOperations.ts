4cc44dcae34bce58cf3e6d1059cadd9125d154a8
Ben Pirt <ben@pirt.co.uk>
2024-06-21 16:19:34 +0100

    Fix build errors after updating typescript

--- packages/core/phase2/sortOperations.ts
+++ packages/core/phase2/sortOperations.ts
@@ -1,6 +1,6 @@
 import type { Operation } from "../types/PncUpdateDataset"
 
 const sortOperations = (operations: Operation[]): Operation[] =>
-  operations.filter((o) => o.code !== "NEWREM").concat(operations.filter((o) => o.code === "NEWREM"))
+  operations.filter((o) => String(o.code) !== "NEWREM").concat(operations.filter((o) => String(o.code) === "NEWREM"))
 
 export default sortOperations
