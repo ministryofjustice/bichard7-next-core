054c86d4b68ad49387507685bc2b7857e3396d56
Ben Pirt <ben@pirt.co.uk>
2024-06-21 14:55:59 +0100

    Add the task to the worker

--- packages/conductor/src/worker.ts
+++ packages/conductor/src/worker.ts
@@ -1,17 +1,18 @@
 import { TaskManager } from "@io-orkes/conductor-javascript"
 import createConductorClient from "@moj-bichard7/common/conductor/createConductorClient"
 import logger from "@moj-bichard7/common/utils/logger"
+import compareFiles from "@moj-bichard7/core/comparison/conductor-tasks/compareFiles"
+import generateRerunTasks from "@moj-bichard7/core/comparison/conductor-tasks/generateRerunTasks"
+import rerunPeriod from "@moj-bichard7/core/comparison/conductor-tasks/rerunPeriod"
 import persistPhase1 from "@moj-bichard7/core/conductor-tasks/bichard_phase_1/persistPhase1"
 import processPhase1 from "@moj-bichard7/core/conductor-tasks/bichard_phase_1/processPhase1"
 import sendToPhase2 from "@moj-bichard7/core/conductor-tasks/bichard_phase_1/sendToPhase2"
 import deleteS3File from "@moj-bichard7/core/conductor-tasks/common/deleteS3File"
+import lockS3File from "@moj-bichard7/core/conductor-tasks/common/lockS3File"
 import storeAuditLogEvents from "@moj-bichard7/core/conductor-tasks/common/storeAuditLogEvents"
 import alertCommonPlatform from "@moj-bichard7/core/conductor-tasks/incomingMessageHandler/alertCommonPlatform"
 import convertSpiToAho from "@moj-bichard7/core/conductor-tasks/incomingMessageHandler/convertSpiToAho"
 import createAuditLogRecord from "@moj-bichard7/core/conductor-tasks/incomingMessageHandler/createAuditLogRecord"
-import compareFiles from "@moj-bichard7/core/comparison/conductor-tasks/compareFiles"
-import generateRerunTasks from "@moj-bichard7/core/comparison/conductor-tasks/generateRerunTasks"
-import rerunPeriod from "@moj-bichard7/core/comparison/conductor-tasks/rerunPeriod"
 import { captureWorkerExceptions } from "./captureWorkerExceptions"
 import { configureWorker, defaultConcurrency, defaultPollInterval } from "./configureWorker"
 
@@ -23,6 +24,7 @@ const tasks = [
   createAuditLogRecord,
   deleteS3File,
   generateRerunTasks,
+  lockS3File,
   persistPhase1,
   processPhase1,
   rerunPeriod,
