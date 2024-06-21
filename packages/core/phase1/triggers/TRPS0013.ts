96d2e3c7dc8805b8936cf8f686ec9cc98fc03934
Sen <65850511+Senny10@users.noreply.github.com>
2024-06-21 08:56:04 +0100

    Refactor TRPS0011 and TRPS0013 trigger generators

--- packages/core/phase1/triggers/TRPS0013.ts
+++ packages/core/phase1/triggers/TRPS0013.ts
@@ -1,16 +1,36 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { TriggerGenerator } from "../../phase1/types/TriggerGenerator"
+import disarrCompatibleResultClass from "../../phase2/lib/deriveOperationSequence/disarrCompatibleResultClass"
+import type { Offence } from "../../types/AnnotatedHearingOutcome"
 import Phase from "../../types/Phase"
+import type { PncUpdateDataset } from "../../types/PncUpdateDataset"
+import { isPncUpdateDataset } from "../../types/PncUpdateDataset"
 
 const triggerCode = TriggerCode.TRPS0013
 
-const generator: TriggerGenerator = (_hearingOutcome, options) => {
-  if (options?.phase !== Phase.PNC_UPDATE) {
+const hasCompletedDisarr = (pncUpdateDataset: PncUpdateDataset, offence: Offence) =>
+  pncUpdateDataset.PncOperations.some(
+    ({ code, status, data }) =>
+      code === "DISARR" &&
+      status === "Completed" &&
+      (!data?.courtCaseReference || data.courtCaseReference === offence.CourtCaseReferenceNumber)
+  )
+
+const generator: TriggerGenerator = (hearingOutcome, options) => {
+  if (options?.phase !== Phase.PNC_UPDATE || !isPncUpdateDataset(hearingOutcome)) {
     return []
   }
 
-  const shouldRaiseTrigger = false
-  return shouldRaiseTrigger ? [{ code: triggerCode }] : []
+  const offences = hearingOutcome.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
+
+  const triggers = offences
+    .filter(
+      (offence) =>
+        offence.AddedByTheCourt && hasCompletedDisarr(hearingOutcome, offence) && disarrCompatibleResultClass(offence)
+    )
+    .map((offence) => ({ code: triggerCode, offenceSequenceNumber: offence.CourtOffenceSequenceNumber }))
+
+  return triggers
 }
 
 export default generator
