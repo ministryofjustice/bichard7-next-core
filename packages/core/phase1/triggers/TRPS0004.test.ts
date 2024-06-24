a3eefdee4010ca001359aeafdd2b41bbcee92566
Sen <65850511+Senny10@users.noreply.github.com>
2024-06-24 11:30:52 +0100

    Refactor TRPS0004 trigger test

--- packages/core/phase1/triggers/TRPS0004.test.ts
+++ packages/core/phase1/triggers/TRPS0004.test.ts
@@ -3,14 +3,7 @@ import generatePncUpdateDatasetFromOffenceList from "../../phase2/tests/fixtures
 import type { Offence } from "../../types/AnnotatedHearingOutcome"
 import Phase from "../../types/Phase"
 import TRPS0004 from "./TRPS0004"
-/*
-  return empty when PncOperations is not in hearingOutcome
-  return empty when PncOperations is empty
-  return empty when all PncOperations are non NEWREM
-  return 1 trigger when PncOperations has one NEWREM
-  return 1 trigger when PncOperations has multiple NEWREMs
 
-*/
 describe("TRPS0004", () => {
   it("should return an empty array if PncOperations is not in hearingOutcome", () => {
     const pncUpdateDataset = generateAhoFromOffenceList([
@@ -91,6 +84,36 @@ describe("TRPS0004", () => {
       }
     ] as Offence[])
     pncUpdateDataset.PncOperations = [
+      {
+        code: "NEWREM",
+        status: "Completed"
+      }
+    ]
+    const options = { phase: Phase.PNC_UPDATE }
+    const result = TRPS0004(pncUpdateDataset, options)
+    expect(result).toEqual([{ code: "TRPS0004" }])
+  })
+
+  it('should return trigger TRPS0004 if PncOperations contains multiple operations with code "NEWREM"', () => {
+    const pncUpdateDataset = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        }
+      }
+    ] as Offence[])
+    pncUpdateDataset.PncOperations = [
+      {
+        code: "NEWREM",
+        status: "Completed"
+      },
       {
         code: "NEWREM",
         status: "Completed"
