b243afddac787b6ed92b19a623426fe8a533316b
Emad Karamad <emad.karamad@madetech.com>
2024-06-26 12:42:21 +0100

    Remove document type check in Phase 2

--- packages/core/phase2/lib/deriveOperationSequence/handleSentence.ts
+++ packages/core/phase2/lib/deriveOperationSequence/handleSentence.ts
@@ -1,6 +1,5 @@
 import addExceptionsToAho from "../../../phase1/exceptions/addExceptionsToAho"
 import errorPaths from "../../../phase1/lib/errorPaths"
-import DocumentType from "../../../types/DocumentType"
 import { ExceptionCode } from "../../../types/ExceptionCode"
 import addNewOperationToOperationSetIfNotPresent from "../../addNewOperationToOperationSetIfNotPresent"
 import addSubsequentVariationOperations from "./addSubsequentVariationOperations"
@@ -19,8 +18,6 @@ export const handleSentence: ResultClassHandler = ({
   offenceIndex,
   resultIndex
 }) => {
-  const docType = aho.AnnotatedHearingOutcome.HearingOutcome.Hearing.SourceReference.DocumentType
-
   if (fixedPenalty) {
     addNewOperationToOperationSetIfNotPresent("PENHRG", ccrId ? { courtCaseReference: ccrId } : undefined, operations)
     return
@@ -34,23 +31,18 @@ export const handleSentence: ResultClassHandler = ({
     return
   }
 
-  if (docType === DocumentType.CommittalRecordSheet) {
-    //TODO: Remove this once we've confirmed we don't handle committal record sheets
-    addNewOperationToOperationSetIfNotPresent("COMSEN", ccrId ? { courtCaseReference: ccrId } : undefined, operations)
-  } else if (docType === DocumentType.SpiResult) {
-    if (!areAnyPncResults2007(aho, offence)) {
-      addNewOperationToOperationSetIfNotPresent("SENDEF", ccrId ? { courtCaseReference: ccrId } : undefined, operations)
-    } else {
-      addSubsequentVariationOperations(
-        resubmitted,
-        operations,
-        aho,
-        ExceptionCode.HO200104,
-        allResultsAlreadyOnPnc,
-        offenceIndex,
-        resultIndex,
-        ccrId ? { courtCaseReference: ccrId } : undefined
-      )
-    }
+  if (!areAnyPncResults2007(aho, offence)) {
+    addNewOperationToOperationSetIfNotPresent("SENDEF", ccrId ? { courtCaseReference: ccrId } : undefined, operations)
+  } else {
+    addSubsequentVariationOperations(
+      resubmitted,
+      operations,
+      aho,
+      ExceptionCode.HO200104,
+      allResultsAlreadyOnPnc,
+      offenceIndex,
+      resultIndex,
+      ccrId ? { courtCaseReference: ccrId } : undefined
+    )
   }
 }
