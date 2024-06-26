b6f3221b992ee528ae8069ca7bc82db2850622f3
Sen <65850511+Senny10@users.noreply.github.com>
2024-06-26 16:28:24 +0100

    Remove unused trigger TRPR0003A, TRPR0003B, and TRPR0025A (#822)

--- packages/core/phase2/pncUpdateDataset/getResultCodeValuesForTriggerCode.ts
+++ packages/core/phase2/pncUpdateDataset/getResultCodeValuesForTriggerCode.ts
@@ -6,8 +6,6 @@ const resultCodeValues: Record<string, number[]> = {
     1100, 1177, 1178, 3034, 3041, 3047, 3068, 3078, 3080, 3082, 3106, 3100, 3115, 3121, 3122, 3123, 3124, 3125, 3133,
     3284, 3285, 3288, 4590, 3324
   ],
-  TRPR0003A: [1141, 1142, 1143],
-  TRPR0003B: [3104, 3105, 3107],
   TRPR0004: [3052, 3081, 3085, 3086, 3087, 3088, 3089, 3090, 3091, 1179, 1181, 3281, 3282],
   TRPR0009: [],
   TRPR0010: [4597],
@@ -15,8 +13,7 @@ const resultCodeValues: Record<string, number[]> = {
   TRPR0015: [4592],
   TRPR0020: [1029, 1030, 1031, 1032, 3501],
   TRPR0025: [4584],
-  TRPS0001: [],
-  TRPR0025A: [3049]
+  TRPS0001: []
 }
 
 const getResultCodeValuesForTriggerCode = (triggerCode: TriggerCode): number[] => {
