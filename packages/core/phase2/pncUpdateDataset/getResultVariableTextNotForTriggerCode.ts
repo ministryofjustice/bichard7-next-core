6bfccdf8928c112e2460744e88b0219a13e192f9
Sen <65850511+Senny10@users.noreply.github.com>
2024-06-27 12:16:43 +0100

    Remove unused result variable regex in getResultVariableTextNotForTriggerCode

--- packages/core/phase2/pncUpdateDataset/getResultVariableTextNotForTriggerCode.ts
+++
@@ -1,7 +0,0 @@
-import type TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
-
-const getResultVariableTextNotForTriggerCode = (triggerCode: TriggerCode): string => {
-  return ""
-}
-
-export default getResultVariableTextNotForTriggerCode
