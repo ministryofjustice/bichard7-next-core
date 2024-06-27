a8718fb9b8e0bdae447d1e8174cbe6184f8b5a66
Sen <65850511+Senny10@users.noreply.github.com>
2024-06-27 17:39:38 +0100

    Remove unused result variable regex for TriggerCode.TRPS0001

--- packages/core/phase2/pncUpdateDataset/getResultVariableTextNotForTriggerCode.test.ts
+++ packages/core/phase2/pncUpdateDataset/getResultVariableTextNotForTriggerCode.test.ts
@@ -2,12 +2,6 @@ import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import getResultVariableTextNotForTriggerCode from "./getResultVariableTextNotForTriggerCode"
 
 describe("getResultVariableTextNotForTriggerCode", () => {
-  it("returns a result variable regex", () => {
-    const resultVariableText = getResultVariableTextNotForTriggerCode(TriggerCode.TRPS0001)
-
-    expect(resultVariableText).toBe("until further order,until (0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\\d\\d")
-  })
-
   it("returns empty string if there is no result variable regex", () => {
     const resultVariableText = getResultVariableTextNotForTriggerCode(TriggerCode.TRPR0001)
 
