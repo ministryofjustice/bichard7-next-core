7bda078af96d828d38393a7c37c8c9de1a1347db
Emad Karamad <emad.karamad@madetech.com>
2024-06-27 15:00:33 +0100

    Remove getCourtCaseFromQueryResults

--- packages/core/phase2/getCourtCaseFromQueryResults.ts
+++
@@ -1,8 +0,0 @@
-import type { PncCourtCase, PncQueryResult } from "../types/PncQueryResult"
-
-const getCourtCaseFromQueryResults = (
-  courtCaseRef: string,
-  PncQuery: PncQueryResult | undefined
-): PncCourtCase | undefined => PncQuery?.courtCases?.find((x) => x.courtCaseReference === courtCaseRef)
-
-export default getCourtCaseFromQueryResults
