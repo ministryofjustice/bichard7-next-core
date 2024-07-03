442d69291eea3664889dfa9d489bdad16e6c02f5
Sen <65850511+Senny10@users.noreply.github.com>
2024-07-03 18:03:42 +0100

    Refactor TRPS0004 trigger code and add unit test

--- packages/core/phase1/triggers/TRPS0004.test.ts
+++ packages/core/phase1/triggers/TRPS0004.test.ts
@@ -1,3 +1,4 @@
+import generateAhoFromOffenceList from "../../phase2/tests/fixtures/helpers/generateAhoFromOffenceList"
 import generatePncUpdateDatasetFromOffenceList from "../../phase2/tests/fixtures/helpers/generatePncUpdateDatasetFromOffenceList"
 import type { Offence } from "../../types/AnnotatedHearingOutcome"
 import Phase from "../../types/Phase"
@@ -23,8 +24,15 @@ describe("TRPS0004", () => {
     const result = TRPS0004(generatedHearingOutcome, options)
     expect(result).toEqual([])
   })
-  
+
   it("should return an empty array if is not a PNC update dataset", () => {
+    const options = { phase: Phase.PNC_UPDATE }
+    const generatedHearingOutcome = generateAhoFromOffenceList([] as Offence[])
+    const result = TRPS0004(generatedHearingOutcome, options)
+    expect(result).toEqual([])
+  })
+
+  it("should return an empty array if there are no PncOperations with code NEWREM", () => {
     const options = { phase: Phase.PNC_UPDATE }
     const generatedHearingOutcome = generatePncUpdateDatasetFromOffenceList([
       {
@@ -40,7 +48,34 @@ describe("TRPS0004", () => {
         }
       }
     ] as Offence[])
+    generatedHearingOutcome.PncOperations = []
     const result = TRPS0004(generatedHearingOutcome, options)
     expect(result).toEqual([])
+  })
+
+  it("should return an array with TRPS0004 if NEWREM operation is present", () => {
+    const options = { phase: Phase.PNC_UPDATE }
+    const generatedHearingOutcome = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        }
+      }
+    ] as Offence[])
+    generatedHearingOutcome.PncOperations = [
+      {
+        status: "Completed",
+        code: "NEWREM"
+      }
+    ]
+    const result = TRPS0004(generatedHearingOutcome, options)
+    expect(result).toEqual([{ code: "TRPS0004" }])
   })
 })
