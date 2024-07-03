ad7c0de2ccf024cd10551fa58776adf727ed807d
Sen <65850511+Senny10@users.noreply.github.com>
2024-07-03 18:31:35 +0100

    Refactor unit test TRPS0008 trigger code

--- packages/core/phase1/triggers/TRPS0008.test.ts
+++ packages/core/phase1/triggers/TRPS0008.test.ts
@@ -24,7 +24,7 @@ describe("TRPS0008", () => {
     expect(result).toEqual([])
   })
 
-  it("should return a trigger and offence sequence number if CJSresultCode equals 3105", () => {
+  it("should return trigger and offence sequence number if offence code equals 3105", () => {
     const options = { phase: Phase.PNC_UPDATE }
     const generatedHearingOutcome = generatePncUpdateDatasetFromOffenceList([
       {
@@ -49,5 +49,127 @@ describe("TRPS0008", () => {
     ] as Offence[])
     const result = TRPS0008(generatedHearingOutcome, options)
     expect(result).toEqual([{ code: "TRPS0008", offenceSequenceNumber: 1 }])
+  })
+
+  it("should return trigger and offence sequence number if CJSresultCode equals 3105", () => {
+    const options = { phase: Phase.PNC_UPDATE }
+    const generatedHearingOutcome = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 3105
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason",
+            OffenceCode: {
+              __type: "NonMatchingOffenceCode",
+              ActOrSource: "Act",
+              Reason: "test",
+              FullCode: "3105"
+            }
+          }
+        },
+        CourtOffenceSequenceNumber: 1
+      }
+    ] as Offence[])
+    const result = TRPS0008(generatedHearingOutcome, options)
+    expect(result).toEqual([{ code: "TRPS0008", offenceSequenceNumber: 1 }])
+  })
+
+  it("should return a empty array if neither offence code nor CJSresultCode equals 3105", () => {
+    const options = { phase: Phase.PNC_UPDATE }
+    const generatedHearingOutcome = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason",
+            OffenceCode: {
+              __type: "NonMatchingOffenceCode",
+              ActOrSource: "Act",
+              Reason: "test",
+              FullCode: "3105"
+            }
+          }
+        }
+      }
+    ] as Offence[])
+    const result = TRPS0008(generatedHearingOutcome, options)
+    expect(result).toEqual([])
+  })
+
+  it("should return multiple triggers for multiple matches", () => {
+    const options = { phase: Phase.PNC_UPDATE }
+    const generatedHearingOutcome = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 3105
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason",
+            OffenceCode: {
+              __type: "NonMatchingOffenceCode",
+              ActOrSource: "Act",
+              Reason: "test",
+              FullCode: "3105"
+            }
+          }
+        },
+        CourtOffenceSequenceNumber: 1
+      },
+      {
+        Result: [
+          {
+            CJSresultCode: 3105
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason",
+            OffenceCode: {
+              __type: "NonMatchingOffenceCode",
+              ActOrSource: "Act",
+              Reason: "test",
+              FullCode: "3105"
+            }
+          }
+        },
+        CourtOffenceSequenceNumber: 2
+      },
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason",
+            OffenceCode: {
+              __type: "NonMatchingOffenceCode",
+              ActOrSource: "Act",
+              Reason: "test",
+              FullCode: "3105"
+            }
+          }
+        },
+        CourtOffenceSequenceNumber: 3
+      }
+    ] as Offence[])
+    const result = TRPS0008(generatedHearingOutcome, options)
+    expect(result).toEqual([
+      { code: "TRPS0008", offenceSequenceNumber: 1 },
+      { code: "TRPS0008", offenceSequenceNumber: 2 },
+      { code: "TRPS0008", offenceSequenceNumber: 3 }
+    ])
   })
 })
