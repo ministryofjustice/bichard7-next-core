09efbfd90d21c32f8c749d1575dc1965fe146986
Sen <65850511+Senny10@users.noreply.github.com>
2024-07-08 12:11:30 +0100

    Refactor TRPS0003 test to handle multiple offences with specific exceptions code

--- packages/core/phase1/triggers/TRPS0003.test.ts
+++ packages/core/phase1/triggers/TRPS0003.test.ts
@@ -87,5 +87,107 @@ describe("TRPS0003", () => {
     ])
   })
 
-  // it("should return trigger and offence sequence number if multiple offences have the specific exceptions code HO200200", () => {})
+  it("should return trigger and offence sequence number if multiple offences have the specific exceptions code HO200200", () => {
+    const options = { phase: Phase.PNC_UPDATE }
+    const generatedHearingOutcome = generatePncUpdateDatasetFromOffenceList([
+      {
+        Result: [
+          {
+            CJSresultCode: 9999
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        },
+        CourtOffenceSequenceNumber: 1
+      },
+      {
+        Result: [
+          {
+            CJSresultCode: 9991
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        },
+        CourtOffenceSequenceNumber: 2
+      },
+      {
+        Result: [
+          {
+            CJSresultCode: 9992
+          }
+        ],
+        CriminalProsecutionReference: {
+          OffenceReason: {
+            __type: "NationalOffenceReason"
+          }
+        },
+        CourtOffenceSequenceNumber: 3
+      }
+    ] as Offence[])
+    generatedHearingOutcome.Exceptions = [
+      {
+        code: "HO200200",
+        path: [
+          "AnnotatedHearingOutcome",
+          "HearingOutcome",
+          "Case",
+          "HearingDefendant",
+          "Offence",
+          0,
+          "Result",
+          0,
+          "ResultVariableText"
+        ]
+      },
+      {
+        code: "HO200200",
+        path: [
+          "AnnotatedHearingOutcome",
+          "HearingOutcome",
+          "Case",
+          "HearingDefendant",
+          "Offence",
+          1,
+          "Result",
+          0,
+          "ResultVariableText"
+        ]
+      },
+      {
+        code: "HO200200",
+        path: [
+          "AnnotatedHearingOutcome",
+          "HearingOutcome",
+          "Case",
+          "HearingDefendant",
+          "Offence",
+          2,
+          "Result",
+          0,
+          "ResultVariableText"
+        ]
+      }
+    ] as Exception[]
+    const result = TRPS0003(generatedHearingOutcome, options)
+    expect(result).toEqual([
+      {
+        code: "TRPS0003",
+        offenceSequenceNumber: 1
+      },
+      {
+        code: "TRPS0003",
+        offenceSequenceNumber: 2
+      },
+      {
+        code: "TRPS0003",
+        offenceSequenceNumber: 3
+      }
+    ])
+  })
 })
