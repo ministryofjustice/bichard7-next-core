852f974b8b2051acca59a3591a336db1f4067141
BushraAbdullahi <bushra1175@gmail.com>
2024-07-11 16:04:17 +0100

    removed export for result codes and instead duplicated them for tests

--- packages/core/phase1/triggers/TRPR0002.test.ts
+++ packages/core/phase1/triggers/TRPR0002.test.ts
@@ -1,10 +1,10 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import generateAhoFromOffenceList from "../../phase2/tests/fixtures/helpers/generateAhoFromOffenceList"
 import type { Offence } from "../../types/AnnotatedHearingOutcome"
-import TRPR0002, { resultCodes } from "./TRPR0002"
+import TRPR0002 from "./TRPR0002"
 
 describe("TRPR0002", () => {
-  it.each(resultCodes)(
+  it.each([4575, 4576, 4577, 4585, 4586])(
     "should raise a trigger if result code equals %i and does not have a result qualifier code of EO",
     (resultCode) => {
       const generatedHearingOutcome = generateAhoFromOffenceList([
@@ -28,7 +28,7 @@ describe("TRPR0002", () => {
     }
   )
 
-  it.each(resultCodes)(
+  it.each([4575, 4576, 4577, 4585, 4586])(
     "should not raise a trigger if result code equals %i and has a result qualifier code of EO",
     (resultCode) => {
       const generatedHearingOutcome = generateAhoFromOffenceList([
