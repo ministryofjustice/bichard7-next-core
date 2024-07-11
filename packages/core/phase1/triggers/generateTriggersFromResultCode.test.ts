f71a4132975c47f6fe766bc6f16f4e3369bf3803
Sen <65850511+Senny10@users.noreply.github.com>
2024-07-11 15:41:31 +0100

    WIP

--- packages/core/phase1/triggers/generateTriggersFromResultCode.test.ts
+++ packages/core/phase1/triggers/generateTriggersFromResultCode.test.ts
@@ -1,8 +1,12 @@
-import TriggerCode from "bichard7-next-data-latest/types/TriggerCode"
+import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import generateAhoFromOffenceList from "../../phase2/tests/fixtures/helpers/generateAhoFromOffenceList"
 import type { Offence } from "../../types/AnnotatedHearingOutcome"
+import isCaseRecordable from "../lib/isCaseRecordable"
 import TriggerRecordable from "../types/TriggerRecordable"
 import generateTriggersFromResultCode from "./generateTriggersFromResultCode"
+jest.mock("../lib/isCaseRecordable")
+
+const mockedIsCaseRecordable = isCaseRecordable as jest.Mock
 
 describe("generateTriggersFromResultCode", () => {
   it("should throw an exception if result code is undefined", () => {
@@ -18,5 +22,33 @@ describe("generateTriggersFromResultCode", () => {
     }
 
     expect(fn).toThrow("resultCodesForTrigger is undefined")
+  })
+
+  it("should not return a trigger if the hearing outcome is not recordable and the trigger is recordable", () => {
+    mockedIsCaseRecordable.mockReturnValue(false)
+    const generatedHearingOutcome = generateAhoFromOffenceList([] as Offence[])
+
+    const result = generateTriggersFromResultCode(generatedHearingOutcome, {
+      triggerCode: TriggerCode.TRPR0004,
+      resultCodesForTrigger: [1234],
+      triggerRecordable: TriggerRecordable.Yes,
+      caseLevelTrigger: false
+    })
+
+    expect(result).toEqual([])
+  })
+
+  it("should not return a trigger if the hearing outcome is recordable and the trigger is not recordable", () => {
+    mockedIsCaseRecordable.mockReturnValue(true)
+    const generatedHearingOutcome = generateAhoFromOffenceList([] as Offence[])
+
+    const result = generateTriggersFromResultCode(generatedHearingOutcome, {
+      triggerCode: TriggerCode.TRPR0004,
+      resultCodesForTrigger: [1234],
+      triggerRecordable: TriggerRecordable.No,
+      caseLevelTrigger: false
+    })
+
+    expect(result).toEqual([])
   })
 })
