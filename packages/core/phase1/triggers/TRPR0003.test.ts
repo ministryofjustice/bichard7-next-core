62d3dba93f7235d9ef55e61368f6cf583d26573e
Harriet <harriet.hall@madetech.com>
2024-07-12 11:44:10 +0100

    refactor TRPR0003 test with a mock generator

--- packages/core/phase1/triggers/TRPR0003.test.ts
+++ packages/core/phase1/triggers/TRPR0003.test.ts
@@ -1,154 +1,54 @@
 import generator from "./TRPR0003"
 import generateAhoFromOffenceList from "../../phase2/tests/fixtures/helpers/generateAhoFromOffenceList"
-import type { Offence } from "../../types/AnnotatedHearingOutcome"
+import type { AnnotatedHearingOutcome, Offence } from "../../types/AnnotatedHearingOutcome"
+
+const generateMockAho = (offenceResults: number[][]): AnnotatedHearingOutcome =>
+  generateAhoFromOffenceList(
+    offenceResults.map(
+      (offenceResult, index) =>
+        ({
+          Result: offenceResult.map((result) => ({ CJSresultCode: result })),
+          CriminalProsecutionReference: {
+            OffenceReason: {
+              __type: "NationalOffenceReason"
+            }
+          },
+          CourtOffenceSequenceNumber: index + 1
+        }) as Offence
+    )
+  )
 
 describe("TRPR0003", () => {
   it("should return a trigger if an offence has result codes that exist in the mainResultCodes list", () => {
-    const generatedHearingOutcome = generateAhoFromOffenceList([
-      {
-        Result: [
-          {
-            CJSresultCode: 1100
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 1
-      }
-    ] as Offence[])
-
+    const generatedHearingOutcome = generateMockAho([[1100]])
     expect(generator(generatedHearingOutcome)).toEqual([{ code: "TRPR0003", offenceSequenceNumber: 1 }])
   })
 
   it("should return a trigger if an offence has result codes that exist in the yroResultCodes list and the yroSpeceficRequirementResultCodes list", () => {
-    const generatedHearingOutcome = generateAhoFromOffenceList([
-      {
-        Result: [
-          {
-            CJSresultCode: 1141
-          },
-          {
-            CJSresultCode: 3104
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 2
-      }
-    ] as Offence[])
-    expect(generator(generatedHearingOutcome)).toEqual([{ code: "TRPR0003", offenceSequenceNumber: 2 }])
+    const generatedHearingOutcome = generateMockAho([[1141, 3104]])
+    expect(generator(generatedHearingOutcome)).toEqual([{ code: "TRPR0003", offenceSequenceNumber: 1 }])
   })
 
   it("should not return a trigger if an offence has result codes that do not exist in the mainResultCodes list or the yroResultCodes list, but does exist in the yroSpeceficRequirementResultCodes list", () => {
-    const generatedHearingOutcome = generateAhoFromOffenceList([
-      {
-        Result: [
-          {
-            CJSresultCode: 1234
-          },
-          {
-            CJSresultCode: 3104
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 2
-      }
-    ] as Offence[])
+    const generatedHearingOutcome = generateMockAho([[1234, 3104]])
     expect(generator(generatedHearingOutcome)).toEqual([])
   })
 
   it("should not return a trigger if an offence has result codes that do not exist in the mainResultCodes list or the yroSpeceficRequirementResultCodes list, but does exist in the yroResultCodes list", () => {
-    const generatedHearingOutcome = generateAhoFromOffenceList([
-      {
-        Result: [
-          {
-            CJSresultCode: 1142
-          },
-          {
-            CJSresultCode: 1234
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 2
-      }
-    ] as Offence[])
+    const generatedHearingOutcome = generateMockAho([[1234, 1142]])
     expect(generator(generatedHearingOutcome)).toEqual([])
   })
 
   it("should return a single trigger if offence has multiple result codes that exist in mainResultCodes, yroResultCodes, and yroSpeceficRequirementResultCodes lists", () => {
-    const generatedHearingOutcome = generateAhoFromOffenceList([
-      {
-        Result: [
-          {
-            CJSresultCode: 3104
-          },
-          {
-            CJSresultCode: 1142
-          },
-          {
-            CJSresultCode: 1178
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 2
-      }
-    ] as Offence[])
-    expect(generator(generatedHearingOutcome)).toEqual([{ code: "TRPR0003", offenceSequenceNumber: 2 }])
+    const generatedHearingOutcome = generateMockAho([[3104, 1142, 1178]])
+    expect(generator(generatedHearingOutcome)).toEqual([{ code: "TRPR0003", offenceSequenceNumber: 1 }])
   })
 
   it("should return multiple triggers when multiple offences have result codes that match the criteria", () => {
-    const generatedHearingOutcome = generateAhoFromOffenceList([
-      {
-        Result: [
-          {
-            CJSresultCode: 3041
-          },
-          {
-            CJSresultCode: 1234
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 1
-      },
-      {
-        Result: [
-          {
-            CJSresultCode: 1142
-          },
-          {
-            CJSresultCode: 3104
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 2
-      }
-    ] as Offence[])
+    const generatedHearingOutcome = generateMockAho([
+      [3041, 1234],
+      [1142, 3104]
+    ])
     expect(generator(generatedHearingOutcome)).toEqual([
       { code: "TRPR0003", offenceSequenceNumber: 1 },
       { code: "TRPR0003", offenceSequenceNumber: 2 }
@@ -156,53 +56,7 @@ describe("TRPR0003", () => {
   })
 
   it("should return the correct number of triggers when multiple offences have result codes that both match and do not match the criteria", () => {
-    const generatedHearingOutcome = generateAhoFromOffenceList([
-      {
-        Result: [
-          {
-            CJSresultCode: 3041
-          },
-          {
-            CJSresultCode: 1234
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 1
-      },
-      {
-        Result: [
-          {
-            CJSresultCode: 1142
-          },
-          {
-            CJSresultCode: 3104
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 2
-      },
-      {
-        Result: [
-          {
-            CJSresultCode: 1234
-          }
-        ],
-        CriminalProsecutionReference: {
-          OffenceReason: {
-            __type: "NationalOffenceReason"
-          }
-        },
-        CourtOffenceSequenceNumber: 1
-      }
-    ] as Offence[])
+    const generatedHearingOutcome = generateMockAho([[3041, 1234], [1142, 3104], [1234]])
     expect(generator(generatedHearingOutcome)).toEqual([
       { code: "TRPR0003", offenceSequenceNumber: 1 },
       { code: "TRPR0003", offenceSequenceNumber: 2 }
