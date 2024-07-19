76965fec1961ece4643b171b73fb1712a39f245e
Emad Karamad <emad.karamad@madetech.com>
2024-07-19 11:46:17 +0100

    Fix imports

--- packages/core/lib/database/updateErrorListRecord.integration.test.ts
+++ packages/core/lib/database/updateErrorListRecord.integration.test.ts
@@ -1,9 +1,9 @@
 import createDbConfig from "@moj-bichard7/common/db/createDbConfig"
 import ExceptionCode from "bichard7-next-data-latest/dist/types/ExceptionCode"
 import postgres from "postgres"
-import errorPaths from "../../phase1/lib/errorPaths"
 import generateMockPhase1Result from "../../phase1/tests/helpers/generateMockPhase1Result"
 import type ErrorListRecord from "../../types/ErrorListRecord"
+import errorPaths from "../exceptions/errorPaths"
 import insertErrorListRecord from "./insertErrorListRecord"
 import updateErrorListRecord from "./updateErrorListRecord"
 
