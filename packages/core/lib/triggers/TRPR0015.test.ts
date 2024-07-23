d84622cdd9d44751695fb4f597b094ba7a98a092
BushraAbdullahi <bushra1175@gmail.com>
2024-07-23 15:21:34 +0100

    test all possible combinations

--- packages/core/lib/triggers/TRPR0015.test.ts
+++ packages/core/lib/triggers/TRPR0015.test.ts
@@ -1,7 +1,7 @@
-import type { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import TRPR0015 from "./TRPR0015"
 import Phase from "../../types/Phase"
+import type { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
 
 describe("TRPR0015", () => {
   const triggerOptions = {
@@ -12,60 +12,122 @@ describe("TRPR0015", () => {
 
   const triggerCode = TriggerCode.TRPR0015
 
-  const generateMockAho = (pncId: string, resultCode: number, RecordableOnPNCindicator: string) =>
+  const generateMockAho = (hasPncQuery: boolean, resultCode: number, RecordableOnPNCindicator: string | undefined) =>
     ({
       AnnotatedHearingOutcome: {
         HearingOutcome: {
           Case: {
             HearingDefendant: {
-              Offence: {
-                Result: {
-                  CJSresultCode: resultCode
-                },
-                RecordableOnPNCindicator: RecordableOnPNCindicator
-              }
+              Offence: [
+                {
+                  Result: [
+                    {
+                      CJSresultCode: resultCode
+                    }
+                  ],
+                  RecordableOnPNCindicator: RecordableOnPNCindicator
+                }
+              ]
             }
           }
         }
       },
-      PncQuery: {
-        pncId: pncId
-      }
+      ...(hasPncQuery ? { PncQuery: { PncId: "foo" } } : {})
     }) as unknown as AnnotatedHearingOutcome
 
   it("Should generate trigger TRPR0015 if result code is 4592, the case is recordable (has pnc query and RecordableOnPNCindicator fields), and has triggers", () => {
-    const result = TRPR0015(generateMockAho("2000/0448754K", 4592, "Yes"), triggerOptions)
+    const result = TRPR0015(generateMockAho(true, 4592, "Yes"), triggerOptions)
 
-    expect(result).toEqual({ code: triggerCode })
+    expect(result).toEqual([{ code: triggerCode }])
   })
 
   it("Should generate trigger TRPR0015 if result code is 4592, the case is recordable (has pnc query and RecordableOnPNCindicator fields), and no triggers", () => {
-    const result = TRPR0015(generateMockAho("2000/0448754K", 4592, "Yes"))
+    const result = TRPR0015(generateMockAho(true, 4592, "Yes"))
 
-    expect(result).toEqual({ code: triggerCode })
+    expect(result).toEqual([{ code: triggerCode }])
   })
 
-  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and has triggers", () => {})
+  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and has triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 4592, "Yes"), triggerOptions)
 
-  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and has triggers", () => {})
+    expect(result).toEqual([{ code: triggerCode }])
+  })
 
-  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and has triggers", () => {})
+  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and has triggers", () => {
+    const result = TRPR0015(generateMockAho(true, 4592, undefined), triggerOptions)
 
-  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and no triggers", () => {})
+    expect(result).toEqual([{ code: triggerCode }])
+  })
 
-  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and no triggers", () => {})
+  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and has triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 4592, undefined), triggerOptions)
 
-  it("Should generate trigger TRPR0015 if result code is 4592, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and no triggers", () => {})
+    expect(result).toEqual([{ code: triggerCode }])
+  })
 
-  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and has triggers", () => {})
+  it("Should not generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and no triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 4592, undefined))
 
-  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and has triggers", () => {})
+    expect(result).toHaveLength(0)
+  })
 
-  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and has triggers", () => {})
+  it("Should not generate trigger TRPR0015 if result code is 4592, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and no triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 4592, "Yes"))
 
-  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and no triggers", () => {})
+    expect(result).toEqual([{ code: triggerCode }])
+  })
 
-  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and no triggers", () => {})
+  it("Should not generate trigger TRPR0015 if result code is 4592, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and no triggers", () => {
+    const result = TRPR0015(generateMockAho(true, 4592, undefined))
 
-  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and no triggers", () => {})
+    expect(result).toEqual([{ code: triggerCode }])
+  })
+
+  it("Should generate trigger TRPR0015 if result code is 9999, the case is recordable (has pnc query and RecordableOnPNCindicator fields), and has triggers", () => {
+    const result = TRPR0015(generateMockAho(true, 9999, "Yes"), triggerOptions)
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("Should generate trigger TRPR0015 if result code is 9999, the case is recordable (has pnc query and RecordableOnPNCindicator fields), and no triggers", () => {
+    const result = TRPR0015(generateMockAho(true, 9999, "Yes"))
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and has triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 9999, "Yes"), triggerOptions)
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and has triggers", () => {
+    const result = TRPR0015(generateMockAho(true, 9999, undefined), triggerOptions)
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("Should generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and has triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 9999, undefined), triggerOptions)
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("Should not generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query and no RecordableOnPNCindicator field), and no triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 9999, undefined))
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("Should not generate trigger TRPR0015 if result code is 9999, the case is not recordable (no pnc query but has RecordableOnPNCindicator field), and no triggers", () => {
+    const result = TRPR0015(generateMockAho(false, 9999, "Yes"))
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("Should not generate trigger TRPR0015 if result code is 9999, the case is not recordable (has pnc query but no RecordableOnPNCindicator field), and no triggers", () => {
+    const result = TRPR0015(generateMockAho(true, 9999, undefined))
+
+    expect(result).toHaveLength(0)
+  })
 })
