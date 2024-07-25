7d3e083067c4b74d0ae999d11553013d8a07d8ea
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-07-25 12:38:40 +0100

    Update tests for sendToPhase 2 Conductor task

--- packages/core/conductor-tasks/bichard_phase_1/sendToPhase2.integration.test.ts
+++ packages/core/conductor-tasks/bichard_phase_1/sendToPhase2.integration.test.ts
@@ -1,5 +1,4 @@
 import "../../phase1/tests/helpers/setEnvironmentVariables"
-
 import { dateReviver } from "@moj-bichard7/common/axiosDateTransformer"
 import createS3Config from "@moj-bichard7/common/s3/createS3Config"
 import putFileToS3 from "@moj-bichard7/common/s3/putFileToS3"
@@ -9,15 +8,37 @@ import TestMqGateway from "../../lib/mq/TestMqGateway"
 import createMqConfig from "../../lib/mq/createMqConfig"
 import serialiseToXml from "../../lib/serialise/ahoXml/serialiseToXml"
 import type Phase1Result from "../../phase1/types/Phase1Result"
-import sendToPhase2 from "./sendToPhase2"
+import { waitForCompletedWorkflow } from "@moj-bichard7/common/test/conductor/waitForCompletedWorkflow"
+import { createAuditLogRecord } from "@moj-bichard7/common/test/audit-log-api/createAuditLogRecord"
+
+jest.setTimeout(50_000)
 
 const queueName = process.env.PHASE_2_QUEUE_NAME
 const taskDataBucket = process.env.TASK_DATA_BUCKET_NAME
 
 const testMqGateway = new TestMqGateway(createMqConfig())
+const s3Config = createS3Config()
+const phase1ResultFixture = String(fs.readFileSync("phase1/tests/fixtures/input-message-001-phase1-result.json"))
+
+const getPhase1Result = () => {
+  const s3TaskDataPath = `${randomUUID()}.json`
+  const correlationId = randomUUID()
+  const phase1Result = phase1ResultFixture.replace(/EXTERNAL_CORRELATION_ID/g, correlationId)
+  const parsedPhase1Result = JSON.parse(phase1Result, dateReviver) as Phase1Result
+
+  return { phase1Result, parsedPhase1Result, s3TaskDataPath }
+}
+
+const sendToPhase2 = async (canaryRatio: string | undefined, inputData: Record<string, unknown>) => {
+  process.env.PHASE2_CORE_CANARY_RATIO = canaryRatio
+
+  const sendToPhase2Fn = (await import("./sendToPhase2")).default
+  return sendToPhase2Fn.execute({ inputData })
+}
 
 describe("sendToPhase2", () => {
   beforeEach(async () => {
+    jest.resetModules()
     await testMqGateway.getMessages(queueName!)
   })
 
@@ -25,25 +46,55 @@ describe("sendToPhase2", () => {
     await testMqGateway.dispose()
   })
 
-  it("should send a message to the queue", async () => {
-    const s3TaskDataPath = `${randomUUID()}.json`
-    const s3Config = createS3Config()
-    const phase1Result = String(fs.readFileSync("phase1/tests/fixtures/input-message-001-phase1-result.json"))
-    const parsedResult = JSON.parse(phase1Result, dateReviver) as Phase1Result
-    await putFileToS3(phase1Result, s3TaskDataPath, taskDataBucket!, s3Config)
-    const result = await sendToPhase2.execute({ inputData: { s3TaskDataPath } })
+  describe("when Phase 2 canary ratio is set to use MQ", () => {
+    it.each(["0.0", undefined, "ABC"])(
+      "should send a message to MQ when canary ratio is %s",
+      async (phase2CoreCanaryRatio) => {
+        const { phase1Result, parsedPhase1Result, s3TaskDataPath } = getPhase1Result()
 
-    expect(result.status).toBe("COMPLETED")
-    expect(result.outputData).toHaveProperty("auditLogEvents")
-    expect(result.outputData?.auditLogEvents).toHaveLength(1)
-    expect(result.outputData?.auditLogEvents[0].eventCode).toBe("hearing-outcome.submitted-phase-2")
+        await putFileToS3(phase1Result, s3TaskDataPath, taskDataBucket!, s3Config)
 
-    const message = await testMqGateway.getMessage(queueName!)
-    expect(message).toEqual(serialiseToXml(parsedResult.hearingOutcome))
+        const result = await sendToPhase2(phase2CoreCanaryRatio, { s3TaskDataPath })
+
+        expect(result.status).toBe("COMPLETED")
+        expect(result.outputData).toHaveProperty("auditLogEvents")
+        expect(result.outputData?.auditLogEvents).toHaveLength(1)
+        expect(result.outputData?.auditLogEvents[0].eventCode).toBe("hearing-outcome.submitted-phase-2")
+
+        const message = await testMqGateway.getMessage(queueName!)
+        expect(message).toEqual(serialiseToXml(parsedPhase1Result.hearingOutcome))
+      }
+    )
   })
 
-  it("should fail if the aho S3 path hasn't been provided", async () => {
-    const result = await sendToPhase2.execute({ inputData: {} })
+  describe("when Phase 2 Core canary ratio is set to use Conductor", () => {
+    const phase2CoreCanaryRatio = "1.0"
+
+    it("should start Phase 2 Conductor workflow", async () => {
+      const { phase1Result, parsedPhase1Result, s3TaskDataPath } = getPhase1Result()
+
+      await createAuditLogRecord(parsedPhase1Result.correlationId)
+      await putFileToS3(phase1Result, s3TaskDataPath, taskDataBucket!, s3Config)
+
+      const result = await sendToPhase2(phase2CoreCanaryRatio, { s3TaskDataPath })
+
+      expect(result.status).toBe("COMPLETED")
+      expect(result.outputData).toHaveProperty("auditLogEvents")
+      expect(result.outputData?.auditLogEvents).toHaveLength(1)
+      expect(result.outputData?.auditLogEvents[0].eventCode).toBe("hearing-outcome.submitted-phase-2")
+
+      const workflow = await waitForCompletedWorkflow(
+        parsedPhase1Result.correlationId,
+        "COMPLETED",
+        60_000,
+        "bichard_phase_2"
+      )
+      expect(workflow).toBeDefined()
+    })
+  })
+
+  it("should fail if the AHO S3 path hasn't been provided", async () => {
+    const result = await sendToPhase2(undefined, {})
 
     expect(result.status).toBe("FAILED_WITH_TERMINAL_ERROR")
     expect(result.logs?.map((l) => l.log)).toContain(
@@ -52,7 +103,7 @@ describe("sendToPhase2", () => {
   })
 
   it("should fail if there is a problem retrieving the file", async () => {
-    const result = await sendToPhase2.execute({ inputData: { s3TaskDataPath: "unknown.json" } })
+    const result = await sendToPhase2(undefined, { s3TaskDataPath: "unknown.json" })
 
     expect(result.status).toBe("FAILED")
   })
