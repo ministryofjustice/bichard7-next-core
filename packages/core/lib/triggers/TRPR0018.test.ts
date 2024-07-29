71b042bb9cc53846d92f7fe03dd45700bfed6373
BushraAbdullahi <bushra1175@gmail.com>
2024-07-29 17:17:53 +0100

    cover all test cases

--- packages/core/lib/triggers/TRPR0018.test.ts
+++ packages/core/lib/triggers/TRPR0018.test.ts
@@ -1,13 +1,9 @@
+import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
 import type { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
-import type { PncQueryResult } from "../../types/PncQueryResult"
-import { datesAreDifferent, findMatchingPncOffence } from "./TRPR0018"
+import type { PncOffence, PncQueryResult } from "../../types/PncQueryResult"
+import TRPR0018, { datesAreDifferent, findMatchingPncOffence } from "./TRPR0018"
 
-const generateMockAho = (
-  offenceReasonSequence: string,
-  courtCaseReferenceNumber: string,
-  startDate: Date,
-  endDate: Date
-) =>
+const generateMockAho = (hasOffenceReasonSequence: boolean, startDate: Date, endDate: Date) =>
   ({
     AnnotatedHearingOutcome: {
       HearingOutcome: {
@@ -16,11 +12,12 @@ const generateMockAho = (
             Offence: [
               {
                 CriminalProsecutionReference: {
-                  OffenceReasonSequence: offenceReasonSequence
+                  ...(hasOffenceReasonSequence ? { OffenceReasonSequence: "001" } : {})
                 },
-                CourtCaseReferenceNumber: courtCaseReferenceNumber,
+                CourtCaseReferenceNumber: "97/1626/008395Q",
                 ActualOffenceStartDate: { StartDate: startDate },
-                ActualOffenceEndDate: { EndDate: endDate }
+                ActualOffenceEndDate: { EndDate: endDate },
+                CourtOffenceSequenceNumber: 1
               }
             ]
           }
@@ -32,10 +29,6 @@ const generateMockAho = (
 const generateMockPncQuery = (
   includeCourtCases: boolean,
   includePenaltyCases: boolean,
-  courtCaseReference: string | undefined,
-  penaltyCaseReference: string | undefined,
-  sqn1: number,
-  sqn2: number,
   startDate: Date,
   endDate: Date
 ) =>
@@ -44,10 +37,10 @@ const generateMockPncQuery = (
       ? {
           courtCases: [
             {
-              courtCaseReference: courtCaseReference,
+              courtCaseReference: "97/1626/008395Q",
               offences: [
-                { offence: { sequenceNumber: sqn1, startDate: startDate, endDate: endDate } },
-                { offence: { sequenceNumber: sqn2, startDate: startDate, endDate: endDate } }
+                { offence: { sequenceNumber: 1, startDate: startDate, endDate: endDate } },
+                { offence: { sequenceNumber: 2, startDate: startDate, endDate: endDate } }
               ]
             }
           ]
@@ -57,10 +50,10 @@ const generateMockPncQuery = (
       ? {
           penaltyCases: [
             {
-              penaltyCaseReference: penaltyCaseReference,
+              penaltyCaseReference: "97/1626/008395Q",
               offences: [
-                { offence: { sequenceNumber: sqn1, startDate: startDate, endDate: endDate } },
-                { offence: { sequenceNumber: sqn2, startDate: startDate, endDate: endDate } }
+                { offence: { sequenceNumber: 1, startDate: startDate, endDate: endDate } },
+                { offence: { sequenceNumber: 2, startDate: startDate, endDate: endDate } }
               ]
             }
           ]
@@ -68,16 +61,15 @@ const generateMockPncQuery = (
       : {})
   }) as PncQueryResult
 
-const offenceReasonSequence = "001"
 const startDate = new Date(2010, 10, 28)
 const endDate = new Date(2010, 11, 28)
-const courtCaseReference = "97/1626/008395Q"
+const triggerCode = TriggerCode.TRPR0018
 
 describe("findMatchingPncOffence", () => {
   it("Should find matching PNC offence when pnc query has a court case field but no penalty case", () => {
-    const pncQuery = generateMockPncQuery(true, false, courtCaseReference, undefined, 1, 2, startDate, endDate)
-    const ahoOffence = generateMockAho(offenceReasonSequence, courtCaseReference, startDate, endDate)
-      .AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
+    const pncQuery = generateMockPncQuery(true, false, startDate, endDate)
+    const ahoOffence = generateMockAho(true, startDate, endDate).AnnotatedHearingOutcome.HearingOutcome.Case
+      .HearingDefendant.Offence[0]
 
     const result = findMatchingPncOffence(pncQuery, undefined, ahoOffence)
     expect(result).toEqual({
@@ -86,9 +78,9 @@ describe("findMatchingPncOffence", () => {
   })
 
   it("Should find matching PNC offence when pnc query has a penalty case field but no court case", () => {
-    const pncQuery = generateMockPncQuery(false, true, undefined, courtCaseReference, 1, 2, startDate, endDate)
-    const ahoOffence = generateMockAho(offenceReasonSequence, courtCaseReference, startDate, endDate)
-      .AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
+    const pncQuery = generateMockPncQuery(true, false, startDate, endDate)
+    const ahoOffence = generateMockAho(true, startDate, endDate).AnnotatedHearingOutcome.HearingOutcome.Case
+      .HearingDefendant.Offence[0]
 
     const result = findMatchingPncOffence(pncQuery, undefined, ahoOffence)
     expect(result).toEqual({
@@ -99,13 +91,59 @@ describe("findMatchingPncOffence", () => {
 
 describe("datesAreDifferent", () => {
   it("Should return true as start dates do not match", () => {
-    const pncOffence = generateMockPncQuery(false, true, undefined, courtCaseReference, 1, 2, startDate, endDate)
-      .penaltyCases?.[0].offences[0].offence
-    const ahoOffence = generateMockAho(offenceReasonSequence, courtCaseReference, new Date(2010, 10, 27), endDate)
-      .AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
+    const pncOffence = generateMockPncQuery(false, true, startDate, endDate).penaltyCases?.[0]
+      .offences[0] as unknown as PncOffence
+    const ahoOffence = generateMockAho(true, new Date(2010, 10, 27), endDate).AnnotatedHearingOutcome.HearingOutcome
+      .Case.HearingDefendant.Offence[0]
 
     const result = datesAreDifferent(ahoOffence, pncOffence)
 
     expect(result).toBe(true)
+  })
+
+  it("Should return false as start dates match", () => {
+    const pncOffence = generateMockPncQuery(false, true, startDate, endDate).penaltyCases?.[0]
+      .offences[0] as unknown as PncOffence
+    const ahoOffence = generateMockAho(true, startDate, endDate).AnnotatedHearingOutcome.HearingOutcome.Case
+      .HearingDefendant.Offence[0]
+
+    const result = datesAreDifferent(ahoOffence, pncOffence)
+
+    expect(result).toBe(false)
+  })
+})
+
+describe("TRPR0018", () => {
+  const aho = generateMockAho(true, startDate, endDate)
+  const courtOffenceSequenceNumber =
+    aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0].CourtOffenceSequenceNumber
+
+  it("Should generate a trigger when the aho has an OffenceReasonSequence and the dates are different", () => {
+    const pncQuery = generateMockPncQuery(true, false, new Date(2010, 10, 27), endDate)
+    const result = TRPR0018({ ...aho, PncQuery: { ...pncQuery } })
+
+    expect(result).toEqual([{ code: triggerCode, offenceSequenceNumber: courtOffenceSequenceNumber }])
+  })
+
+  it("Should not generate a trigger when the aho has an OffenceReasonSequence and the dates are the same", () => {
+    const pncQuery = generateMockPncQuery(true, false, startDate, endDate)
+    const result = TRPR0018({ ...aho, PncQuery: { ...pncQuery } })
+
+    expect(result).toEqual([])
+  })
+
+  it("Should not generate a trigger when the aho doesn't have an OffenceReasonSequence and the dates are different", () => {
+    const noOffenceReasonSequenceAho = generateMockAho(false, startDate, endDate)
+    const pncQuery = generateMockPncQuery(true, false, new Date(2010, 10, 27), endDate)
+    const result = TRPR0018({ ...noOffenceReasonSequenceAho, PncQuery: { ...pncQuery } })
+
+    expect(result).toEqual([])
+  })
+
+  it("Should not generate a trigger when there is no PNC offence", () => {
+    const pncQuery = generateMockPncQuery(false, false, startDate, endDate)
+    const result = TRPR0018({ ...aho, PncQuery: { ...pncQuery } })
+
+    expect(result).toEqual([])
   })
 })
