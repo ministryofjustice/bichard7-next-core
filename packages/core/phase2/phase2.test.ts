64efbefbc993233e58e60ddcf88b6d343f172156
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-07-31 12:00:51 +0100

    Add test for when no operations and only a HO200200 exception

--- packages/core/phase2/phase2.test.ts
+++ packages/core/phase2/phase2.test.ts
@@ -9,6 +9,7 @@ import { parsePncUpdateDataSetXml } from "./parse/parsePncUpdateDataSetXml"
 import phase2Handler from "./phase2"
 import { Phase2ResultType } from "./types/Phase2Result"
 import ResultClass from "../types/ResultClass"
+import type { PncOffence, PncQueryResult } from "../types/PncQueryResult"
 
 describe("Bichard Core Phase 2 processing logic", () => {
   const inputAhoMessage = fs.readFileSync("phase2/tests/fixtures/AnnotatedHO1.xml").toString()
@@ -115,6 +116,78 @@ describe("Bichard Core Phase 2 processing logic", () => {
       const result = phase2Handler(inputMessageWithException, auditLogger)
 
       expect(result.resultType).toBe(Phase2ResultType.exceptions)
+    }
+  )
+
+  it.each([
+    { ...ahoTestCase, resultType: Phase2ResultType.ignored, resultDescription: "an ignored" },
+    { ...pncUpdateDataSetTestCase, resultType: Phase2ResultType.success, resultDescription: "a successful" }
+  ])(
+    "returns $resultDescription result when no operations and only a HO200200 exception is generated from getting operations for $type",
+    ({ inputMessage, resultType }) => {
+      const inputMessageWithException = structuredClone(inputMessage)
+      inputMessageWithException.AnnotatedHearingOutcome.HearingOutcome.Hearing.DateOfHearing = new Date("05/22/2024")
+      inputMessageWithException.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence = [
+        {
+          ActualOffenceStartDate: { StartDate: new Date() },
+          CriminalProsecutionReference: {
+            OffenceReason: { __type: "NationalOffenceReason" },
+            OffenceReasonSequence: "001"
+          },
+          Result: [
+            {
+              PNCDisposalType: 2063,
+              DateSpecifiedInResult: [{ Date: new Date("05/22/2024"), Sequence: 1 }],
+              ResultQualifierVariable: [{ Code: "A" }],
+              ResultVariableText: `NOT ENTER ${"A".repeat(100)} THIS EXCLUSION REQUIREMENT LASTS FOR TIME`,
+              CJSresultCode: 3106,
+              AmountSpecifiedInResult: [{ Amount: 25, DecimalPlaces: 2 }],
+              Duration: [{ DurationUnit: "Y", DurationLength: 3, DurationType: "" }]
+            }
+          ]
+        }
+      ] as Offence[]
+
+      inputMessageWithException.PncQuery = {
+        forceStationCode: "06",
+        pncId: "123",
+        courtCases: [
+          {
+            courtCaseReference:
+              inputMessageWithException.AnnotatedHearingOutcome.HearingOutcome.Case.CourtCaseReferenceNumber,
+            offences: [
+              {
+                offence: {
+                  sequenceNumber: 1,
+                  cjsOffenceCode: "offence-code",
+                  startDate: new Date("05/22/2024")
+                },
+                adjudication: {
+                  sentenceDate: new Date("05/22/2024"),
+                  verdict: "NON-CONVICTION",
+                  offenceTICNumber: 0,
+                  plea: ""
+                },
+                disposals: [
+                  {
+                    type: 2063,
+                    qtyDate: "22052024",
+                    qtyDuration: "Y3",
+                    qtyMonetaryValue: "25",
+                    qtyUnitsFined: "Y3  220520240000000.0000",
+                    qualifiers: "A",
+                    text: "EXCLUDED FROM LOCATION"
+                  }
+                ]
+              } as PncOffence
+            ]
+          }
+        ]
+      } as PncQueryResult
+
+      const result = phase2Handler(inputMessageWithException, auditLogger)
+
+      expect(result.resultType).toBe(resultType)
     }
   )
 
