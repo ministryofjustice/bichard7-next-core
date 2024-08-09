28562cc8072d51ad2db5aea9e9417f6a8c470c52
Emad Karamad <emad.karamad@madetech.com>
2024-08-09 11:15:13 +0100

    Update persistePhase2 function

--- packages/core/conductor-tasks/bichard_phase_2/persistPhase2.ts
+++ packages/core/conductor-tasks/bichard_phase_2/persistPhase2.ts
@@ -1,29 +1,30 @@
 import type { ConductorWorker } from "@io-orkes/conductor-javascript"
 import completed from "@moj-bichard7/common/conductor/helpers/completed"
+import failed from "@moj-bichard7/common/conductor/helpers/failed"
 import failedTerminal from "@moj-bichard7/common/conductor/helpers/failedTerminal"
 import s3TaskDataFetcher from "@moj-bichard7/common/conductor/middleware/s3TaskDataFetcher"
-import getAnnotatedDatasetFromDataset from "../../phase2/lib/getAnnotatedDatasetFromDataset"
-import putPncUpdateError from "../../phase2/persist/putPncUpdateError"
-import putTriggerEvent from "../../phase2/persist/putTriggerEvent"
+import createDbConfig from "@moj-bichard7/common/db/createDbConfig"
+import { isError } from "@moj-bichard7/common/types/Result"
+import postgres from "postgres"
+import saveErrorListRecord from "../../lib/database/saveErrorListRecord"
 import { phase2ResultSchema } from "../../phase2/schemas/phase2Result"
 import type Phase2Result from "../../phase2/types/Phase2Result"
 
+const dbConfig = createDbConfig()
+
 const persistPhase2: ConductorWorker = {
   taskDefName: "persist_phase2",
-  execute: s3TaskDataFetcher<Phase2Result>(phase2ResultSchema, (task) => {
+  execute: s3TaskDataFetcher<Phase2Result>(phase2ResultSchema, async (task) => {
     const { s3TaskData } = task.inputData
+    const db = postgres(dbConfig)
 
     if (s3TaskData.triggers.length === 0 && s3TaskData.outputMessage.Exceptions.length === 0) {
       return failedTerminal("No triggers or exceptions present but persist_phase2 was called")
     }
 
-    if (s3TaskData.outputMessage.Exceptions.length > 0) {
-      const annotatedPncUpdateDataset = getAnnotatedDatasetFromDataset(s3TaskData.outputMessage)
-      putPncUpdateError(annotatedPncUpdateDataset)
-    }
-
-    if (s3TaskData.triggers.length > 0) {
-      putTriggerEvent(getAnnotatedDatasetFromDataset(s3TaskData.outputMessage), s3TaskData.triggers)
+    const dbResult = await saveErrorListRecord(db, s3TaskData)
+    if (isError(dbResult)) {
+      return failed("Error saving to the database", dbResult.message)
     }
 
     return completed("Phase 2 result persisted successfully")
