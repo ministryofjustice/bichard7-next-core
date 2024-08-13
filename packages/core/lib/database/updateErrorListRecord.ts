c5eba41373e5260025fba97b79f9bc251090aad5
Emad Karamad <emad.karamad@madetech.com>
2024-08-13 17:40:40 +0100

    Update updateErrorListRecord to correctly set error_status

--- packages/core/lib/database/updateErrorListRecord.ts
+++ packages/core/lib/database/updateErrorListRecord.ts
@@ -1,6 +1,7 @@
 import type { PostgresError, Sql } from "postgres"
 import type ErrorListRecord from "../../types/ErrorListRecord"
 import type PhaseResult from "../../types/PhaseResult"
+import { getAnnotatedHearingOutcome } from "../../types/PhaseResult"
 import ResolutionStatus from "../../types/ResolutionStatus"
 import convertResultToErrorListRecord from "./convertResultToErrorListRecord"
 
@@ -26,8 +27,18 @@ const generateUpdateFields = (result: PhaseResult): Partial<ErrorListRecord> => 
 const updateErrorListRecord = async (db: Sql, recordId: number, result: PhaseResult): Promise<void> => {
   try {
     const updateFields = generateUpdateFields(result)
+    delete updateFields.error_status
+    const exceptionsCount = getAnnotatedHearingOutcome(result).Exceptions.length
+
     const updateResult = await db<ErrorListRecord[]>`
-      UPDATE br7own.error_list SET ${db(updateFields)} WHERE error_id = ${recordId}`
+      UPDATE br7own.error_list SET ${db(updateFields)},
+        error_status = CASE
+          WHEN ${exceptionsCount}::integer > 0 THEN ${ResolutionStatus.UNRESOLVED}::integer
+          WHEN error_status = ${ResolutionStatus.SUBMITTED}::integer THEN ${ResolutionStatus.RESOLVED}::integer
+          ELSE NULL
+        END
+      WHERE error_id = ${recordId}`
+
     if (updateResult.count !== 1) {
       throw new Error("Error updating error_list table - no rows updated")
     }
