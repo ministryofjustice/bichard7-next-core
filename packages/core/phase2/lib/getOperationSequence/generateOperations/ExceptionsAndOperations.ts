558e0ad4d2a3ab2b4a6ab728c792c985d3d7c22f
Emad Karamad <emad.karamad@madetech.com>
2024-08-13 12:13:03 +0100

    Add events to ExceptionsAndOperations type

--- packages/core/phase2/lib/getOperationSequence/generateOperations/ExceptionsAndOperations.ts
+++ packages/core/phase2/lib/getOperationSequence/generateOperations/ExceptionsAndOperations.ts
@@ -1,9 +1,11 @@
+import type EventCode from "@moj-bichard7/common/types/EventCode"
 import type Exception from "../../../../types/Exception"
 import type { Operation } from "../../../../types/PncUpdateDataset"
 
 type ExceptionsAndOperations = {
   exceptions: Exception[]
   operations: Operation[]
+  events?: EventCode[]
 }
 
 export default ExceptionsAndOperations
