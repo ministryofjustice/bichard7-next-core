b29cb39ded837c74b81dfd60484c6113307418ce
Emad Karamad <emad.karamad@madetech.com>
2024-08-22 18:20:05 +0100

    Remove redundant functions

--- packages/core/conductor-tasks/bichard_phase_2/persistPhase2.ts
+++ packages/core/conductor-tasks/bichard_phase_2/persistPhase2.ts
@@ -1,31 +1,12 @@
 import type { ConductorWorker } from "@io-orkes/conductor-javascript"
 import completed from "@moj-bichard7/common/conductor/helpers/completed"
-import failedTerminal from "@moj-bichard7/common/conductor/helpers/failedTerminal"
 import s3TaskDataFetcher from "@moj-bichard7/common/conductor/middleware/s3TaskDataFetcher"
-import getAnnotatedDatasetFromDataset from "../../phase2/lib/getAnnotatedDatasetFromDataset"
-import putPncUpdateError from "../../phase2/persist/putPncUpdateError"
-import putTriggerEvent from "../../phase2/persist/putTriggerEvent"
 import { phase2ResultSchema } from "../../phase2/schemas/phase2Result"
 import type Phase2Result from "../../phase2/types/Phase2Result"
 
 const persistPhase2: ConductorWorker = {
   taskDefName: "persist_phase2",
-  execute: s3TaskDataFetcher<Phase2Result>(phase2ResultSchema, (task) => {
-    const { s3TaskData } = task.inputData
-
-    if (s3TaskData.triggers.length === 0 && s3TaskData.outputMessage.Exceptions.length === 0) {
-      return failedTerminal("No triggers or exceptions present but persist_phase2 was called")
-    }
-
-    if (s3TaskData.outputMessage.Exceptions.length > 0) {
-      const annotatedPncUpdateDataset = getAnnotatedDatasetFromDataset(s3TaskData.outputMessage)
-      putPncUpdateError(annotatedPncUpdateDataset)
-    }
-
-    if (s3TaskData.triggers.length > 0) {
-      putTriggerEvent(getAnnotatedDatasetFromDataset(s3TaskData.outputMessage), s3TaskData.triggers)
-    }
-
+  execute: s3TaskDataFetcher<Phase2Result>(phase2ResultSchema, (_task) => {
     return completed("Phase 2 result persisted successfully")
   })
 }
--- packages/core/phase2/persist/putTriggerEvent.ts
+++
@@ -1,8 +0,0 @@
-import type { AnnotatedPNCUpdateDataset } from "../../types/AnnotatedPNCUpdateDataset"
-import type { Trigger } from "../../types/Trigger"
-
-const putTriggerEvent = (_annotatedPncUpdateDataset: AnnotatedPNCUpdateDataset, _triggersList: Trigger[]): void => {
-  console.log("To be implemented: PNCUpdateChoreographyUtils.java:136")
-}
-
-export default putTriggerEvent
