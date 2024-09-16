274ece05c784734d4e5291c7553f2eff3c3ecf81
Richard Race <1273965+richrace@users.noreply.github.com>
2024-09-16 17:03:40 +0100

    Refactor building the fastify app
    Co-authored-by: Richard Cane <richard.cane@madetech.com>

---
+++ packages/api/src/app.ts
@@ -0,0 +1,48 @@
+import AutoLoad from "@fastify/autoload"
+import fastifySwagger from "@fastify/swagger"
+import fastifySwaggerUI from "@fastify/swagger-ui"
+import { fastify as Fastify, type FastifyInstance } from "fastify"
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import fs from "fs"
+import type { IncomingMessage, Server, ServerResponse } from "http"
+import path from "path"
+
+export default async function () {
+  let fastify: FastifyInstance<Server, IncomingMessage, ServerResponse>
+
+  if (process.env.USE_SSL === "true") {
+    fastify = Fastify({
+      https: { key: fs.readFileSync("/certs/server.key"), cert: fs.readFileSync("/certs/server.crt") },
+      logger: true
+    })
+  } else {
+    fastify = Fastify({ logger: true })
+  }
+
+  fastify.setValidatorCompiler(validatorCompiler)
+  fastify.setSerializerCompiler(serializerCompiler)
+
+  fastify.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: "Bichard API",
+        description: "API documentation about Bichard",
+        version: "0.0.1"
+      },
+      servers: []
+    },
+    transform: jsonSchemaTransform
+  })
+
+  fastify.register(fastifySwaggerUI, {
+    routePrefix: "/swagger"
+  })
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    dirNameRoutePrefix: false,
+    matchFilter: (path) => path.includes("plugin")
+  })
+
+  return fastify
+}
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,52 +1,10 @@
-import AutoLoad from "@fastify/autoload"
-import fastifySwagger from "@fastify/swagger"
-import fastifySwaggerUI from "@fastify/swagger-ui"
-import { fastify as Fastify, type FastifyInstance } from "fastify"
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
-import fs from "fs"
-import type { IncomingMessage, Server, ServerResponse } from "http"
-import path from "path"
+import build from "./app"
 
-const PORT: number = parseInt(process.env.PORT || "3333", 10)
-let fastify: FastifyInstance<Server, IncomingMessage, ServerResponse>
+async function start() {
+  const port: number = parseInt(process.env.PORT || "3333", 10)
+  const app = await build()
 
-if (process.env.USE_SSL === "true") {
-  fastify = Fastify({
-    https: { key: fs.readFileSync("/certs/server.key"), cert: fs.readFileSync("/certs/server.crt") },
-    logger: true
-  })
-} else {
-  fastify = Fastify({ logger: true })
+  app.listen({ port })
 }
 
-fastify.setValidatorCompiler(validatorCompiler)
-fastify.setSerializerCompiler(serializerCompiler)
-
-fastify.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "Bichard API",
-      description: "API documentation about Bichard",
-      version: "0.0.1"
-    },
-    servers: []
-  },
-  transform: jsonSchemaTransform
-})
-
-fastify.register(fastifySwaggerUI, {
-  routePrefix: "/swagger"
-})
-
-fastify.register(AutoLoad, {
-  dir: path.join(__dirname, "plugins"),
-  dirNameRoutePrefix: false,
-  matchFilter: (path) => path.includes("plugin")
-})
-
-fastify.listen({ port: PORT }, (err) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-})
+start()
