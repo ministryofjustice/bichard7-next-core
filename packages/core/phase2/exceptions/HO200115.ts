0cd06917d12ff0ababc8defb174c1145bce59488
Emad Karamad <emad.karamad@madetech.com>
2024-09-18 15:35:18 +0100

    Add phase 2 exception generators

---
+++ packages/core/phase2/exceptions/HO200115.ts
@@ -0,0 +1,8 @@
+import type Exception from "../../types/Exception"
+import type { ExceptionGenerator } from "../../types/ExceptionGenerator"
+
+const generator: ExceptionGenerator = (_aho: AnnotatedHearingOutcome, _options): Exception[] => {
+  return []
+}
+
+export default generator
