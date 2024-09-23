6a52a41030e88bca9c996f1f160758d1086cba83
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-09-23 10:39:29 +0100

    Rename generateOperations test descriptions

--- packages/core/phase2/lib/getOperationSequence/generateOperations/generateOperations.test.ts
+++ packages/core/phase2/lib/getOperationSequence/generateOperations/generateOperations.test.ts
@@ -42,69 +42,66 @@ describe("generateOperations", () => {
   })
 
   it.each([
-    { resultClass: ResultClass.ADJOURNMENT, expectedFn: mockedHandleAdjournment },
-    { resultClass: ResultClass.ADJOURNMENT_POST_JUDGEMENT, expectedFn: mockedHandleAdjournmentPostJudgement },
-    { resultClass: ResultClass.ADJOURNMENT_PRE_JUDGEMENT, expectedFn: mockedHandleAdjournmentPreJudgement },
-    { resultClass: ResultClass.ADJOURNMENT_WITH_JUDGEMENT, expectedFn: mockedHandleAdjournmentWithJudgement },
-    { resultClass: ResultClass.JUDGEMENT_WITH_FINAL_RESULT, expectedFn: mockedHandleJudgementWithFinalResult },
-    { resultClass: ResultClass.SENTENCE, expectedFn: mockedHandleSentence }
-  ])(
-    "should call $expectedFn function when result class is $resultClass and offence has value",
-    ({ resultClass, expectedFn }) => {
-      mockedAreAllResultsAlreadyPresentOnPnc.mockReturnValue({ value: false, exceptions: [] })
-      const resubmitted = false
-      const aho = generateAhoFromOffenceList([
-        {
-          Result: [{ ResultClass: resultClass, PNCDisposalType: 1001 }]
-        }
-      ] as Offence[])
-      expectedFn.mockImplementation(() => {
-        return {
-          operations: [
-            {
-              code: PncOperation.REMAND,
-              data: { courtCaseReference: "1", isAdjournmentPreJudgement: true },
-              status: "NotAttempted"
-            }
-          ],
-          exceptions: []
-        }
-      })
+    { resultClass: ResultClass.ADJOURNMENT, resultClassHandler: mockedHandleAdjournment },
+    { resultClass: ResultClass.ADJOURNMENT_POST_JUDGEMENT, resultClassHandler: mockedHandleAdjournmentPostJudgement },
+    { resultClass: ResultClass.ADJOURNMENT_PRE_JUDGEMENT, resultClassHandler: mockedHandleAdjournmentPreJudgement },
+    { resultClass: ResultClass.ADJOURNMENT_WITH_JUDGEMENT, resultClassHandler: mockedHandleAdjournmentWithJudgement },
+    { resultClass: ResultClass.JUDGEMENT_WITH_FINAL_RESULT, resultClassHandler: mockedHandleJudgementWithFinalResult },
+    { resultClass: ResultClass.SENTENCE, resultClassHandler: mockedHandleSentence }
+  ])("calls $resultClassHandler when offence result class is $resultClass", ({ resultClass, resultClassHandler }) => {
+    mockedAreAllResultsAlreadyPresentOnPnc.mockReturnValue({ value: false, exceptions: [] })
+    const resubmitted = false
+    const aho = generateAhoFromOffenceList([
+      {
+        Result: [{ ResultClass: resultClass, PNCDisposalType: 1001 }]
+      }
+    ] as Offence[])
+    resultClassHandler.mockImplementation(() => {
+      return {
+        operations: [
+          {
+            code: PncOperation.REMAND,
+            data: { courtCaseReference: "1", isAdjournmentPreJudgement: true },
+            status: "NotAttempted"
+          }
+        ],
+        exceptions: []
+      }
+    })
 
-      const { operations } = generateOperations(aho, resubmitted)
+    const { operations } = generateOperations(aho, resubmitted)
 
-      expect(operations).toStrictEqual([
-        {
-          code: PncOperation.REMAND,
-          data: { courtCaseReference: "1", isAdjournmentPreJudgement: true },
-          status: "NotAttempted"
-        }
-      ])
-      expect(expectedFn).toHaveBeenCalledTimes(1)
-      expect(expectedFn.mock.calls[0][0]).toStrictEqual({
-        aho: {
-          AnnotatedHearingOutcome: {
-            HearingOutcome: {
-              Case: {
-                HearingDefendant: {
-                  Offence: [{ Result: [{ PNCDisposalType: 1001, ResultClass: resultClass }] }]
-                }
+    expect(operations).toStrictEqual([
+      {
+        code: PncOperation.REMAND,
+        data: { courtCaseReference: "1", isAdjournmentPreJudgement: true },
+        status: "NotAttempted"
+      }
+    ])
+    expect(resultClassHandler).toHaveBeenCalledTimes(1)
+    expect(resultClassHandler.mock.calls[0][0]).toStrictEqual({
+      aho: {
+        AnnotatedHearingOutcome: {
+          HearingOutcome: {
+            Case: {
+              HearingDefendant: {
+                Offence: [{ Result: [{ PNCDisposalType: 1001, ResultClass: resultClass }] }]
               }
             }
-          },
-          Exceptions: []
+          }
         },
-        allResultsAlreadyOnPnc: false,
-        offence: { Result: [{ PNCDisposalType: 1001, ResultClass: resultClass }] },
-        offenceIndex: 0,
-        resubmitted: false,
-        result: { PNCDisposalType: 1001, ResultClass: resultClass },
-        resultIndex: 0
-      })
-    }
-  )
+        Exceptions: []
+      },
+      allResultsAlreadyOnPnc: false,
+      offence: { Result: [{ PNCDisposalType: 1001, ResultClass: resultClass }] },
+      offenceIndex: 0,
+      resubmitted: false,
+      result: { PNCDisposalType: 1001, ResultClass: resultClass },
+      resultIndex: 0
+    })
+  })
 
-  it("should ignore Unresulted result class", () => {
+  it("generates no operations for Unresulted result class", () => {
     mockedAreAllResultsAlreadyPresentOnPnc.mockReturnValue({ value: false, exceptions: [] })
     const resubmitted = false
     const aho = {
@@ -129,7 +126,7 @@ describe("generateOperations", () => {
     expect(operations).toStrictEqual([])
   })
 
-  it("should generate exception HO200118 when there are no operations, no recordable results, and there are no exceptions", () => {
+  it("generates HO200118 exception when there are no operations, no recordable results, and no operation exceptions", () => {
     mockedAreAllResultsAlreadyPresentOnPnc.mockReturnValue({ value: false, exceptions: [] })
     const resubmitted = false
     const aho = {
@@ -159,7 +156,7 @@ describe("generateOperations", () => {
     ])
   })
 
-  it("should return OAAC DISARR operation when OAAC DISARR ccrId matches remand ccrId", () => {
+  it("generates disposal operation when offence added by the Court disposal ccrId matches remand ccrId", () => {
     mockedAreAllResultsAlreadyPresentOnPnc.mockReturnValue({ value: false, exceptions: [] })
     const resubmitted = false
     const aho = {
@@ -219,7 +216,7 @@ describe("generateOperations", () => {
     ])
   })
 
-  it("should generate exception HO200121 when there are no recordable offences", () => {
+  it("generates HO200121 exception when there are no recordable offences", () => {
     mockedAreAllResultsAlreadyPresentOnPnc.mockReturnValue({ value: false, exceptions: [] })
     const resubmitted = false
     const aho = {
@@ -251,7 +248,7 @@ describe("generateOperations", () => {
     ])
   })
 
-  it("should generate exception HO200121 when there are no offences", () => {
+  it("generates HO200121 exception when there are no offences", () => {
     mockedAreAllResultsAlreadyPresentOnPnc.mockReturnValue({ value: false, exceptions: [] })
     const resubmitted = false
     const aho = {
