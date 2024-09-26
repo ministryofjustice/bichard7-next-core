fa50bdbedce6b69d745b527995d977f4c6381c61
Richard Race <1273965+richrace@users.noreply.github.com>
2024-09-26 16:38:09 +0100

    Create a JWT for testing

---
+++ packages/api/src/server/auth/jwtHelper.ts
@@ -0,0 +1,29 @@
+import type { User } from "@moj-bichard7/common/types/User"
+import { randomUUID } from "crypto"
+import jwt from "jsonwebtoken"
+
+// Same config as UserService
+const jwtConfig = {
+  tokenExpiresIn: "10 minutes",
+  tokenIssuer: "Bichard",
+  tokenSecret: "OliverTwist"
+}
+
+// Test helper function
+export function generateTestJwtToken(user: User, jwd_id?: string) {
+  const options: jwt.SignOptions = {
+    expiresIn: jwtConfig.tokenExpiresIn,
+    issuer: jwtConfig.tokenIssuer
+  }
+
+  const payload = {
+    username: user.username,
+    exclusionList: [],
+    inclusionList: [],
+    emailAddress: [],
+    groups: [],
+    id: jwd_id ?? randomUUID()
+  }
+
+  return jwt.sign(payload, jwtConfig.tokenSecret!, options)
+}
