a4f88bb8009bb82e47d91e5c574160c8e4b4dba8
Harriet Hall <harriet.hall@madetech.com>
2024-09-27 09:03:33 +0100

    remove scripts from packages ui as they are not at top level

--- packages/ui/.circleci/scripts/build-ui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "Building UI ..."
-
-# Build UI
-make build
--- packages/ui/.circleci/scripts/run-component-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ $MS_EDGE == "true" ]]; then
-  npm run cypress:run:component:docker:ms-edge
-else
-  npm run cypress:run:component:docker
-fi
