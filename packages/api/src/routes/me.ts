eb02c0fe00399c7ea69b5b7f61c3d80cbd1bf78c
Richard Cane <richard.cane@madetech.com>
2024-09-27 15:52:57 +0100

    Omit JWT_ID from auth user payload

--- packages/api/src/routes/me.ts
+++ packages/api/src/routes/me.ts
@@ -10,7 +10,7 @@ const schema = {
   ...auth,
   tags: ["Root"],
   response: {
-    [OK]: UserSchema.omit({ id: true }).openapi({
+    [OK]: UserSchema.omit({ id: true, jwt_id: true }).openapi({
       description: "Returns details of authorised user"
     }),
     ...unauthorizedError
