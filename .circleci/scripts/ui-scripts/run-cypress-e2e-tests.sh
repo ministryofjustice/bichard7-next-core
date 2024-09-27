0ab8b43fbf7b3ccb10de868dbc0c08fef39df06a
Harriet Hall <harriet.hall@madetech.com>
2024-09-27 15:16:03 +0100

    make spec arguments a string

--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -2,7 +2,7 @@
 
 TEST_FILES="$(circleci tests glob "cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
 
-options="--config baseUrl=https://localhost:4443 --spec ${TEST_FILES//$'\n'/','} --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
+options="--config baseUrl=https://localhost:4443 --spec "${TEST_FILES//$'\n'/','}" --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 if [[ $MS_EDGE == "true" ]]; then
   npx cypress run --browser edge ${options}
