635a1816ab695169f687414b4f9714a636b05067
Harriet Hall <harriet.hall@madetech.com>
2024-09-27 15:46:28 +0100

    echo TEST_FILES to see how they are formatted

--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 TEST_FILES="$(circleci tests glob "cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
-
-options="--config baseUrl=https://localhost:4443 --spec "${TEST_FILES//$'\n'/','}" --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
+echo "$TEST_FILES"
+options="--config baseUrl=https://localhost:4443 --spec ${TEST_FILES//$'\n'/','} --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 if [[ $MS_EDGE == "true" ]]; then
   npx cypress run --browser edge ${options}
