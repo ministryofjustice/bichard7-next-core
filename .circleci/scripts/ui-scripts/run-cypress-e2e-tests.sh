37d0e583739d90d2c2d298e48c337b46d507f555
joe-fol <joefolkard@protonmail.com>
2024-09-30 12:34:34 +0100

    Testing ssh

--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -3,7 +3,7 @@
 TEST_FILES="$(circleci tests glob "~/project/packages/ui/cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
 
 options="--config baseUrl=https://localhost:4443 --spec ${TEST_FILES//$'\n'/','} --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
-
+echo "test"
 if [[ $MS_EDGE == "true" ]]; then
   npx cypress run --browser edge ${options}
 else
