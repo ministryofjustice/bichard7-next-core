479288855273b008fbb04205d40665f9d80214fe
Harriet Hall <harriet.hall@madetech.com>
2024-09-30 12:01:43 +0100

    add ~/ to path

--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TEST_FILES="$(circleci tests glob "project/packages/ui/cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
+TEST_FILES="$(circleci tests glob "~/project/packages/ui/cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
 
 options="--config baseUrl=https://localhost:4443 --spec ${TEST_FILES//$'\n'/','} --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
