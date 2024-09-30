0e8d0eb41758d99f37514fc26ab668aa7ba47937
Harriet Hall <harriet.hall@madetech.com>
2024-09-30 10:50:20 +0100

    update path in run-cypress-e2e-tests

--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-TEST_FILES="$(circleci tests glob "cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
-echo "$TEST_FILES"
+TEST_FILES="$(circleci tests glob "project/packages/ui/cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
+
 options="--config baseUrl=https://localhost:4443 --spec ${TEST_FILES//$'\n'/','} --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 if [[ $MS_EDGE == "true" ]]; then
   npx cypress run --browser edge ${options}
 else
   npx cypress run ${options}
-fi
+ficir
