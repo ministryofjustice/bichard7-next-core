f171f42617132866af1e35f3b36d60d264a00cff
Harriet Hall <harriet.hall@madetech.com>
2024-10-01 08:49:17 +0100

    remove ~ from path to cypress e2e tests, add full route instead

--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ~/project/packages/ui
 
-TEST_FILES="$(circleci tests glob "~/project/packages/ui/cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
+TEST_FILES="$(circleci tests glob "/home/circleci/project/packages/ui/cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)"
 
 options="--config baseUrl=https://localhost:4443 --spec ${TEST_FILES//$'\n'/','} --reporter cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 echo "test"
