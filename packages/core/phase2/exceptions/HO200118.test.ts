35e1f20dd0312b842c07803c4de09abd933c137d
Emad Karamad <emad.karamad@madetech.com>
2024-10-02 13:42:26 +0100

    Add HO200118

---
+++ packages/core/phase2/exceptions/HO200118.test.ts
@@ -0,0 +1,123 @@
+import type { Offence } from "../../types/AnnotatedHearingOutcome"
+import generateAhoFromOffenceList from "../tests/fixtures/helpers/generateAhoFromOffenceList"
+import HO200118 from "./HO200118"
+
+describe("HO200118", () => {
+  it("should return exception when there is a recordable offence with non-recordable results", () => {
+    const aho = generateAhoFromOffenceList([
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: undefined
+        },
+        Result: [
+          {
+            PNCDisposalType: 2068
+          },
+          {
+            PNCDisposalType: 1000
+          }
+        ]
+      },
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: "1"
+        },
+        Result: [
+          {
+            PNCDisposalType: 1000
+          },
+          {
+            PNCDisposalType: 2068
+          }
+        ]
+      },
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: "2"
+        },
+        Result: [
+          {
+            PNCDisposalType: 1000
+          },
+          {
+            PNCDisposalType: 1000
+          }
+        ]
+      }
+    ] as Offence[])
+
+    const exceptions = HO200118(aho)
+
+    expect(exceptions).toEqual([
+      {
+        code: "HO200118",
+        path: ["AnnotatedHearingOutcome", "HearingOutcome", "Case", "HearingDefendant", "ArrestSummonsNumber"]
+      }
+    ])
+  })
+
+  it("should not return exception when a recordable offence has recordable results", () => {
+    const aho = generateAhoFromOffenceList([
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: "1"
+        },
+        Result: [
+          {
+            PNCDisposalType: 2068
+          },
+          {
+            PNCDisposalType: 1000
+          }
+        ]
+      },
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: undefined
+        },
+        Result: [
+          {
+            PNCDisposalType: 2068
+          },
+          {
+            PNCDisposalType: 1000
+          }
+        ]
+      }
+    ] as Offence[])
+
+    const exceptions = HO200118(aho)
+
+    expect(exceptions).toHaveLength(0)
+  })
+
+  it("should not return exception when there are no recordable offences", () => {
+    const aho = generateAhoFromOffenceList([
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: undefined
+        },
+        Result: [
+          {
+            PNCDisposalType: 2068
+          },
+          {
+            PNCDisposalType: 1000
+          }
+        ]
+      }
+    ] as Offence[])
+
+    const exceptions = HO200118(aho)
+
+    expect(exceptions).toHaveLength(0)
+  })
+
+  it("should not return exception when there are no offences", () => {
+    const aho = generateAhoFromOffenceList([])
+
+    const exceptions = HO200118(aho)
+
+    expect(exceptions).toHaveLength(0)
+  })
+})
