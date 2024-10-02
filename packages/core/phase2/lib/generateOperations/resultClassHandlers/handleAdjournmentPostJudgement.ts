7c215acda219f019ef08e4d00bbbac0f7dbb2bbf
BushraAbdullahi <103956365+BushraAbdullahi@users.noreply.github.com>
2024-10-02 10:55:24 +0100

    Add HO200103 (#985)
    * move exception logic
    
    * write test
    
    * edit test
    
    * WIP
    
    * WIP

--- packages/core/phase2/lib/generateOperations/resultClassHandlers/handleAdjournmentPostJudgement.ts
+++ packages/core/phase2/lib/generateOperations/resultClassHandlers/handleAdjournmentPostJudgement.ts
@@ -1,17 +1,9 @@
-import ExceptionCode from "bichard7-next-data-latest/dist/types/ExceptionCode"
-import errorPaths from "../../../../lib/exceptions/errorPaths"
 import createRemandOperation from "../createRemandOperation"
 import type { ResultClassHandler } from "./ResultClassHandler"
 
-export const handleAdjournmentPostJudgement: ResultClassHandler = ({ offenceIndex, offence, resultIndex, result }) => {
+export const handleAdjournmentPostJudgement: ResultClassHandler = ({ offence, result }) => {
   if (result.PNCAdjudicationExists) {
     return createRemandOperation(result, offence?.CourtCaseReferenceNumber)
-  } else if (!offence.AddedByTheCourt) {
-    const exception = {
-      code: ExceptionCode.HO200103,
-      path: errorPaths.offence(offenceIndex).result(resultIndex).resultClass
-    }
-    return { operations: [], exceptions: [exception] }
   }
 
   return { operations: [], exceptions: [] }
