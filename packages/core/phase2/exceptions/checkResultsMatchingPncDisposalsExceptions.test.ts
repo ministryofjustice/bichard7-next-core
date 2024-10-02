b76da5beaee37b835054e9f7cd4b778c1750ef4e
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-10-02 14:53:52 +0100

    Rename exceptionCheckFn to checkExceptionFn

--- packages/core/phase2/exceptions/checkResultsMatchingPncDisposalsExceptions.test.ts
+++ packages/core/phase2/exceptions/checkResultsMatchingPncDisposalsExceptions.test.ts
@@ -5,13 +5,13 @@ import checkResultsMatchingPncDisposalsExceptions from "./checkResultsMatchingPn
 type TestInput = GenerateAhoMatchingPncAdjudicationAndDisposalsOptions & { when: string }
 
 describe("checkResultsMatchingPncDisposalsExceptions", () => {
-  it("should call the exception check function when the PNC adjudication matches", () => {
-    const exceptionCheckFn = jest.fn()
+  it("should call the check exception function when the PNC adjudication matches", () => {
+    const checkExceptionFn = jest.fn()
     const aho = generateAhoMatchingPncAdjudicationAndDisposals({ hasPncId: true })
 
-    checkResultsMatchingPncDisposalsExceptions(aho, exceptionCheckFn)
+    checkResultsMatchingPncDisposalsExceptions(aho, checkExceptionFn)
 
-    expect(exceptionCheckFn).toHaveBeenCalledTimes(2)
+    expect(checkExceptionFn).toHaveBeenCalledTimes(2)
   })
 
   it.each([
@@ -21,48 +21,48 @@ describe("checkResultsMatchingPncDisposalsExceptions", () => {
     { when: "offence sequence number is not set", hasOffenceReasonSequence: false },
     { when: "PNC case has no offences", hasPncOffences: false },
     { when: "case has no matching PNC adjudication", hasMatchingPncAdjudication: false }
-  ] satisfies TestInput[])("should not call the exception check function when $when", (options) => {
-    const exceptionCheckFn = jest.fn()
+  ] satisfies TestInput[])("should not call the check exception function when $when", (options) => {
+    const checkExceptionFn = jest.fn()
     const aho = generateAhoMatchingPncAdjudicationAndDisposals(options)
 
-    checkResultsMatchingPncDisposalsExceptions(aho, exceptionCheckFn)
+    checkResultsMatchingPncDisposalsExceptions(aho, checkExceptionFn)
 
-    expect(exceptionCheckFn).toHaveBeenCalledTimes(0)
+    expect(checkExceptionFn).toHaveBeenCalledTimes(0)
   })
 
-  it("should call the exception check function once when the first result is recordable and does not match a PNC disposal", () => {
-    const exceptionCheckFn = jest.fn()
+  it("should call the check exception function once when the first result is recordable and does not match a PNC disposal", () => {
+    const checkExceptionFn = jest.fn()
     const aho = generateAhoMatchingPncAdjudicationAndDisposals({
       firstResultDisposalType: 2063,
       firstPncDisposalType: 2060
     })
 
-    checkResultsMatchingPncDisposalsExceptions(aho, exceptionCheckFn)
+    checkResultsMatchingPncDisposalsExceptions(aho, checkExceptionFn)
 
-    expect(exceptionCheckFn).toHaveBeenCalledTimes(1)
+    expect(checkExceptionFn).toHaveBeenCalledTimes(1)
   })
 
-  it("should call the exception check function twice when the first result is recordable but matches a PNC disposal", () => {
-    const exceptionCheckFn = jest.fn()
+  it("should call the check exception function twice when the first result is recordable but matches a PNC disposal", () => {
+    const checkExceptionFn = jest.fn()
     const aho = generateAhoMatchingPncAdjudicationAndDisposals({
       firstResultDisposalType: 2063,
       firstPncDisposalType: 2063
     })
 
-    checkResultsMatchingPncDisposalsExceptions(aho, exceptionCheckFn)
+    checkResultsMatchingPncDisposalsExceptions(aho, checkExceptionFn)
 
-    expect(exceptionCheckFn).toHaveBeenCalledTimes(2)
+    expect(checkExceptionFn).toHaveBeenCalledTimes(2)
   })
 
-  it("should call the exception check function twice when the first result does not match a PNC disposal but is non-recordable", () => {
-    const exceptionCheckFn = jest.fn()
+  it("should call the check exception function twice when the first result does not match a PNC disposal but is non-recordable", () => {
+    const checkExceptionFn = jest.fn()
     const aho = generateAhoMatchingPncAdjudicationAndDisposals({
       firstResultDisposalType: 1000,
       firstPncDisposalType: 2063
     })
 
-    checkResultsMatchingPncDisposalsExceptions(aho, exceptionCheckFn)
+    checkResultsMatchingPncDisposalsExceptions(aho, checkExceptionFn)
 
-    expect(exceptionCheckFn).toHaveBeenCalledTimes(2)
+    expect(checkExceptionFn).toHaveBeenCalledTimes(2)
   })
 })
