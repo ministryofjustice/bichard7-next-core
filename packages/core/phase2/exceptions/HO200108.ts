ef36dd35dd75a57507955dbbd0f468e69724afb5
BushraAbdullahi <bushra1175@gmail.com>
2024-10-04 01:09:31 +0100

    move exception logic

--- packages/core/phase2/exceptions/HO200108.ts
+++ packages/core/phase2/exceptions/HO200108.ts
@@ -7,25 +7,37 @@ import ResultClass from "../../types/ResultClass"
 import checkResultClassExceptions from "./checkResultClassExceptions"
 import areAllPncResults2007 from "../lib/areAllPncResults2007"
 import { areAllResultsOnPnc } from "../lib/generateOperations/areAllResultsOnPnc"
+import checkCaseRequiresRccButHasNoReportableOffences from "../lib/generateOperations/checkCaseRequiresRccButHasNoReportableOffences"
+import hasUnmatchedPncOffences from "../lib/generateOperations/hasUnmatchedPncOffences"
 
 const generator: ExceptionGenerator = (aho: AnnotatedHearingOutcome): Exception[] => {
   const exceptions: Exception[] = []
-  const fixedPenalty = aho.AnnotatedHearingOutcome.HearingOutcome.Case.PenaltyNoticeCaseReferenceNumber
+  const fixedPenalty = !!aho.AnnotatedHearingOutcome.HearingOutcome.Case.PenaltyNoticeCaseReferenceNumber
   const allResultsAlreadyOnPnc = areAllResultsOnPnc(aho)
-  
+
   if (fixedPenalty) {
     return []
   }
 
   checkResultClassExceptions(aho, (offence, result, offenceIndex, resultIndex) => {
+    const ccrId = offence?.CourtCaseReferenceNumber || undefined
+
+    if (!allResultsAlreadyOnPnc && hasUnmatchedPncOffences(aho, ccrId) && !offence.AddedByTheCourt) {
+      return
+    }
+
+    if (result.PNCAdjudicationExists || areAllPncResults2007(aho, ccrId)) {
+      return
+    }
+
     if (
-      result.ResultClass === ResultClass.JUDGEMENT_WITH_FINAL_RESULT &&
-      !result.PNCAdjudicationExists &&
-      !offence.AddedByTheCourt &&
-      !areAllPncResults2007(aho, offence?.CourtCaseReferenceNumber || undefined)
+      (result.ResultClass === ResultClass.JUDGEMENT_WITH_FINAL_RESULT ||
+        result.ResultClass === ResultClass.ADJOURNMENT_WITH_JUDGEMENT) &&
+      result.PNCDisposalType === 2060 &&
+      checkCaseRequiresRccButHasNoReportableOffences(aho, ccrId)
     ) {
       const exception = {
-        code: ExceptionCode.HO200103,
+        code: ExceptionCode.HO200108,
         path: errorPaths.offence(offenceIndex).result(resultIndex).resultClass
       }
       exceptions.push(exception)
