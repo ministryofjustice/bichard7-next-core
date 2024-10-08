c092ee4cb54b3b9165b7a35ad429eb0924a21435
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-10-08 15:52:12 +0100

    Refactor validateOperations function (#995)
    * Move check for PENHRG and SENDEF out of for loop
    
    * Move check for NEWREM, remand CCRs and SENDEF out of for loop
    
    * Move PENHRG and court case specific operations out of for loop
    
    * Export courtCaseSpecificOperations from operationCourtCaseReference
    
    * Move remand CCRs and SENDEF out of for loop
    
    * Move DISARR and SENDEF out of for loop
    
    * Move DISARR and SUBVAR out of for loop
    
    * Move SENDEF and SUBVAR out of for loop
    
    * Refactor DISARR and SENDEF to use checkForClashingCourtCaseOperations
    
    * Refactor DISARR and SUBVAR to use checkForClashingCourtCaseOperations
    
    * Refactor SENDEF and SUBVAR to use checkForClashingCourtCaseOperations
    
    * Refactor checkForClashingCourtCaseOperations to use tuple type
    
    * Fix tests so only invalid if court case operations with same CCR
    
    * Refactor same court case specific operation with same CCR
    
    * Refactor to add findClashingCourtCaseOperation
    
    * Ensure consistent variable names
    
    * Refactor PENHRG checks to remove nested if statements
    
    * Update boolean condition names for invalid operations
    
    * Inline checkForClashingCourtCaseOperations checks
    
    * Rename checkForClashingCourtCaseOperations
    
    * Combine HO200113 checks for new remand and sentencing
    
    * Rename hasSameCourtCaseOperationWithSameCcr

--- packages/core/phase2/lib/generateOperations/validateOperations.ts
+++ packages/core/phase2/lib/generateOperations/validateOperations.ts
@@ -3,84 +3,94 @@ import isEqual from "lodash.isequal"
 import errorPaths from "../../../lib/exceptions/errorPaths"
 import type Exception from "../../../types/Exception"
 import type { Operation } from "../../../types/PncUpdateDataset"
-import operationCourtCaseReference from "./operationCourtCaseReference"
+import operationCourtCaseReference, { courtCaseSpecificOperations } from "./operationCourtCaseReference"
 import { PncOperation } from "../../../types/PncOperation"
 
 const errorPath = errorPaths.case.asn
 
 const validateOperations = (operations: Operation[], remandCcrs: Set<string>): Exception | void => {
-  let sendefExists = false
-  let newremExists = false
-  let penhrgExists = false
-  const courtCaseSpecificOperations: Operation[] = []
+  const hasOperation = (pncOperation: PncOperation) => operations.some((operation) => operation.code === pncOperation)
 
-  for (const operation of operations) {
-    penhrgExists ||= operation.code === PncOperation.PENALTY_HEARING
-    newremExists ||= operation.code === PncOperation.REMAND
-    sendefExists ||= operation.code === PncOperation.SENTENCE_DEFERRED
+  if (hasOperation(PncOperation.PENALTY_HEARING) && hasOperation(PncOperation.SENTENCE_DEFERRED)) {
+    return { code: ExceptionCode.HO200114, path: errorPath }
+  }
 
-    if (penhrgExists && sendefExists) {
-      return { code: ExceptionCode.HO200114, path: errorPath }
-    }
+  const operationsWithCourtCase: Operation[] = operations.filter((operation) =>
+    courtCaseSpecificOperations.includes(operation.code)
+  )
 
-    if (newremExists && remandCcrs.size === 0 && sendefExists) {
-      return { code: ExceptionCode.HO200113, path: errorPath }
-    }
+  if (
+    hasOperation(PncOperation.PENALTY_HEARING) &&
+    operationsWithCourtCase.some((operationWithCourtCase) =>
+      [PncOperation.DISPOSAL_UPDATED, PncOperation.PENALTY_HEARING].includes(operationWithCourtCase.code)
+    )
+  ) {
+    return { code: ExceptionCode.HO200109, path: errorPath }
+  }
 
-    if (penhrgExists && courtCaseSpecificOperations.length > 0) {
-      const incompatibleCode = courtCaseSpecificOperations[courtCaseSpecificOperations.length - 1].code
-      if ([PncOperation.DISPOSAL_UPDATED, PncOperation.PENALTY_HEARING].includes(incompatibleCode)) {
-        return { code: ExceptionCode.HO200109, path: errorPath }
-      }
+  if (
+    hasOperation(PncOperation.PENALTY_HEARING) &&
+    operationsWithCourtCase.some(
+      (operationWithCourtCase) => operationWithCourtCase.code === PncOperation.NORMAL_DISPOSAL
+    )
+  ) {
+    return { code: ExceptionCode.HO200115, path: errorPath }
+  }
 
-      if (incompatibleCode === PncOperation.NORMAL_DISPOSAL) {
-        return { code: ExceptionCode.HO200115, path: errorPath }
-      }
-    }
-
+  const hasNewRemandAndSentencing = operations.some((operation) => {
     const courtCaseReference = operationCourtCaseReference(operation)
-
-    if (
-      [PncOperation.SENTENCE_DEFERRED, PncOperation.DISPOSAL_UPDATED, PncOperation.NORMAL_DISPOSAL].includes(
-        operation.code as PncOperation
-      )
-    ) {
-      const clashingOperation = courtCaseSpecificOperations.find(
-        (op) => operationCourtCaseReference(op) == courtCaseReference
-      )
-      if (clashingOperation) {
-        const sortedOperations = [operation.code, clashingOperation.code].sort()
-        if (operation.code === clashingOperation.code) {
-          return { code: ExceptionCode.HO200109, path: errorPath }
-        }
-
-        if (isEqual(sortedOperations, [PncOperation.NORMAL_DISPOSAL, PncOperation.SENTENCE_DEFERRED])) {
-          return { code: ExceptionCode.HO200112, path: errorPath }
-        }
-
-        if (isEqual(sortedOperations, [PncOperation.NORMAL_DISPOSAL, PncOperation.DISPOSAL_UPDATED])) {
-          return { code: ExceptionCode.HO200115, path: errorPath }
-        }
-
-        if (isEqual(sortedOperations, [PncOperation.SENTENCE_DEFERRED, PncOperation.DISPOSAL_UPDATED])) {
-          return { code: ExceptionCode.HO200114, path: errorPath }
-        }
-
-        break
-      }
-
-      courtCaseSpecificOperations.push(operation)
-    }
-
     const remandCcrsContainCourtCaseReference = !!courtCaseReference && remandCcrs.has(courtCaseReference)
 
-    if (
-      [PncOperation.SENTENCE_DEFERRED].includes(operation.code as PncOperation) &&
-      remandCcrsContainCourtCaseReference
-    ) {
-      return { code: ExceptionCode.HO200113, path: errorPath }
-    }
+    return (
+      PncOperation.SENTENCE_DEFERRED === operation.code &&
+      ((hasOperation(PncOperation.REMAND) && remandCcrs.size === 0) || remandCcrsContainCourtCaseReference)
+    )
+  })
+
+  if (hasNewRemandAndSentencing) {
+    return { code: ExceptionCode.HO200113, path: errorPath }
   }
+
+  const findClashingCourtCaseOperation = (operation: Operation) =>
+    operationsWithCourtCase.find(
+      (operationWithCourtCase) =>
+        operationCourtCaseReference(operationWithCourtCase) == operationCourtCaseReference(operation)
+    )
+
+  const hasClashingCourtCaseOperations = (clashingCourtCaseOperations: [PncOperation, PncOperation]) =>
+    operationsWithCourtCase.some((operation) => {
+      const clashingCourtCaseOperation = findClashingCourtCaseOperation(operation)
+
+      return isEqual([operation.code, clashingCourtCaseOperation?.code].sort(), clashingCourtCaseOperations)
+    })
+
+  if (hasClashingCourtCaseOperations([PncOperation.NORMAL_DISPOSAL, PncOperation.SENTENCE_DEFERRED])) {
+    return { code: ExceptionCode.HO200112, path: errorPath }
+  }
+
+  if (hasClashingCourtCaseOperations([PncOperation.NORMAL_DISPOSAL, PncOperation.DISPOSAL_UPDATED])) {
+    return { code: ExceptionCode.HO200115, path: errorPath }
+  }
+
+  if (hasClashingCourtCaseOperations([PncOperation.SENTENCE_DEFERRED, PncOperation.DISPOSAL_UPDATED])) {
+    return { code: ExceptionCode.HO200114, path: errorPath }
+  }
+
+  const hasSameCourtCaseOperationWithSameCcr = operationsWithCourtCase.some((operation, index) => {
+    const clashingCourtCaseOperation = findClashingCourtCaseOperation(operation)
+
+    return (
+      clashingCourtCaseOperation &&
+      index !== operationsWithCourtCase.indexOf(clashingCourtCaseOperation) &&
+      operation.code === clashingCourtCaseOperation.code
+    )
+  })
+
+  if (hasSameCourtCaseOperationWithSameCcr) {
+    return { code: ExceptionCode.HO200109, path: errorPath }
+  }
+
+  return undefined
 }
 
 export default validateOperations
