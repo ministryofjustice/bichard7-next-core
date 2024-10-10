84351511aff8eabc553820969ef44d837bddbf6d
BushraAbdullahi <103956365+BushraAbdullahi@users.noreply.github.com>
2024-10-10 11:29:46 +0100

    Add HO200124 (#999)
    * move exception logic
    
    * add new test file, remove old imports
    
    * remove old tests, refactor exception logic
    
    * remove unused import
    
    * remove more unused imports
    
    * Update packages/core/phase2/exceptions/HO200124.ts
    
    Co-authored-by: Emad Karamad <emad.karamad@madetech.com>
    
    * refactor test
    
    * refactor handleAdjournementWithJudgement test
    
    * remove test
    
    * refactor test
    
    ---------
    
    Co-authored-by: Emad Karamad <emad.karamad@madetech.com>

--- packages/core/phase2/exceptions/HO200124.ts
+++ packages/core/phase2/exceptions/HO200124.ts
@@ -1,9 +1,44 @@
+import ExceptionCode from "bichard7-next-data-latest/dist/types/ExceptionCode"
 import type { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
 import type Exception from "../../types/Exception"
 import type { ExceptionGenerator } from "../../types/ExceptionGenerator"
+import ResultClass from "../../types/ResultClass"
+import { areAllResultsOnPnc } from "../lib/generateOperations/areAllResultsOnPnc"
+import hasUnmatchedPncOffences from "../lib/generateOperations/hasUnmatchedPncOffences"
+import checkResultClassExceptions from "./checkResultClassExceptions"
+import errorPaths from "../../lib/exceptions/errorPaths"
 
-const generator: ExceptionGenerator = (_aho: AnnotatedHearingOutcome, _options): Exception[] => {
-  return []
+const generator: ExceptionGenerator = (aho: AnnotatedHearingOutcome): Exception[] => {
+  const exceptions: Exception[] = []
+  const fixedPenalty = aho.AnnotatedHearingOutcome.HearingOutcome.Case.PenaltyNoticeCaseReferenceNumber
+  const allResultsAlreadyOnPnc = areAllResultsOnPnc(aho)
+
+  if (fixedPenalty || allResultsAlreadyOnPnc) {
+    return []
+  }
+
+  checkResultClassExceptions(aho, (offence, result, offenceIndex, resultIndex) => {
+    const ccrId = offence?.CourtCaseReferenceNumber || undefined
+
+    if (result.PNCAdjudicationExists) {
+      return
+    }
+
+    if (
+      result.ResultClass &&
+      [ResultClass.JUDGEMENT_WITH_FINAL_RESULT, ResultClass.ADJOURNMENT_WITH_JUDGEMENT].includes(result.ResultClass) &&
+      hasUnmatchedPncOffences(aho, ccrId) &&
+      !offence.AddedByTheCourt
+    ) {
+      const exception = {
+        code: ExceptionCode.HO200124,
+        path: errorPaths.offence(offenceIndex).result(resultIndex).resultClass
+      }
+      exceptions.push(exception)
+    }
+  })
+
+  return exceptions
 }
 
 export default generator
--- packages/core/phase2/lib/generateOperations/resultClassHandlers/handleJudgementWithFinalResult.ts
+++ packages/core/phase2/lib/generateOperations/resultClassHandlers/handleJudgementWithFinalResult.ts
@@ -1,5 +1,3 @@
-import ExceptionCode from "bichard7-next-data-latest/dist/types/ExceptionCode"
-import errorPaths from "../../../../lib/exceptions/errorPaths"
 import type { Operation } from "../../../../types/PncUpdateDataset"
 import createOperation from "../createOperation"
 import hasUnmatchedPncOffences from "../hasUnmatchedPncOffences"
@@ -11,8 +9,6 @@ export const handleJudgementWithFinalResult: ResultClassHandler = ({
   resubmitted,
   aho,
   allResultsAlreadyOnPnc,
-  offenceIndex,
-  resultIndex,
   offence,
   result
 }) => {
@@ -31,11 +27,7 @@ export const handleJudgementWithFinalResult: ResultClassHandler = ({
   }
 
   if (!allResultsAlreadyOnPnc && hasUnmatchedPncOffences(aho, ccrId) && !offence.AddedByTheCourt) {
-    const exception = {
-      code: ExceptionCode.HO200124,
-      path: errorPaths.offence(offenceIndex).result(resultIndex).resultClass
-    }
-    return { operations: [], exceptions: [exception] }
+    return { operations: [], exceptions: [] }
   }
 
   const contains2007Result = !!offence?.Result.some((r) => r.PNCDisposalType === 2007)
