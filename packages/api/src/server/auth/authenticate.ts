f823d1fecd6e3e0b328700ab3a86cde712edab3b
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-10 14:46:42 +0100

    Remove named paths
    Docker doesn't work them.

--- packages/api/src/server/auth/authenticate.ts
+++ packages/api/src/server/auth/authenticate.ts
@@ -1,9 +1,9 @@
-import jwtParser from "@/server/auth/jwtParser"
-import jwtVerify from "@/server/auth/jwtVerify"
-import type Gateway from "@/services/gateways/interfaces/gateway"
 import type { User } from "@moj-bichard7/common/types/User"
 import type { FastifyReply, FastifyRequest } from "fastify"
 import { UNAUTHORIZED } from "http-status"
+import type Gateway from "../../services/gateways/interfaces/gateway"
+import jwtParser from "./jwtParser"
+import jwtVerify from "./jwtVerify"
 
 const validApiKey = process.env.WORKSPACE === "production" ? process.env.API_KEY : "password"
 
