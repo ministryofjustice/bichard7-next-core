b61c1e12a581734cf21e5381189f4c692c3f40d1
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-11 12:17:48 +0100

    Add check for unresolved cases

--- packages/api/src/services/gateways/interfaces/gateway.ts
+++ packages/api/src/services/gateways/interfaces/gateway.ts
@@ -2,11 +2,7 @@ import type { User } from "@moj-bichard7/common/types/User"
 
 interface Gateway {
   fetchUserByUsername: (username: string) => Promise<User>
-  filterUserHasSameForceAsCaseAndLockedByUser: (
-    username: string,
-    caseId: number,
-    forceIds: number[]
-  ) => Promise<boolean>
+  caseCanBeResubmitted: (username: string, caseId: number, forceIds: number[]) => Promise<boolean>
 }
 
 export default Gateway
---
+++ packages/api/src/services/gateways/postgres/caseCanBeResubmitted.ts
@@ -0,0 +1,25 @@
+import type postgres from "postgres"
+
+export interface CaseCanBeResubmittedResult {
+  locked_by_user: boolean
+  case_in_force: boolean
+  case_is_unresolved: boolean
+}
+
+export default async (sql: postgres.Sql, username: string, caseId: number, forceIds: number[]): Promise<boolean> => {
+  const [result]: [CaseCanBeResubmittedResult?] = await sql`
+      SELECT
+        (el.error_locked_by_id = ${username})::INTEGER as locked_by_user,
+        (br7own.force_code(el.org_for_police_filter) = ANY((${forceIds}::SMALLINT[])))::INTEGER as case_in_force,
+        (el.resolution_ts IS NULL)::INTEGER as case_is_unresolved
+      FROM br7own.error_list el
+      WHERE
+        el.error_id = ${caseId}
+    `
+
+  if (!result) {
+    throw new Error("Case not found")
+  }
+
+  return result.locked_by_user && result.case_in_force && result.case_is_unresolved
+}
