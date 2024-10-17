e11ce451f3fce376625231fe29269c684e3fbe26
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-17 11:24:04 +0100

    Add setup helper for e2e tests

--- packages/api/src/plugins/health/plugin.e2e.test.ts
+++ packages/api/src/plugins/health/plugin.e2e.test.ts
@@ -1,16 +1,15 @@
 import type { FastifyInstance } from "fastify"
 import { OK } from "http-status"
-import build from "../../app"
+import { SetupAppEnd2EndHelper } from "../../tests/helpers/setupAppEnd2EndHelper"
 import HealthRoutes from "./routes"
 
 describe("health plugin", () => {
-  const port = 8888
+  let helper: SetupAppEnd2EndHelper
   let app: FastifyInstance
 
   beforeAll(async () => {
-    app = await build()
-    await app.ready()
-    app.listen({ port })
+    helper = await SetupAppEnd2EndHelper.setup()
+    app = helper.app
   })
 
   afterAll(async () => {
@@ -18,14 +17,14 @@ describe("health plugin", () => {
   })
 
   it("GET /health should return Ok using the HealthRoutes enum", async () => {
-    const response = await fetch(`http://localhost:${port}${HealthRoutes.HEALTH}`)
+    const response = await fetch(`${helper.address}${HealthRoutes.HEALTH}`)
 
     expect(response.status).toBe(OK)
     expect(await response.text()).toBe("Ok")
   })
 
   it("GET /health should return Ok using the a string as the route", async () => {
-    const response = await fetch(`http://localhost:${port}/health`)
+    const response = await fetch(`${helper.address}/health`)
 
     expect(response.status).toBe(OK)
     expect(await response.text()).toBe("Ok")
