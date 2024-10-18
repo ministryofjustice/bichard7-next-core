555f423cd07883581f984a87c44a2def0b7bd2d2
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-18 12:05:06 +0100

    Remove function

--- packages/api/src/tests/helpers/userHelper.ts
+++ packages/api/src/tests/helpers/userHelper.ts
@@ -1,7 +1,6 @@
 import type { User } from "@moj-bichard7/common/types/User"
 import type { UserGroup } from "@moj-bichard7/common/types/UserGroup"
 import { randomUUID } from "crypto"
-import End2EndPostgresGateway from "../testGateways/e2ePostgresGateway"
 import { generateTestJwtTokenAndSplit } from "./jwtHelper"
 
 export const generateJwtForStaticUser = (userGroups: UserGroup[] = []): [string, User] => {
@@ -16,9 +15,4 @@ export const generateJwtForStaticUser = (userGroups: UserGroup[] = []): [string,
   } satisfies User
 
   return [generateTestJwtTokenAndSplit(user, jwtId), user]
-}
-
-export const createUser = async (user: Partial<User>): Promise<User> => {
-  const gateway = new End2EndPostgresGateway()
-  return await gateway.createUser(user)
 }
