7c73e19d374312aab6345b90c8ba31095b7f6602
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-18 12:08:07 +0100

    Expose the gateway

--- packages/api/src/tests/helpers/setupAppEnd2EndHelper.ts
+++ packages/api/src/tests/helpers/setupAppEnd2EndHelper.ts
@@ -1,20 +1,23 @@
 import type { FastifyInstance } from "fastify"
 import build from "../../app"
+import End2EndPostgresGateway from "../testGateways/e2ePostgresGateway"
 
 export class SetupAppEnd2EndHelper {
   static async setup(port: number = 8888): Promise<SetupAppEnd2EndHelper> {
-    const app = await build()
+    const gateway = new End2EndPostgresGateway()
+    const app = await build(gateway)
     await app.ready()
     app.listen({ port })
 
-    return new SetupAppEnd2EndHelper(port, app)
+    return new SetupAppEnd2EndHelper(port, app, gateway)
   }
 
   readonly address: string
 
   constructor(
     readonly port: number,
-    readonly app: FastifyInstance
+    readonly app: FastifyInstance,
+    readonly gateway: End2EndPostgresGateway
   ) {
     this.address = `http://localhost:${port}`
   }
