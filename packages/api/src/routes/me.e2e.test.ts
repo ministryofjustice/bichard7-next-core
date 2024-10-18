ad23e34afc646393ec103e75d1734ea0df14045c
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-18 15:20:00 +0100

    Add e2e test for /me endpoint

---
+++ packages/api/src/routes/me.e2e.test.ts
@@ -0,0 +1,46 @@
+import { UserGroup } from "@moj-bichard7/common/types/UserGroup"
+import type { FastifyInstance } from "fastify"
+import { OK } from "http-status"
+import { SetupAppEnd2EndHelper } from "../tests/helpers/setupAppEnd2EndHelper"
+import { createUserAndJwtToken } from "../tests/helpers/userHelper"
+
+describe("/me e2e", () => {
+  const endpoint = "/me"
+  let helper: SetupAppEnd2EndHelper
+  let app: FastifyInstance
+
+  beforeAll(async () => {
+    helper = await SetupAppEnd2EndHelper.setup()
+    app = helper.app
+  })
+
+  beforeEach(async () => {
+    await helper.gateway.clearDb()
+  })
+
+  afterAll(async () => {
+    await app.close()
+    await helper.gateway.close()
+  })
+
+  it("will return the current user with a correct JWT", async () => {
+    const [encodedJwt, user] = await createUserAndJwtToken(helper.gateway)
+
+    const response = await fetch(`${helper.address}${endpoint}`, {
+      method: "GET",
+      headers: {
+        "X-API-Key": "password",
+        Authorization: `Bearer ${encodedJwt}`
+      }
+    })
+
+    const responseUser = {
+      username: user.username,
+      email: user.email,
+      groups: [UserGroup.GeneralHandler]
+    }
+
+    expect(response.status).toBe(OK)
+    expect(await response.json()).toEqual(responseUser)
+  })
+})
