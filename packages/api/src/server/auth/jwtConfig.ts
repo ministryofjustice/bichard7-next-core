5bbd8c7b382b17d2b69fa93ba39867cb9c3966b5
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-22 14:51:02 +0100

    Refactor and verify JWT bearer token

---
+++ packages/api/src/server/auth/jwtConfig.ts
@@ -0,0 +1,18 @@
+import type jwt from "jsonwebtoken"
+
+export type JWTConfigType = {
+  tokenExpiresIn: string | number
+  tokenIssuer: string
+  tokenSecret: string
+}
+
+export const jwtConfig: JWTConfigType = {
+  tokenExpiresIn: process.env.TOKEN_EXPIRES_IN ? process.env.TOKEN_EXPIRES_IN : "10 minutes",
+  tokenIssuer: process.env.TOKEN_ISSUER ? process.env.TOKEN_ISSUER : "Bichard",
+  tokenSecret: process.env.TOKEN_SECRET ? process.env.TOKEN_SECRET : "OliverTwist"
+}
+
+export const jwtSignOptions: jwt.SignOptions = {
+  expiresIn: jwtConfig.tokenExpiresIn,
+  issuer: jwtConfig.tokenIssuer
+}
--- packages/api/src/tests/helpers/jwtHelper.ts
+++ packages/api/src/tests/helpers/jwtHelper.ts
@@ -1,32 +1,17 @@
 import type { User } from "@moj-bichard7/common/types/User"
 import { randomUUID } from "crypto"
 import jwt from "jsonwebtoken"
-
-// Same config as UserService
-const jwtConfig = {
-  tokenExpiresIn: "10 minutes",
-  tokenIssuer: "Bichard",
-  tokenSecret: "OliverTwist"
-}
+import { jwtConfig, jwtSignOptions } from "../../server/auth/jwtConfig"
 
 export const generateTestJwtToken = (user: User, jwt_id?: string): string => {
-  const options: jwt.SignOptions = {
-    expiresIn: jwtConfig.tokenExpiresIn,
-    issuer: jwtConfig.tokenIssuer
-  }
-
   const payload = {
     username: user.username,
     exclusionList: [],
     inclusionList: [],
-    email: user.email ? user.email : undefined,
+    emailAddress: user.email ? user.email : undefined,
     groups: user.groups ? user.groups : [],
     id: jwt_id ?? randomUUID()
   }
 
-  return jwt.sign(payload, jwtConfig.tokenSecret, options)
-}
-
-export const generateTestJwtTokenAndSplit = (user: User, jwt_id?: string): string => {
-  return generateTestJwtToken(user, jwt_id).split(".")[1]
+  return jwt.sign(payload, jwtConfig.tokenSecret, jwtSignOptions)
 }
