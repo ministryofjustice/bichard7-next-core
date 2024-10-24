ba34b90197ca1cd388d347b3719fa46298d04187
Richard Race <1273965+richrace@users.noreply.github.com>
2024-10-24 12:25:34 +0100

    Add insertCase function

--- packages/common/types/Case.ts
+++ packages/common/types/Case.ts
@@ -1,8 +1,20 @@
 import { z } from "zod"
 
-export const Case = z.object({
+export const CaseSchema = z.object({
   error_id: z.number(),
+  message_id: z.string(),
+  phase: z.number().gt(0).lte(3),
+  trigger_count: z.number(),
+  is_urgent: z.number(),
+  annotated_msg: z.string(),
+  error_report: z.string().max(1000),
+  create_ts: z.string().datetime(),
+  error_count: z.number(),
+  user_updated_flag: z.number(),
+  msg_received_ts: z.string().datetime(),
+  court_reference: z.string().max(11),
+  total_pnc_failure_resubmissions: z.number().default(0),
   org_for_police_filter: z.string()
 })
 
-export type Case = z.infer<typeof Case>
+export type Case = z.infer<typeof CaseSchema>
