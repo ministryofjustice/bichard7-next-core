663ae9f4afecd8644b9782f00795a7f7a982d2b2
BushraAbdullahi <bushra1175@gmail.com>
2024-10-28 16:33:11 +0000

    remove duplicate imports

--- packages/ui/cypress/component/DefendantDetails.cy.tsx
+++ packages/ui/cypress/component/DefendantDetails.cy.tsx
@@ -2,8 +2,6 @@ import { GenderCode } from "@moj-bichard7-developers/bichard7-next-data/dist/typ
 import { HearingDefendant } from "@moj-bichard7/core/types/AnnotatedHearingOutcome"
 import { CurrentUserContext } from "context/CurrentUserContext"
 import { format } from "date-fns"
-import Permission from "types/Permission"
-import { DisplayFullUser } from "types/display/Users"
 import { CourtCaseContext } from "../../src/context/CourtCaseContext"
 import { DefendantDetails } from "../../src/features/CourtCaseDetails/Tabs/Panels/DefendantDetails"
 import { DisplayFullCourtCase } from "../../src/types/display/CourtCases"
