cc0547fbb812cc142d178bdc0e7d5c6f0713fab0
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-10-28 16:47:05 +0000

    Update areAnyPncResults2007 to use findPncCourtCase function

--- packages/core/phase2/lib/areAnyPncResults2007.ts
+++ packages/core/phase2/lib/areAnyPncResults2007.ts
@@ -1,22 +1,15 @@
 import type { AnnotatedHearingOutcome, Offence } from "../../types/AnnotatedHearingOutcome"
+import findPncCourtCase from "./findPncCourtCase"
 
 const areAnyPncResults2007 = (aho: AnnotatedHearingOutcome, offence: Offence): boolean => {
-  const offenceReasonSequence = offence.CriminalProsecutionReference.OffenceReasonSequence
-  const courtCaseReferenceNumber =
-    offence.CourtCaseReferenceNumber || aho.AnnotatedHearingOutcome.HearingOutcome.Case.CourtCaseReferenceNumber
-  if (!offenceReasonSequence || !aho.PncQuery || !courtCaseReferenceNumber) {
-    return false
-  }
+  const matchingPncCourtCase = findPncCourtCase(aho, offence)
 
-  const matchingCase = aho.PncQuery?.courtCases?.find((x) => x.courtCaseReference === courtCaseReferenceNumber)
-  const hasAny2007PncResults = !!matchingCase?.offences?.some(
+  return !!matchingPncCourtCase?.offences?.some(
     (o) =>
       o.offence.sequenceNumber &&
-      o.offence.sequenceNumber === Number(offenceReasonSequence) &&
+      o.offence.sequenceNumber === Number(offence.CriminalProsecutionReference.OffenceReasonSequence) &&
       o.disposals?.some((disposal) => disposal.type === 2007)
   )
-
-  return hasAny2007PncResults
 }
 
 export default areAnyPncResults2007
