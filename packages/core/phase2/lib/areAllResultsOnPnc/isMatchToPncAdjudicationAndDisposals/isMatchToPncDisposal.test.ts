7210eb0438c5356566c9c6e841cb1719796d6ddb
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-10-29 11:22:02 +0000

    Move arePncDisposalsMatching into isMatchToPncDisposal
    - Update isMatchToPncDisposal tests to include those in arePncDisposalsMatching.
    - Remove arePncDisposalsMatching and its tests.

--- packages/core/phase2/lib/areAllResultsOnPnc/isMatchToPncAdjudicationAndDisposals/isMatchToPncDisposal.test.ts
+++ packages/core/phase2/lib/areAllResultsOnPnc/isMatchToPncAdjudicationAndDisposals/isMatchToPncDisposal.test.ts
@@ -3,6 +3,46 @@ import type { PncDisposal } from "../../../../types/PncQueryResult"
 import isMatchToPncDisposal from "./isMatchToPncDisposal"
 
 describe("isMatchToPncDisposal", () => {
+  const pncDisposal: PncDisposal = {
+    qtyDuration: "A4",
+    qtyDate: "21052024",
+    qtyMonetaryValue: "25000",
+    qualifiers: "Q",
+    type: 9999,
+    text: "EXCLUDED FROM LOCATION"
+  }
+  const ahoResult: Result = {
+    PNCDisposalType: 9999,
+    CJSresultCode: 3041,
+    ResultQualifierVariable: [{ Code: "Q" }],
+    ResultVariableText: "DEFENDANT EXCLUDED FROM LOCATION FOR A PERIOD OF TIME",
+    AmountSpecifiedInResult: [
+      {
+        Amount: 25000,
+        DecimalPlaces: 2
+      }
+    ],
+    DateSpecifiedInResult: [
+      {
+        Date: new Date("05/21/2024"),
+        Sequence: 1
+      }
+    ],
+    Duration: [
+      {
+        DurationLength: 4,
+        DurationType: "",
+        DurationUnit: "A"
+      }
+    ]
+  } as Result
+
+  it("returns true when an AHO result matches a PNC disposal on all its matching fields", () => {
+    const result = isMatchToPncDisposal([pncDisposal], ahoResult)
+
+    expect(result).toBe(true)
+  })
+
   it("returns false when disposals list is empty", () => {
     const ahoResult = { ResultQualifierVariable: [] } as unknown as Result
 
@@ -11,42 +51,111 @@ describe("isMatchToPncDisposal", () => {
     expect(result).toBe(false)
   })
 
-  it("returns true when an AHO result matches the pncDisposal on all its matching fields", () => {
-    const pncDisposal: PncDisposal = {
-      qtyDuration: "A4",
-      qtyDate: "21052024",
-      qtyMonetaryValue: "25000",
-      qualifiers: "Q",
-      type: 9999,
-      text: "EXCLUDED FROM LOCATION"
-    }
-    const ahoResult: Result = {
-      PNCDisposalType: 9999,
-      CJSresultCode: 3041,
-      ResultQualifierVariable: [{ Code: "Q" }],
-      ResultVariableText: "DEFENDANT EXCLUDED FROM LOCATION FOR A PERIOD OF TIME",
-      AmountSpecifiedInResult: [
-        {
-          Amount: 25000,
-          DecimalPlaces: 2
-        }
-      ],
+  it("returns false when an AHO result has a different type to the PNC disposal", () => {
+    const nonMatchingAhoResult = { ...ahoResult, PNCDisposalType: 1234 }
+
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when an AHO result has different qualifiers to the PNC disposal", () => {
+    const nonMatchingAhoResult = { ...ahoResult, ResultQualifierVariable: [{ Code: "XXX" }] }
+
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when an AHO result has different dates to the PNC disposal", () => {
+    const nonMatchingAhoResult = {
+      ...ahoResult,
       DateSpecifiedInResult: [
         {
-          Date: new Date("05/21/2024"),
+          Date: new Date("03/01/1997"),
           Sequence: 1
         }
-      ],
-      Duration: [
+      ]
+    }
+
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when an AHO result has different monetary values to the PNC disposal", () => {
+    const nonMatchingAhoResult = {
+      ...ahoResult,
+      AmountSpecifiedInResult: [
         {
-          DurationLength: 4,
-          DurationType: "",
-          DurationUnit: "A"
+          Amount: 11111,
+          DecimalPlaces: 2
         }
       ]
-    } as Result
+    }
 
-    const result = isMatchToPncDisposal([pncDisposal], ahoResult)
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when an AHO result has different durations to the PNC disposal", () => {
+    const nonMatchingAhoResult = {
+      ...ahoResult,
+      Duration: [
+        {
+          DurationLength: 1234,
+          DurationType: "",
+          DurationUnit: "B"
+        }
+      ]
+    }
+
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when an AHO result doesn't have any durations", () => {
+    const nonMatchingAhoResult = {
+      ...ahoResult,
+      Duration: []
+    }
+
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when an AHO result has different text to the PNC disposal", () => {
+    const nonMatchingAhoResult = {
+      ...ahoResult,
+      ResultVariableText: "SOMETHING DIFFERENT"
+    }
+
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when an AHO result doesn't have any text", () => {
+    const nonMatchingAhoResult = {
+      ...ahoResult,
+      ResultVariableText: undefined
+    }
+
+    const result = isMatchToPncDisposal([pncDisposal], nonMatchingAhoResult)
+
+    expect(result).toBe(false)
+  })
+
+  it("returns true when an AHO result has same text with case difference to the PNC disposal", () => {
+    const matchingAhoResult = {
+      ...ahoResult,
+      ResultVariableText: ahoResult.ResultVariableText?.toUpperCase()
+    }
+
+    const result = isMatchToPncDisposal([pncDisposal], matchingAhoResult)
 
     expect(result).toBe(true)
   })
--- packages/core/phase2/lib/areAllResultsOnPnc/isMatchToPncAdjudicationAndDisposals/isMatchToPncDisposal.ts
+++ packages/core/phase2/lib/areAllResultsOnPnc/isMatchToPncAdjudicationAndDisposals/isMatchToPncDisposal.ts
@@ -1,11 +1,22 @@
 import type { Result } from "../../../../types/AnnotatedHearingOutcome"
 import type { PncDisposal } from "../../../../types/PncQueryResult"
-import arePncDisposalsMatching from "./arePncDisposalsMatching"
 import { createPncDisposalFromResult } from "./createPncDisposalFromResult"
 
 const isMatchToPncDisposal = (pncDisposals: PncDisposal[], result: Result): boolean =>
   createPncDisposalFromResult(result).every((ahoDisposal) =>
     pncDisposals.some((pncDisposal) => arePncDisposalsMatching(ahoDisposal, pncDisposal))
   )
+
+const arePncDisposalsMatching = (firstDisposal: PncDisposal, secondDisposal: PncDisposal): boolean =>
+  firstDisposal.type === secondDisposal.type &&
+  areStringsEqual(firstDisposal.qtyDuration, secondDisposal.qtyDuration) &&
+  areStringsEqual(firstDisposal.qtyDate, secondDisposal.qtyDate) &&
+  ((!firstDisposal.qtyMonetaryValue && !secondDisposal.qtyMonetaryValue) ||
+    Number(firstDisposal.qtyMonetaryValue) === Number(secondDisposal.qtyMonetaryValue)) &&
+  areStringsEqual(firstDisposal.qualifiers, secondDisposal.qualifiers) &&
+  areStringsEqual(firstDisposal.text?.toUpperCase(), secondDisposal.text?.toUpperCase())
+
+const areStringsEqual = (firstObject: string | undefined, secondObject: string | undefined) =>
+  (!firstObject && !secondObject) || firstObject === secondObject
 
 export default isMatchToPncDisposal
