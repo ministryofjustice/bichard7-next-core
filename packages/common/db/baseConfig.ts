c260aed4a0bfe9a974703d37b3d8723afa8687a7
Sen <65850511+Senny10@users.noreply.github.com>
2024-10-30 17:12:14 +0000

    Refactor database configuration to use a bespoked typed interface

--- packages/common/db/baseConfig.ts
+++ packages/common/db/baseConfig.ts
@@ -1,10 +1,19 @@
-import type { IClient, IConnectionParameters } from "pg-promise/typescript/pg-subset"
+interface DatabaseConfig {
+  host: string
+  user: string
+  password: string
+  database: string
+  port: number
+  ssl: boolean
+  schema: string
+}
 
-export const baseConfig: IConnectionParameters<IClient> = {
+export const baseConfig: DatabaseConfig = {
   host: process.env.DB_HOST ?? process.env.DB_AUTH_HOST ?? "localhost",
   user: process.env.DB_USER ?? process.env.DB_AUTH_USER ?? "bichard",
   password: process.env.DB_PASSWORD ?? process.env.DB_AUTH_PASSWORD ?? "password",
   database: process.env.DB_DATABASE ?? process.env.DB_AUTH_DATABASE ?? "bichard",
   port: parseInt(process.env.DB_PORT ?? process.env.DB_AUTH_PORT ?? "5432", 10),
-  ssl: (process.env.DB_SSL ?? process.env.DB_AUTH_SSL) === "true"
+  ssl: (process.env.DB_SSL ?? process.env.DB_AUTH_SSL) === "true",
+  schema: process.env.DB_SCHEMA ?? "br7own"
 }
