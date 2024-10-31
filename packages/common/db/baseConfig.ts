a7b3c0dcee80f3afd54eda973bb0ab85358f7409
Sen <65850511+Senny10@users.noreply.github.com>
2024-10-31 12:41:01 +0000

    Refactor baseConfig.ts to use Number() instead of parseInt() for port configuration

--- packages/common/db/baseConfig.ts
+++ packages/common/db/baseConfig.ts
@@ -13,7 +13,7 @@ export const baseConfig: DatabaseConfig = {
   user: process.env.DB_USER ?? process.env.DB_AUTH_USER ?? "bichard",
   password: process.env.DB_PASSWORD ?? process.env.DB_AUTH_PASSWORD ?? "password",
   database: process.env.DB_DATABASE ?? process.env.DB_AUTH_DATABASE ?? "bichard",
-  port: parseInt(process.env.DB_PORT ?? process.env.DB_AUTH_PORT ?? "5432", 10),
+  port: Number(process.env.DB_PORT ?? process.env.DB_AUTH_PORT ?? "5432"),
   ssl: (process.env.DB_SSL ?? process.env.DB_AUTH_SSL) === "true",
   schema: process.env.DB_SCHEMA ?? "br7own"
 }
