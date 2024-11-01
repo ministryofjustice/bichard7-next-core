5220771ebf7498f34451d5bdf7755a67ac930d3f
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-11-01 15:30:59 +0000

    Update isMatchToPncAdjudication
    - To create adjudication instead of it being passed in.
    - To disallow offenceReasonSequence as undefined.
    
    Also update and improve tests.

--- packages/core/phase2/lib/isMatchToPncAdjudication.test.ts
+++ packages/core/phase2/lib/isMatchToPncAdjudication.test.ts
@@ -1,151 +1,111 @@
-import type { PncAdjudication, PncOffence } from "../../types/PncQueryResult"
+import type { PncOffence } from "../../types/PncQueryResult"
 import isMatchToPncAdjudication from "./isMatchToPncAdjudication"
+import type { Result } from "../../types/AnnotatedHearingOutcome"
 
-describe("check isMatchToPncAdj", () => {
-  it("Given an undefined date on the aho adjudication, adjudications do not match", () => {
-    const hoAdjudication = {
-      verdict: "",
-      sentenceDate: undefined,
+describe("isMatchToPncAdjudication", () => {
+  const matchingResult = { PNCDisposalType: 2063, Verdict: "G", PleaStatus: "G" } as Result
+  const matchingResults = [matchingResult]
+  const matchingHearingDate = new Date(2024, 3, 1)
+  const matchingOffenceReasonSequence = "001"
+  const matchingPncOffence = {
+    offence: { sequenceNumber: 1 },
+    adjudication: {
+      verdict: "GUILTY",
+      plea: "GUILTY",
+      sentenceDate: matchingHearingDate,
       offenceTICNumber: 0,
-      plea: "GUILTY"
-    } as unknown as PncAdjudication
+      weedFlag: undefined
+    }
+  } as PncOffence
 
-    const pncOffence = {
-      offence: {
-        sequenceNumber: 1,
-        qualifier1: ""
-      },
-      adjudication: {
-        verdict: "GUILTY",
-        plea: "GUILTY",
-        sentenceDate: new Date(),
-        offenceTICNumber: 0,
-        weedFlag: undefined
-      }
-    } as PncOffence
-    const result = isMatchToPncAdjudication(hoAdjudication, pncOffence, "001")
+  it("returns false when a PNC adjudication cannot be created from results", () => {
+    const results = [] as Result[]
+
+    const result = isMatchToPncAdjudication(
+      results,
+      matchingHearingDate,
+      matchingPncOffence,
+      matchingOffenceReasonSequence
+    )
+
     expect(result).toBe(false)
   })
 
-  it("Given an undefined date on the pnc adjudication, adjudications do not match", () => {
-    const hoAdjudication = {
-      verdict: "",
-      sentenceDate: new Date(),
-      offenceTICNumber: 0,
-      plea: "GUILTY"
-    } as unknown as PncAdjudication
+  it("returns false when the PNC offence doesn't have an adjudication", () => {
+    const pncOffence = { ...matchingPncOffence, adjudication: undefined }
 
-    const pncOffence = {
-      offence: {
-        sequenceNumber: 1,
-        qualifier1: ""
-      },
-      adjudication: {
-        verdict: "GUILTY",
-        plea: "GUILTY",
-        offenceTICNumber: 0,
-        weedFlag: undefined
-      }
-    } as PncOffence
-    const result = isMatchToPncAdjudication(hoAdjudication, pncOffence, "001")
+    const result = isMatchToPncAdjudication(
+      matchingResults,
+      matchingHearingDate,
+      pncOffence,
+      matchingOffenceReasonSequence
+    )
+
     expect(result).toBe(false)
   })
 
-  it("Given matching date on the aho adjudication, adjudications match", () => {
-    const hoAdjudication = {
-      verdict: "MATCHING-VERDICT",
-      sentenceDate: new Date("2024-02-02"),
-      offenceTICNumber: 0,
-      plea: "GUILTY"
-    } as PncAdjudication
+  it("returns false when offence reason sequence doesn't match", () => {
+    const offenceReasonSequence = "999"
 
-    const pncOffence = {
-      offence: {
-        sequenceNumber: 1,
-        qualifier1: ""
-      },
-      adjudication: {
-        verdict: "MATCHING-VERDICT",
-        plea: "GUILTY",
-        sentenceDate: new Date("2024-02-02"),
-        offenceTICNumber: 0,
-        weedFlag: undefined
-      }
-    } as PncOffence
-    const result = isMatchToPncAdjudication(hoAdjudication, pncOffence, "001")
-    expect(result).toBe(true)
-  })
+    const result = isMatchToPncAdjudication(
+      matchingResults,
+      matchingHearingDate,
+      matchingPncOffence,
+      offenceReasonSequence
+    )
 
-  it("Given no dates, adjudications match", () => {
-    const hoAdjudication = {
-      verdict: "MATCHING-VERDICT",
-      offenceTICNumber: 0,
-      plea: "GUILTY"
-    } as PncAdjudication
-
-    const pncOffence = {
-      offence: {
-        sequenceNumber: 1,
-        qualifier1: ""
-      },
-      adjudication: {
-        verdict: "MATCHING-VERDICT",
-        plea: "GUILTY",
-        offenceTICNumber: 0,
-        weedFlag: undefined
-      }
-    } as PncOffence
-    const result = isMatchToPncAdjudication(hoAdjudication, pncOffence, "001")
-    expect(result).toBe(true)
-  })
-
-  it("Given undefined offenceReasonSequence, adjudications do not match", () => {
-    const hoAdjudication = {
-      verdict: "MATCHING-VERDICT",
-      sentenceDate: new Date("2024-02-02"),
-      offenceTICNumber: 0,
-      plea: "GUILTY"
-    } as PncAdjudication
-
-    const pncOffence = {
-      offence: {
-        sequenceNumber: 1,
-        qualifier1: ""
-      },
-      adjudication: {
-        verdict: "MATCHING-VERDICT",
-        plea: "GUILTY",
-        sentenceDate: new Date("2024-02-02"),
-        offenceTICNumber: 0,
-        weedFlag: undefined
-      }
-    } as PncOffence
-    const result = isMatchToPncAdjudication(hoAdjudication, pncOffence, undefined)
     expect(result).toBe(false)
   })
 
-  it("Given non matching verdicts, adjudications do not match", () => {
-    const hoAdjudication = {
-      verdict: "NON-MATCHING-VERDICT",
-      sentenceDate: new Date("2024-02-02"),
-      offenceTICNumber: 0,
-      plea: "GUILTY"
-    } as PncAdjudication
+  it("returns false when verdict doesn't match", () => {
+    const results = [{ ...matchingResult, Verdict: "NG" }]
 
-    const pncOffence = {
-      offence: {
-        sequenceNumber: 1,
-        qualifier1: ""
-      },
-      adjudication: {
-        verdict: "GUILTY",
-        plea: "GUILTY",
-        sentenceDate: new Date("2024-02-02"),
-        offenceTICNumber: 0,
-        weedFlag: undefined
-      }
-    } as PncOffence
-    const result = isMatchToPncAdjudication(hoAdjudication, pncOffence, "001")
+    const result = isMatchToPncAdjudication(
+      results,
+      matchingHearingDate,
+      matchingPncOffence,
+      matchingOffenceReasonSequence
+    )
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when plea doesn't match", () => {
+    const results = [{ ...matchingResult, PleaStatus: "DEN" }]
+
+    const result = isMatchToPncAdjudication(
+      results,
+      matchingHearingDate,
+      matchingPncOffence,
+      matchingOffenceReasonSequence
+    )
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when offence TIC number doesn't match", () => {
+    const results = [{ ...matchingResult, NumberOfOffencesTIC: 999 }]
+
+    const result = isMatchToPncAdjudication(
+      results,
+      matchingHearingDate,
+      matchingPncOffence,
+      matchingOffenceReasonSequence
+    )
+
+    expect(result).toBe(false)
+  })
+
+  it("returns false when hearing date doesn't match", () => {
+    const hearingDate = new Date(1999, 9, 9)
+
+    const result = isMatchToPncAdjudication(
+      matchingResults,
+      hearingDate,
+      matchingPncOffence,
+      matchingOffenceReasonSequence
+    )
+
     expect(result).toBe(false)
   })
 })
--- packages/core/phase2/lib/isMatchToPncAdjudication.ts
+++ packages/core/phase2/lib/isMatchToPncAdjudication.ts
@@ -1,15 +1,19 @@
-import type { PncAdjudication, PncOffence } from "../../types/PncQueryResult"
+import type { PncOffence } from "../../types/PncQueryResult"
+import type { Result } from "../../types/AnnotatedHearingOutcome"
+import createPncAdjudicationFromAho from "./createPncAdjudicationFromAho"
 
 const isMatchToPncAdjudication = (
-  hoAdjudication: PncAdjudication,
+  results: Result[],
+  hearingDate: Date,
   pncOffence: PncOffence,
-  offenceReasonSequence: string | undefined
+  offenceReasonSequence: string
 ): boolean => {
-  const pncOffenceSequence = pncOffence.offence.sequenceNumber.toString().padStart(3, "0")
+  const hoAdjudication = createPncAdjudicationFromAho(results, hearingDate)
   const pncAdjudication = pncOffence.adjudication
+  const pncOffenceSequence = pncOffence.offence.sequenceNumber.toString().padStart(3, "0")
 
   return (
-    !!offenceReasonSequence &&
+    !!hoAdjudication &&
     !!pncAdjudication &&
     offenceReasonSequence === pncOffenceSequence &&
     hoAdjudication.verdict === pncAdjudication.verdict &&
