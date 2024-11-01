d4f73d25624ce77f71f07e511448f04958a093cf
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-11-01 11:35:29 +0000

    Update areResultsMatchingAPncDisposal to use checkExceptionFn
    And fix and update tests.

--- packages/core/phase2/lib/areResultsMatchingAPncDisposal.ts
+++ packages/core/phase2/lib/areResultsMatchingAPncDisposal.ts
@@ -2,8 +2,20 @@ import type { Offence } from "../../types/AnnotatedHearingOutcome"
 import type { PncDisposal } from "../../types/PncQueryResult"
 import isRecordableResult from "./isRecordableResult"
 import isMatchToPncDisposal from "./isMatchToPncDisposal"
+import type { CheckExceptionFn } from "./isMatchToPncAdjudicationAndDisposals"
 
-const areResultsMatchingAPncDisposal = (offence: Offence, disposals: PncDisposal[]): boolean =>
-  offence.Result.every((result) => !isRecordableResult(result) || isMatchToPncDisposal(disposals, result))
+const areResultsMatchingAPncDisposal = (
+  offence: Offence,
+  disposals: PncDisposal[],
+  offenceIndex?: number,
+  checkExceptionFn?: CheckExceptionFn
+): boolean =>
+  offence.Result.every((result, resultIndex) => {
+    if (checkExceptionFn && offenceIndex !== undefined) {
+      checkExceptionFn(result, offenceIndex, resultIndex)
+    }
+
+    return !isRecordableResult(result) || isMatchToPncDisposal(disposals, result)
+  })
 
 export default areResultsMatchingAPncDisposal
