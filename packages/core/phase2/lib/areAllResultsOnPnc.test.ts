20ea6e023c5ec4a55ee1bef00702d88b9d3a74ea
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-11-04 11:55:15 +0000

    Refactor tests to use generateAhoMatchingPncAdjudicationAndDisposals

--- packages/core/phase2/lib/areAllResultsOnPnc.test.ts
+++ packages/core/phase2/lib/areAllResultsOnPnc.test.ts
@@ -1,87 +1,20 @@
 import areAllResultsOnPnc from "./areAllResultsOnPnc"
-import type { AnnotatedHearingOutcome, Offence } from "../../types/AnnotatedHearingOutcome"
+import generateAhoMatchingPncAdjudicationAndDisposals from "../tests/helpers/generateAhoMatchingPncAdjudicationAndDisposals"
 
 describe("areAllResultsOnPnc", () => {
-  const matchingAho = {
-    AnnotatedHearingOutcome: {
-      HearingOutcome: {
-        Hearing: { DateOfHearing: new Date(2024, 3, 1) },
-        Case: {
-          HearingDefendant: {
-            Offence: [
-              {
-                CourtCaseReferenceNumber: "1",
-                Result: [
-                  {
-                    PNCDisposalType: 2063,
-                    Verdict: "G",
-                    PleaStatus: "G",
-                    ResultQualifierVariable: [{ Code: "A" }]
-                  }
-                ],
-                CriminalProsecutionReference: { OffenceReasonSequence: "001" }
-              }
-            ]
-          }
-        }
-      }
-    },
-    PncQuery: {
-      pncId: "1",
-      courtCases: [
-        {
-          courtCaseReference: "1",
-          offences: [
-            {
-              offence: { sequenceNumber: 1 },
-              adjudication: {
-                verdict: "GUILTY",
-                plea: "GUILTY",
-                sentenceDate: new Date(2024, 3, 1),
-                offenceTICNumber: 0,
-                weedFlag: undefined
-              },
-              disposals: [
-                {
-                  qtyDate: "",
-                  qtyDuration: "",
-                  type: 2063,
-                  qtyUnitsFined: "",
-                  qtyMonetaryValue: "",
-                  qualifiers: "A",
-                  text: ""
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  } as AnnotatedHearingOutcome
-
   it("returns true when all offences match to the PNC adjudication and disposals", () => {
-    const result = areAllResultsOnPnc(matchingAho)
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({ hasAdditionalMatchingOffence: true })
+
+    const result = areAllResultsOnPnc(aho)
 
     expect(result).toBe(true)
   })
 
   it("returns false when not all offences match to the PNC adjudication and disposals", () => {
-    const aho = structuredClone(matchingAho)
-    aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence = [
-      matchingAho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0],
-      {
-        CourtCaseReferenceNumber: "2",
-        Result: [
-          {
-            PNCDisposalType: 2063,
-            Verdict: "NG",
-            PleaStatus: "G",
-            ResultQualifierVariable: [{ Code: "A" }]
-          }
-        ],
-        CriminalProsecutionReference: { OffenceReasonSequence: "002" }
-      }
-    ] as Offence[]
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({
+      hasAdditionalMatchingOffence: true,
+      firstResultDisposalType: 9999
+    })
 
     const result = areAllResultsOnPnc(aho)
 
--- packages/core/phase2/lib/isMatchToPncAdjudicationAndDisposals.test.ts
+++ packages/core/phase2/lib/isMatchToPncAdjudicationAndDisposals.test.ts
@@ -1,70 +1,11 @@
-import type { AnnotatedHearingOutcome, Result } from "../../types/AnnotatedHearingOutcome"
+import type { Result } from "../../types/AnnotatedHearingOutcome"
 import isMatchToPncAdjudicationAndDisposals from "./isMatchToPncAdjudicationAndDisposals"
-import type { PncDisposal, PncQueryResult } from "../../types/PncQueryResult"
+import generateAhoMatchingPncAdjudicationAndDisposals from "../tests/helpers/generateAhoMatchingPncAdjudicationAndDisposals"
 
 describe("isMatchToPncAdjudicationAndDisposals", () => {
-  const matchingHearingDate = new Date(2024, 3, 1)
-  const matchingAho = {
-    AnnotatedHearingOutcome: {
-      HearingOutcome: {
-        Hearing: { DateOfHearing: matchingHearingDate },
-        Case: {
-          HearingDefendant: {
-            Offence: [
-              {
-                CourtCaseReferenceNumber: "1",
-                Result: [
-                  {
-                    PNCDisposalType: 2063,
-                    Verdict: "G",
-                    PleaStatus: "G",
-                    ResultQualifierVariable: [{ Code: "A" }]
-                  }
-                ],
-                CriminalProsecutionReference: { OffenceReasonSequence: "001" }
-              }
-            ]
-          }
-        }
-      }
-    },
-    PncQuery: {
-      pncId: "1",
-      courtCases: [
-        {
-          courtCaseReference: "1",
-          offences: [
-            {
-              offence: { sequenceNumber: 1 },
-              adjudication: {
-                verdict: "GUILTY",
-                plea: "GUILTY",
-                sentenceDate: matchingHearingDate,
-                offenceTICNumber: 0,
-                weedFlag: undefined
-              },
-              disposals: [
-                {
-                  qtyDate: "",
-                  qtyDuration: "",
-                  type: 2063,
-                  qtyUnitsFined: "",
-                  qtyMonetaryValue: "",
-                  qualifiers: "A",
-                  text: ""
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  } as AnnotatedHearingOutcome
-
   it("returns true when no results", () => {
-    const aho = structuredClone(matchingAho)
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({ hasResults: false })
     const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
-    offence.Result = []
 
     const result = isMatchToPncAdjudicationAndDisposals(aho, offence)
 
@@ -72,9 +13,8 @@ describe("isMatchToPncAdjudicationAndDisposals", () => {
   })
 
   it("returns true when no offence reason sequence and no recordable results", () => {
-    const aho = structuredClone(matchingAho)
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({ hasOffenceReasonSequence: false })
     const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
-    offence.CriminalProsecutionReference.OffenceReasonSequence = undefined
     offence.Result = [{ PNCDisposalType: 1000 }] as Result[]
 
     const result = isMatchToPncAdjudicationAndDisposals(aho, offence)
@@ -83,10 +23,8 @@ describe("isMatchToPncAdjudicationAndDisposals", () => {
   })
 
   it("returns false when no offence reason sequence and recordable results", () => {
-    const aho = structuredClone(matchingAho)
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({ hasOffenceReasonSequence: false })
     const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
-    offence.CriminalProsecutionReference.OffenceReasonSequence = undefined
-    offence.Result = [{ PNCDisposalType: 2063 }] as Result[]
 
     const result = isMatchToPncAdjudicationAndDisposals(aho, offence)
 
@@ -94,7 +32,7 @@ describe("isMatchToPncAdjudicationAndDisposals", () => {
   })
 
   it("returns false when no PNC ID", () => {
-    const aho = { ...matchingAho, PncQuery: { pncId: undefined } as unknown as PncQueryResult }
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({ hasPncId: false })
     const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
 
     const result = isMatchToPncAdjudicationAndDisposals(aho, offence)
@@ -103,9 +41,8 @@ describe("isMatchToPncAdjudicationAndDisposals", () => {
   })
 
   it("returns false when no PNC court cases", () => {
-    const aho = structuredClone(matchingAho)
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({ hasPncOffences: false })
     const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
-    aho.PncQuery!.courtCases = []
 
     const result = isMatchToPncAdjudicationAndDisposals(aho, offence)
 
@@ -113,27 +50,17 @@ describe("isMatchToPncAdjudicationAndDisposals", () => {
   })
 
   it("returns true when results match PNC adjudication and disposals", () => {
-    const offence = matchingAho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({})
+    const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
 
-    const result = isMatchToPncAdjudicationAndDisposals(matchingAho, offence)
+    const result = isMatchToPncAdjudicationAndDisposals(aho, offence)
 
     expect(result).toBe(true)
   })
 
   it("returns false when results match PNC adjudication but not disposals", () => {
-    const aho = structuredClone(matchingAho)
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({ firstPncDisposalType: 9999 })
     const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
-    aho.PncQuery!.courtCases![0].offences[0].disposals = [
-      {
-        qtyDate: "",
-        qtyDuration: "",
-        type: 9999,
-        qtyUnitsFined: "",
-        qtyMonetaryValue: "",
-        qualifiers: "A",
-        text: ""
-      }
-    ] as PncDisposal[]
 
     const result = isMatchToPncAdjudicationAndDisposals(aho, offence)
 
@@ -141,7 +68,7 @@ describe("isMatchToPncAdjudicationAndDisposals", () => {
   })
 
   it("returns false when results match PNC disposals but not adjudication", () => {
-    const aho = structuredClone(matchingAho)
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({})
     const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
     offence.Result[0].Verdict = "NG"
 
@@ -152,9 +79,10 @@ describe("isMatchToPncAdjudicationAndDisposals", () => {
 
   it("checks for exceptions when an offence index and function is provided", () => {
     const checkExceptionFn = jest.fn()
-    const offence = matchingAho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
+    const aho = generateAhoMatchingPncAdjudicationAndDisposals({})
+    const offence = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence[0]
 
-    isMatchToPncAdjudicationAndDisposals(matchingAho, offence, 0, checkExceptionFn)
+    isMatchToPncAdjudicationAndDisposals(aho, offence, 0, checkExceptionFn)
 
     expect(checkExceptionFn).toHaveBeenCalled()
   })
