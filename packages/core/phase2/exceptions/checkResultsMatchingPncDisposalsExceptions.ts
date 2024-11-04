4fdedce9f9d1a2ac5b9778cf1e6cc5a6a695a7f1
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-11-04 11:16:38 +0000

    Update checkResultsMatchingPncDisposalsExceptions
    So it doesn't have its own isMatchToPncAdjudicationAndDisposals and
    areResultsMatchingAPncDisposal functions.

--- packages/core/phase2/exceptions/checkResultsMatchingPncDisposalsExceptions.ts
+++ packages/core/phase2/exceptions/checkResultsMatchingPncDisposalsExceptions.ts
@@ -1,63 +1,14 @@
-import type { AnnotatedHearingOutcome, Offence, Result } from "../../types/AnnotatedHearingOutcome"
-import isMatchToPncDisposal from "../lib/isMatchToPncDisposal"
-import type { PncDisposal } from "../../types/PncQueryResult"
-import isRecordableResult from "../lib/isRecordableResult"
-import createPncAdjudicationFromAho from "../lib/createPncAdjudicationFromAho"
-import isMatchToPncAdjudication from "../lib/isMatchToPncAdjudication"
-import findPncCourtCase from "../lib/findPncCourtCase"
-
-type CheckExceptionFn = (result: Result, offenceIndex: number, resultIndex: number) => void
-
-const areResultsMatchingAPncDisposal = (
-  offence: Offence,
-  offenceIndex: number,
-  disposals: PncDisposal[],
-  checkExceptionFn: CheckExceptionFn
-): boolean => {
-  return offence.Result.every((result, resultIndex) => {
-    checkExceptionFn(result, offenceIndex, resultIndex)
-
-    return !isRecordableResult(result) || isMatchToPncDisposal(disposals, result)
-  })
-}
-
-const isMatchToPncAdjudicationAndDisposals = (
-  aho: AnnotatedHearingOutcome,
-  offence: Offence,
-  offenceIndex: number,
-  checkExceptionFn: CheckExceptionFn
-): boolean => {
-  const offenceReasonSequence = offence.CriminalProsecutionReference?.OffenceReasonSequence ?? undefined
-  const adjFromAho = createPncAdjudicationFromAho(
-    offence.Result,
-    aho.AnnotatedHearingOutcome.HearingOutcome.Hearing.DateOfHearing
-  )
-
-  return (
-    !!adjFromAho &&
-    !!findPncCourtCase(aho, offence)?.offences.some(
-      (pncOffence) =>
-        isMatchToPncAdjudication(adjFromAho, pncOffence, offenceReasonSequence) &&
-        areResultsMatchingAPncDisposal(offence, offenceIndex, pncOffence.disposals ?? [], checkExceptionFn)
-    )
-  )
-}
+import type { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
+import type { CheckExceptionFn } from "../lib/isMatchToPncAdjudicationAndDisposals"
+import isMatchToPncAdjudicationAndDisposals from "../lib/isMatchToPncAdjudicationAndDisposals"
 
 const checkResultsMatchingPncDisposalsExceptions = (
   aho: AnnotatedHearingOutcome,
   checkExceptionFn: CheckExceptionFn
 ): void => {
-  if (!aho.PncQuery?.pncId) {
-    return
-  }
-
-  aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence.every((offence, offenceIndex) => {
-    if (offence.Result.length === 0 || !offence.CriminalProsecutionReference?.OffenceReasonSequence) {
-      return !offence.Result.some(isRecordableResult)
-    }
-
-    return isMatchToPncAdjudicationAndDisposals(aho, offence, offenceIndex, checkExceptionFn)
-  })
+  aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence.every((offence, offenceIndex) =>
+    isMatchToPncAdjudicationAndDisposals(aho, offence, offenceIndex, checkExceptionFn)
+  )
 }
 
 export default checkResultsMatchingPncDisposalsExceptions
