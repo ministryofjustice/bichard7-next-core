6e938343a8e0c9fc651c5352c3cfc51bd85b0277
Richard Race <1273965+richrace@users.noreply.github.com>
2024-11-05 15:58:01 +0000

    Update notes

--- packages/api/src/services/gateways/dataStoreGateways/postgres.ts
+++ packages/api/src/services/gateways/dataStoreGateways/postgres.ts
@@ -4,8 +4,23 @@ import type DataStoreGateway from "../interfaces/dataStoreGateway"
 import caseCanBeResubmitted from "./postgres/canCaseBeResubmitted"
 import fetchUserByUsername from "./postgres/fetchUserByUsername"
 
-// TODO: Check what permission the user has (triggers or exceptions) and add lock
-// TODO: Add AuditLogEvent
+/*
+ * We don't seem to have any logic for updating the AHO in core. It is all in the UI
+ *
+ * TODO: Check user's feature flags
+ * TODO: UI `courtCase.canResolveOrSubmit(user)` only permission Exceptions so do we care about triggers?
+ * TODO: Check the User doesn't have excluded trigger codes
+ * TODO: Check what permission the user has (triggers or exceptions) and add lock
+ * TODO: If case needs to be locked add AuditLogEvent
+ *       Should be handled by court-cases/:caseId/index
+ * TODO: amend the court case
+ *       Shouldn't auto save done this?
+ *       The UI creates a force owner and edits the aho if one doesn't exist
+ *       Applies amendments to the aho
+ *       Only updates if Phase 1
+ *       Inserts notes
+ *       Returns updated AHO
+ */
 class Postgres implements DataStoreGateway {
   protected readonly db = postgresFactory()
 
