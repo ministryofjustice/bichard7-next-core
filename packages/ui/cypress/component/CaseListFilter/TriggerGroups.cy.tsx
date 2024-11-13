5b89b6aee351c31f5bae44095d64fc3882aa3a7e
Tausif Patel <tausifpatel.dev@gmail.com>
2024-11-13 09:47:13 +0000

    Wrap triggerGroups component with CurrentUserContext.Provider

--- packages/ui/cypress/component/CaseListFilter/TriggerGroups.cy.tsx
+++ packages/ui/cypress/component/CaseListFilter/TriggerGroups.cy.tsx
@@ -1,31 +1,50 @@
 import TriggerGroups from "components/SearchFilters/TriggerGroups"
+import { CurrentUserContext } from "context/CurrentUserContext"
+import { DisplayFullUser } from "types/display/Users"
 
 describe("TriggerGroups", () => {
   const dispatch = () => {}
+  const currentUser = { excludedTriggers: [] } as unknown as DisplayFullUser
 
   it("contains Bails checkbox", () => {
-    cy.mount(<TriggerGroups dispatch={dispatch} reasonCodes={[]} />)
+    cy.mount(
+      <CurrentUserContext.Provider value={{ currentUser }}>
+        <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
+      </CurrentUserContext.Provider>
+    )
 
     cy.get("input#bails[type=checkbox]")
     cy.get("label[for=bails]").contains("Bails")
   })
 
   it("contains Custody checkbox", () => {
-    cy.mount(<TriggerGroups dispatch={dispatch} reasonCodes={[]} />)
+    cy.mount(
+      <CurrentUserContext.Provider value={{ currentUser }}>
+        <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
+      </CurrentUserContext.Provider>
+    )
 
     cy.get("input#custody[type=checkbox]")
     cy.get("label[for=custody]").contains("Custody")
   })
 
   it("contains Orders checkbox", () => {
-    cy.mount(<TriggerGroups dispatch={dispatch} reasonCodes={[]} />)
+    cy.mount(
+      <CurrentUserContext.Provider value={{ currentUser }}>
+        <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
+      </CurrentUserContext.Provider>
+    )
 
     cy.get("input#orders[type=checkbox]")
     cy.get("label[for=orders]").contains("Orders")
   })
 
   it("contains Warrants checkbox", () => {
-    cy.mount(<TriggerGroups dispatch={dispatch} reasonCodes={[]} />)
+    cy.mount(
+      <CurrentUserContext.Provider value={{ currentUser }}>
+        <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
+      </CurrentUserContext.Provider>
+    )
 
     cy.get("input#warrants[type=checkbox]")
     cy.get("label[for=warrants]").contains("Warrants")
