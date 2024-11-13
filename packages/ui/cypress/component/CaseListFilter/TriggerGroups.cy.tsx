d91c67aea1fcbff5b2e5cfa4e1c1d818015bcd26
Tausif Patel <tausifpatel.dev@gmail.com>
2024-11-13 11:50:48 +0000

    Refactor to remove duplications

--- packages/ui/cypress/component/CaseListFilter/TriggerGroups.cy.tsx
+++ packages/ui/cypress/component/CaseListFilter/TriggerGroups.cy.tsx
@@ -5,47 +5,30 @@ import { DisplayFullUser } from "types/display/Users"
 describe("TriggerGroups", () => {
   const dispatch = () => {}
   const currentUser = { excludedTriggers: [] } as unknown as DisplayFullUser
-
-  it("contains Bails checkbox", () => {
+  beforeEach(() => {
     cy.mount(
       <CurrentUserContext.Provider value={{ currentUser }}>
         <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
       </CurrentUserContext.Provider>
     )
+  })
 
+  it("contains Bails checkbox", () => {
     cy.get("input#bails[type=checkbox]")
     cy.get("label[for=bails]").contains("Bails")
   })
 
   it("contains Custody checkbox", () => {
-    cy.mount(
-      <CurrentUserContext.Provider value={{ currentUser }}>
-        <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
-      </CurrentUserContext.Provider>
-    )
-
     cy.get("input#custody[type=checkbox]")
     cy.get("label[for=custody]").contains("Custody")
   })
 
   it("contains Orders checkbox", () => {
-    cy.mount(
-      <CurrentUserContext.Provider value={{ currentUser }}>
-        <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
-      </CurrentUserContext.Provider>
-    )
-
     cy.get("input#orders[type=checkbox]")
     cy.get("label[for=orders]").contains("Orders")
   })
 
   it("contains Warrants checkbox", () => {
-    cy.mount(
-      <CurrentUserContext.Provider value={{ currentUser }}>
-        <TriggerGroups dispatch={dispatch} reasonCodes={[]} />
-      </CurrentUserContext.Provider>
-    )
-
     cy.get("input#warrants[type=checkbox]")
     cy.get("label[for=warrants]").contains("Warrants")
   })
