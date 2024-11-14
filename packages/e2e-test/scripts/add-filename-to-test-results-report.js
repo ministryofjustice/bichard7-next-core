d2b43f8ce5c34276191643ba3d0a4b9c727e4608
Richard Race <1273965+richrace@users.noreply.github.com>
2024-11-14 14:50:01 +0000

    Made the script more complete

--- packages/e2e-test/scripts/add-filename-to-test-results-report.js
+++ packages/e2e-test/scripts/add-filename-to-test-results-report.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable no-param-reassign */
 const { XMLParser, XMLBuilder } = require("fast-xml-parser")
 const fs = require("node:fs")
@@ -6,6 +7,17 @@ const glob = require("tiny-glob")
 
 const reportFilename = path.join(__dirname, "..", "test-results", "results", "report.xml")
 const data = fs.readFileSync(reportFilename, "utf8")
+
+const featuresThatDoNotHaveFileNumbers = [
+  "(Exception|Trigger|General) handler",
+  "Supervisor",
+  "Audit",
+  "case list",
+  "case details",
+  "old bichard user"
+]
+const featuresThatDoNotHaveFileNumbersRegex = new RegExp(`${featuresThatDoNotHaveFileNumbers.join("|")}`, "i")
+const featuresThatDoHaveFileNumbersRegex = /\d{3}[a-e]?/
 
 async function addFileToFeatureReport() {
   const features = await glob("features/**/*.feature")
@@ -19,17 +31,28 @@ async function addFileToFeatureReport() {
   const parser = new XMLParser(options)
   const jObj = parser.parse(data)
 
+  if (jObj.testsuite.testcase === undefined) {
+    return
+  }
+
   jObj.testsuite.testcase.forEach((element) => {
     if (element["@file"]) {
       return
     }
 
-    const regex = /(\d{3}|(Exception|Trigger|General) handler|Supervisor|Audit)/i
+    const className = element["@classname"]
+    let featureName
 
-    let featureName = element["@classname"].match(regex)[0]
-
-    if (!/\d{3}/.test(featureName)) {
-      featureName = `${featureName.toLowerCase().replaceAll(" ", "-")}.feature`
+    try {
+      if (className.match(featuresThatDoNotHaveFileNumbersRegex)) {
+        featureName = className.match(featuresThatDoNotHaveFileNumbersRegex)[0]
+        featureName = `${featureName.toLowerCase().replaceAll(" ", "-")}.feature`
+      } else {
+        featureName = className.match(featuresThatDoHaveFileNumbersRegex)[0]
+      }
+    } catch (err) {
+      console.error(`Error happned in: "${className}"`)
+      throw err
     }
 
     const file = features.find((feature) => feature.includes(featureName))
