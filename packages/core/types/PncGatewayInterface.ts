b83f59c9536f88f87f042ce084e7e1a16b8ba66e
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-11-18 16:54:25 +0000

    Update PncGateway to return PncApiError

--- packages/core/types/PncGatewayInterface.ts
+++ packages/core/types/PncGatewayInterface.ts
@@ -1,9 +1,10 @@
 import type { PromiseResult } from "@moj-bichard7/common/types/Result"
 import type { PncQueryResult } from "./PncQueryResult"
 import type PncUpdateRequest from "../phase3/types/PncUpdateRequest"
+import type { PncApiError } from "../lib/PncGateway"
 
 interface PncGatewayInterface {
-  query: (asn: string, correlationId: string) => PromiseResult<PncQueryResult | undefined>
+  query: (asn: string, correlationId: string) => PromiseResult<PncQueryResult | PncApiError | undefined>
   queryTime: Date | undefined
   update: (request: PncUpdateRequest, correlationId: string) => PromiseResult<void>
 }
