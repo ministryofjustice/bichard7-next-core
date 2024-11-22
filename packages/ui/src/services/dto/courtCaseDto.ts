725a800383a46fba8802f03321a4c8e1305d33c5
BushraAbdullahi <bushra1175@gmail.com>
2024-11-22 14:33:14 +0000

    test

--- packages/ui/src/services/dto/courtCaseDto.ts
+++ packages/ui/src/services/dto/courtCaseDto.ts
@@ -24,7 +24,7 @@ export const courtCaseToDisplayPartialCourtCaseDto = (
       hasAccessToExceptions(currentUser) &&
       courtCase.errorStatus === "Unresolved",
     isUrgent: courtCase.isUrgent,
-    notes: sortBy(courtCase.notes.map(noteToDisplayNoteDto), "createdAt").reverse(),
+    notes: sortBy(courtCase.notes.map(noteToDisplayNoteDto), "createdAt"),
     ptiurn: courtCase.ptiurn,
     resolutionTimestamp: courtCase.resolutionTimestamp ? courtCase.resolutionTimestamp.toISOString() : null,
     triggerLockedByUsername: courtCase.triggerLockedByUsername,
