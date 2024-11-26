72106ae9743a782a28f15fead348fc26d60a621c
Richard Race <1273965+richrace@users.noreply.github.com>
2024-11-26 11:43:15 +0000

    Fix options

--- .circleci/scripts/ui-scripts/run-component-tests.sh
+++ .circleci/scripts/ui-scripts/run-component-tests.sh
@@ -4,7 +4,7 @@ cd ~/project/packages/ui
 options="--component --reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 if [[ $MS_EDGE == "true" ]]; then
-  options += " --browser edge"
+  options+=" --browser edge"
 fi
 
 circleci tests glob "cypress/component/**/*.cy.tsx" | circleci tests run --command="xargs npx cypress run ${options} --spec" --verbose
--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -4,7 +4,7 @@ cd ~/project/packages/ui
 options="--config baseUrl=https://localhost:4443 --reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 if [[ $MS_EDGE == "true" ]]; then
-  options += " --browser edge"
+  options+=" --browser edge"
 fi
 
 circleci tests glob "cypress/e2e/**/*.cy.ts" | circleci tests run --command="xargs npx cypress run ${options} --spec" --verbose --split-by=timings
