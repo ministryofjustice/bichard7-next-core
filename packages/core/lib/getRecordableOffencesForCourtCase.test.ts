84e11b74b4feb2cb36035d8a341e539b2bdf0d92
Emad Karamad <emad.karamad@madetech.com>
2024-11-26 16:05:23 +0000

    Add test for getRecordableOffencesForCourtCase

--- packages/core/lib/getRecordableOffencesForCourtCase.test.ts
+++ packages/core/lib/getRecordableOffencesForCourtCase.test.ts
@@ -1,3 +1,76 @@
+import generateAhoFromOffenceList from "../phase2/tests/fixtures/helpers/generateAhoFromOffenceList"
+import type { Offence } from "../types/AnnotatedHearingOutcome"
+import getRecordableOffencesForCourtCase from "./getRecordableOffencesForCourtCase"
+
+const offences = [
+  {
+    CriminalProsecutionReference: {
+      OffenceReasonSequence: "001"
+    },
+    CourtCaseReferenceNumber: 1
+  },
+  {
+    CourtCaseReferenceNumber: 99,
+    OffenceCategory: "B7" // Non-recordable
+  },
+  {
+    CriminalProsecutionReference: {
+      OffenceReasonSequence: "002"
+    }
+  },
+  {
+    CriminalProsecutionReference: {
+      OffenceReasonSequence: "003"
+    },
+    CourtCaseReferenceNumber: 1
+  },
+  {
+    CriminalProsecutionReference: {
+      OffenceReasonSequence: "004"
+    }
+  },
+  {
+    OffenceCategory: "B7" // Non-recordable
+  }
+] as unknown as Offence[]
+
+const recordableOffences = offences.filter((o) => o.OffenceCategory !== "B7")
+
 describe("getRecordableOffencesForCourtCase", () => {
-  it.skip("Write test for getRecordableOffencesForCourtCase", () => {})
+  it("should return empty list when there are no offences", () => {
+    const aho = generateAhoFromOffenceList([])
+
+    const actualOffences = getRecordableOffencesForCourtCase(aho)
+
+    expect(actualOffences).toHaveLength(0)
+  })
+
+  it("should return all recordable offences when CCR is undefined", () => {
+    const aho = generateAhoFromOffenceList(offences as unknown as Offence[])
+
+    const actualOffences = getRecordableOffencesForCourtCase(aho)
+
+    expect(actualOffences).toStrictEqual(recordableOffences)
+  })
+
+  it("should return recordable offences that has the same CCR", () => {
+    const aho = generateAhoFromOffenceList(offences as unknown as Offence[])
+
+    const actualOffences = getRecordableOffencesForCourtCase(aho, 1)
+
+    expect(actualOffences).toStrictEqual([
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: "001"
+        },
+        CourtCaseReferenceNumber: 1
+      },
+      {
+        CriminalProsecutionReference: {
+          OffenceReasonSequence: "003"
+        },
+        CourtCaseReferenceNumber: 1
+      }
+    ])
+  })
 })
