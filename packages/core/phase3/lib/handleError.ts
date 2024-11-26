4043de2ce088b16c2467e86a6ee6949161bd7b47
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-11-26 10:02:39 +0000

    Remove unnecessary error functions

--- packages/core/phase3/lib/handleError.ts
+++
@@ -1,7 +0,0 @@
-import type { Operation, PncUpdateDataset } from "../../types/PncUpdateDataset"
-
-const handleError = (_pncUpdateDataset: PncUpdateDataset, _operation: Operation, _error: Error) => {
-  // TODO: Implement PNCUpdateProcessor.java:94
-}
-
-export default handleError
