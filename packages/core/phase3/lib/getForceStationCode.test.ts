85464fac72e66059604e18e9cf04660ccba81a8c
joe-fol <joefolkard@protonmail.com>
2024-11-27 15:41:55 +0000

    Add test for the function getForceStationCode

---
+++ packages/core/phase3/lib/getForceStationCode.test.ts
@@ -0,0 +1,48 @@
+import { AnnotatedHearingOutcome } from "../../types/AnnotatedHearingOutcome"
+import getForceStationCode from "./getForceStationCode"
+
+const getForceOwner = (secondLevelCode: string, thirdLevelCode: string): AnnotatedHearingOutcome => {
+  const aho = {
+    Exceptions: [],
+    AnnotatedHearingOutcome: {
+      HearingOutcome: {
+        Case: {
+          ForceOwner: {
+            SecondLevelCode: secondLevelCode,
+            ThirdLevelCode: thirdLevelCode
+          }
+        }
+      }
+    }
+  } as unknown as AnnotatedHearingOutcome
+  return aho
+}
+
+describe("getForceStationCode", () => {
+  it("Should use the default third level code 'YZ' if useSpecialStationCode is set to true", () => {
+    const aho = getForceOwner("05", "")
+
+    const useSpecialStationCode = true
+    const forceStationCode = getForceStationCode(aho, useSpecialStationCode)
+
+    expect(forceStationCode).toBe("05YZ")
+  })
+
+  it("Should use the force owners third level code if useSpecialStationCode is set to false", () => {
+    const aho = getForceOwner("24", "ZD")
+
+    const useSpecialStationCode = false
+    const forceStationCode = getForceStationCode(aho, useSpecialStationCode)
+
+    expect(forceStationCode).toBe("24ZD")
+  })
+
+  it("Should return '0000' if the secondLevelCode is not present", () => {
+    const aho = getForceOwner("", "AB")
+
+    const useSpecialStationCode = false
+    const forceStationCode = getForceStationCode(aho, useSpecialStationCode)
+
+    expect(forceStationCode).toBe("0000")
+  })
+})
