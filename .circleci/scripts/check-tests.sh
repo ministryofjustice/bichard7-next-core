aa80e2782a8cd1fa615da36a5bed76f66eedab2b
Richard Race <1273965+richrace@users.noreply.github.com>
2024-11-29 15:08:43 +0000

    Add info about what the script does

--- .circleci/scripts/check-tests.sh
+++ .circleci/scripts/check-tests.sh
@@ -3,6 +3,11 @@
 # $1 = The package the tests live in
 # $2 = The glob we are looking for eg "features/**/*.feature"
 
+# We search for files that been run and failed.
+# We put the failed files in `tmp/tests.txt`.
+# When a parallel job runs and if it contains tests it will continue.
+# If the job doesn't contain any tests, it will stop the job.
+
 cd ~/project/packages/$1
 
 mkdir -p ./tmp && \
