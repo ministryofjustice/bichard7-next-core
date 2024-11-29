16a14ab4d72de4c2326f081fb0ef667b37586ee4
Richard Race <1273965+richrace@users.noreply.github.com>
2024-11-29 12:10:17 +0000

    Try to halt the CI run without any tests for cucumber e2e

---
+++ .circleci/scripts/check-e2e-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/project/packages/e2e-tests
+
+mkdir -p ./tmp && \
+>./tmp/tests.txt && \
+circleci tests glob "features/**/*.feature" | circleci tests run --command=">./tmp/tests.txt xargs echo" --split-by=timings
+
+[ -s tmp/tests.txt ] || circleci-agent step halt
