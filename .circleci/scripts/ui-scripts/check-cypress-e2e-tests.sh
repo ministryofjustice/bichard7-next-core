8e7771668974793841c6cfe5ff38eb3da2bba653
Richard Race <1273965+richrace@users.noreply.github.com>
2024-11-29 15:05:28 +0000

    Remove missed file

--- .circleci/scripts/ui-scripts/check-cypress-e2e-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/project/packages/ui
-
-mkdir -p ./tmp && \
->./tmp/tests.txt && \
-circleci tests glob "cypress/e2e/**/*.cy.ts" | circleci tests run --command=">./tmp/tests.txt xargs echo" --split-by=timings
-
-[ -s tmp/tests.txt ] || circleci-agent step halt
