ff6d53da328e5426328c440f9046e31c9678e091
Richard Race <1273965+richrace@users.noreply.github.com>
2024-12-05 10:57:24 +0000

    Fix feedback in phase banner

--- packages/ui/src/components/PhaseBanner/PhaseBanner.tsx
+++ packages/ui/src/components/PhaseBanner/PhaseBanner.tsx
@@ -1,18 +1,31 @@
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useSearchParams } from "next/navigation"
 interface PhaseBannerProps {
   phase: string
 }
 
+const generateLink = (pathname: string, searchParams: string) => {
+  switch (pathname) {
+    case "/feedback":
+      return `/feedback?${searchParams}`
+    case "/switching-feedback":
+      return `/switching-feedback?${searchParams}`
+    default:
+      return `/feedback?previousPath=${pathname}`
+  }
+}
+
 const PhaseBanner: React.FC<PhaseBannerProps> = ({ phase }: PhaseBannerProps) => {
+  const link = generateLink(usePathname(), useSearchParams().toString())
+
   return (
     <div className="govuk-phase-banner">
       <p className="govuk-phase-banner__content">
         <strong className="govuk-tag govuk-phase-banner__content__tag">{phase}</strong>
         <span className="govuk-phase-banner__text">
-          {"This is a new service – your "}
+          {"This is a new service - your "}
           {/* TODO: Get /bichard from config */}
-          <Link href={`/feedback?previousPath=${usePathname()}`} className="govuk-link">
+          <Link href={link} className="govuk-link">
             {"feedback"}
           </Link>
           {" will help us to improve it."}
