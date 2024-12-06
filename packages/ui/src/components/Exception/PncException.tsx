cc4f8be2bceff50c152ae6d2e1214ec68e5334fe
BushraAbdullahi <bushra1175@gmail.com>
2024-12-06 14:01:30 +0000

    refactor message rendering

--- packages/ui/src/components/Exception/PncException.tsx
+++ packages/ui/src/components/Exception/PncException.tsx
@@ -10,36 +10,16 @@ type Props = {
   message?: string
 }
 
-const pncExceptionDescriptions: Record<string, Record<string, string>> = {
-  [ExceptionCode.HO100402]: {
-    "I1008 - GWAY - ENQUIRY ERROR INVALID ADJUDICATION": "Check the offence matching",
-    "I1008 - GWAY - ENQUIRY ERROR MORE THAN 3 DISPOSAL GROUPS": "Enquiry error more than 3 disposal groups",
-    "I1008 - GWAY - ENQUIRY ERROR NO SUITABLE DISPOSAL GROUPS":
-      "Create DH page on PNC, then Submit the case on Bichard 7",
-    "I1008 - GWAY - ENQUIRY ERROR RECORD CORRUPTION: INCORRECT CHARGE COUNT ON COURT CASE":
-      "Check PNC record and re-submit",
-    "I1008 - GWAY - ENQUIRY ERROR TOO MANY DISPOSALS": "Check PNC record and re-submit",
-    "I1036 - Error encountered processing enquiry": "Re-submit case to the PNC"
-  },
-  [ExceptionCode.HO100404]: {
-    "Unexpected PNC communication error": "Re-submit case to the PNC"
-  }
-}
-
-const getPncExceptionDescription = (code: ExceptionCode, message?: string) => {
-  const descriptions = pncExceptionDescriptions[code]
-
-  if (descriptions) {
-    return Object.entries(descriptions).find(([descriptionKey]) => message?.includes(descriptionKey))?.[1]
-  }
-
-  return message
-}
-
 const PncException = ({ code, message }: Props) => {
-  const isPncQueryExceptionCode = [ExceptionCode.HO100302, ExceptionCode.HO100314].includes(code)
-  const description = getPncExceptionDescription(code, message)
-
+  const isPncQueryExceptionCode = [
+    ExceptionCode.HO100301,
+    ExceptionCode.HO100302,
+    ExceptionCode.HO100314,
+    ExceptionCode.HO100401,
+    ExceptionCode.HO100403,
+    ExceptionCode.HO100402,
+    ExceptionCode.HO100404
+  ].includes(code)
   return (
     <ExceptionRow className={`moj-exception-row`}>
       <GridRow className="exception-row exception-row__header">
@@ -57,11 +37,11 @@ const PncException = ({ code, message }: Props) => {
 
       <GridRow className="exception-row exception-row__help">
         <GridCol>
-          {description && (
+          {message && (
             <Accordion id={`exception-${code.toLocaleLowerCase()}`} heading="PNC error message">
               <InsetText className="b7-inset-text">
                 <InsetTextHeading className="b7-inset-text__heading">{"PNC error message"}</InsetTextHeading>
-                <span className="b7-inset-text__content">{description}</span>
+                <span className="b7-inset-text__content">{message}</span>
               </InsetText>
             </Accordion>
           )}
