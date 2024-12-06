b3cf007d1a08e62fc78977b58db61de2367b0331
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-12-06 16:41:04 +0000

    Add generateBasePncUpdateRequest function

---
+++ packages/core/phase3/lib/generateBasePncUpdateRequest.ts
@@ -0,0 +1,16 @@
+import type { PncUpdateDataset } from "../../types/PncUpdateDataset"
+
+import getForceStationCode from "./getForceStationCode"
+import getPncCheckname from "./getPncCheckname"
+import preProcessPncIdentifier from "./preProcessPncIdentifier"
+
+const generateBasePncUpdateRequest = (pncUpdateDataset: PncUpdateDataset) => ({
+  croNumber: pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.CRONumber ?? null,
+  forceStationCode: getForceStationCode(pncUpdateDataset, true),
+  pncCheckName: getPncCheckname(pncUpdateDataset),
+  pncIdentifier: preProcessPncIdentifier(
+    pncUpdateDataset.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.PNCIdentifier
+  )
+})
+
+export default generateBasePncUpdateRequest
