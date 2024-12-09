4073d3d571ab29344231cd86b92e9109c783685f
BushraAbdullahi <bushra1175@gmail.com>
2024-12-09 15:03:24 +0000

    write test for PNC exception error messages, covering all possible error codes

--- packages/ui/cypress/e2e/case-details/view-case-details/view-pnc-error-message.cy.ts
+++ packages/ui/cypress/e2e/case-details/view-case-details/view-pnc-error-message.cy.ts
@@ -2,67 +2,88 @@ import ExceptionCode from "@moj-bichard7-developers/bichard7-next-data/dist/type
 import { loginAndVisit } from "../../../support/helpers"
 
 describe("View PNC Error Message", () => {
-  const exceptionCodeMap: { [key in ExceptionCode]?: string } = {
-    [ExceptionCode.HO100301]: "I1008 - GWAY - ENQUIRY ERROR ARREST/SUMMONS REF (11/01ZD/01/410843C) NOT FOUND",
-    [ExceptionCode.HO100302]: "I1008 - PNCAM - CONNECTION FAILURE AFTER 3 ATTEMPTS.",
-    [ExceptionCode.HO100313]: "I0208 - PNC Query returned a business error",
-    [ExceptionCode.HO100314]: "I0007 - PNC Query returned a system fault",
-    [ExceptionCode.HO100315]: "I5001 - PNC Query returned a warning",
-    [ExceptionCode.HO100401]: "I0034 - PNC Query returned a business error",
-    [ExceptionCode.HO100402]:
-      "I0001 - THE FOLLOWING ELEMENT(S) IN THE DIS SEGMENT CONTAIN INVALID DATA: DISPOSAL TYPE , DISPOSAL QUANTITY",
-    [ExceptionCode.HO100403]:
-      "I5005 - WARNING - REMAND INFORMATION INDICATES THAT THE SUBJECT WAS ON BAIL AT THE TIME OFFENCE 000 ENTERED FOR ARREST/SUMMONS 24/0000/00/0000000G ON RECORD PNCID/CHECKNAME: 24/000000M XXXXXXX WAS COMMITTED - CHECK DETAILS AND AMEND RECORD AS NECESSARY",
-    [ExceptionCode.HO100404]: "PNCUE - PNCUE PNC update communication failed"
+  const exceptionCodeMap: { [key in ExceptionCode]?: string[] } = {
+    [ExceptionCode.HO100301]: ["I0013", "I0022"],
+    [ExceptionCode.HO100302]: ["PNCAM", "PNCUE"],
+    [ExceptionCode.HO100313]: ["I0208", "I0209", "I0212", "I0256"],
+    [ExceptionCode.HO100314]: [
+      "I0007",
+      "I0008",
+      "I0014",
+      "I0015",
+      "I0021",
+      "I0023",
+      "I0031",
+      "I0036",
+      "I1001",
+      "I1041",
+      "I6001",
+      "I6002"
+    ],
+    [ExceptionCode.HO100315]: ["I5001", "I5999"],
+    [ExceptionCode.HO100401]: [
+      "I0034",
+      "I0035",
+      "I0036",
+      "I0007",
+      "I0015",
+      "I0023",
+      "I0014",
+      "I0008",
+      "I0021",
+      "I0031",
+      "I0032",
+      "I0033",
+      "I0013",
+      "I0022",
+      "I0208",
+      "I0209",
+      "I0212",
+      "I0256"
+    ],
+    [ExceptionCode.HO100402]: [
+      "I0001",
+      "I0002",
+      "I0003",
+      "I0004",
+      "I0005",
+      "I0017",
+      "I0024",
+      "I0030",
+      ...Array.from({ length: 41 }, (_, i) => `I10${String(i + 1).padStart(2, "0")}`)
+    ],
+    [ExceptionCode.HO100403]: [...Array.from({ length: 999 }, (_, i) => `I5${String(i + 1).padStart(3, "0")}`)],
+    [ExceptionCode.HO100404]: ["I6001", "I6002", "PNCAM", "PNCUE"]
   }
-
-  before(() => {
-    loginAndVisit("Supervisor")
-  })
 
   beforeEach(() => {
     cy.task("clearCourtCases")
+    loginAndVisit("Supervisor")
   })
 
-  Object.entries(exceptionCodeMap).forEach(([exceptionCode, expectedMessage]) => {
-    it(`displays the PNC error message for ${exceptionCode} in the dropdown on the Exceptions tab`, () => {
-      cy.task("insertCourtCaseWithPncException", {
-        exceptions: {
-          pncExceptionCode: exceptionCode as ExceptionCode,
-          pncExceptionMessage: expectedMessage
-        },
-        case: {
-          errorLockedByUsername: null,
-          triggerLockedByUsername: null,
-          orgForPoliceFilter: "01"
-        }
+  Object.entries(exceptionCodeMap).forEach(([exceptionCode, messages]) => {
+    if (Array.isArray(messages)) {
+      messages.forEach((expectedCode) => {
+        it(`displays a PNC error message containing "${expectedCode}" for ${exceptionCode} in the dropdown on the Exceptions tab`, () => {
+          cy.task("insertCourtCaseWithPncException", {
+            exceptions: {
+              pncExceptionCode: exceptionCode,
+              pncExceptionMessage: `${expectedCode} - TEST ERROR MESSAGE`
+            },
+            case: {
+              errorLockedByUsername: null,
+              triggerLockedByUsername: null,
+              orgForPoliceFilter: "01"
+            }
+          })
+
+          cy.visit("/bichard/court-cases/0")
+          cy.get("#exceptions-tab").click()
+          cy.contains("PNC error message").click()
+
+          cy.contains(expectedCode).should("be.visible")
+        })
       })
-      cy.contains("NAME Defendant").should("be.visible").click()
-
-      cy.get("#exceptions-tab").click()
-      cy.contains("PNC error message").click()
-
-      cy.contains(expectedMessage).should("be.visible")
-    })
+    }
   })
 })
-//   it(`displays the PNC error message for Exception Code HO100301 in the dropdown on the Exceptions tab`, () => {
-//     cy.task("insertCourtCaseWithPncException", {
-//       exceptions: {
-//         pncExceptionCode: ExceptionCode.HO100301,
-//         pncExceptionMessage: exceptionCodeMap[ExceptionCode.HO100301]
-//       },
-//       case: {
-//         errorLockedByUsername: null,
-//         triggerLockedByUsername: null,
-//         orgForPoliceFilter: "01"
-//       }
-//     })
-//     cy.contains("NAME Defendant").should("be.visible").click()
-
-//     cy.get("#exceptions-tab").click()
-//     cy.contains("PNC error message").click()
-
-//     cy.contains(exceptionCodeMap[ExceptionCode.HO100301] as string).should("be.visible")
-//   })
-// })
