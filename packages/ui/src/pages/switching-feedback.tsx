f4eb0106de86bfe820a84729232e77ebbe04efda
Sen <65850511+Senny10@users.noreply.github.com>
2024-12-10 14:56:19 +0000

    Update email body in SwitchingFeedbackPage to request additional details and screenshots

--- packages/ui/src/pages/switching-feedback.tsx
+++ packages/ui/src/pages/switching-feedback.tsx
@@ -155,7 +155,7 @@ const SwitchingFeedbackPage: NextPage<Props> = ({ user, previousPath, csrfToken 
 
   const emailSubject = "Feedback: <Subject here>"
   const emailBody =
-    "Include Username and Force. Please describe the issues you have experienced - the more detail the better"
+    "Please describe the issues you have experienced, including Username and Force - the more detail the better. If you have any screenshots, please attach them to the email."
   const emailHref = `mailto:moj-bichard7@madetech.com?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(emailBody)}`
 
   return (
