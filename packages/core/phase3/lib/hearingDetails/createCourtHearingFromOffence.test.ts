9737eb0ea2d0448bfc694050875edc3a7c4d41e4
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-12-16 14:09:04 +0000

    Add tests for createCourtHearingFromOffence

---
+++ packages/core/phase3/lib/hearingDetails/createCourtHearingFromOffence.test.ts
@@ -0,0 +1,23 @@
+import type { Offence } from "../../../types/AnnotatedHearingOutcome"
+
+import { createCourtHearingFromOffence } from "./createCourtHearingFromOffence"
+
+describe("createCourtHearingFromOffence", () => {
+  it("creates a court hearing from an offence", () => {
+    const courtHearing = createCourtHearingFromOffence({
+      CriminalProsecutionReference: {
+        OffenceReason: {
+          __type: "LocalOffenceReason",
+          LocalOffenceCode: { OffenceCode: "11412HSD", AreaCode: "56" }
+        },
+        OffenceReasonSequence: "1"
+      }
+    } as Offence)
+
+    expect(courtHearing).toStrictEqual({
+      courtOffenceSequenceNumber: "001",
+      offenceReason: "11412HSD",
+      type: "ORDINARY"
+    })
+  })
+})
