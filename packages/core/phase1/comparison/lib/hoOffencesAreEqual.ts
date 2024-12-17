25773b2f3b8024e649cd37aa9bca34aff396b6d8
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-12-17 10:04:00 +0000

    Remove unused hoOffencesAreEqual function

--- packages/core/phase1/comparison/lib/hoOffencesAreEqual.ts
+++
@@ -1,9 +0,0 @@
-import type { Offence } from "../../../types/AnnotatedHearingOutcome"
-
-import getOffenceCode from "../../../lib/getOffenceCode"
-import offencesHaveEqualResults from "../../enrichAho/enrichFunctions/matchOffencesToPnc/offencesHaveEqualResults"
-
-const hoOffencesAreEqual = (offence1: Offence, offence2: Offence): boolean =>
-  getOffenceCode(offence1) === getOffenceCode(offence2) && offencesHaveEqualResults([offence1, offence2])
-
-export default hoOffencesAreEqual
