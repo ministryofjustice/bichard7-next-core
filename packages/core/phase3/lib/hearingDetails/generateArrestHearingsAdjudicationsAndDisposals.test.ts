d36184ce88c4d7c4c400510d66d46f9488d41b4e
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-12-18 17:04:03 +0000

    Add tests for hearing details functions (#1202)
    * Add tests for createArrestHearingFromOffence
    
    * Add tests for generateHearingsAndDisposals
    
    * Add tests for generateHearingsAdjudicationsAndDisposals
    
    * Add tests for generateArrestHearingsAdjudicationsAndDisposals
    
    * Sort NON_DATE_DISPOSALS in createPncAdjudication
    
    * Improve test in createAdjudicationFromOffence
    
    So we're checking all the fields for the adjudication.
    
    * Remove duplicated code in createPncDisposalFromOffence

---
+++ packages/core/phase3/lib/hearingDetails/generateArrestHearingsAdjudicationsAndDisposals.test.ts
@@ -0,0 +1,128 @@
+import type { Offence } from "../../../types/AnnotatedHearingOutcome"
+import type { PncUpdateDataset } from "../../../types/PncUpdateDataset"
+
+import ResultClass from "../../../types/ResultClass"
+import { generateArrestHearingsAdjudicationsAndDisposals } from "./generateArrestHearingsAdjudicationsAndDisposals"
+
+describe("generateArrestHearingsAdjudicationsAndDisposals", () => {
+  const courtCaseReference = "123"
+  const result = {
+    CJSresultCode: 1100,
+    DateSpecifiedInResult: [{ Date: new Date("2024-12-17"), Sequence: 1 }],
+    ResultQualifierVariable: [{ Code: "C" }],
+    ResultVariableText: "THE DEFENDANT IS NOT TO ENTER PLACE",
+    AmountSpecifiedInResult: [{ Amount: 120000.99 }],
+    Duration: [
+      { DurationUnit: "D", DurationLength: 123 },
+      { DurationUnit: "A", DurationLength: 999 }
+    ],
+    ResultClass: ResultClass.JUDGEMENT_WITH_FINAL_RESULT,
+    PNCDisposalType: 2060,
+    NumberOfOffencesTIC: 1
+  }
+  const recordableOffence = {
+    AddedByTheCourt: true,
+    ActualOffenceStartDate: { StartDate: new Date("2024-12-10") },
+    ActualOffenceEndDate: { EndDate: new Date("2024-12-16") },
+    CommittedOnBail: "Y",
+    CriminalProsecutionReference: {
+      OffenceReason: {
+        __type: "LocalOffenceReason",
+        LocalOffenceCode: { OffenceCode: "11412HSD", AreaCode: "56" }
+      },
+      OffenceReasonSequence: "1"
+    },
+    LocationOfOffence: "Some High Street",
+    OffenceTime: "09:00",
+    OffenceEndTime: "15:00",
+    Result: [result, result]
+  } as Offence
+  const recordableOffenceNotAddedByTheCourt = { ...recordableOffence, AddedByTheCourt: false }
+
+  it("returns an empty list when only recordable offences not added by the court", () => {
+    const pncUpdateDataset = {
+      AnnotatedHearingOutcome: {
+        HearingOutcome: { Case: { HearingDefendant: { Offence: [recordableOffenceNotAddedByTheCourt] } } }
+      }
+    } as PncUpdateDataset
+
+    const arrestHearingsAndDisposals = generateArrestHearingsAdjudicationsAndDisposals(
+      pncUpdateDataset,
+      courtCaseReference
+    )
+
+    expect(arrestHearingsAndDisposals).toHaveLength(0)
+  })
+
+  it("returns an empty list when only recordable offences incompatible with disposal", () => {
+    const incompatibleRecordableOffence = {
+      ...recordableOffence,
+      Result: [{ ...result, ResultClass: ResultClass.SENTENCE }]
+    }
+    const pncUpdateDataset = {
+      AnnotatedHearingOutcome: {
+        HearingOutcome: { Case: { HearingDefendant: { Offence: [incompatibleRecordableOffence] } } }
+      }
+    } as PncUpdateDataset
+
+    const arrestHearingsAndDisposals = generateArrestHearingsAdjudicationsAndDisposals(
+      pncUpdateDataset,
+      courtCaseReference
+    )
+
+    expect(arrestHearingsAndDisposals).toHaveLength(0)
+  })
+
+  it("returns a hearing, adjudication and disposals for each compatible recordable offence added by the court", () => {
+    const pncUpdateDataset = {
+      AnnotatedHearingOutcome: {
+        HearingOutcome: {
+          Case: { HearingDefendant: { Offence: [recordableOffence, recordableOffenceNotAddedByTheCourt] } },
+          Hearing: { DateOfHearing: new Date("2024-12-25") }
+        }
+      }
+    } as PncUpdateDataset
+
+    const arrestHearingsAndDisposals = generateArrestHearingsAdjudicationsAndDisposals(
+      pncUpdateDataset,
+      courtCaseReference
+    )
+
+    expect(arrestHearingsAndDisposals).toStrictEqual([
+      {
+        committedOnBail: "Y",
+        courtOffenceSequenceNumber: "001",
+        locationOfOffence: "Some High Street",
+        offenceEndDate: "16122024",
+        offenceEndTime: "1500",
+        offenceLocationFSCode: "0000",
+        offenceReason: "11412HSD",
+        offenceReasonSequence: "001",
+        offenceStartDate: "10122024",
+        offenceStartTime: "0900",
+        type: "ARREST"
+      },
+      {
+        hearingDate: "25122024",
+        numberOffencesTakenIntoAccount: "0002",
+        pleaStatus: "",
+        type: "ADJUDICATION",
+        verdict: "NON-CONVICTION"
+      },
+      {
+        disposalQualifiers: "C ",
+        disposalQuantity: "D123171220240120000.9900",
+        disposalText: "EXCLUDED FROM PLACE",
+        disposalType: "2063",
+        type: "DISPOSAL"
+      },
+      {
+        disposalQualifiers: "C ",
+        disposalQuantity: "D123171220240120000.9900",
+        disposalText: "EXCLUDED FROM PLACE",
+        disposalType: "2063",
+        type: "DISPOSAL"
+      }
+    ])
+  })
+})
