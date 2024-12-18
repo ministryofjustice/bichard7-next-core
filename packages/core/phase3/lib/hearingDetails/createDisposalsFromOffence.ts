369d9ac200eb566e971c02f4d52e12e09c6b448b
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-12-18 16:33:09 +0000

    Remove duplicated code in createPncDisposalFromOffence

--- packages/core/phase3/lib/hearingDetails/createDisposalsFromOffence.ts
+++ packages/core/phase3/lib/hearingDetails/createDisposalsFromOffence.ts
@@ -4,36 +4,9 @@ import type { Disposal } from "../../types/HearingDetails"
 
 import { createPncDisposalsFromResult } from "../../../phase2/lib/createPncDisposalsFromResult"
 import createPncDisposal from "../../../phase2/lib/createPncDisposalsFromResult/createPncDisposal"
-import findPncCourtCase from "../../../phase2/lib/findPncCourtCase"
 import isRecordableResult from "../../../phase2/lib/isRecordableResult"
 import { HearingDetailsType } from "../../types/HearingDetails"
-
-const ADJOURNED_SINE_DIE_DISPOSAL_CODE = 2007
-const getConvictionDateFromPncAdjudicationIfOffenceIsAdjournedSineDie = (
-  aho: AnnotatedHearingOutcome,
-  offence: Offence
-) => {
-  if (
-    !offence.CriminalProsecutionReference.OffenceReasonSequence ||
-    !aho.PncQuery ||
-    aho.AnnotatedHearingOutcome.HearingOutcome.Case.PenaltyNoticeCaseReferenceNumber
-  ) {
-    return undefined
-  }
-
-  const courtCase = findPncCourtCase(aho, offence)
-  const matchingOffence = courtCase?.offences.find(
-    (pncOffence) =>
-      pncOffence.offence.sequenceNumber === Number(offence.CriminalProsecutionReference.OffenceReasonSequence)
-  )
-
-  const areAllDisposals2007 =
-    matchingOffence?.disposals &&
-    matchingOffence.disposals.length > 0 &&
-    matchingOffence.disposals.every((disposal) => disposal.type === ADJOURNED_SINE_DIE_DISPOSAL_CODE)
-
-  return areAllDisposals2007 ? matchingOffence.adjudication?.sentenceDate : undefined
-}
+import getConvictionDateFromPncAdjudicationIfOffenceIsAdjournedSineDie from "../getConvictionDateFromPncAdjudicationIfOffenceIsAdjournedSineDie"
 
 const createPncDisposalFromOffence = (aho: AnnotatedHearingOutcome, offence: Offence): PncDisposal[] => {
   const results = offence.Result
