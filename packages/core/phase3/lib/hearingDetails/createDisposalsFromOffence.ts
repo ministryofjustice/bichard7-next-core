6bd204618d24f134045fdffe39b6e17de7cac9b5
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-12-19 15:59:51 +0000

    Rename boolean variables

--- packages/core/phase3/lib/hearingDetails/createDisposalsFromOffence.ts
+++ packages/core/phase3/lib/hearingDetails/createDisposalsFromOffence.ts
@@ -10,47 +10,47 @@ import getConvictionDateFromPncAdjudicationIfOffenceIsAdjournedSineDie from "../
 
 const createPncDisposalFromOffence = (aho: AnnotatedHearingOutcome, offence: Offence): PncDisposal[] => {
   const results = offence.Result
-  const adjournmentExists = results.some((result) => result.ResultClass?.includes("Adjournment"))
-  const found2050Result = results.some((result) => result.PNCDisposalType === 2050)
+  const hasAdjournmentResult = results.some((result) => result.ResultClass?.includes("Adjournment"))
+  const has2050Result = results.some((result) => result.PNCDisposalType === 2050)
 
   let pncDisposals: PncDisposal[] = []
   let disposalFor2060Result: null | PncDisposal[] = null
-  let found3027 = false
-  let found2063Result = false
-  let converted2060Result = false
+  let has3027Disposal = false
+  let has2063Result = false
+  let hasConverted2060Result = false
 
   const recordableResults = results.filter(isRecordableResult).sort((a, b) => a.CJSresultCode - b.CJSresultCode)
 
   for (const recordableResult of recordableResults) {
     const disposalCode = recordableResult.PNCDisposalType
     const resultCode = recordableResult.CJSresultCode
-    let ignore2063Disposal = false
-    found3027 ||= disposalCode === 3027
+    let shouldIgnore2063Disposal = false
+    has3027Disposal ||= disposalCode === 3027
     const pncDisposalsFromResult = createPncDisposalsFromResult(recordableResult)
 
     if (disposalCode === 2060 && disposalFor2060Result == null) {
       disposalFor2060Result = pncDisposalsFromResult
     } else if (disposalCode === 2063) {
       if (resultCode === 2060) {
-        converted2060Result = true
+        hasConverted2060Result = true
         disposalFor2060Result = pncDisposalsFromResult
       } else {
-        found2063Result = true
+        has2063Result = true
       }
 
-      ignore2063Disposal = converted2060Result && found2063Result
+      shouldIgnore2063Disposal = hasConverted2060Result && has2063Result
     }
 
-    if ((disposalCode !== 3052 || !adjournmentExists) && !ignore2063Disposal) {
+    if ((disposalCode !== 3052 || !hasAdjournmentResult) && !shouldIgnore2063Disposal) {
       pncDisposals.push(...pncDisposalsFromResult)
     }
   }
 
-  if (disposalFor2060Result && (found2050Result || found2063Result) && pncDisposals.length == 2) {
+  if (disposalFor2060Result && (has2050Result || has2063Result) && pncDisposals.length == 2) {
     pncDisposals = disposalFor2060Result
   }
 
-  if (found3027 || adjournmentExists) {
+  if (has3027Disposal || hasAdjournmentResult) {
     return pncDisposals
   }
 
