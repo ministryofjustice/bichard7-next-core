cea242122e86acb8fe60ada29c99007cc0f25abe
Richard Race <1273965+richrace@users.noreply.github.com>
2024-12-19 11:55:34 +0000

    Remove SonarCloud issues

--- packages/api/src/endpoints/versionedEndpoints.ts
+++ packages/api/src/endpoints/versionedEndpoints.ts
@@ -1,10 +1,13 @@
-/* eslint-disable @typescript-eslint/prefer-literal-enum-member */
 import { Endpoints } from "./endpoints"
 import { Versions } from "./versions"
 
-export const enum VersionedEndpoints {
-  V1CaseResubmit = Versions.V1 + Endpoints.CaseResubmit,
-  V1Health = Versions.V1 + Endpoints.Health,
-  V1Me = Versions.V1 + Endpoints.Me,
-  V2Me = Versions.V2 + Endpoints.Me
+// We can't use enum as we get `Explicit enum value must only be a literal value (string, number, boolean, etc)`.
+// We can turn off the eslint rule locally, but we still get the issue on SonarCloud.
+// That's why we use a hash/record over enum which would be our preferred solution.
+
+export const VersionedEndpoints: Record<string, string> = {
+  V1CaseResubmit: Versions.V1 + Endpoints.CaseResubmit,
+  V1Health: Versions.V1 + Endpoints.Health,
+  V1Me: Versions.V1 + Endpoints.Me,
+  V2Me: Versions.V2 + Endpoints.Me
 }
