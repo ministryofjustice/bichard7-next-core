6e369cf741d4388daaee678f393445db37f025cf
Richard Race <1273965+richrace@users.noreply.github.com>
2024-12-19 12:22:21 +0000

    Comment is no longer valid

--- packages/api/src/endpoints/versionedEndpoints.ts
+++ packages/api/src/endpoints/versionedEndpoints.ts
@@ -1,10 +1,6 @@
 import { Endpoints } from "./endpoints"
 import { Versions } from "./versions"
 
-// We can't use enum as we get `Explicit enum value must only be a literal value (string, number, boolean, etc)`.
-// We can turn off the eslint rule locally, but we still get the issue on SonarCloud.
-// That's why we use a hash/record over enum which would be our preferred solution.
-
 export const VersionedEndpoints: Record<string, Record<string, string>> = {
   V1: {
     CaseResubmit: Versions.V1 + Endpoints.CaseResubmit,
