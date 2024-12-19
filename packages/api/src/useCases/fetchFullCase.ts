d400419755f81e8d26170d6d07045d6ae507fb2b
Tausif Patel <tausifpatel.dev@gmail.com>
2024-12-19 16:53:11 +0000

    add unit tests for fetchFullCase

--- packages/api/src/useCases/fetchFullCase.ts
+++ packages/api/src/useCases/fetchFullCase.ts
@@ -7,6 +7,10 @@ import formatForceNumbers from "../services/formatForceNumbers"
 const fetchFullCase = async (user: User, db: DataStoreGateway, caseId: number) => {
   const forceIds = formatForceNumbers(user.visible_forces)
 
+  if (forceIds.length === 0) {
+    throw new Error("No force associated to User")
+  }
+
   return await db.fetchFullCase(caseId, forceIds)
 }
 
---
+++ packages/api/src/useCases/fetchFullCase.unit.test.ts
@@ -0,0 +1,39 @@
+import type { User } from "@moj-bichard7/common/types/User"
+
+import FakeDataStore from "../services/gateways/dataStoreGateways/fakeDataStore"
+import fetchFullCase from "./fetchFullCase"
+
+describe("fetchFullCase", () => {
+  const db = new FakeDataStore()
+
+  it("returns a case", async () => {
+    const user = { visible_forces: "001" } as User
+    const result = await fetchFullCase(user, db, 0)
+
+    expect(result).toEqual({
+      annotated_msg: "",
+      court_reference: "",
+      create_ts: "2022-06-30T08:44:03.930Z",
+      error_count: 1,
+      error_id: 0,
+      error_locked_by_id: null,
+      error_report: "",
+      error_status: 1,
+      is_urgent: 0,
+      message_id: "",
+      msg_received_ts: "2022-06-30T08:44:03.930Z",
+      org_for_police_filter: "",
+      phase: 1,
+      resolution_ts: null,
+      total_pnc_failure_resubmissions: 0,
+      trigger_count: 0,
+      user_updated_flag: 0
+    })
+  })
+
+  it("returns error when no force associated to a user", async () => {
+    const user = { visible_forces: "" } as User
+
+    await expect(fetchFullCase(user, db, 0)).rejects.toThrow("No force associated to User")
+  })
+})
