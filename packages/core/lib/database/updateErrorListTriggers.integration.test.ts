6def2a4de2ad969f99b04eca2a35939851f95699
Emad Karamad <emad.karamad@madetech.com>
2025-01-07 13:56:42 +0000

    Fix issue with duplicate triggers with offence sequence number 0 (#1224)

--- packages/core/lib/database/updateErrorListTriggers.integration.test.ts
+++ packages/core/lib/database/updateErrorListTriggers.integration.test.ts
@@ -62,4 +62,67 @@ describe("updateErrorListTriggers", () => {
     expect(updateResults.deleted).toStrictEqual([originalTriggers[1], newTrigger])
     expect(updateResults.added).toHaveLength(0)
   })
+
+  it.each([0, undefined])(
+    "should not add duplicate trigger when offence sequence number is %s",
+    async (sequenceNumber) => {
+      const originalTriggers = [{ code: TriggerCode.TRPR0003, offenceSequenceNumber: sequenceNumber }]
+      let phase1Result = generateMockPhase1Result({
+        triggers: [originalTriggers[0]]
+      })
+      const recordId = await insertErrorListRecord(db, phase1Result)
+      await insertErrorListTriggers(db, recordId, phase1Result.triggers)
+      await db`
+      UPDATE br7own.error_list_triggers SET status = 2 WHERE error_id = ${recordId}`
+      let dbTriggers = await db<ErrorListNoteRecord[]>`
+      SELECT * FROM br7own.error_list_triggers WHERE error_id = ${recordId}`
+
+      expect(dbTriggers).toHaveLength(1)
+
+      const newTrigger = { code: TriggerCode.TRPR0003, offenceSequenceNumber: sequenceNumber }
+      phase1Result = generateMockPhase1Result({
+        triggers: [newTrigger]
+      })
+      const updateResults = await updateErrorListTriggers(db, recordId, phase1Result)
+      dbTriggers = await db<ErrorListNoteRecord[]>`
+      SELECT * FROM br7own.error_list_triggers WHERE error_id = ${recordId}`
+
+      expect(dbTriggers).toHaveLength(1)
+      expect(updateResults.deleted).toHaveLength(0)
+      expect(updateResults.added).toHaveLength(0)
+    }
+  )
+
+  it.each([
+    { existingSequenceNumber: 0, newSequenceNumber: undefined },
+    { existingSequenceNumber: undefined, newSequenceNumber: 0 }
+  ])(
+    "should add the trigger when existing trigger's offence sequence number is $existingSequenceNumber but the new trigger's offence sequence number is $newSequenceNumber",
+    async ({ existingSequenceNumber, newSequenceNumber }) => {
+      const originalTriggers = [{ code: TriggerCode.TRPR0003, offenceSequenceNumber: existingSequenceNumber }]
+      let phase1Result = generateMockPhase1Result({
+        triggers: [originalTriggers[0]]
+      })
+      const recordId = await insertErrorListRecord(db, phase1Result)
+      await insertErrorListTriggers(db, recordId, phase1Result.triggers)
+      await db`
+      UPDATE br7own.error_list_triggers SET status = 2 WHERE error_id = ${recordId}`
+      let dbTriggers = await db<ErrorListNoteRecord[]>`
+      SELECT * FROM br7own.error_list_triggers WHERE error_id = ${recordId}`
+
+      expect(dbTriggers).toHaveLength(1)
+
+      const newTrigger = { code: TriggerCode.TRPR0003, offenceSequenceNumber: newSequenceNumber }
+      phase1Result = generateMockPhase1Result({
+        triggers: [newTrigger]
+      })
+      const updateResults = await updateErrorListTriggers(db, recordId, phase1Result)
+      dbTriggers = await db<ErrorListNoteRecord[]>`
+      SELECT * FROM br7own.error_list_triggers WHERE error_id = ${recordId}`
+
+      expect(dbTriggers).toHaveLength(2)
+      expect(updateResults.deleted).toHaveLength(0)
+      expect(updateResults.added).toHaveLength(1)
+    }
+  )
 })
