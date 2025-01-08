a77e0b5c05210d9a0d88e661235026e554b2ab54
Richard Race <1273965+richrace@users.noreply.github.com>
2025-01-08 13:03:12 +0000

    Fix SonarCloud issue

--- packages/api/src/services/parseHearingOutcome.ts
+++ packages/api/src/services/parseHearingOutcome.ts
@@ -5,7 +5,7 @@ import { isError } from "@moj-bichard7/common/types/Result"
 import parseAhoXml from "@moj-bichard7/core/lib/parse/parseAhoXml/parseAhoXml"
 import parseAnnotatedPncUpdateDatasetXml from "@moj-bichard7/core/phase2/parse/parseAnnotatedPncUpdateDatasetXml/parseAnnotatedPncUpdateDatasetXml"
 
-const isPncUpdateDatasetFromXml = (message: string) => message.match(/<AnnotatedPNCUpdateDataset/)
+const isPncUpdateDatasetFromXml = (message: string) => /<AnnotatedPNCUpdateDataset/.exec(message)
 
 const parseHearingOutcome = (hearingOutcome: string, logger?: FastifyBaseLogger): AnnotatedHearingOutcome | Error => {
   let aho: AnnotatedHearingOutcome | Error
