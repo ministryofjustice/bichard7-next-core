4e2bbd16e3bdac13d197403201e182882811e6d0
Tausif Patel <tausifpatel.dev@gmail.com>
2025-01-13 12:58:12 +0000

    Add an e2e test for fetchCaseData end point
    Co-Authored-By: Richard Race <richard.race@madetech.com>

--- packages/api/src/routes/v1/cases/case.e2e.test.ts
+++ packages/api/src/routes/v1/cases/case.e2e.test.ts
@@ -5,6 +5,7 @@ import type { FastifyInstance } from "fastify"
 import { OK } from "http-status"
 
 import { VersionedEndpoints } from "../../../endpoints/versionedEndpoints"
+import { testAhoJsonStr, testAhoXml } from "../../../tests/helpers/ahoHelper"
 import { createCase } from "../../../tests/helpers/caseHelper"
 import { SetupAppEnd2EndHelper } from "../../../tests/helpers/setupAppEnd2EndHelper"
 import { createUserAndJwtToken, createUsers } from "../../../tests/helpers/userHelper"
@@ -26,6 +27,67 @@ describe("/v1/case e2e", () => {
   afterAll(async () => {
     await app.close()
     await helper.db.close()
+  })
+
+  it("returns case data", async () => {
+    const [encodedJwt] = await createUserAndJwtToken(helper.db)
+    const dummyCase = {
+      annotated_msg: testAhoXml,
+      asn: "1901ID0100000006148H",
+      court_code: "ABC",
+      court_date: new Date(),
+      court_name: "Kingston Crown Court",
+      court_reference: "ABC",
+      create_ts: new Date(2025, 1, 1),
+      defendant_name: "Defendant",
+      error_count: 0,
+      error_id: 0,
+      error_locked_by_id: "user1",
+      error_report: "HO100304||br7:ArrestSummonsNumber",
+      error_status: 1,
+      is_urgent: 1,
+      message_id: "ABC-DEF",
+      msg_received_ts: new Date(2025, 1, 1),
+      org_for_police_filter: "01",
+      phase: 1,
+      ptiurn: "00112233",
+      resolution_ts: new Date(2025, 1, 2),
+      trigger_count: 1,
+      trigger_locked_by_id: "user2",
+      trigger_status: 1,
+      updated_msg: testAhoXml
+    }
+
+    const testCase = await createCase(helper.db, dummyCase)
+
+    const response = await fetch(`${helper.address}${endpoint.replace(":caseId", testCase.error_id.toString())}`, {
+      headers: {
+        Authorization: `Bearer ${encodedJwt}`
+      },
+      method: "GET"
+    })
+
+    expect(response.status).toBe(OK)
+    const responseJson = (await response.json()) satisfies CaseDto
+    expect(responseJson.aho).toEqual(testAhoJsonStr)
+    expect(responseJson.asn).toBe(testCase.asn)
+    expect(responseJson.courtCode).toBe(testCase.court_code)
+    expect(responseJson.courtDate).toBe(testCase.court_date?.toISOString())
+    expect(responseJson.courtReference).toBe(testCase.court_reference)
+    expect(responseJson.defendantName).toBe(testCase.defendant_name)
+    expect(responseJson.errorId).toBe(testCase.error_id)
+    expect(responseJson.errorLockedByUsername).toBe(testCase.error_locked_by_id)
+    expect(responseJson.errorReport).toBe(testCase.error_report)
+    expect(responseJson.errorStatus).toBe("Unresolved")
+    expect(responseJson.isUrgent).toBe(testCase.is_urgent)
+    expect(responseJson.orgForPoliceFilter).toBe(testCase.org_for_police_filter)
+    expect(responseJson.phase).toBe(testCase.phase)
+    expect(responseJson.ptiurn).toBe(testCase.ptiurn)
+    expect(responseJson.resolutionTimestamp).toBe(testCase.resolution_ts?.toISOString())
+    expect(responseJson.triggerCount).toBe(testCase.trigger_count)
+    expect(responseJson.triggerLockedByUsername).toBe(testCase.trigger_locked_by_id)
+    expect(responseJson.triggerStatus).toBe("Unresolved")
+    expect(responseJson.updatedHearingOutcome).toEqual(testAhoJsonStr)
   })
 
   it("returns errorLockedByUsername and errorLockedByUserFullName", async () => {
--- packages/api/src/tests/testGateways/e2ePostgres/insertCase.ts
+++ packages/api/src/tests/testGateways/e2ePostgres/insertCase.ts
@@ -1,5 +1,6 @@
-import type { RawCaseFullData } from "@moj-bichard7/common/types/Case"
 import type postgres from "postgres"
+
+import { type RawCaseFullData } from "@moj-bichard7/common/types/Case"
 
 export default async function (sql: postgres.Sql, partialCase: Partial<RawCaseFullData>): Promise<RawCaseFullData> {
   if (
@@ -22,33 +23,11 @@ export default async function (sql: postgres.Sql, partialCase: Partial<RawCaseFu
     throw new Error("Missing required attributes")
   }
 
-  const caseToInsert = partialCase as unknown as RawCaseFullData
+  const caseColumns = Object.keys(partialCase).sort()
 
   const [result]: [RawCaseFullData?] = await sql`
     INSERT INTO br7own.error_list
-      (annotated_msg, court_reference, create_ts, error_count, error_report, is_urgent, message_id, msg_received_ts,
-        org_for_police_filter, phase, total_pnc_failure_resubmissions, trigger_count, user_updated_flag,
-        error_locked_by_id, resolution_ts, error_status, trigger_locked_by_id)
-    VALUES
-      (
-        ${caseToInsert.annotated_msg},
-        ${caseToInsert.court_reference},
-        ${caseToInsert.create_ts},
-        ${caseToInsert.error_count},
-        ${caseToInsert.error_report},
-        ${caseToInsert.is_urgent},
-        ${caseToInsert.message_id},
-        ${caseToInsert.msg_received_ts},
-        ${caseToInsert.org_for_police_filter},
-        ${caseToInsert.phase},
-        ${caseToInsert.total_pnc_failure_resubmissions},
-        ${caseToInsert.trigger_count},
-        ${caseToInsert.user_updated_flag},
-        ${caseToInsert.error_locked_by_id},
-        ${caseToInsert.resolution_ts},
-        ${caseToInsert.error_status},
-        ${caseToInsert.trigger_locked_by_id}
-      )
+      ${sql(partialCase as never, caseColumns)}
     RETURNING *;
   `
 
