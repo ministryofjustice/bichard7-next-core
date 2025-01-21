ddfa6ce70f048e8dce3539d8ef79bbb99befd60c
Richard Cane <richard.cane@madetech.com>
2025-01-21 15:31:43 +0000

    Add HO100300, HO100314 and HO100333 to ASN exception list

--- packages/ui/src/utils/exceptions/isException/isAsnException.ts
+++ packages/ui/src/utils/exceptions/isException/isAsnException.ts
@@ -1,7 +1,14 @@
 import ExceptionCode from "@moj-bichard7-developers/bichard7-next-data/dist/types/ExceptionCode"
 import type { Exception } from "types/exceptions"
 
-const asnExceptions = [ExceptionCode.HO100206, ExceptionCode.HO100301, ExceptionCode.HO100321]
+const asnExceptions = [
+  ExceptionCode.HO100206,
+  ExceptionCode.HO100300,
+  ExceptionCode.HO100301,
+  ExceptionCode.HO100314,
+  ExceptionCode.HO100321,
+  ExceptionCode.HO100333
+]
 
 const isAsnException = (exceptions: Exception[]): boolean =>
   exceptions.some((exception) => asnExceptions.includes(exception.code))
