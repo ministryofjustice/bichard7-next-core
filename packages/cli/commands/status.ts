de3548e83d43d1d7c4d16c76de76fa1ed8cfbf26
joe-fol <joefolkard@protonmail.com>
2025-01-23 10:50:39 +0000

    Add option to notify when connection has restored

--- packages/cli/commands/status.ts
+++ packages/cli/commands/status.ts
@@ -1,25 +1,61 @@
-import { bold } from "cli-color"
-import { Command } from "commander"
-import { checkConnection } from "../utils/checkConnection"
-import { watch } from "../utils/watch"
+import { bold } from "cli-color";
+import { Command } from "commander";
+import { checkConnection } from "../utils/checkConnection";
+import { exec } from "child_process";
 
 export function status(): Command {
-  const command = new Command("status")
+  const command = new Command("status");
 
-  command.description("Get healthcheck endpoint output from production").action(async () => {
-    const endpoint = "https://proxy.bichard7.service.justice.gov.uk/bichard-backend/Connectivity"
+  command
+    .description("Get healthcheck endpoint output from production")
+    .option("-n, --notify", "Notify the user when the PNC connection has restored")
+    .action(async (options) => {
+      const endpoint = "https://proxy.bichard7.service.justice.gov.uk/bichard-backend/Connectivity";
 
-    let ok = false
-    try {
-      ok = await checkConnection(endpoint)
-    } catch (err) {
-      console.error(`Failed to connect to ${bold(endpoint)}\nAre you connected to the VPN?`)
-    }
-    if (!ok) return
+      let ok = false;
+      try {
+        ok = await checkConnection(endpoint);
+      } catch (err) {
+        console.error(`Failed to connect to ${bold(endpoint)}\nAre you connected to the VPN?`);
+      }
+      if (!ok) return;
 
-    const command = `curl -s ${endpoint} | jq '.pncConnectionHealth'`
-    watch(command)
-  })
+      if (options.notify) {
+        const notify = `
+          until curl -s ${endpoint} | jq '.pncConnectionHealth' | grep -q '"healthy": true'; do
+            echo -n '.';
+            sleep 5;
+          done
+        `;
+        exec(notify, (error) => {
+          if (error) {
+            console.error(`Error during watch: ${error.message}`);
+            return;
+          }
 
-  return command
+          const osascriptCommand = `
+            osascript -e 'display notification "${new Date().toLocaleTimeString()}" with title "PNC Connection Restored"'
+          `;
+          exec(osascriptCommand, (osascriptError) => {
+            if (osascriptError) {
+              console.error(`osascript failed: ${osascriptError.message}`);
+            }
+          });
+        });
+        return;
+      } else {
+        const curlCommand = `curl -s ${endpoint} | jq '.pncConnectionHealth'`;
+        exec(curlCommand, (error, stdout) => {
+          if (error) {
+            console.error(`Error fetching endpoint status: ${error.message}`);
+          } else {
+            console.log(stdout);
+          }
+        });
+      }
+
+    });
+
+  return command;
 }
+
