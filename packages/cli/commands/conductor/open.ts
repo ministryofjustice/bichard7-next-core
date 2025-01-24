0d861ec347ad90a16fbee05816602ed67901135e
Richard Cane <richard.cane@madetech.com>
2025-01-24 15:24:17 +0000

    Connection check for conductor open

--- packages/cli/commands/conductor/open.ts
+++ packages/cli/commands/conductor/open.ts
@@ -1,8 +1,25 @@
 import { exec } from "child_process"
+import { bold } from "cli-color"
 import { Command } from "commander"
 import { getEnvironment } from "../../env"
 import awsVault from "../../utils/awsVault"
+import { checkConnection } from "../../utils/checkConnection"
 import { getDefaultBrowserForOSAScript } from "../../utils/getDefaultBrowserForOSAScript"
+
+const connect = async (url: string) => {
+  let ok = false
+
+  try {
+    await checkConnection(`https://${url}`)
+    ok = true
+  } catch (err) {
+    console.error(
+      `Failed to connect to ${bold(url)}\nMake sure you're connected to the correct VPN and that dev SGs have been applied.`
+    )
+  }
+
+  return ok
+}
 
 const FETCH_CONDUCTOR_PASSWORD_SECRET_ARN = `\
     aws secretsmanager list-secrets --query "SecretList[*].{Name: Name, ARN: ARN}" \
@@ -22,29 +39,30 @@ export function open(): Command {
         domain,
         aws: { profile }
       } = getEnvironment()
+      const conductor = `conductor.${domain}`
 
-      console.log("Querying for Conductor password ...")
+      const connectionEstablished = await connect(conductor)
+      if (!connectionEstablished) return
+
       // get conductor password
+      console.log("Querying for Conductor password ...")
       const arn = await awsVault.exec(profile, FETCH_CONDUCTOR_PASSWORD_SECRET_ARN)
       const password = await awsVault.exec(profile, FETCH_CONDUCTOR_PASSWORD(arn))
-      exec(`osascript -e 'open location "https://bichard:${encodeURIComponent(password)}@conductor.${domain}"'`)
 
       // this tomfoolery is required because it won't log in otherwise.
       console.log("Logging in ...")
+      exec(`osascript -e 'open location "https://bichard:${encodeURIComponent(password)}@${conductor}"'`)
+
       const browser = await getDefaultBrowserForOSAScript()
       setTimeout(async () => {
         exec(`osascript -e 'tell application "${browser}" to reload active tab of window 1'`)
         setTimeout(
           () =>
             exec(
-              `osascript -e 'tell application "${browser}" to set URL of active tab of window 1 to "https://conductor.${domain}"'`
+              `osascript -e 'tell application "${browser}" to set URL of active tab of window 1 to "https://${conductor}"'`
             ),
           1000
         )
       }, 1000)
-
-      // TODO: conductor doesn't load properly when you use a basic auth link to log in
-      // we could use osascript to detect the default browser and then navigate the newly
-      // opened tab to the regular conductor URL
     })
 }
