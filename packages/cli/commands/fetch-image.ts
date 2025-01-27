46c2e1aee2037be2e179d5ad1478cd1bce1338fd
joe-fol <joefolkard@protonmail.com>
2025-01-27 16:11:39 +0000

    Rename and fix log stream for fetch-image

--- packages/cli/commands/fetch-image.ts
+++ packages/cli/commands/fetch-image.ts
@@ -1,20 +1,25 @@
 import { Command } from "commander"
 import path from "path"
 import awsVault from "../utils/awsVault"
-import { env, Environment } from "../config"
+import type { Environment } from "../config"
+import { env } from "../config"
 
 export function fetchImage(): Command {
   const command = new Command("fetch-image")
 
   command
-    .description(`Fetch images from AWS Elastic Container Repository`)
+    .description("Fetch images from AWS Elastic Container Repository")
     .usage("<image>")
     .argument("<image>", "The image you want to download from ECR")
     .action(async (image) => {
       const scriptPath = path.resolve(__dirname, "../../../../scripts/fetch-docker-image.sh")
       const { aws }: Environment = env.SHARED
-      const command = `bash ${scriptPath} ${image}`
-      await awsVault.exec(aws.profile, command)
+      await awsVault.exec({
+        awsProfile: aws.profile,
+        command: `bash ${scriptPath} ${image}`,
+        logExecution: true,
+        streamOutput: true
+      })
     })
 
   return command
