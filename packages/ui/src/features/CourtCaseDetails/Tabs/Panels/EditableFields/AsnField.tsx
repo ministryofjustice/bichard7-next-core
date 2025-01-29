9479eada61452a168eb30b16cc0e597134b868b4
Sen <65850511+Senny10@users.noreply.github.com>
2025-01-29 14:31:50 +0000

    Add htmlFor prop to InputField and EditableFieldTableRow for improved accessibility

--- packages/ui/src/features/CourtCaseDetails/Tabs/Panels/EditableFields/AsnField.tsx
+++ packages/ui/src/features/CourtCaseDetails/Tabs/Panels/EditableFields/AsnField.tsx
@@ -86,6 +86,7 @@ export const AsnField = () => {
       isEditable={isAsnEditable}
       inputLabel="Enter the ASN"
       hintText="ASN format: Last 2 digits of year / 4 divisional ID location characters / 2 digits from owning force / 1 to 11 digits and 1 check letter \n Example: 22/49AB/49/1234C"
+      htmlFor={"asn"}
     >
       <div>
         <div>
