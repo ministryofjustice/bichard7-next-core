1784dcef2fb35330d8cea8a5d2781599d8da9302
joe-fol <joefolkard@protonmail.com>
2025-01-30 09:30:59 +0000

    Add userservice to command to search by user email

--- packages/cli/commands/cloudwatch/index.ts
+++ packages/cli/commands/cloudwatch/index.ts
@@ -1,12 +1,14 @@
 import { Command } from "commander"
 import { postfix } from "./postfix"
+import { userService } from "./userService"
 
 export function cloudwatch(): Command {
-  const command = new Command("cloudwatch-user-event")
-    .name("cloudwatch-user-event")
+  const command = new Command("cloudwatch")
+    .name("cloudwatch")
     .description("A method for querying cloudwatch for user events")
     .usage("b7 cloudwatch-user-event postfix -h 3 steve@example.com")
     .addCommand(postfix())
+    .addCommand(userService())
 
   return command
 }
