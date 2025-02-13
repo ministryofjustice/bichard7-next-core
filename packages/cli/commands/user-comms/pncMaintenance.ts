ee29f2e92c57aa98d361688907c9f615f5cee4b1
joe-fol <joefolkard@protonmail.com>
2025-02-13 16:13:10 +0000

    Update pnc maintenance to take multiple dates

--- packages/cli/commands/user-comms/pncMaintenance.ts
+++ packages/cli/commands/user-comms/pncMaintenance.ts
@@ -2,30 +2,54 @@ import fs from "fs"
 import path from "path"
 import { input, confirm } from "@inquirer/prompts"
 import nunjucks from "nunjucks"
-import { parse, format, isValid } from "date-fns"
+import { parse, format, isValid, isMatch } from "date-fns"
+
+const validateTime = (time: string): boolean => {
+  return isMatch(time, "HH:mm")
+}
 
 export const pncMaintenance = async () => {
   const templateFile = "pnc-maintenance.txt"
   const templatePath = path.join("./commands/user-comms/templates", templateFile)
   const templateContent = fs.readFileSync(templatePath, "utf-8")
 
-  const dateInput = await input({ message: "Enter the date (dd/MM/yyyy)" })
-  const startTime = await input({ message: "Enter start time (HH:mm)" })
-  const endTime = await input({ message: "Enter end time (HH:mm)" })
+  const maintenanceWindows: { date: string; startTime: string; endTime: string }[] = []
 
-  const parsedDate = parse(dateInput, "dd/MM/yyyy", new Date())
+  let addAnotherDate = true
 
-  if (!isValid(parsedDate)) {
-    console.error("Invalid date format. Please use 'dd/MM/yyyy' (e.g., '17/03/2025').")
-    return
+  while (addAnotherDate) {
+    const dateInput = await input({ message: "Enter the date (dd/MM/yyyy)" })
+    const parsedDate = parse(dateInput, "dd/MM/yyyy", new Date())
+    if (!isValid(parsedDate)) {
+      console.error("Invalid date format. Please use 'dd/MM/yyyy' (e.g., '17/03/2025').")
+      continue
+    }
+
+    const startTime = await input({ message: "Enter start time (HH:mm)" })
+    if (!validateTime(startTime)) {
+      console.error("Invalid time format. Please use 'HH:mm' (e.g., '09:30')")
+      continue
+    }
+
+    const endTime = await input({ message: "Enter end time (HH:mm)" })
+    if (!validateTime(endTime)) {
+      console.error("Invalid time format. Please use 'HH:mm' (e.g., '15:30')")
+      continue
+    }
+
+    maintenanceWindows.push({
+      date: format(parsedDate, "EEEE d MMMM yyyy"),
+      startTime,
+      endTime
+    })
+
+    addAnotherDate = await confirm({
+      message: "Are there any other planned maintenance windows you would like to notify users about?"
+    })
   }
 
-  const formattedDate = format(parsedDate, "EEEE d MMMM yyyy")
-
   const renderedEmail = nunjucks.renderString(templateContent, {
-    date: formattedDate,
-    startTime: startTime,
-    endTime: endTime
+    maintenanceWindows
   })
 
   console.log("\n=== Preview Email ===\n")
