6d6b2c115cd4d88cb6b871467dc1bcb1ba10c83d
Sen <65850511+Senny10@users.noreply.github.com>
2025-02-17 09:47:05 +0000

    Update locked icon test to check for triggers-locked-tag instead of header container

--- packages/ui/cypress/e2e/case-details/handle-triggers-and-exceptions/triggers/locked-icon.cy.ts
+++ packages/ui/cypress/e2e/case-details/handle-triggers-and-exceptions/triggers/locked-icon.cy.ts
@@ -72,7 +72,7 @@ describe("Locked icon", () => {
     ])
     cy.task("insertTriggers", { caseId: 0, triggers: unresolvedTriggers })
     cy.visit(caseURL)
-    cy.get("#header-container").should("contain.text", "Bichard Test User Force 04")
+    cy.get("div.triggers-locked-tag").should("contain.text", "Bichard Test User Force 04")
     cy.get("#header-container").should("not.contain.text", "GeneralHandler")
     cy.get("#triggers").should("contain.text", "Bichard Test User Force 04")
     cy.get("#triggers").should("not.contain.text", "GeneralHandler")
