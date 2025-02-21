22553c22a5d74c40a153b327e63c7c0368b4699b
Richard Cane <richard.cane@madetech.com>
2025-02-21 12:22:46 +0000

    Import non annotated function first

--- packages/ui/src/utils/parseHearingOutcome.ts
+++ packages/ui/src/utils/parseHearingOutcome.ts
@@ -1,6 +1,7 @@
 import parseAhoXml from "@moj-bichard7/core/lib/parse/parseAhoXml/parseAhoXml"
-import parseAnnotatedPncUpdateDatasetXml from "@moj-bichard7/core/lib/parse/parseAnnotatedPncUpdateDatasetXml/parseAnnotatedPncUpdateDatasetXml"
 import parsePncUpdateDatasetXml from "@moj-bichard7/core/lib/parse/parsePncUpdateDatasetXml/parsePncUpdateDatasetXml"
+
+import parseAnnotatedPncUpdateDatasetXml from "@moj-bichard7/core/lib/parse/parseAnnotatedPncUpdateDatasetXml/parseAnnotatedPncUpdateDatasetXml"
 import type { AnnotatedHearingOutcome } from "@moj-bichard7/core/types/AnnotatedHearingOutcome"
 import type { PncUpdateDataset } from "@moj-bichard7/core/types/PncUpdateDataset"
 import { isError } from "../types/Result"
