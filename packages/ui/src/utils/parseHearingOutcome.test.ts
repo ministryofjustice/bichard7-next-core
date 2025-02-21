4f0b28700f18cf92b9ef62ffc9bb12cc85d1952b
Richard Cane <richard.cane@madetech.com>
2025-02-21 16:16:25 +0000

    Add test for parsePncUpdateDataset in parseHearingOutcome

--- packages/ui/src/utils/parseHearingOutcome.test.ts
+++ packages/ui/src/utils/parseHearingOutcome.test.ts
@@ -1,5 +1,6 @@
 import * as parseAhoXmlModule from "@moj-bichard7/core/lib/parse/parseAhoXml/parseAhoXml"
 import * as parseAnnotatedPncUpdateDatasetXmlModule from "@moj-bichard7/core/lib/parse/parseAnnotatedPncUpdateDatasetXml/parseAnnotatedPncUpdateDatasetXml"
+import * as parsePncUpdateDataSetXmlModule from "@moj-bichard7/core/lib/parse/parsePncUpdateDataSetXml/parsePncUpdateDataSetXml"
 import fs from "fs"
 import dummyAho from "../../test/test-data/AnnotatedHO1.json"
 import parseHearingOutcome from "./parseHearingOutcome"
@@ -9,13 +10,22 @@ describe("parseHearingOutcome", () => {
     expect(parseHearingOutcome("not an XML")).toEqual(new Error("Could not parse AHO XML"))
   })
 
-  it("Should call parseAnnotatedPncUpdateDatasetXml when the XML is a isPncUpdateDataset", () => {
+  it("Should call parseAnnotatedPncUpdateDatasetXml when the XML is an AnnotatedPncUpdateDataset", () => {
     const spy = jest.spyOn(parseAnnotatedPncUpdateDatasetXmlModule, "default")
-    const dummyPNCUpdateDataset = fs.readFileSync("test/test-data/AnnotatedPNCUpdateDataset.xml").toString()
-    parseHearingOutcome(dummyPNCUpdateDataset)
+    const annotatedPncUpdateDataset = fs.readFileSync("test/test-data/AnnotatedPNCUpdateDataset.xml").toString()
+    parseHearingOutcome(annotatedPncUpdateDataset)
 
     expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(dummyPNCUpdateDataset)
+    expect(spy).toHaveBeenCalledWith(annotatedPncUpdateDataset)
+  })
+
+  it("Should call parsePncUpdateDatasetXml when the XML is a pncUpdateDataset", () => {
+    const spy = jest.spyOn(parsePncUpdateDataSetXmlModule, "default")
+    const pncUpdateDataset = fs.readFileSync("test/test-data/PNCUpdateDataset.xml").toString()
+    parseHearingOutcome(pncUpdateDataset)
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(pncUpdateDataset)
   })
 
   it("Should call parseAhoXml when the XML is not a isPncUpdateDataset", () => {
