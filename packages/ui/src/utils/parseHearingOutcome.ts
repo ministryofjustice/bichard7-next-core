e31a031c758bf6d516f26f7ff237642dffd4afb5
Richard Cane <richard.cane@madetech.com>
2025-02-21 14:56:18 +0000

    Fix import casing

--- packages/ui/src/utils/parseHearingOutcome.ts
+++ packages/ui/src/utils/parseHearingOutcome.ts
@@ -1,7 +1,6 @@
 import parseAhoXml from "@moj-bichard7/core/lib/parse/parseAhoXml/parseAhoXml"
-import parsePncUpdateDatasetXml from "@moj-bichard7/core/lib/parse/parsePncUpdateDatasetXml/parsePncUpdateDatasetXml"
-
 import parseAnnotatedPncUpdateDatasetXml from "@moj-bichard7/core/lib/parse/parseAnnotatedPncUpdateDatasetXml/parseAnnotatedPncUpdateDatasetXml"
+import parsePncUpdateDataSetXml from "@moj-bichard7/core/lib/parse/parsePncUpdateDataSetXml/parsePncUpdateDataSetXml"
 import type { AnnotatedHearingOutcome } from "@moj-bichard7/core/types/AnnotatedHearingOutcome"
 import type { PncUpdateDataset } from "@moj-bichard7/core/types/PncUpdateDataset"
 import { isError } from "../types/Result"
@@ -12,7 +11,7 @@ const parseHearingOutcome = (hearingOutcome: string): AnnotatedHearingOutcome | 
     return annotatedPncUpdateDataset.AnnotatedPNCUpdateDataset.PNCUpdateDataset
   }
 
-  const pncUpdateDataset = parsePncUpdateDatasetXml(hearingOutcome)
+  const pncUpdateDataset = parsePncUpdateDataSetXml(hearingOutcome)
   if (!isError(pncUpdateDataset)) {
     return pncUpdateDataset
   }
