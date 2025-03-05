7744c72af64138e627c2d55d5341bdd7ea4d4c8a
Sen <65850511+Senny10@users.noreply.github.com>
2025-03-05 12:29:22 +0000

    add helper function to format ASN with dividers

---
+++ packages/ui/src/helpers/formatAsn.ts
@@ -0,0 +1,13 @@
+// Usage: formatAsnWithDivider('123456789A') => '12/3456/78/9A'
+
+const formatAsnWithDivider = (asn: string): string => {
+  const year = asn.slice(0, 2)
+  const divisionalId = asn.slice(2, 6)
+  const owningForce = asn.slice(6, 8)
+  const checkLetter = asn.slice(-1)
+  const digits = asn.slice(8, -1)
+
+  return `${year}/${divisionalId}/${owningForce}/${digits}${checkLetter}`
+}
+
+export default formatAsnWithDivider
