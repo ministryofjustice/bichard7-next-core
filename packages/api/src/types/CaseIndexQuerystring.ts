d09ccfded45e23d5eee0dc7cf9f8d193226424f9
Richard Race <1273965+richrace@users.noreply.github.com>
2025-03-05 16:43:34 +0000

    Add failing test

--- packages/api/src/types/CaseIndexQuerystring.ts
+++ packages/api/src/types/CaseIndexQuerystring.ts
@@ -1,5 +1,7 @@
 import z from "zod"
 
+import { ResolutionStatus } from "../useCases/dto/convertResolutionStatus"
+
 export enum Order {
   asc = "asc",
   desc = "desc"
@@ -12,17 +14,26 @@ export enum OrderBy {
   ptiurn = "ptiurn"
 }
 
+export enum Reason {
+  All = "All",
+  Exceptions = "Exceptions",
+  Triggers = "Triggers"
+}
+
 export const CaseIndexQuerystringSchema = z.object({
+  caseState: z.nativeEnum(ResolutionStatus).optional(),
   courtName: z.string().optional(),
   defendantName: z.string().optional().openapi({ example: "De*Name" }),
   maxPerPage: z.coerce.number().min(25).max(200).default(50),
-  order: z.enum(Object.keys(Order) as [keyof typeof Order]).optional(),
-  orderBy: z.enum(Object.keys(OrderBy) as [keyof typeof OrderBy]).optional(),
-  pageNum: z.coerce.number().min(1).default(1)
+  order: z.nativeEnum(Order).optional(),
+  orderBy: z.nativeEnum(OrderBy).optional(),
+  pageNum: z.coerce.number().min(1).default(1),
+  reason: z.nativeEnum(Reason).optional(),
+  reasonCodes: z.array(z.string()).optional()
 })
 
 export type CaseIndexQuerystring = z.infer<typeof CaseIndexQuerystringSchema>
 
-export type Filters = Pick<CaseIndexQuerystring, "courtName" | "defendantName">
+export type Filters = Pick<CaseIndexQuerystring, "caseState" | "courtName" | "defendantName" | "reason" | "reasonCodes">
 export type Pagination = Pick<CaseIndexQuerystring, "maxPerPage" | "pageNum">
 export type SortOrder = Pick<CaseIndexQuerystring, "order" | "orderBy">
