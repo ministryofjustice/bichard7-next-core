902feae1fd83acc5fa0d5f473842332975dacb7a
Tausif Patel <tausifpatel.dev@gmail.com>
2025-03-07 12:01:25 +0000

    Remove extra argument from fetchCase function

--- packages/api/src/services/gateways/dataStoreGateways/fakeDataStore.ts
+++ packages/api/src/services/gateways/dataStoreGateways/fakeDataStore.ts
@@ -3,7 +3,8 @@ import type { Trigger } from "@moj-bichard7/common/types/Trigger"
 import type { User } from "@moj-bichard7/common/types/User"
 import type postgres from "postgres"
 
-import type { CaseDataForDto, CaseDataForIndexDto, CaseMessageId, Pagination } from "../../../types/Case"
+import type { CaseDataForDto, CaseDataForIndexDto, CaseMessageId } from "../../../types/Case"
+import type { Pagination } from "../../../types/CaseIndexQuerystring"
 import type { LockReason } from "../../../types/LockReason"
 import type DataStoreGateway from "../interfaces/dataStoreGateway"
 
