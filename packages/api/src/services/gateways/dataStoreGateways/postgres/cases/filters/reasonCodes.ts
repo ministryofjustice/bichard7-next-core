9d571c2809fb241000d5e0a4b2816e53f4fa469b
Richard Race <1273965+richrace@users.noreply.github.com>
2025-03-10 17:07:06 +0000

    Update query

--- packages/api/src/services/gateways/dataStoreGateways/postgres/cases/filters/reasonCodes.ts
+++ packages/api/src/services/gateways/dataStoreGateways/postgres/cases/filters/reasonCodes.ts
@@ -1,5 +1,7 @@
 import type postgres from "postgres"
 
+import { isEmpty } from "lodash"
+
 import type { Filters } from "../../../../../../types/CaseIndexQuerystring"
 
 export const filterByReasonCodes = (sql: postgres.Sql, filters: Filters) => {
@@ -7,13 +9,26 @@ export const filterByReasonCodes = (sql: postgres.Sql, filters: Filters) => {
     return sql``
   }
 
+  const queries = []
+
   const triggerCodes = filters.reasonCodes?.filter((rc) => rc.startsWith("TRP")) ?? [""]
   const exceptionCodes = filters.reasonCodes?.filter((rc) => rc.startsWith("HO")).map((rc) => `%${rc}%`) ?? [""]
 
-  return sql`
-    AND (
-      elt.trigger_code ILIKE ANY(${triggerCodes})
-      OR el.error_report ILIKE ANY(${exceptionCodes})
-    )
-  `
+  if (!isEmpty(triggerCodes)) {
+    queries.push(sql`elt.trigger_code ILIKE ANY(${triggerCodes})`)
+  }
+
+  if (!isEmpty(exceptionCodes)) {
+    queries.push(sql`el.error_report ILIKE ANY(${exceptionCodes})`)
+  }
+
+  const query = queries.map((q, i) => {
+    if (queries.length === 2 && i === 0) {
+      return sql`${q} OR`
+    }
+
+    return sql`${q}`
+  })
+
+  return sql`AND (${query})`
 }
