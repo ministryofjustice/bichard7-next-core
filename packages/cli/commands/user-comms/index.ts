008f3ac4fab2f3dbfe17cccf027c0b325fe472a7
joe-fol <joefolkard@protonmail.com>
2025-03-12 16:58:11 +0000

    Refactor main index funtion for readability

--- packages/cli/commands/user-comms/index.ts
+++ packages/cli/commands/user-comms/index.ts
@@ -1,9 +1,10 @@
-import { select } from "@inquirer/prompts"
 import { Command } from "commander"
 import nunjucks from "nunjucks"
 import { pncMaintenance } from "./pncMaintenance"
 import { pncMaintenanceExtended } from "./pncMaintenanceExtended"
 import { outageComms } from "./outageComms"
+import { selectTemplate } from "./selectTemplate"
+import { selectOutageType } from "./selectOutageType"
 
 nunjucks.configure("templates", { autoescape: true })
 
@@ -11,78 +12,18 @@ export function userComms(): Command {
   const program = new Command("user-comms")
 
   program.description("A way to send group communications to all users").action(async () => {
-    const templateChoice = await select({
-      message: "Select a template to use",
-      choices: [
-        {
-          name: "PNC Scheduled Maintenance",
-          value: "PNC maintenance",
-          description: "Notify users of upcoming scheduled maintenance"
-        },
-        {
-          name: "PNC Maintenance Window Extended",
-          value: "PNC maintenance extended",
-          description: "Notify users that the maintenance window has been extended"
-        },
-
-        {
-          name: "Outage Notification",
-          value: "Outage",
-          description: "Notify users about an ongoing outage"
-        },
-        {
-          name: "Outage Resolved",
-          value: "Outage Resolved",
-          description: "Notify users that the outage has been resolved"
-        }
-      ]
-    })
+    const selectedTemplate = await selectTemplate()
 
     let outageType = ""
-    let outageResolved = false
+    let hasOutageResolved = false
 
-    if (templateChoice === "Outage") {
-      outageType = await select({
-        message: "What type of outage are we reporting?",
-        choices: [
-          {
-            name: "PNC Outage",
-            value: "PNC",
-            description: "Notify users of a PNC Outage"
-          },
-          {
-            name: "PSN Outage",
-            value: "PSN",
-            description: "Notify users of a PSN Outage"
-          },
-          { name: "Bichard Outage", value: "Bichard", description: "Notify users of a Bichard Outage" }
-        ]
-      })
-    } else if (templateChoice === "Outage Resolved") {
-      outageResolved = true
-      outageType = await select({
-        message: "What type of outage are your sending resolution communications about?",
-        choices: [
-          {
-            name: "PNC Outage Resolved",
-            value: "PNC",
-            description: "Notify users that the PNC outage has been resolved"
-          },
-          {
-            name: "PSN Outage Resolved",
-            value: "PSN",
-            description: "Notify users that the PSN outage has been resolved"
-          },
-          {
-            name: "Bichard Outage Resolved",
-            value: "Bichard",
-            description: "Notify users that the Bichard outage has been resolved"
-          }
-        ]
-      })
+    if (selectedTemplate === "Outage" || selectedTemplate === "Outage Resolved") {
+      const outageValues = await selectOutageType(selectedTemplate)
+      outageType = outageValues.outageType
+      hasOutageResolved = outageValues.hasOutageResolved
     }
 
-    switch (templateChoice) {
+    switch (selectedTemplate) {
       case "PNC maintenance":
         pncMaintenance()
         break
@@ -90,13 +31,10 @@ export function userComms(): Command {
         pncMaintenanceExtended()
         break
       case "Outage":
-        outageComms(outageType, outageResolved)
-        break
       case "Outage Resolved":
-        outageComms(outageType, outageResolved)
+        outageComms(outageType, hasOutageResolved)
         break
     }
   })
-
   return program
 }
