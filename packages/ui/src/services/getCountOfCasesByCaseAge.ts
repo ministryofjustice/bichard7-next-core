65e13302be9a3766b6e3134ec7c662df0cce5673
Richard Race <1273965+richrace@users.noreply.github.com>
2025-03-12 14:49:01 +0000

    Refactor function

--- packages/ui/src/services/getCountOfCasesByCaseAge.ts
+++ packages/ui/src/services/getCountOfCasesByCaseAge.ts
@@ -1,14 +1,11 @@
 import type { DataSource } from "typeorm"
 import { Brackets } from "typeorm"
 import type PromiseResult from "types/PromiseResult"
-import { isError } from "types/Result"
 import { CaseAgeOptions } from "utils/caseAgeOptions"
 import { formatFormInputDateString } from "utils/date/formattedDate"
 import CourtCase from "./entities/CourtCase"
 import type User from "./entities/User"
 import courtCasesByOrganisationUnitQuery from "./queries/courtCasesByOrganisationUnitQuery"
-
-const asKey = (caseAgeOption: string) => "_" + caseAgeOption.toLowerCase().replace(/ /g, "")
 
 const getCountOfCasesByCaseAge = async (connection: DataSource, user: User): PromiseResult<Record<string, number>> => {
   const repository = connection.getRepository(CourtCase)
@@ -16,18 +13,16 @@ const getCountOfCasesByCaseAge = async (connection: DataSource, user: User): Pro
   query = courtCasesByOrganisationUnitQuery(query, user)
 
   Object.keys(CaseAgeOptions).forEach((slaCaseAgeOption, i) => {
-    const key = asKey(slaCaseAgeOption)
+    const key = slaCaseAgeOption
     const slaDateFrom = formatFormInputDateString(CaseAgeOptions[slaCaseAgeOption]().from)
     const slaDateTo = formatFormInputDateString(CaseAgeOptions[slaCaseAgeOption]().to)
 
+    const count = `Count(CASE WHEN court_date >= '${slaDateFrom}' AND court_date <= '${slaDateTo}' THEN 1 END) as "${key}"`
+
     if (i === 0) {
-      query.select(
-        `Count(CASE WHEN court_date >= '${slaDateFrom}' AND court_date <= '${slaDateTo}' THEN 1 END) as ${key}`
-      )
+      query.select(count)
     } else {
-      query.addSelect(
-        `Count(CASE WHEN court_date >= '${slaDateFrom}' AND court_date <= '${slaDateTo}' THEN 1 END) as ${key}`
-      )
+      query.addSelect(count)
     }
   })
 
@@ -39,16 +34,7 @@ const getCountOfCasesByCaseAge = async (connection: DataSource, user: User): Pro
     })
   )
 
-  const response = await query.getRawOne().catch((error: Error) => error)
-
-  return isError(response)
-    ? response
-    : (Object.keys(CaseAgeOptions).reduce(
-        (result: Record<string, number>, caseAge: string) => (
-          (result[caseAge] = response ? response[asKey(caseAge)] : "0"), result
-        ),
-        {}
-      ) as Record<string, number>)
+  return await query.getRawOne().catch((error: Error) => error)
 }
 
 export default getCountOfCasesByCaseAge
