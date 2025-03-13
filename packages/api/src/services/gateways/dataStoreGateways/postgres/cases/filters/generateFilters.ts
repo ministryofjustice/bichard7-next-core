081ff8628acdf0997cdd448726077e20beb0f5e5
Richard Race <1273965+richrace@users.noreply.github.com>
2025-03-13 17:15:38 +0000

    Update filename

--- packages/api/src/services/gateways/dataStoreGateways/postgres/cases/filters/generateFilters.ts
+++ packages/api/src/services/gateways/dataStoreGateways/postgres/cases/filters/generateFilters.ts
@@ -4,9 +4,9 @@ import type { Row } from "postgres"
 
 import type { Filters } from "../../../../../../types/CaseIndexQuerystring"
 
-import { filterByAsn } from "./asn"
 import { filterByCourtName } from "./courtName"
 import { filterByDefendantName } from "./defendantName"
+import { filterByAsn } from "./filterByAsn"
 import { filterByPtiurn } from "./filterByPtiurn"
 import { filterByReasonAndResolutionStatus } from "./filterByReasonAndResolutionStatus"
 import { filterByReasonCodes } from "./reasonCodes"
