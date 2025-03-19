14712c8679f1aaa8e2e6fb51b7a0618a3de4fe35
Tausif Patel <tausifpatel.dev@gmail.com>
2025-03-19 12:22:08 +0000

    Fix API_LOCATION env variable

--- packages/ui/src/config.ts
+++ packages/ui/src/config.ts
@@ -10,7 +10,7 @@ export const OUT_OF_AREA_TRIGGER_CODE = TriggerCode.TRPR0027
 export const SWITCHING_FEEDBACK_FORM_FREQUENCY_IN_HOURS = 3
 export const COOKIES_SECURE_OPTION = (process.env.COOKIES_SECURE ?? "true") === "true"
 
-export const API_LOCATION = process.env.API_URL ?? "http://localhost:3333"
+export const API_LOCATION = process.env.API_URL ?? "https://localhost:3333"
 export const USE_API = (process.env.USE_API ?? "false") === "true"
 export const USE_API_CASE_ENDPOINT = (process.env.USE_API_CASE_ENDPOINT ?? "false") === "true"
 
