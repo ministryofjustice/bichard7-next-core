867f9fa3b0e9c74fd3440c3530deba66a8abafca
Richard Race <1273965+richrace@users.noreply.github.com>
2025-03-25 17:34:20 +0000

    Nginx handles SSL
    Co-Authored-By: Tausif Patel <tausifpatel.dev@gmail.com>
    Co-Authored-By: Wen Ting Wang <wenting@madetech.com>

--- packages/api/src/server/createFastify.ts
+++ packages/api/src/server/createFastify.ts
@@ -2,23 +2,11 @@ import type { FastifyInstance } from "fastify"
 import type { IncomingMessage, Server, ServerResponse } from "http"
 
 import { fastify as Fastify } from "fastify"
-import fs from "fs"
 
 import logger from "./logger"
 
 export default function (): FastifyInstance<Server, IncomingMessage, ServerResponse> {
-  let fastify: FastifyInstance<Server, IncomingMessage, ServerResponse>
-
   const options = { logger: logger(process.env.NODE_ENV) }
 
-  if (process.env.USE_SSL === "true") {
-    fastify = Fastify({
-      ...options,
-      https: { cert: fs.readFileSync("/certs/server.crt"), key: fs.readFileSync("/certs/server.key") }
-    })
-  } else {
-    fastify = Fastify(options)
-  }
-
-  return fastify
+  return Fastify(options)
 }
