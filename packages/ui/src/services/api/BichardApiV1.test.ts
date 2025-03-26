f37f6a0955061ee9f5a9b89606324632b663f7ec
Tausif Patel <tausifpatel.dev@gmail.com>
2025-03-26 11:15:33 +0000

    Add fetch cases gateway

--- packages/ui/src/services/api/BichardApiV1.test.ts
+++ packages/ui/src/services/api/BichardApiV1.test.ts
@@ -1,5 +1,8 @@
 import { V1 } from "@moj-bichard7/common/apiEndpoints/versionedEndpoints"
 
+import { Reason, type ApiCaseQuery } from "@moj-bichard7/common/types/ApiCaseQuery"
+import type { CaseIndexMetadata } from "@moj-bichard7/common/types/Case"
+import { CaseAge } from "@moj-bichard7/common/types/CaseAge"
 import { isError } from "@moj-bichard7/common/types/Result"
 import type { DisplayFullCourtCase } from "types/display/CourtCases"
 import FakeApiClient from "../../../test/helpers/api/fakeApiClient"
@@ -10,6 +13,11 @@ describe("BichardApiV1", () => {
   let client: FakeApiClient
   let gateway: BichardApiGateway
   const caseId = 1
+  const apiCaseQuerystring: ApiCaseQuery = {
+    maxPerPage: 1,
+    pageNum: 1,
+    reason: Reason.All
+  }
 
   beforeEach(() => {
     client = new FakeApiClient("jwt")
@@ -48,6 +56,70 @@ describe("BichardApiV1", () => {
 
       expect(isError(result)).toBe(true)
       expect(result).toEqual(new Error("Error"))
+    })
+  })
+
+  describe("#fetchCases", () => {
+    it("calls apiClient#get with a route", async () => {
+      const endpoint = V1.Cases
+
+      jest.spyOn(client, "get").mockResolvedValue({} as CaseIndexMetadata)
+
+      await gateway.fetchCases(apiCaseQuerystring)
+
+      expect(client.get).toHaveBeenCalledWith(`${endpoint}?maxPerPage=1&pageNum=1&reason=All`)
+    })
+
+    it("calls apiClient#get with a route and returns a CaseIndexMetadata", async () => {
+      const expectedData = {} as CaseIndexMetadata
+      jest.spyOn(client, "get").mockResolvedValue({} as CaseIndexMetadata)
+
+      const result = await gateway.fetchCases(apiCaseQuerystring)
+
+      expect(result).toEqual(expectedData)
+    })
+
+    it("fetchCases accepts filter queries", async () => {
+      const endpoint = V1.Cases
+      const apiCaseQuerystring: ApiCaseQuery = {
+        maxPerPage: 1,
+        pageNum: 1,
+        reason: Reason.All
+      }
+      const expectedData = {} as CaseIndexMetadata
+
+      jest.spyOn(client, "get").mockResolvedValue({} as CaseIndexMetadata)
+
+      const result = await gateway.fetchCases(apiCaseQuerystring)
+
+      expect(result).toEqual(expectedData)
+      expect(client.get).toHaveBeenCalledWith(`${endpoint}?maxPerPage=1&pageNum=1&reason=All`)
+    })
+
+    it("fetchCases accepts filter queries with arrays", async () => {
+      const sortQuery = (query: string) => query.split("?")[1].split("&").sort().join("&")
+      const endpoint = V1.Cases
+      const apiCaseQuerystring: ApiCaseQuery = {
+        maxPerPage: 1,
+        pageNum: 1,
+        reason: Reason.All,
+        caseAge: [CaseAge.Today, CaseAge.Yesterday],
+        reasonCodes: ["HO100206", "HO100323"]
+      }
+
+      const expectedData = {} as CaseIndexMetadata
+
+      jest.spyOn(client, "get").mockResolvedValue({} as CaseIndexMetadata)
+
+      const result = await gateway.fetchCases(apiCaseQuerystring)
+
+      const expectedQuery = sortQuery((client.get as jest.Mock).mock.calls[0][0])
+      const receivedQuery = sortQuery(
+        `${endpoint}?caseAge=Today&caseAge=Yesterday&maxPerPage=1&pageNum=1&reason=All&reasonCodes=HO100206&reasonCodes=HO100323`
+      )
+
+      expect(result).toEqual(expectedData)
+      expect(expectedQuery).toEqual(receivedQuery)
     })
   })
 })
--- packages/ui/src/services/api/BichardApiV1.ts
+++ packages/ui/src/services/api/BichardApiV1.ts
@@ -1,5 +1,7 @@
 import { V1 } from "@moj-bichard7/common/apiEndpoints/versionedEndpoints"
 
+import type { ApiCaseQuery } from "@moj-bichard7/common/types/ApiCaseQuery"
+import type { CaseIndexMetadata } from "@moj-bichard7/common/types/Case"
 import type { DisplayFullCourtCase } from "types/display/CourtCases"
 import type ApiClient from "./ApiClient"
 import type BichardApiGateway from "./interfaces/BichardApiGateway"
@@ -13,5 +15,19 @@ export default class BichardApiV1 implements BichardApiGateway {
 
   async fetchCase(caseId: number): Promise<DisplayFullCourtCase | Error> {
     return await this.apiClient.get<DisplayFullCourtCase>(V1.Case.replace(":caseId", `${caseId}`))
+  }
+
+  async fetchCases(apiCaseQuery: ApiCaseQuery): Promise<CaseIndexMetadata | Error> {
+    const urlSearchParams = new URLSearchParams()
+
+    Object.entries(apiCaseQuery).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        value.forEach((v) => urlSearchParams.append(key, String(v)))
+      } else {
+        urlSearchParams.append(key, String(value))
+      }
+    })
+
+    return await this.apiClient.get<CaseIndexMetadata>(`${V1.Cases}?${urlSearchParams.toString()}`)
   }
 }
