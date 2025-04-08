3dbbf69f73cde427498e7609a566e4d5ab42c6af
joe-fol <joefolkard@protonmail.com>
2025-04-08 11:04:32 +0100

    Create parse function for db users query response

--- packages/cli/commands/user-comms/outageComms.ts
+++ packages/cli/commands/user-comms/outageComms.ts
@@ -1,21 +1,50 @@
 import fs from "fs"
 import path from "path"
-import { confirm } from "@inquirer/prompts"
-import nunjucks from "nunjucks"
+import { confirm, input } from "@inquirer/prompts"
+import getUsersFromDb from "./utils/getUsersFromDb"
+import renderTemplate from "./utils/renderTemplate"
+import type { Content } from "./templateTypes"
 
-export const outageComms = async (outageType: string, outageResolved: boolean) => {
-  const templateFile = outageResolved ? "outage-resolved.txt" : "outage.txt"
-  const templatePath = path.join(__dirname, "templates", templateFile)
+const parseDbUserResponse = (users: string) => {
+  const formattedUsers = users.replace(/'/g, '"')
+  const parseUsers = JSON.parse(formattedUsers)
+  const mapUsers = parseUsers.map(([name, email, message]: [string, string, string]) => ({ name, email, message }))
+  return mapUsers
+}
+
+export const outageComms = async (content: Content, templateFile: string) => {
+  const template = templateFile
+  const templatePath = path.join(__dirname, "templates", template)
   const templateContent = fs.readFileSync(templatePath, "utf-8")
 
-  const renderedEmail = nunjucks.renderString(templateContent, { outageType })
+  renderTemplate(templateContent, { ...content })
 
-  console.log("\n=== Preview Email ===\n")
-  console.log(renderedEmail)
+  const users = await getUsersFromDb()
 
-  const answer = await confirm({ message: "Do you want to use this template?" })
+  const parsedUsers = parseDbUserResponse(users)
+  const randomEntry = parsedUsers[Math.floor(Math.random() * parsedUsers.length)]
+  console.log(`To: ${randomEntry.email}`)
+  renderTemplate(templateContent, { firstName: randomEntry.name, ...content })
 
-  if (!answer) {
+  const confirmUpdatedTemplate = await confirm({ message: "Are you happy with the updated template?" })
+
+  if (!confirmUpdatedTemplate) {
     process.exit(1)
   }
+
+  const answer = await input({
+    message: `Are you sure you want to send this email to ${parsedUsers.length} users?\n Type 'confirm send' to send, anything else to abort:`,
+    validate: (value) => {
+      if (value.toLowerCase() !== "confirm send") {
+        return "Please type 'confirm send' to send or anything to abort."
+      }
+      return true
+    }
+  })
+
+  if (answer.toLowerCase() !== "confirm send") {
+    console.log("Operation cancelled.")
+    process.exit(1)
+  }
+  return { parsedUsers, templateContent }
 }
