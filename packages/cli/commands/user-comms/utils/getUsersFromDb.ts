de733e6c50104dc5132b05d073200129776e218b
joe-fol <joefolkard@protonmail.com>
2025-04-09 13:21:55 +0100

    Move confirm function out of getUsersFromDb

--- packages/cli/commands/user-comms/utils/getUsersFromDb.ts
+++ packages/cli/commands/user-comms/utils/getUsersFromDb.ts
@@ -1,14 +1,8 @@
 import { env } from "../../../config"
 import awsVault from "../../../utils/awsVault"
-import { confirm } from "@inquirer/prompts"
 
 const getUsersFromDb = async (): Promise<string> => {
   const { aws } = env.PROD
-  const confirmTemplateChoice = await confirm({ message: "Do you want to use this template?" })
-
-  if (!confirmTemplateChoice) {
-    process.exit(1)
-  }
 
   console.log("Fetching users from the database\n")
 
