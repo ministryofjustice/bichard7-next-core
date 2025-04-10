5457449ebe7312ee2a106cdab0eaa5cb24340f89
joe-fol <joefolkard@protonmail.com>
2025-04-10 08:59:54 +0100

    Fix query to get all users from database

--- packages/cli/commands/user-comms/utils/fetchUserDetailsForComms.ts
+++ packages/cli/commands/user-comms/utils/fetchUserDetailsForComms.ts
@@ -9,8 +9,7 @@ export async function fetchUserDetailsForComms(postgres: DataSource): Promise<st
       `
         SELECT u.forenames, u.email 
         FROM br7own.users AS u
-        WHERE u.email ILIKE '%madete%' 
-          AND u.deleted_at IS NULL
+        WHERE u.deleted_at IS NULL
         ORDER BY u.forenames
       `
     )
