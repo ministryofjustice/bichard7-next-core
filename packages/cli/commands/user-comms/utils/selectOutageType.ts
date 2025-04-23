ae3cbba57d4720969570a71e4be31ccca9402f56
joe-fol <joefolkard@protonmail.com>
2025-04-23 12:33:18 +0100

    Simplify values so we can use in subject of email

--- packages/cli/commands/user-comms/utils/selectOutageType.ts
+++ packages/cli/commands/user-comms/utils/selectOutageType.ts
@@ -8,15 +8,15 @@ export const selectOutageType = async (templateChoice: string) => {
       choices: [
         {
           name: "PNC Outage",
-          value: "PNC Outage",
+          value: "PNC",
           description: "Notify users of a PNC Outage"
         },
         {
           name: "PSN Outage",
-          value: "PSN Outage",
+          value: "PSN",
           description: "Notify users of a PSN Outage"
         },
-        { name: "Bichard Outage", value: "Bichard Outage", description: "Notify users of a Bichard Outage" }
+        { name: "Bichard", value: "Bichard", description: "Notify users of a Bichard Outage" }
       ]
     })
   } else if (templateChoice === "Outage Resolved") {
@@ -25,17 +25,17 @@ export const selectOutageType = async (templateChoice: string) => {
       choices: [
         {
           name: "PNC Outage Resolved",
-          value: "PNC Outage Resolved",
+          value: "PNC",
           description: "Notify users that the PNC outage has been resolved"
         },
         {
           name: "PSN Outage Resolved",
-          value: "PSN Outage Resolved",
+          value: "PSN",
           description: "Notify users that the PSN outage has been resolved"
         },
         {
           name: "Bichard Outage Resolved",
-          value: "Bichard Outage Resolved",
+          value: "Bichard",
           description: "Notify users that the Bichard outage has been resolved"
         }
       ]
