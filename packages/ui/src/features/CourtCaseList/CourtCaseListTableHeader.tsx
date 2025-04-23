69f7ced6789baaff2992f939b1ec44c8aff2fcbe
Sen <65850511+Senny10@users.noreply.github.com>
2025-04-23 12:21:39 +0100

    Bicaws7 3378 dynamic changes not announced by screen readers (#1400)
    * Add ARIA attributes for improved accessibility in CourtCaseDetails and Sidebar
    
    * Add ARIA attributes for improved accessibility in CourtCaseFilter
    
    * Refactor aria-label in CourtCaseFilter for clarity
    
    * Add aria-sort attributes for improved accessibility in CourtCaseListTableHeader
    
    * wip
    
    * Add ARIA attributes for improved accessibility in PaginationNavigation
    
    * Refactor PaginationNavigation to simplify Link component and improve accessibility
    
    * force screen reader focus on filter changes
    
    * wip
    
    * Enhance accessibility in Pagination component with live region announcements and ARIA attributes
    
    * Enhance accessibility in CourtCaseList by adding live region announcements and improving focus handling on header clicks
    
    * Refactor CaseListQueryParams type by removing unused properties and simplifying reasonCodes definition
    
    * Remove unnecessary ARIA attributes from Triggers tab in Sidebar component
    
    * Enhance accessibility in OffenceDetails component by adding ARIA attributes for live region announcements
    
    ---------
    
    Co-authored-by: Richard Race <1273965+richrace@users.noreply.github.com>

--- packages/ui/src/features/CourtCaseList/CourtCaseListTableHeader.tsx
+++ packages/ui/src/features/CourtCaseList/CourtCaseListTableHeader.tsx
@@ -12,6 +12,16 @@ export const CourtCaseListTableHeader = ({ order }: CourtCaseListTableHeaderProp
   const { basePath, query } = useRouter()
   const orderByParams = (orderBy: string) => `${basePath}/?${new URLSearchParams({ ...query, orderBy, order })}`
   const className = "govuk-table__header table-column-header-cell govuk-body-s"
+  const handleHeaderClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    const target = event.target as HTMLElement
+    const headerId = target.closest("a")?.id
+    if (headerId) {
+      const headerElement = document.getElementById(headerId)
+      if (headerElement) {
+        headerElement.focus()
+      }
+    }
+  }
 
   return (
     <tr className="govuk-table__row">
@@ -23,37 +33,61 @@ export const CourtCaseListTableHeader = ({ order }: CourtCaseListTableHeaderProp
           className={"table-column-header-link"}
           href={orderByParams("defendantName")}
           id="defendant-name-sort"
+          aria-live="polite"
+          aria-sort={query.orderBy === "defendantName" ? (query.order === "asc" ? "ascending" : "descending") : "none"}
+          onClick={handleHeaderClick}
         >
           {"Defendant name"}
           <ColumnOrderIcons columnName={"defendantName"} currentOrder={query.order} orderBy={query.orderBy} />
         </HeaderLink>
       </HeaderCell>
       <HeaderCell className={className} style={{ width: "115px" }}>
-        <HeaderLink className={"table-column-header-link"} href={orderByParams("courtDate")} id="court-date-sort">
+        <HeaderLink
+          className={"table-column-header-link"}
+          href={orderByParams("courtDate")}
+          id="court-date-sort"
+          aria-live="polite"
+          aria-sort={query.orderBy === "courtDate" ? (query.order === "asc" ? "ascending" : "descending") : "none"}
+          onClick={handleHeaderClick}
+        >
           {"Court date"}
           <ColumnOrderIcons columnName={"courtDate"} currentOrder={query.order} orderBy={query.orderBy} />
         </HeaderLink>
       </HeaderCell>
       <HeaderCell className={className}>
-        <HeaderLink className={"table-column-header-link"} href={orderByParams("courtName")} id="court-name-sort">
+        <HeaderLink
+          className={"table-column-header-link"}
+          href={orderByParams("courtName")}
+          id="court-name-sort"
+          aria-live="polite"
+          aria-sort={query.orderBy === "courtName" ? (query.order === "asc" ? "ascending" : "descending") : "none"}
+          onClick={handleHeaderClick}
+        >
           {"Court name"}
           <ColumnOrderIcons columnName={"courtName"} currentOrder={query.order} orderBy={query.orderBy} />
         </HeaderLink>
       </HeaderCell>
       <HeaderCell className={className}>
-        <HeaderLink className={"table-column-header-link"} href={orderByParams("ptiurn")} id="ptiurn-sort">
+        <HeaderLink
+          className={"table-column-header-link"}
+          href={orderByParams("ptiurn")}
+          id="ptiurn-sort"
+          aria-live="polite"
+          aria-sort={query.orderBy === "ptiurn" ? (query.order === "asc" ? "ascending" : "descending") : "none"}
+          onClick={handleHeaderClick}
+        >
           {"PTIURN"}
           <ColumnOrderIcons columnName={"ptiurn"} currentOrder={query.order} orderBy={query.orderBy} />
         </HeaderLink>
       </HeaderCell>
       <HeaderCell className={className}>
-        <ColumnHeading>{"Notes"}</ColumnHeading>
+        <ColumnHeading aria-sort="none">{"Notes"}</ColumnHeading>
       </HeaderCell>
       <HeaderCell className={className}>
-        <ColumnHeading>{"Reason"}</ColumnHeading>
+        <ColumnHeading aria-sort="none">{"Reason"}</ColumnHeading>
       </HeaderCell>
       <HeaderCell className={className}>
-        <ColumnHeading>{"Locked by"}</ColumnHeading>
+        <ColumnHeading aria-sort="none">{"Locked by"}</ColumnHeading>
       </HeaderCell>
     </tr>
   )
