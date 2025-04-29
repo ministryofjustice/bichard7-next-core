e7d6a7be49e7ce7dc47e206c8558ce1d416fdfe8
Richard Cane <richard.cane@madetech.com>
2025-04-29 12:59:00 +0100

    Add LEDS VPN config to CLI importer (#1428)

--- scripts/import-openvpn-profiles.sh
+++ scripts/import-openvpn-profiles.sh
@@ -14,6 +14,10 @@ WORKSPACE=uat aws-vault exec bichard7-shared-uat -- ./get-vpn-config.sh
 "$OPENVPN_CLIENT" --remove-profile="B7 UAT"
 "$OPENVPN_CLIENT" --import-profile="~/cjse-uat-config.ovpn" --name="B7 UAT"
 
+WORKSPACE=leds aws-vault exec bichard7-shared-uat -- ./get-vpn-config.sh
+"$OPENVPN_CLIENT" --remove-profile="B7 LEDS"
+"$OPENVPN_CLIENT" --import-profile="~/cjse-leds-config.ovpn" --name="B7 LEDS"
+
 WORKSPACE=e2e-test aws-vault exec bichard7-shared-e2e-test -- ./get-vpn-config.sh
 "$OPENVPN_CLIENT" --remove-profile="B7 e2e test"
 "$OPENVPN_CLIENT" --import-profile="~/cjse-e2e-test-config.ovpn" --name="B7 e2e test"
