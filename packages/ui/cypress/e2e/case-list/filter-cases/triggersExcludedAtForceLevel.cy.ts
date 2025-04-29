b54eff248dc19097b34cb4d0cf00fa58d05e52dd
Richard Race <1273965+richrace@users.noreply.github.com>
2025-04-29 15:15:08 +0100

    Fix more cypress e2e tests

--- packages/ui/cypress/e2e/case-list/filter-cases/triggersExcludedAtForceLevel.cy.ts
+++ packages/ui/cypress/e2e/case-list/filter-cases/triggersExcludedAtForceLevel.cy.ts
@@ -10,23 +10,26 @@ describe("Triggers excluded at force level", () => {
 
     cy.get(".govuk-checkboxes__item #trpr0008 + label").should("not.exist")
     cy.get(".govuk-checkboxes__item #trpr0010 + label").contains("PR10 - Conditional bail").should("exist")
-    cy.get(".govuk-checkboxes__item #trpr0020 + label").contains("PR20 - Breach").should("exist")
+    cy.get(".govuk-checkboxes__item #trpr0019 + label").contains("PR19 - Bail direction").should("exist")
     cy.get(".govuk-checkboxes__item #trpr0030 + label").contains("PR30 - Pre-charge bail application").should("exist")
 
-    cy.get("input#reasonCodes").should("value", "PR10 PR20 PR30")
+    cy.get("input#reasonCodes").should("value", "PR10 PR19 PR30")
+  })
+
+  it("should not show excluded triggers in trigger groups and reason codes in the reason codes input when we click on results", () => {
+    cy.get("input#results").should("exist")
+    cy.get("input#results").click()
+
+    cy.get(".govuk-checkboxes__item #trpr0006").should("not.exist")
+    cy.get(".govuk-checkboxes__item #trpr0016").should("not.exist")
+    cy.get(".govuk-checkboxes__item #trpr0020 + label").should("exist")
+    cy.get(".govuk-checkboxes__item #trpr0025").should("not.exist")
+
+    cy.get("input#reasonCodes").should("value", "PR20")
   })
 
   it("should not show excluded triggers in trigger groups and reason codes in the reason codes input when we click on custody", () => {
-    cy.get("input#custody").should("exist")
-    cy.get("input#custody").click()
-
-    cy.get(".govuk-checkboxes__item #trpr0001 + label").contains("PR01 - Disqualified driver").should("exist")
-    cy.get(".govuk-checkboxes__item #trpr0005 + label").should("not.exist")
-    cy.get(".govuk-checkboxes__item #trpr0006 + label").should("not.exist")
-    cy.get(".govuk-checkboxes__item #trpr0019 + label").contains("PR19 - Bail direction").should("exist")
-    cy.get(".govuk-checkboxes__item #trpr0021 + label").should("not.exist")
-
-    cy.get("input#reasonCodes").should("value", "PR01 PR19")
+    cy.get("input#custody").should("not.exist")
   })
 
   it("should not show excluded triggers in trigger groups and reason codes in the reason codes input when we click on Orders", () => {
@@ -34,11 +37,10 @@ describe("Triggers excluded at force level", () => {
     cy.get("input#orders").click()
 
     cy.get(".govuk-checkboxes__item #trpr0003 + label").should("not.exist")
-    cy.get(".govuk-checkboxes__item #trpr0016 + label").should("not.exist")
-    cy.get(".govuk-checkboxes__item #trpr0025 + label").should("not.exist")
+    cy.get(".govuk-checkboxes__item #trps0008 + label").should("not.exist")
+    cy.get(".govuk-checkboxes__item #trpr0021 + label").should("not.exist")
     cy.get(".govuk-checkboxes__item #trpr0026 + label").contains("PR26 - Disq. Suspended").should("exist")
     cy.get(".govuk-checkboxes__item #trpr0029 + label").contains("PR29 - Civil Proceedings").should("exist")
-    cy.get(".govuk-checkboxes__item #trps0008 + label").should("not.exist")
 
     cy.get("input#reasonCodes").should("value", "PR26 PR29")
   })
