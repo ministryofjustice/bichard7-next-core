775ca508383d377b12071431814303cddc3e2576
Tausif Patel <tausifpatel.dev@gmail.com>
2025-05-13 14:23:54 +0100

    Refine usage report (#1323)
    * Update usage report to capture exceptions generated and resolved by force
    
    * Add a function to fetch forces separately
    
    * Modify fetchEvents function to include worker that runs promises in parallel, reducing time taken to generate report
    
    * Refine report generation to generate reports on the basis of force
    
    * Tiny refactor
    
    * Refactor
    
    ---------
    
    Co-authored-by: Sen <65850511+Senny10@users.noreply.github.com>

--- scripts/analytics/usage-report/generateReportData.ts
+++ scripts/analytics/usage-report/generateReportData.ts
@@ -1,6 +1,42 @@
 import EventCode from "@moj-bichard7/common/types/EventCode"
-import { AuditLogEvent } from "../../../packages/common/types/AuditLogEvent"
-import { ReportDataResult, getDateString, isNewUIEvent } from "./common"
+import { FullAuditLogEvent, ReportDataResult, eventCodesToDisplay, getDateString, isNewUIEvent } from "./common"
+
+type UpdateReportDataOptions = {
+  event: FullAuditLogEvent
+  eventCodeKey: string
+  numberToAdd: number
+}
+
+const editableExceptions = [
+  "HO100102",
+  "HO100323",
+  "HO100200",
+  "HO100300",
+  "HO100322",
+  "HO100206",
+  "HO100301",
+  "HO100321",
+  "H0100329",
+  "H0100332"
+]
+
+const AUTO_RESUBMISSION_EXCEPTIONS = ["HO100314", "HO100302", "HO100404", "HO100402"]
+
+const eventsPostExceptionsResolved = [
+  EventCode.PncUpdated,
+  EventCode.IgnoredAlreadyOnPNC,
+  EventCode.IgnoredAncillary,
+  EventCode.IgnoredAppeal,
+  EventCode.IgnoredDisabled,
+  EventCode.IgnoredNoOffences,
+  EventCode.IgnoredNonrecordable,
+  EventCode.IgnoredReopened
+]
+
+const currentExceptions: Record<string, string[]> = {}
+const currentResubmissionUi: Record<string, "new-ui" | "old-ui"> = {}
+const exceptionsResolvedManually = EventCode.ExceptionsResolved
+const exceptionsResubmittedEvents = [EventCode.HearingOutcomeResubmittedPhase1, EventCode.HearingOutcomeResubmittedPhase2]
 
 const getDates = (start: Date, end: Date) => {
   let date = new Date(start)
@@ -13,142 +49,230 @@ const getDates = (start: Date, end: Date) => {
   return dates
 }
 
-const getEventCodeKey = (event: AuditLogEvent) => (isNewUIEvent(event) ? "new-ui." : "old-ui.") + event.eventCode
+const getEventCodeKey = (event: FullAuditLogEvent) => (isNewUIEvent(event) ? "new-ui." : "old-ui.") + event.eventCode
 
-const generateReportData = (events: AuditLogEvent[], start: Date, end: Date): ReportDataResult => {
-  const allEventCodes = new Set<string>()
-  const newUiEventCodes = new Set<string>()
-  const usersWithNewUiEvent = new Set<string>()
+const getResolvedExceptions = (event: FullAuditLogEvent) => {
+  if (event.eventCode !== EventCode.ExceptionsGenerated) {
+    return currentExceptions[event._messageId]
+  }
 
-  const allDailyData = {} as Record<string, Record<string, number>>
-  const allMonthlyData = {} as Record<string, Record<string, number>>
-  const allDailyUserData = {} as Record<string, Record<string, Record<string, number>>>
-  const allMonthlyUserData = {} as Record<string, Record<string, Record<string, number>>>
-  const allUserData = {} as Record<string, Record<string, number>>
-  const allSummary = {} as Record<EventCode, number>
+  const previousExceptions = currentExceptions[event._messageId]
+  const newExceptions = extractExceptionCodes(event)
+  return previousExceptions.filter((previousException) => !newExceptions.includes(previousException))
+}
 
-  const newUiDailyData = {} as Record<string, Record<string, number>>
-  const newUiMonthlyData = {} as Record<string, Record<string, number>>
-  const newUiDailyUserData = {} as Record<string, Record<string, Record<string, number>>>
-  const newUiMonthlyUserData = {} as Record<string, Record<string, Record<string, number>>>
-  const newUiUserData = {} as Record<string, Record<string, number>>
-  const newUiSummary = {} as Record<EventCode, number>
+const extractExceptionCodes = (event: FullAuditLogEvent): string[] =>
+  Object.keys(event.attributes ?? {})
+    .filter((key) => /Error \d{1,2} Details/.test(key))
+    .map((key) => event.attributes?.[key]?.toString().split("||")[0]) as string[]
+
+const excludeAutoResubmissionExceptions = (exceptionCode: string): boolean =>
+  !AUTO_RESUBMISSION_EXCEPTIONS.includes(exceptionCode)
+
+const filterAutoResubmissionExceptions = (exceptionCode: string): boolean =>
+  AUTO_RESUBMISSION_EXCEPTIONS.includes(exceptionCode)
+
+const createUpdateReportDataFunction =
+  (forceOwners: Record<string, number>, date: string, month: string) =>
+  async (reportData: any, { event, eventCodeKey, numberToAdd }: UpdateReportDataOptions) => {
+    reportData.allSummary[eventCodeKey] = (reportData.allSummary[eventCodeKey] || 0) + numberToAdd
+
+    reportData.allDailyData[date] = {
+      ...(reportData.allDailyData[date] ?? {}),
+      [eventCodeKey]: (reportData.allDailyData[date]?.[eventCodeKey] || 0) + numberToAdd
+    }
+
+    reportData.allMonthlyData[month] = {
+      ...(reportData.allMonthlyData[month] ?? {}),
+      [eventCodeKey]: (reportData.allMonthlyData[month]?.[eventCodeKey] || 0) + numberToAdd
+    }
+
+    if (event.eventCode !== EventCode.ExceptionsGenerated) {
+      let username = event.user ?? "unknown"
+      reportData.allUserData[username] = {
+        ...(reportData.allUserData[username] ?? {}),
+        [eventCodeKey]: (reportData.allUserData[username]?.[eventCodeKey] || 0) + numberToAdd
+      }
+
+      reportData.allDailyUserData[date] = {
+        ...(reportData.allDailyUserData[date] ?? {}),
+        [username]: {
+          ...(reportData.allDailyUserData[date]?.[username] ?? {}),
+          [eventCodeKey]: (reportData.allDailyUserData[date]?.[username]?.[eventCodeKey] || 0) + numberToAdd
+        }
+      }
+
+      reportData.allMonthlyUserData[month] = {
+        ...(reportData.allMonthlyUserData[month] ?? {}),
+        [username]: {
+          ...(reportData.allMonthlyUserData[month]?.[username] ?? {}),
+          [eventCodeKey]: (reportData.allMonthlyUserData[month]?.[username]?.[eventCodeKey] || 0) + numberToAdd
+        }
+      }
+    }
+
+    const forceOwner = forceOwners[event._messageId]
+    reportData.allMonthlyForceData[month] = {
+      ...(reportData.allMonthlyForceData[month] ?? {}),
+      [forceOwner!]: {
+        ...(reportData.allMonthlyForceData[month]?.[forceOwner!] ?? {}),
+        [eventCodeKey]: (reportData.allMonthlyForceData[month]?.[forceOwner!]?.[eventCodeKey] || 0) + numberToAdd
+      }
+    }
+  }
+
+const generateReportData = async (
+  events: FullAuditLogEvent[],
+  start: Date,
+  end: Date,
+  forceOwners: Record<string, number>
+): Promise<ReportDataResult> => {
+  const reportData = {
+    allEventCodes: new Set<string>(),
+    newUiEventCodes: new Set<string>(),
+    usersWithNewUiEvent: new Set<string>(),
+
+    allDailyData: {} as Record<string, Record<string, number>>,
+    allMonthlyData: {} as Record<string, Record<string, number>>,
+    allDailyUserData: {} as Record<string, Record<string, Record<string, number>>>,
+    allMonthlyUserData: {} as Record<string, Record<string, Record<string, number>>>,
+    allMonthlyForceData: {} as Record<string, Record<string, Record<string, number>>>,
+    allUserData: {} as Record<string, Record<string, number>>,
+    allSummary: {} as Record<EventCode, number>,
+
+    newUiDailyData: {} as Record<string, Record<string, number>>,
+    newUiMonthlyData: {} as Record<string, Record<string, number>>,
+    newUiDailyUserData: {} as Record<string, Record<string, Record<string, number>>>,
+    newUiMonthlyUserData: {} as Record<string, Record<string, Record<string, number>>>,
+    newUiUserData: {} as Record<string, Record<string, number>>,
+    newUiSummary: {} as Record<EventCode, number>
+  }
 
   getDates(start, end).forEach((date: string) => {
     const month = date.substring(0, 7)
-    allDailyData[date] = {}
-    allMonthlyData[month] = {}
-    newUiDailyData[date] = {}
-    newUiMonthlyData[month] = {}
+    reportData.allDailyData[date] = {}
+    reportData.allMonthlyData[month] = {}
+    reportData.newUiDailyData[date] = {}
+    reportData.newUiMonthlyData[month] = {}
   })
 
-  events.forEach((event) => {
+  for (const event of events) {
+    if (event.eventSource === "ResubmitFailedPNCMessages") {
+      if (currentExceptions[event._messageId]) {
+        delete currentExceptions[event._messageId]
+      }
+      continue
+    }
+
+    if (event.eventCode === EventCode.HearingOutcomeDetails) {
+      const forceOwner = event.attributes?.["Force Owner"]?.toString()?.substring(0, 2)
+      if (forceOwner) {
+        forceOwners[event._messageId] = Number(forceOwner)
+      }
+
+      continue
+    }
+
     const date = getDateString(event.timestamp)
     const month = date.substring(0, 7)
-    const eventCodeKey = getEventCodeKey(event)
-    const username = event.user ?? "unknown"
-    allEventCodes.add(eventCodeKey)
-    if (isNewUIEvent(event)) {
-      usersWithNewUiEvent.add(username)
-    }
+    const updateReportData = createUpdateReportDataFunction(forceOwners, date, month)
 
-    allSummary[eventCodeKey] = (allSummary[eventCodeKey] || 0) + 1
+    let eventCodeKey = getEventCodeKey(event)
+    let numberToAdd = 1
 
-    allUserData[username] = {
-      ...(allUserData[username] ?? {}),
-      [eventCodeKey]: (allUserData[username]?.[eventCodeKey] || 0) + 1
-    }
+    if (
+      currentExceptions[event._messageId] &&
+      currentResubmissionUi[event._messageId] &&
+      (event.eventCode === EventCode.ExceptionsGenerated || eventsPostExceptionsResolved.includes(event.eventCode))
+    ) {
+      const customEventCodeKey = `${currentResubmissionUi[event._messageId]}.Resolved exceptions`
+      reportData.allEventCodes.add(customEventCodeKey)
+      const resolvedExceptions = getResolvedExceptions(event)
 
-    allDailyData[date] = {
-      ...(allDailyData[date] ?? {}),
-      [eventCodeKey]: (allDailyData[date]?.[eventCodeKey] || 0) + 1
-    }
+      await updateReportData(reportData, {
+        event,
+        numberToAdd: resolvedExceptions.length,
+        eventCodeKey: customEventCodeKey
+      })
 
-    allMonthlyData[month] = {
-      ...(allMonthlyData[month] ?? {}),
-      [eventCodeKey]: (allMonthlyData[month]?.[eventCodeKey] || 0) + 1
-    }
-
-    allDailyUserData[date] = {
-      ...(allDailyUserData[date] ?? {}),
-      [username]: {
-        ...(allDailyUserData[date]?.[username] ?? {}),
-        [eventCodeKey]: (allDailyUserData[date]?.[username]?.[eventCodeKey] || 0) + 1
+      // Editable exceptions
+      const resolvedEditableExceptions = resolvedExceptions.filter((exceptionCode) =>
+        editableExceptions.includes(exceptionCode)
+      )
+      if (resolvedEditableExceptions.length > 0) {
+        numberToAdd = resolvedEditableExceptions.length
+        const customEventCodeKey = `${currentResubmissionUi[event._messageId]}.Resolved editable exceptions`
+        reportData.allEventCodes.add(customEventCodeKey)
+        await updateReportData(reportData, {
+          event,
+          numberToAdd,
+          eventCodeKey: customEventCodeKey
+        })
       }
     }
 
-    allMonthlyUserData[month] = {
-      ...(allMonthlyUserData[month] ?? {}),
-      [username]: {
-        ...(allMonthlyUserData[month]?.[username] ?? {}),
-        [eventCodeKey]: (allMonthlyUserData[month]?.[username]?.[eventCodeKey] || 0) + 1
+    if (event.eventCode === EventCode.ExceptionsGenerated) {
+      eventCodeKey = ".Total exceptions"
+      currentExceptions[event._messageId] = extractExceptionCodes(event)
+      const numberOfExceptions = currentExceptions[event._messageId].filter(excludeAutoResubmissionExceptions).length
+      numberToAdd = numberOfExceptions
+    }
+
+    if (event.eventCode === exceptionsResolvedManually) {
+      const autoResubmissionExceptions =
+        currentExceptions[event._messageId]?.filter(filterAutoResubmissionExceptions).length || 0
+
+      if (autoResubmissionExceptions > 0) {
+        await updateReportData(reportData, {
+          event,
+          numberToAdd: autoResubmissionExceptions,
+          eventCodeKey: ".Total exceptions"
+        })
+      }
+      numberToAdd = currentExceptions[event._messageId]?.length || 0
+    }
+
+    if (event.eventCode === EventCode.TriggersGenerated) {
+      numberToAdd = Number(event?.attributes?.["Number of Triggers"] || 0)
+      eventCodeKey = ".Total triggers"
+    }
+
+    if (event.eventCode === EventCode.TriggersResolved) {
+      numberToAdd = Number(event?.attributes?.["Number Of Triggers"] || 0)
+    }
+
+    if (exceptionsResubmittedEvents.includes(event.eventCode)) {
+      const ui = isNewUIEvent(event) ? "new-ui" : "old-ui"
+      eventCodeKey = `${ui}.Exceptions resubmitted`
+      currentResubmissionUi[event._messageId] = ui
+      numberToAdd = currentExceptions[event._messageId]?.length ?? 0
+      if (event.user === "System") {
+        numberToAdd = 0
       }
     }
-  })
 
-  events
-    .filter((event) => usersWithNewUiEvent.has(event.user ?? "Unknown"))
-    .forEach((event) => {
-      const date = getDateString(event.timestamp)
-      const month = date.substring(0, 7)
-      const eventCodeKey = getEventCodeKey(event)
-      const username = event.user ?? "unknown"
-      newUiEventCodes.add(eventCodeKey)
+    reportData.allEventCodes.add(eventCodeKey)
 
-      newUiSummary[eventCodeKey] = (newUiSummary[eventCodeKey] || 0) + 1
+    await updateReportData(reportData, { event, numberToAdd, eventCodeKey })
+  }
 
-      newUiUserData[username] = {
-        ...(newUiUserData[username] ?? {}),
-        [eventCodeKey]: (newUiUserData[username]?.[eventCodeKey] || 0) + 1
-      }
-
-      newUiDailyData[date] = {
-        ...(newUiDailyData[date] ?? {}),
-        [eventCodeKey]: (newUiDailyData[date]?.[eventCodeKey] || 0) + 1
-      }
-
-      newUiMonthlyData[month] = {
-        ...(newUiMonthlyData[month] ?? {}),
-        [eventCodeKey]: (newUiMonthlyData[month]?.[eventCodeKey] || 0) + 1
-      }
-
-      newUiDailyUserData[date] = {
-        ...(newUiDailyUserData[date] ?? {}),
-        [username]: {
-          ...(newUiDailyUserData[date]?.[username] ?? {}),
-          [eventCodeKey]: (newUiDailyUserData[date]?.[username]?.[eventCodeKey] || 0) + 1
-        }
-      }
-
-      newUiMonthlyUserData[month] = {
-        ...(newUiMonthlyUserData[month] ?? {}),
-        [username]: {
-          ...(newUiMonthlyUserData[month]?.[username] ?? {}),
-          [eventCodeKey]: (newUiMonthlyUserData[month]?.[username]?.[eventCodeKey] || 0) + 1
-        }
-      }
-    })
+  reportData.allEventCodes = new Set(
+    Array.from(reportData.allEventCodes).filter((eventCode) =>
+      eventCodesToDisplay.some((eventCodeToDisplay) => eventCode.includes(eventCodeToDisplay))
+    )
+  )
 
   return {
     allEvents: {
-      summary: allSummary,
-      users: allUserData,
-      daily: allDailyData,
-      monthly: allMonthlyData,
-      dailyUsers: allDailyUserData,
-      monthlyUsers: allMonthlyUserData,
-      eventCodes: Array.from(allEventCodes)
-    },
-    withNewUiEvents: {
-      summary: newUiSummary,
-      users: newUiUserData,
-      daily: newUiDailyData,
-      monthly: newUiMonthlyData,
-      dailyUsers: newUiDailyUserData,
-      monthlyUsers: newUiMonthlyUserData,
-      eventCodes: Array.from(newUiEventCodes)
-    },
-    usersWithNewUiEvent: Array.from(usersWithNewUiEvent)
+      summary: reportData.allSummary,
+      users: reportData.allUserData,
+      daily: reportData.allDailyData,
+      monthly: reportData.allMonthlyData,
+      dailyUsers: reportData.allDailyUserData,
+      monthlyUsers: reportData.allMonthlyUserData,
+      monthlyForces: reportData.allMonthlyForceData,
+      eventCodes: Array.from(reportData.allEventCodes)
+    }
   }
 }
 
