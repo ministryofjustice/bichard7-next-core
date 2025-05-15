7741965d7223b27c1cb668ca59503a1049282585
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2025-05-15 10:44:58 +0100

    Remove duplicated setEnvironmentVariables.ts

--- packages/core/conductor-tasks/incomingMessageHandler/convertSpiToAho.integration.test.ts
+++ packages/core/conductor-tasks/incomingMessageHandler/convertSpiToAho.integration.test.ts
@@ -1,4 +1,4 @@
-import "../../phase1/tests/helpers/setEnvironmentVariables"
+import "../../tests/helpers/setEnvironmentVariables"
 
 import type { S3ClientConfig } from "@aws-sdk/client-s3"
 
--- packages/core/phase1/phase1.comparison.test.ts
+++ packages/core/phase1/phase1.comparison.test.ts
@@ -1,6 +1,6 @@
 import "jest-xml-matcher"
 
-import "./tests/helpers/setEnvironmentVariables"
+import "../tests/helpers/setEnvironmentVariables"
 
 import TriggerCode from "@moj-bichard7-developers/bichard7-next-data/dist/types/TriggerCode"
 import { AuditLogEventSource } from "@moj-bichard7/common/types/AuditLogEvent"
