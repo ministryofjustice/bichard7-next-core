c9079f7c699ce95da97e0603d9b2a50a6fd9faad
Tausif Patel <tausifpatel.dev@gmail.com>
2025-06-26 14:25:24 +0100

    Remove padding from the page layout on smaller screen size

--- packages/ui/src/components/NavBar/NavBar.styles.tsx
+++ packages/ui/src/components/NavBar/NavBar.styles.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components"
 const MojNavContainer = styled.div`
   max-width: 100%;
   padding: 0 40px;
+
+  @media (max-width: 1019px) {
+    padding: 0;
+  }
 `
 
 export { MojNavContainer }
--- packages/ui/src/components/PageTemplate.styles.tsx
+++ packages/ui/src/components/PageTemplate.styles.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components"
 const GovukWidthContainer = styled.div`
   max-width: 100%;
   padding: 30px 40px;
+
+  @media (max-width: 1019px) {
+    padding: 0;
+  }
 `
 
 export { GovukWidthContainer }
