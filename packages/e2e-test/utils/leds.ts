034d91bb43dc2bab76ca0a8afb6c8426914027b1
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2025-07-18 15:05:10 +0100

    Update E2E step to clear LEDS mocks

--- packages/e2e-test/utils/leds.ts
+++ packages/e2e-test/utils/leds.ts
@@ -5,11 +5,13 @@ import axios from "axios"
 export const mockLedsDataForTest = async function (this: Bichard) {
   const specFolder = path.dirname(this.featureUri)
   const mocks = await import(`../${specFolder}/mock-leds-responses`)
-  const url = "http://localhost:1080/mockserver/expectation"
+  const baseUrl = "http://localhost:1080/mockserver"
 
   this.mocks = mocks.default(`${specFolder}/pnc-data.xml`, this)
 
+  await axios.put(`${baseUrl}/clear`)
+
   for (const mock of this.mocks) {
-    await axios.put(url, mock)
+    await axios.put(`${baseUrl}/expectation`, mock)
   }
 }
