e0566f4549879cefd560be9e51862f40759d0768
Richard Race <1273965+richrace@users.noreply.github.com>
2025-07-23 16:27:02 +0100

    Fix test config

--- packages/api/src/tests/helpers/dynamoDbConfig.ts
+++ packages/api/src/tests/helpers/dynamoDbConfig.ts
@@ -1,12 +1,14 @@
 import type { DynamoDbConfig } from "../../services/gateways/dynamo"
 
 const auditLogDynamoConfig: DynamoDbConfig = {
-  accessKeyId: "DUMMY",
   auditLogTableName: "auditLogTable",
+  credentials: {
+    accessKeyId: "DUMMY",
+    secretAccessKey: "DUMMY"
+  },
   endpoint: "http://localhost:4566",
   eventsTableName: "auditLogEventsTable",
-  region: "eu-west-2",
-  secretAccessKey: "DUMMY"
+  region: "eu-west-2"
 }
 
 export default auditLogDynamoConfig
