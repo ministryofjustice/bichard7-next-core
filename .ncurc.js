e3a6521624751d327942fa6d99e12191bdf9e323
Richard Race <1273965+richrace@users.noreply.github.com>
2025-07-24 12:12:43 +0100

    Pin zod and zod related packages (#1513)

--- .ncurc.js
+++ .ncurc.js
@@ -8,6 +8,8 @@ const semver = new RegExp(
     - v5 is a breaking change
   - @types/diff
     - changed the Change type to require extra values
+  - zod, fastify-zod-openapi, zod-openapi, zod-validation-error
+    - zod v4 has breaking changes and all of these related packages rely on the new version of zod
 
   Ignored:
   - bichard7-next-data-x.x.x
@@ -18,7 +20,7 @@ const semver = new RegExp(
   - @cucumber/cucumber
     - from tests repo migration, version was pinned to v9
 */
-const pinned = ["chalk", "@types/diff"]
+const pinned = ["chalk", "@types/diff", "zod", "fastify-zod-openapi", "zod-openapi", "zod-validation-error"]
 const ignored = [`bichard7-next-data-(${semver.source})`, "p-limit", "esbuild", "@cucumber/cucumber", "http-status"]
 
 module.exports = {
