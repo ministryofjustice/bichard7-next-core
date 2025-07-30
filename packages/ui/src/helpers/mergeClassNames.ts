1cb62c548a4fa4271f1861a0fbd7fbe1199eb1f9
Tom Watkins <tom.watkins@justice.gov.uk>
2025-07-30 11:34:25 +0100

    Refactor to take multiple class name params

--- packages/ui/src/helpers/mergeClassNames.ts
+++ packages/ui/src/helpers/mergeClassNames.ts
@@ -1,7 +1,3 @@
-export function mergeClassNames(existingClassNames: string, newClassNames: string | undefined): string {
-  if (!newClassNames) {
-    return existingClassNames
-  }
-
-  return [...new Set([...existingClassNames.split(" "), ...newClassNames.split(" ")].filter(Boolean))].join(" ")
+export function mergeClassNames(...classNames: Array<string | undefined>): string {
+  return [...new Set([...classNames.filter(Boolean).flatMap((className) => className?.split(" "))])].join(" ")
 }
