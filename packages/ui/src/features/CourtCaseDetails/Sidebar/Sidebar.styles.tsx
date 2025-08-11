801c31a2947c7071d857af69366eac8f53153bdd
Tom Watkins <tom.watkins@justice.gov.uk>
2025-08-11 14:54:06 +0100

    Make tabs use 1/3 of width

--- packages/ui/src/features/CourtCaseDetails/Sidebar/Sidebar.styles.tsx
+++ packages/ui/src/features/CourtCaseDetails/Sidebar/Sidebar.styles.tsx
@@ -3,12 +3,6 @@ import styled from "styled-components"
 const SidebarContainer = styled.div`
   margin-top: -41px;
 
-  li > a {
-    cursor: pointer;
-    font-weight: bold;
-    text-decoration: none;
-  }
-
   #pnc-details {
     padding: 0;
   }
@@ -17,6 +11,16 @@ const SidebarContainer = styled.div`
 const TabHeaders = styled.ul`
   display: flex;
   flex-wrap: nowrap;
+
+  li {
+    flex: 1;
+  }
+
+  li > a {
+    cursor: pointer;
+    font-weight: bold;
+    text-decoration: none;
+  }
 `
 
 export { SidebarContainer, TabHeaders }
