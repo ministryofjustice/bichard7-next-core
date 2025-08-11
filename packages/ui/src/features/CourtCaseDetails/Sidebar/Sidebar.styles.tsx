b8e967325eed45d0fa74dfdbaa03602d24f2f797
Tom Watkins <tom.watkins@justice.gov.uk>
2025-08-11 15:49:51 +0100

    Header sizing

--- packages/ui/src/features/CourtCaseDetails/Sidebar/Sidebar.styles.tsx
+++ packages/ui/src/features/CourtCaseDetails/Sidebar/Sidebar.styles.tsx
@@ -11,9 +11,15 @@ const SidebarContainer = styled.div`
 const TabHeaders = styled.ul`
   display: flex;
   flex-wrap: nowrap;
+  line-height: 1.3157894737;
+  border-bottom: 1px solid #b1b4b6;
 
   li {
     flex: 1;
+    margin-left: 0;
+    margin-right: 5px;
+    text-align: center;
+    line-height: 1.3157894737;
   }
 
   li:last-child {
@@ -23,16 +29,37 @@ const TabHeaders = styled.ul`
   li > a {
     cursor: pointer;
     text-decoration: underline;
+    font-size: var(--case-details-default-font-size);
+    margin-bottom: 0;
+
+    &:link {
+      color: #222;
+    }
   }
 
   .govuk-tabs__list-item {
     margin-bottom: 5px;
+    margin-top: 5px;
+    box-sizing: border-box;
+    padding: 10px 20px;
+
+    &::before {
+      content: "";
+      margin-left: 0;
+      padding-right: 0;
+    }
   }
 
   .govuk-tabs__list-item--selected {
     margin-bottom: 0;
+    margin-top: 0;
+    padding: 14px 19px 16px;
+    border: 1px solid #b1b4b6;
+    border-bottom: none;
+
     a {
       font-weight: bold;
+      text-decoration: none;
     }
   }
 `
