ee06c62f9f3a8ddb57cac048154664c05c594160
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-08-12 16:48:54 +0100

    BICAWS7-3575 - Case details tab design changes (#1542)
    * Make tabs use 1/3 of width
    
    * Add margin around tabs
    
    * Changed font styling
    
    * Remove last tab margin
    
    * Header sizing
    
    * Tab styling and functionality
    
    * Use colour variable for mid-grey

--- packages/ui/src/features/CourtCaseDetails/Sidebar/Sidebar.styles.tsx
+++ packages/ui/src/features/CourtCaseDetails/Sidebar/Sidebar.styles.tsx
@@ -1,12 +1,17 @@
 import styled from "styled-components"
+import { gdsMidGrey } from "../../../utils/colours"
 
 const SidebarContainer = styled.div`
   margin-top: -41px;
 
-  li > a {
-    cursor: pointer;
-    font-weight: bold;
-    text-decoration: none;
+  .govuk-tabs__panel {
+    border: 1px solid ${gdsMidGrey};
+    border-top: none;
+    padding: 30px 20px;
+  }
+
+  .govuk-tabs__panel--hidden {
+    display: none;
   }
 
   #pnc-details {
@@ -17,6 +22,58 @@ const SidebarContainer = styled.div`
 const TabHeaders = styled.ul`
   display: flex;
   flex-wrap: nowrap;
+  line-height: 1.3157894737;
+  border-bottom: 1px solid ${gdsMidGrey};
+  margin-bottom: 0;
+
+  li {
+    flex: 1;
+    margin-left: 0;
+    margin-right: 5px;
+    text-align: center;
+    line-height: 1.3157894737;
+  }
+
+  li:last-child {
+    margin-right: 0;
+  }
+
+  li > a {
+    cursor: pointer;
+    text-decoration: underline;
+    font-size: var(--case-details-default-font-size);
+    margin-bottom: 0;
+
+    &:link {
+      color: #222;
+    }
+  }
+
+  .govuk-tabs__list-item {
+    margin-bottom: 5px;
+    margin-top: 5px;
+    box-sizing: border-box;
+    padding: 10px 20px;
+
+    &::before {
+      content: "";
+      margin-left: 0;
+      padding-right: 0;
+    }
+  }
+
+  .govuk-tabs__list-item--selected {
+    margin-bottom: 0;
+    margin-top: 0;
+    padding: 14px 19px 16px;
+    border: 1px solid ${gdsMidGrey};
+    border-bottom: none;
+
+    a {
+      font-weight: bold;
+      text-decoration: none;
+    }
+  }
 `
 
 export { SidebarContainer, TabHeaders }
