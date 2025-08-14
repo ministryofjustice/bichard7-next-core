069611d7b48485b31a0a9eaec77a843cfa0d1be5
emmalanigan <emma.keeley@madetech.com>
2025-08-14 16:54:54 +0100

    Remove tests on lock for caseDetailsRow

--- packages/ui/cypress/component/CaseListRow/CaseDetailsRow.cy.tsx
+++ packages/ui/cypress/component/CaseListRow/CaseDetailsRow.cy.tsx
@@ -1,7 +1,6 @@
 import { CaseDetailsRow } from "features/CourtCaseList/CourtCaseListEntry/CaseDetailsRow/CaseDetailsRow"
 import { DisplayPartialCourtCase } from "types/display/CourtCases"
 import { DisplayNote } from "types/display/Notes"
-import { LockReason } from "types/LockReason"
 import { MockNextRouter } from "../../support/MockNextRouter"
 
 describe("CaseDetailsRow", () => {
@@ -21,121 +20,5 @@ describe("CaseDetailsRow", () => {
     )
 
     cy.contains("Court Name")
-  })
-
-  describe("with errorLockedByUsername not null", () => {
-    it("shows error locked icon with LockReason as Exceptions", () => {
-      courtCase.errorLockedByUsername = "bob"
-
-      cy.mount(
-        <MockNextRouter>
-          <CaseDetailsRow
-            courtCase={courtCase}
-            reasonCell={undefined}
-            lockTag={undefined}
-            previousPath={null}
-            lockReason={LockReason.Exceptions}
-          />
-        </MockNextRouter>
-      )
-
-      cy.get(".caseDetailsRow td:nth-child(1) img").should("exist")
-    })
-
-    it("shows error locked icon with LockReason as Exceptions and triggerLockedByUsername", () => {
-      courtCase.errorLockedByUsername = "bob"
-      courtCase.triggerLockedByUsername = "bob"
-
-      cy.mount(
-        <MockNextRouter>
-          <CaseDetailsRow
-            courtCase={courtCase}
-            reasonCell={undefined}
-            lockTag={undefined}
-            previousPath={null}
-            lockReason={LockReason.Exceptions}
-          />
-        </MockNextRouter>
-      )
-
-      cy.get(".caseDetailsRow td:nth-child(1) img").should("exist")
-    })
-
-    it("doesn't show error locked icon with LockReason as Triggers", () => {
-      courtCase.errorLockedByUsername = "bob"
-      courtCase.triggerLockedByUsername = null
-
-      cy.mount(
-        <MockNextRouter>
-          <CaseDetailsRow
-            courtCase={courtCase}
-            reasonCell={undefined}
-            lockTag={undefined}
-            previousPath={null}
-            lockReason={LockReason.Triggers}
-          />
-        </MockNextRouter>
-      )
-
-      cy.get(".caseDetailsRow td:nth-child(1) img").should("not.exist")
-    })
-  })
-
-  describe("with errorLockedByUsername as null", () => {
-    beforeEach(() => {
-      courtCase.errorLockedByUsername = null
-    })
-
-    it("doesn't show error locked icon with LockReason as Exceptions", () => {
-      cy.mount(
-        <MockNextRouter>
-          <CaseDetailsRow
-            courtCase={courtCase}
-            reasonCell={undefined}
-            lockTag={undefined}
-            previousPath={null}
-            lockReason={LockReason.Exceptions}
-          />
-        </MockNextRouter>
-      )
-
-      cy.get(".caseDetailsRow td:nth-child(1) img").should("not.exist")
-    })
-
-    it("shows trigger locked icon with LockReason as Triggers", () => {
-      courtCase.triggerLockedByUsername = "bob"
-
-      cy.mount(
-        <MockNextRouter>
-          <CaseDetailsRow
-            courtCase={courtCase}
-            reasonCell={undefined}
-            lockTag={undefined}
-            previousPath={null}
-            lockReason={LockReason.Triggers}
-          />
-        </MockNextRouter>
-      )
-
-      cy.get(".caseDetailsRow td:nth-child(1) img").should("exist")
-    })
-
-    it("doesn't show trigger locked icon with LockReason as Triggers", () => {
-      courtCase.triggerLockedByUsername = null
-
-      cy.mount(
-        <MockNextRouter>
-          <CaseDetailsRow
-            courtCase={courtCase}
-            reasonCell={undefined}
-            lockTag={undefined}
-            previousPath={null}
-            lockReason={LockReason.Triggers}
-          />
-        </MockNextRouter>
-      )
-
-      cy.get(".caseDetailsRow td:nth-child(1) img").should("not.exist")
-    })
   })
 })
