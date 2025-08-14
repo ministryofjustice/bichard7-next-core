55024ba8546e97b6cb521855ef39769c88d65a31
emmalanigan <emma.keeley@madetech.com>
2025-08-14 16:21:41 +0100

    Remove lock from extra reason row

--- packages/ui/src/features/CourtCaseList/CourtCaseListEntry/ExtraReasonRow.tsx
+++ packages/ui/src/features/CourtCaseList/CourtCaseListEntry/ExtraReasonRow.tsx
@@ -1,7 +1,4 @@
-import ConditionalRender from "components/ConditionalRender"
-import Image from "next/image"
 import { ResolutionStatus } from "types/ResolutionStatus"
-import { LOCKED_ICON_URL } from "utils/icons"
 import ResolutionStatusBadge from "../tags/ResolutionStatusBadge"
 import { CaseListResolutionStatusBadgeWrapper } from "./CaseDetailsRow/CaseDetailsRow.styles"
 import { StyledExtraReasonRow } from "./ExtraReasonRow.styles"
@@ -13,14 +10,9 @@ interface ExtraReasonRowProps {
   resolutionStatus: ResolutionStatus
 }
 
-export const ExtraReasonRow = ({ isLocked, reasonCell, lockTag, resolutionStatus }: ExtraReasonRowProps) => {
+export const ExtraReasonRow = ({ reasonCell, lockTag, resolutionStatus }: ExtraReasonRowProps) => {
   return (
     <StyledExtraReasonRow className={"govuk-table__row extraReasonRow"}>
-      <td className="govuk-table__cell">
-        <ConditionalRender isRendered={isLocked}>
-          <Image src={LOCKED_ICON_URL} width={20} height={20} alt="Lock icon" />
-        </ConditionalRender>
-      </td>
       <td className="govuk-table__cell resolutionStatusBadgeCell">
         <CaseListResolutionStatusBadgeWrapper>
           <ResolutionStatusBadge resolutionStatus={resolutionStatus} />
