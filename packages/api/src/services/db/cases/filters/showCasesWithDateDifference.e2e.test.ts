6240efe5c45215226d29a57d6452552aa0b39390
emmalanigan <emma.keeley@madetech.com>
2025-08-27 14:39:15 +0100

    add showCasesWithDatDifference e2e tests

---
+++ packages/api/src/services/db/cases/filters/showCasesWithDateDifference.e2e.test.ts
@@ -0,0 +1,73 @@
+import type { ApiCaseQuery } from "@moj-bichard7/common/types/ApiCaseQuery"
+import type { CaseIndexMetadata } from "@moj-bichard7/common/types/Case"
+import type { User } from "@moj-bichard7/common/types/User"
+import type { FastifyInstance } from "fastify"
+
+import { Reason } from "@moj-bichard7/common/types/ApiCaseQuery"
+
+import { createCases } from "../../../../tests/helpers/caseHelper"
+import { SetupAppEnd2EndHelper } from "../../../../tests/helpers/setupAppEnd2EndHelper"
+import { createUser } from "../../../../tests/helpers/userHelper"
+import fetchCasesAndFilter from "../../../../useCases/cases/fetchCasesAndFilter"
+
+describe("fetchCasesAndFilter filtering by showCasesWithDateDifference flag e2e", () => {
+  let helper: SetupAppEnd2EndHelper
+  let app: FastifyInstance
+  let user: User
+
+  const defaultQuery: ApiCaseQuery = { maxPerPage: 25, pageNum: 1, reason: Reason.All }
+  const differentCourtDate = new Date("2001-09-26")
+
+  beforeAll(async () => {
+    helper = await SetupAppEnd2EndHelper.setup()
+    app = helper.app
+
+    await helper.postgres.clearDb()
+    await helper.dynamo.clearDynamo()
+
+    user = await createUser(helper.postgres)
+    await createCases(helper.postgres, 2, {
+      0: { courtDate: differentCourtDate }
+    })
+  })
+
+  afterEach(async () => {
+    jest.restoreAllMocks()
+  })
+
+  afterAll(async () => {
+    await app.close()
+    await helper.postgres.close()
+  })
+
+  it("will filter cases with date differences", async () => {
+    const caseMetadata = (await fetchCasesAndFilter(
+      helper.postgres.readonly,
+      {
+        showCasesWithDateDifference: true,
+        ...defaultQuery
+      },
+      user
+    )) as CaseIndexMetadata
+
+    expect(caseMetadata.cases).toHaveLength(1)
+    expect(caseMetadata.totalCases).toBe(1)
+    expect(caseMetadata.returnCases).toBe(1)
+    expect(caseMetadata.cases[0].courtDate).toBe(differentCourtDate)
+  })
+
+  it("will not filter cases with date differences when flag is false", async () => {
+    const caseMetadata = (await fetchCasesAndFilter(
+      helper.postgres.readonly,
+      {
+        showCasesWithDateDifference: false,
+        ...defaultQuery
+      },
+      user
+    )) as CaseIndexMetadata
+
+    expect(caseMetadata.cases).toHaveLength(2)
+    expect(caseMetadata.totalCases).toBe(2)
+    expect(caseMetadata.returnCases).toBe(2)
+  })
+})
