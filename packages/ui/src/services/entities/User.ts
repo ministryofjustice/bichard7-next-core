1e253d8b42ade0dc066d322db921334de7ac64e4
emmalanigan <emma.keeley@madetech.com>
2025-09-02 14:12:34 +0100

    reverse adding useCourtDateReceivedDateMismatchFilter to user featureFlags

--- packages/ui/src/services/entities/User.ts
+++ packages/ui/src/services/entities/User.ts
@@ -40,7 +40,7 @@ export default class User {
   excludedTriggers!: string[]
 
   @Column({ name: "feature_flags", transformer: jsonTransformer, type: "jsonb" })
-  featureFlags!: { useCourtDateReceivedDateMismatchFilter?: boolean } & Record<string, boolean>
+  featureFlags!: Record<string, boolean>
 
   @OneToMany(() => Note, (note) => note.user)
   @JoinColumn({ name: "user_id" })
