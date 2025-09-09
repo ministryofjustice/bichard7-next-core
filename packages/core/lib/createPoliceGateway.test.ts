f062aa92d6463a5d69ed33b9bef8fdd29be8e8e6
Emad Karamad <emad.karamad@madetech.com>
2025-09-09 13:08:10 +0100

    Add createPoliceGateway

---
+++ packages/core/lib/createPoliceGateway.test.ts
@@ -0,0 +1,32 @@
+import createPoliceGateway from "./createPoliceGateway"
+import LedsGateway from "./leds/LedsGateway"
+import PncGateway from "./pnc/PncGateway"
+
+describe("createPoliceGateway", () => {
+  it("should return PNC gateway when USE_LEDS environment variable is not set", () => {
+    delete process.env.USE_LEDS
+
+    const gateway = createPoliceGateway()
+
+    expect(gateway).toBeInstanceOf(PncGateway)
+  })
+
+  it.each(["dummy", "True", "truE", undefined])(
+    "should return PNC gateway when USE_LEDS environment variable value is %s",
+    (environmentVariableValue) => {
+      process.env.USE_LEDS = environmentVariableValue
+
+      const gateway = createPoliceGateway()
+
+      expect(gateway).toBeInstanceOf(PncGateway)
+    }
+  )
+
+  it("should return LEDS gateway when USE_LEDS environment variable is true", () => {
+    process.env.USE_LEDS = "true"
+
+    const gateway = createPoliceGateway()
+
+    expect(gateway).toBeInstanceOf(LedsGateway)
+  })
+})
