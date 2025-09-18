278b73be374f1a81e9455d6267eeaeb306b4f0d1
Tom Watkins <tom.watkins@justice.gov.uk>
2025-09-18 14:19:34 +0100

    Add FormGroup component

---
+++ packages/ui/cypress/component/FormGroup.cy.tsx
@@ -0,0 +1,30 @@
+import { FormGroup } from "components/FormGroup"
+
+describe("FormGroup", () => {
+  it("mounts", () => {
+    cy.mount(<FormGroup id={"my-form-group"}>{"Text"}</FormGroup>)
+  })
+
+  it("renders children", () => {
+    cy.mount(<FormGroup id={"my-form-group"}>{"Text"}</FormGroup>)
+    cy.get("div#my-form-group").should("have.text", "Text")
+  })
+
+  it("merges class names", () => {
+    cy.mount(
+      <FormGroup id={"my-form-group"} className="extra-class">
+        {"Text"}
+      </FormGroup>
+    )
+    cy.get("div#my-form-group").should("have.class", "govuk-form-group").should("have.class", "extra-class")
+  })
+
+  it("shows error state", () => {
+    cy.mount(
+      <FormGroup id={"my-form-group"} showError>
+        {"Text"}
+      </FormGroup>
+    )
+    cy.get("div#my-form-group").should("have.class", "govuk-form-group").should("have.class", "govuk-form-group--error")
+  })
+})
