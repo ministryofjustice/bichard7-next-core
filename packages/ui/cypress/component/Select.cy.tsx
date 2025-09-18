754e5325d5a3414f4f2292514e89f6949aac3c57
Tom Watkins <tom.watkins@justice.gov.uk>
2025-09-18 12:24:04 +0100

    Add onChange test

--- packages/ui/cypress/component/Select.cy.tsx
+++ packages/ui/cypress/component/Select.cy.tsx
@@ -31,4 +31,18 @@ describe("Select", () => {
 
     cy.get("select").should("have.class", "govuk-select").should("have.class", "extra-class")
   })
+
+  it("selecting an option fires a change event", () => {
+    const onChangeSpy = cy.spy().as("onChangeSpy")
+
+    cy.mount(
+      <Select className="extra-class" onChange={onChangeSpy}>
+        <option value="value1">{"Value 1"}</option>
+        <option value="value2">{"Value 2"}</option>
+      </Select>
+    )
+
+    cy.get("select").select("value2")
+    cy.get("@onChangeSpy").should("have.been.called")
+  })
 })
