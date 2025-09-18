98d3968429eb35a96a2a810e74c7c83c020da755
Tom Watkins <tom.watkins@justice.gov.uk>
2025-09-18 12:40:10 +0100

    Add placeholder

--- packages/ui/cypress/component/Select.cy.tsx
+++ packages/ui/cypress/component/Select.cy.tsx
@@ -32,11 +32,22 @@ describe("Select", () => {
     cy.get("select").should("have.class", "govuk-select").should("have.class", "extra-class")
   })
 
+  it("passed in value is selected", () => {
+    cy.mount(
+      <Select value="value2" placeholder="Select a value">
+        <option value="value1">{"Value 1"}</option>
+        <option value="value2">{"Value 2"}</option>
+      </Select>
+    )
+
+    cy.get("select").should("have.value", "value2")
+  })
+
   it("selecting an option fires a change event", () => {
     const onChangeSpy = cy.spy().as("onChangeSpy")
 
     cy.mount(
-      <Select className="extra-class" onChange={onChangeSpy}>
+      <Select onChange={onChangeSpy}>
         <option value="value1">{"Value 1"}</option>
         <option value="value2">{"Value 2"}</option>
       </Select>
@@ -44,5 +55,17 @@ describe("Select", () => {
 
     cy.get("select").select("value2")
     cy.get("@onChangeSpy").should("have.been.called")
+  })
+
+  it("renders placeholder", () => {
+    cy.mount(
+      <Select value="" placeholder="Select a value">
+        <option value="value1">{"Value 1"}</option>
+        <option value="value2">{"Value 2"}</option>
+      </Select>
+    )
+
+    cy.get("select option").should("have.length", 3)
+    cy.get("select option:first-child").should("have.value", "").should("contain.text", "Select a value")
   })
 })
--- packages/ui/src/components/Select.tsx
+++ packages/ui/src/components/Select.tsx
@@ -1,10 +1,19 @@
 import type { ComponentProps } from "react"
 import { mergeClassNames } from "helpers/mergeClassNames"
 
-export const Select = (props: ComponentProps<"select">) => {
+export interface SelectProps extends ComponentProps<"select"> {
+  placeholder?: string
+}
+
+export const Select = ({ placeholder, className, children, ...props }: SelectProps) => {
   return (
-    <select {...props} className={mergeClassNames("govuk-select", props.className)}>
-      {props.children}
+    <select {...props} className={mergeClassNames("govuk-select", className)}>
+      {placeholder ? (
+        <option disabled hidden value="">
+          {placeholder}
+        </option>
+      ) : null}
+      {children}
     </select>
   )
 }
