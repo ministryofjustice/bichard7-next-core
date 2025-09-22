2e3be957cd236dfc5b89ee3d77ac938bffb8a89a
Tom Watkins <tom.watkins@justice.gov.uk>
2025-09-22 15:38:13 +0100

    Call the correct endpoint on form submission

--- packages/ui/cypress/component/Audit/QualityStatusCard.cy.tsx
+++ packages/ui/cypress/component/Audit/QualityStatusCard.cy.tsx
@@ -1,6 +1,13 @@
 import { QualityStatusCard } from "features/CourtCaseDetails/Sidebar/Audit/QualityStatusCard"
 import { CsrfTokenContext } from "context/CsrfTokenContext"
+import { CourtCaseContext } from "context/CourtCaseContext"
+import type { DisplayFullCourtCase } from "types/display/CourtCases"
+import { MockNextRouter } from "../../support/MockNextRouter"
 import "../../../styles/globals.scss"
+
+const courtCase = {
+  errorId: 1
+} as unknown as DisplayFullCourtCase
 
 describe("QualityStatusCard", () => {
   it("mounts", () => {
@@ -9,5 +16,26 @@ describe("QualityStatusCard", () => {
         <QualityStatusCard />
       </CsrfTokenContext.Provider>
     )
+  })
+
+  it("posts form content to the correct URL", () => {
+    cy.intercept(
+      "POST",
+      `${Cypress.config("baseUrl")}/bichard/api/court-cases/${courtCase.errorId}/audit`,
+      cy.spy().as("auditCase")
+    )
+
+    cy.mount(
+      <MockNextRouter>
+        <CourtCaseContext.Provider value={[{ courtCase, amendments: {}, savedAmendments: {} }, () => {}]}>
+          <CsrfTokenContext.Provider value={[{ csrfToken: "ABC" }, () => {}]}>
+            <QualityStatusCard />
+          </CsrfTokenContext.Provider>
+        </CourtCaseContext.Provider>
+      </MockNextRouter>
+    )
+
+    cy.get("#quality-status-submit").click()
+    cy.get("@auditCase").should("have.been.calledOnce")
   })
 })
