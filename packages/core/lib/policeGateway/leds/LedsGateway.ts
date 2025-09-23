acd415d33d6f11b3bdcfc3bac85b19ca4f284c59
Emad Karamad <emad.karamad@madetech.com>
2025-09-23 11:36:02 +0100

    Add LedsApiConfig

--- packages/core/lib/policeGateway/leds/LedsGateway.ts
+++ packages/core/lib/policeGateway/leds/LedsGateway.ts
@@ -1,6 +1,7 @@
 import type { PoliceQueryResult } from "@moj-bichard7/common/types/PoliceQueryResult"
 
 import type PoliceUpdateRequest from "../../../phase3/types/PoliceUpdateRequest"
+import type LedsApiConfig from "../../../types/leds/LedsApiConfig"
 import type PoliceGateway from "../../../types/PoliceGateway"
 import type PoliceApiError from "../PoliceApiError"
 
@@ -8,4 +9,5 @@ export default class LedsGateway implements PoliceGateway {
   query: (asn: string, correlationId: string) => Promise<PoliceApiError | PoliceQueryResult | undefined>
   queryTime: Date | undefined
   update: (request: PoliceUpdateRequest, correlationId: string) => Promise<PoliceApiError | void>
+  constructor(private config: LedsApiConfig) {}
 }
