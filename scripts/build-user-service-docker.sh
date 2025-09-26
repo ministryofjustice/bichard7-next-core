dc16f7cfd194667027abcb31d6d6ca12966f7d9f
Tom Watkins <tom.watkins@justice.gov.uk>
2025-09-26 19:06:46 +0100

    Move docker script into main scripts dir

--- scripts/build-user-service-docker.sh
+++ scripts/build-user-service-docker.sh
@@ -5,6 +5,7 @@ set -e
 NOCACHE=${NOCACHE:-"false"}
 
 readonly DOCKER_REFERENCE="nginx-nodejs-20-2023-supervisord"
+readonly DOCKER_OUTPUT_TAG="user-service"
 
 function has_local_image() {
   IMAGES=$(docker images --filter=reference="${DOCKER_REFERENCE}:*" -q | wc -l)
@@ -45,46 +46,45 @@ function pull_and_build_from_aws() {
   if [ $(arch) = "arm64" ]
   then
     echo "Building for ARM"
-    docker build --no-cache=$NOCACHE --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" --platform=linux/arm64 -t user-service .
+    docker build --no-cache=$NOCACHE --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" -f packages/user-service/Dockerfile --platform=linux/arm64 -t ${DOCKER_OUTPUT_TAG}:latest .
   else
     echo "Building regular image"
-    docker build --no-cache=$NOCACHE --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" -t user-service .
+    docker build --no-cache=$NOCACHE --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" -f packages/user-service/Dockerfile -t ${DOCKER_OUTPUT_TAG}:latest .
   fi
 
   echo "Build complete"
 
   if [[ -n "${CODEBUILD_RESOLVED_SOURCE_VERSION}" && -n "${CODEBUILD_START_TIME}" ]]; then
+    ## Install goss
+    curl -L https://github.com/aelsabbahy/goss/releases/latest/download/goss-linux-amd64 -o /usr/local/bin/goss
+    chmod +rx /usr/local/bin/goss
+    curl -L https://github.com/aelsabbahy/goss/releases/latest/download/dgoss -o /usr/local/bin/dgoss
+    chmod +rx /usr/local/bin/dgoss
 
-      ## Install goss
-      curl -L https://github.com/aelsabbahy/goss/releases/latest/download/goss-linux-amd64 -o /usr/local/bin/goss
-      chmod +rx /usr/local/bin/goss
-      curl -L https://github.com/aelsabbahy/goss/releases/latest/download/dgoss -o /usr/local/bin/dgoss
-      chmod +rx /usr/local/bin/dgoss
+    export GOSS_PATH="/usr/local/bin/goss"
 
-      export GOSS_PATH="/usr/local/bin/goss"
+    ## Run goss tests
+    GOSS_SLEEP=15 dgoss run -e DB_HOST=172.17.0.1 "${DOCKER_OUTPUT_TAG}:latest"
+    docker tag \
+        user-service:latest \
+        ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/${DOCKER_OUTPUT_TAG}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}
 
-      ## Run goss tests
-      GOSS_SLEEP=15 dgoss run -e DB_HOST=172.17.0.1 "user-service:latest"
-      docker tag \
-          user-service:latest \
-          ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/user-service:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}
+    echo "Push docker image on `date`"
+    docker push \
+        ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/${DOCKER_OUTPUT_TAG}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME} | tee /tmp/docker.out
+    export IMAGE_SHA_HASH=$(cat /tmp/docker.out | grep digest | cut -d':' -f3-4 | cut -d' ' -f2)
 
-      echo "Push docker image on `date`"
-      docker push \
-          ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/user-service:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME} | tee /tmp/docker.out
-      export IMAGE_SHA_HASH=$(cat /tmp/docker.out | grep digest | cut -d':' -f3-4 | cut -d' ' -f2)
-
-      if [ "${IS_CD}" = "true" ]; then
-        cat <<EOF>/tmp/user-service.json
+    if [ "${IS_CD}" = "true" ]; then
+      cat <<EOF >/tmp/${DOCKER_OUTPUT_TAG}.json
         {
           "source-hash" : "${CODEBUILD_RESOLVED_SOURCE_VERSION}",
           "build-time": "${CODEBUILD_START_TIME}",
           "image-hash": "${IMAGE_SHA_HASH}"
         }
 EOF
-        aws s3 cp /tmp/user-service.json s3://${ARTIFACT_BUCKET}/semaphores/user-service.json
+        aws s3 cp /tmp/${DOCKER_OUTPUT_TAG}.json s3://${ARTIFACT_BUCKET}/semaphores/${DOCKER_OUTPUT_TAG}.json
         export USER_SERVICE_HASH="${IMAGE_SHA_HASH}"
-      fi
+    fi
   fi
 }
 
@@ -93,10 +93,10 @@ if [[ "$(has_local_image)" -gt 0 ]]; then
   if [ $(arch) = "arm64" ]
   then
       echo "Building for ARM"
-      docker build --no-cache=$NOCACHE --platform=linux/arm64 -t user-service:latest .
+      docker build --no-cache=$NOCACHE --platform=linux/arm64 -t ${DOCKER_OUTPUT_TAG}:latest .
   else
       echo "Building regular image"
-      docker build --no-cache=$NOCACHE --no-cache=$NOCACHE -t user-service:latest .
+      docker build --no-cache=$NOCACHE --no-cache=$NOCACHE -t ${DOCKER_OUTPUT_TAG}:latest .
   fi
 else
   pull_and_build_from_aws
