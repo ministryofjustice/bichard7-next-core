c1c04ffd79b0291c837f5ad04ddd971af3f4fedb
Tom Watkins <tom.watkins@justice.gov.uk>
2025-09-29 12:39:02 +0100

    Copy assets

--- packages/user-service/scripts/copy-govuk-frontend-assets.sh
+++ packages/user-service/scripts/copy-govuk-frontend-assets.sh
@@ -5,12 +5,14 @@ set -e
 SCRIPTS_DIR=$(dirname "$0")
 REPO_ROOT=$(dirname "$SCRIPTS_DIR")
 
-ASSETS_SOURCE="${REPO_ROOT}/node_modules/govuk-frontend/dist/govuk/assets"
-ASSETS_DESTINATION="${REPO_ROOT}/public/"
+ASSETS_SOURCE="${REPO_ROOT}/node_modules/govuk-frontend/dist/govuk/assets/*"
+ASSETS_DESTINATION="${REPO_ROOT}/public/govuk_assets"
 
 if [ ! -d $ASSETS_SOURCE ]; then
     echo "GOVUK assets couldn't be found - have you run 'npm install'?" >&2
     exit 1
 fi
 
-cp -Rv $ASSETS_SOURCE/* $ASSETS_DESTINATION
+rm -rf $ASSETS_DESTINATION
+mkdir -p $ASSETS_DESTINATION
+cp -Rv $ASSETS_SOURCE $ASSETS_DESTINATION
