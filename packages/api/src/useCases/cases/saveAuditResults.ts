2debc6366bb55f2e17589e056cab2de6c30d7794
Tausif Patel <tausifpatel.dev@gmail.com>
2025-09-29 15:55:05 +0100

    Wrap transaction in a try catch block to propogate specific error

--- packages/api/src/useCases/cases/saveAuditResults.ts
+++ packages/api/src/useCases/cases/saveAuditResults.ts
@@ -7,6 +7,7 @@ import type { WritableDatabaseConnection } from "../../types/DatabaseGateway"
 
 import auditCase from "../../services/db/cases/auditCase"
 import insertNote from "../../services/db/cases/insertNote"
+import { NotFoundError } from "../../types/errors/NotFoundError"
 import { UnprocessableEntityError } from "../../types/errors/UnprocessableEntityError"
 
 const saveAuditResults = async (
@@ -16,28 +17,38 @@ const saveAuditResults = async (
   userId: string,
   note: string | undefined
 ): PromiseResult<void> => {
-  return database.transaction(async (transactionDb) => {
-    const auditResultsSaved = await auditCase(transactionDb, caseId, auditQuality)
-    if (isError(auditResultsSaved)) {
-      throw auditResultsSaved
+  try {
+    return await database.transaction(async (transactionDb) => {
+      const auditResultsSaved = await auditCase(transactionDb, caseId, auditQuality)
+
+      if (isError(auditResultsSaved)) {
+        throw auditResultsSaved
+      }
+
+      if (!auditResultsSaved) {
+        throw new UnprocessableEntityError("Audit results could not be saved")
+      }
+
+      const errorListNote = `Trigger quality: ${auditQuality.triggerQuality}. Exception quality: ${auditQuality.errorQuality}. ${note}`
+      const noteSaved = await insertNote(transactionDb, caseId, errorListNote, userId)
+
+      if (isError(noteSaved)) {
+        throw noteSaved
+      }
+
+      if (!noteSaved) {
+        throw new UnprocessableEntityError("Audit note could not be saved")
+      }
+
+      return
+    })
+  } catch (err) {
+    if (err instanceof NotFoundError || err instanceof UnprocessableEntityError) {
+      return err
     }
 
-    if (!auditResultsSaved) {
-      throw new UnprocessableEntityError("Audit results could not be saved")
-    }
-
-    const errorListNote = `Trigger quality: ${auditQuality.triggerQuality}. Exception quality: ${auditQuality.errorQuality}. ${note}`
-    const noteSaved = await insertNote(transactionDb, caseId, errorListNote, userId)
-    if (isError(noteSaved)) {
-      throw noteSaved
-    }
-
-    if (!noteSaved) {
-      throw new UnprocessableEntityError("Audit note could not be saved")
-    }
-
-    return
-  })
+    return Error((err as Error).message)
+  }
 }
 
 export default saveAuditResults
