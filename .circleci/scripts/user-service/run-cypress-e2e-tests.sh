ae66ab964c6bf2310a11132ce0d684fbcc036b23
Tom Watkins <tom.watkins@justice.gov.uk>
2025-10-08 13:24:48 +0100

    Fix quote expansion issue

--- .circleci/scripts/user-service/run-cypress-e2e-tests.sh
+++ .circleci/scripts/user-service/run-cypress-e2e-tests.sh
@@ -7,7 +7,9 @@ if [[ $MS_EDGE == "true" ]]; then
   options+=" --browser edge"
 fi
 
+COMMAND='xargs -I {} npx concurrently --raw --kill-others --success first "npm run start" "npm run cypress:run:file {} '$options'"'
+
 circleci tests glob "cypress/e2e/**/*.cy.js" \
   | circleci tests run \
-      --command="xargs -I {} npx concurrently --raw --kill-others --success first \"npm run start\" \"npm run cypress:run:file {} ${options}\"" \
+      --command="$COMMAND" \
       --split-by=timings
