b7341ba9088abdccf065f6626b576aac55e90a24
Tom Watkins <tom.watkins@justice.gov.uk>
2025-10-15 13:19:31 +0100

    Use grep to exclude api tests

--- .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
+++ .circleci/scripts/ui-scripts/run-cypress-e2e-tests.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 cd ~/project/packages/ui
 
-options="--config baseUrl=https://localhost:4443 --reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
+CYPRESS_OPTS="--config baseUrl=https://localhost:4443 --reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 if [[ $MS_EDGE == "true" ]]; then
-  options+=" --browser edge"
+  CYPRESS_OPTS+=" --browser edge"
 fi
+
+TEST_PATTERN="cypress/e2e/**/*.cy.ts"
+CYPRESS_CMD="xargs npx cypress run $CYPRESS_OPTS --spec"
 
 if [[ $API == "true" ]]; then
-  TEST_PATTERN="cypress/e2e/**/*.cy.ts"
+  echo "Running tests (including API)..."
+  circleci tests glob "$TEST_PATTERN" | circleci tests run --command="$CYPRESS_CMD" --split-by=timings
 else
-  TEST_PATTERN="cypress/e2e/**/!(*.api).cy.ts"
+  echo "Running tests (excluding API)..."
+  circleci tests glob "$TEST_PATTERN" | \
+    grep -v '\.api\.cy\.ts$' | \
+    circleci tests run --command="$CYPRESS_CMD" --split-by=timings
 fi
-
-echo "Running tests ($TEST_PATTERN)..."
-
-circleci tests glob "$TEST_PATTERN" | circleci tests run --command="xargs npx cypress run ${options} --spec" --split-by=timings
