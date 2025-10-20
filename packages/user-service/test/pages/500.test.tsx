962f8bbe340951e723d26fdc2ecdc518689fce3f
Tom Watkins <tom.watkins@justice.gov.uk>
2025-10-20 14:50:12 +0100

    Add mock next router and update snapshots

--- packages/user-service/test/pages/500.test.tsx
+++ packages/user-service/test/pages/500.test.tsx
@@ -1,8 +1,13 @@
 import { render } from "@testing-library/react"
 import Custom500 from "pages/500"
+import { MockNextRouter } from "../MockNextRouter"
 
 it("should render the component and match the snapshot", () => {
-  const { container } = render(<Custom500 />)
+  const { container } = render(
+    <MockNextRouter>
+      <Custom500 />
+    </MockNextRouter>
+  )
 
   expect(container).toMatchSnapshot()
 })
