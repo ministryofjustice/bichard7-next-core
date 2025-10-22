53bf1c8313885135ba5f1a4a65528055ba84c8c0
padlichan <lili.berenyi@gmail.com>
2025-10-22 11:37:42 +0100

    Add RadioItems to feedback page in user service

--- packages/user-service/src/pages/feedback/index.tsx
+++ packages/user-service/src/pages/feedback/index.tsx
@@ -6,6 +6,7 @@ import Form from "components/Form"
 import Layout from "components/Layout"
 import Link from "components/Link"
 import Paragraph from "components/Paragraph"
+import { RadioItem } from "components/RadioItem"
 import SuccessBanner from "components/SuccessBanner"
 import config from "lib/config"
 import { withAuthentication, withCsrf, withMultipleServerSideProps } from "middleware"
@@ -28,7 +29,8 @@ export const getServerSideProps = withMultipleServerSideProps(
       AuthenticationServerSidePropsContext
 
     if (isPost(req)) {
-      const { feedback } = formData as { feedback: string }
+      const { feedback } = formData as { feedback: string; satistactionRating: string }
+      console.log(formData)
       const feedbackResult = await postFeedback(feedback, currentUser?.emailAddress)
       if (isError(feedbackResult)) {
         return {
@@ -98,6 +100,43 @@ const ShareFeedback = ({ csrfToken, currentUser, errorMessage, successMessage }:
                 {". Any issues raised via this page will not be handled."}
               </Paragraph>
             </div>
+
+            <fieldset className="govuk-fieldset">
+              <legend className="govuk-fieldset__legend govuk-!-font-weight-bold">
+                {"Overall, how did you feel about using Bichard7 today?"}
+              </legend>
+              <div id="email-hint" className="govuk-hint">
+                {"Select one"}
+              </div>
+              <div className="govuk-radios" data-module="govuk-radios">
+                <RadioItem
+                  value="very-satisfied"
+                  id="satisfactionRating-1"
+                  name="satisfactionRating"
+                  text="Very satisfied"
+                />
+                <RadioItem value="satisfied" id="satisfactionRating-2" name="satisfactionRating" text="Satisfied" />
+                <RadioItem
+                  value="neither-satisfied-or-dissatisfied"
+                  id="satisfactionRating-3"
+                  name="satisfactionRating"
+                  text="Neither satisfied or dissatisfied"
+                />
+                <RadioItem
+                  value="neither-satisfied-or-dissatisfied"
+                  id="satisfactionRating-4"
+                  name="satisfactionRating"
+                  text="Dissatisfied"
+                />
+                <RadioItem
+                  value="very-dissatisfied"
+                  id="satisfactionRating-5"
+                  name="satisfactionRating"
+                  text="Very dissatisfied"
+                />
+              </div>
+            </fieldset>
+
             <div id="feedback-hint" className="govuk-label">
               {"What improvements would you like to see?"}
             </div>
