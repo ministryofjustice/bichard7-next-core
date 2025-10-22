cbbdbe7279640ed1b8881f8049c2ba903810c2d1
padlichan <lili.berenyi@gmail.com>
2025-10-22 17:51:52 +0100

    Update radioItem component test in user service

--- packages/user-service/test/components/RadioItem.test.tsx
+++ packages/user-service/test/components/RadioItem.test.tsx
@@ -2,6 +2,8 @@ import { render } from "@testing-library/react"
 import { RadioItem } from "components/RadioItem"
 
 it("should render radioItem component", () => {
-  const { container } = render(<RadioItem value="test" id="testRadio" name="testRadio" text="This is a test" />)
+  const { container } = render(
+    <RadioItem value="test" id="testRadio" name="testRadio" text="This is a test" defaultChecked={true} />
+  )
   expect(container).toMatchSnapshot()
 })
