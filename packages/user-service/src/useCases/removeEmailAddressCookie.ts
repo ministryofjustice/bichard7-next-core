4b7d7a44dc6af40b9c7663b3c5106a112b7e2540
Tom Watkins <tom.watkins@justice.gov.uk>
2025-10-23 18:04:52 +0100

    Make existing function general purpose

--- packages/user-service/src/useCases/removeEmailAddressCookie.ts
+++ packages/user-service/src/useCases/removeEmailAddressCookie.ts
@@ -1,8 +1,23 @@
 import type { ServerResponse } from "http"
 import type { UserServiceConfig } from "lib/config"
 import setCookie from "utils/setCookie"
+import type { EmailAddressCookieType } from "../types/EmailAddressCookieType"
 
-export default (response: ServerResponse, config: UserServiceConfig): void => {
-  const { rememberEmailAddressCookieName: cookieName } = config
+export default (
+  response: ServerResponse,
+  config: UserServiceConfig,
+  emailAddressCookieType: EmailAddressCookieType
+): void => {
+  let cookieName: string | null = null
+  if (emailAddressCookieType === "REMEMBER") {
+    cookieName = config.rememberEmailAddressCookieName
+  } else if (emailAddressCookieType === "IN_PROGRESS") {
+    cookieName = config.inProgressEmailAddressCookieName
+  }
+
+  if (!cookieName) {
+    throw new Error("Could not find email address cookie")
+  }
+
   setCookie(response, cookieName, "", { maxAge: 0, path: "/users/login" })
 }
--- packages/user-service/test/useCases/removeEmailAddressCookie.test.ts
+++ packages/user-service/test/useCases/removeEmailAddressCookie.test.ts
@@ -6,12 +6,26 @@ import type { IncomingMessage } from "http"
 import { ServerResponse } from "http"
 import config from "lib/config"
 import { removeEmailAddressCookie } from "useCases"
+import { type EmailAddressCookieType, emailAddressCookieTypes } from "types/EmailAddressCookieType"
 
-it("should expire the email address cookie and empty the value", () => {
+function getEmailAddressCookieName(emailAddressCookieType: EmailAddressCookieType) {
+  if (emailAddressCookieType === "REMEMBER") {
+    return config.rememberEmailAddressCookieName
+  } else if (emailAddressCookieType === "IN_PROGRESS") {
+    return config.inProgressEmailAddressCookieName
+  }
+
+  throw new Error("Unknown email address cookie")
+}
+
+it.each(emailAddressCookieTypes)("should expire the email address cookie and empty the value (%s)", (cookieType) => {
   const response = new ServerResponse({} as IncomingMessage)
-  removeEmailAddressCookie(response, config)
+  removeEmailAddressCookie(response, config, cookieType)
 
+  const cookieName = getEmailAddressCookieName(cookieType)
   const cookieValues = response.getHeader("Set-Cookie") as string[]
   expect(cookieValues).toHaveLength(1)
-  expect(cookieValues[0]).toMatch(/LOGIN_EMAIL=; Max-Age=0; Path=\/users\/login; HttpOnly/)
+  expect(cookieValues[0]).toMatch(
+    new RegExp(`${cookieName}=; Max-Age=0; Path=/users/login; HttpOnly; Secure; SameSite=Strict`)
+  )
 })
