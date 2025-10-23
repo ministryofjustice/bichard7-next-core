278b9965f3fc11b868a23d3b75a81f91dac64bff
emmalanigan <emma.keeley@madetech.com>
2025-10-23 16:17:45 +0100

    amend failing test

--- packages/user-service/cypress/e2e/forgotPassword.cy.js
+++ packages/user-service/cypress/e2e/forgotPassword.cy.js
@@ -7,10 +7,10 @@ describe("Forgot password", () => {
   it("should prompt the user to check their email after entering an email address", () => {
     cy.visit("/login")
     cy.get("a[data-test='reset-password']").click()
-    cy.get("body").contains(/reset password/i)
+    cy.get("body").contains(/We need to confirm your email address/i)
     cy.get("input[type=email]").type("bichard01@example.com")
     cy.get("button[type=submit]").click()
-    cy.get("body").contains(/sent you an email/i)
+    cy.get("body").contains(/We have sent a code to/i)
   })
 
   it("should not allow submission of something that isn't an email address", () => {
