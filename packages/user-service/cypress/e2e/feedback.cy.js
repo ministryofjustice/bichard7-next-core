2e053c1adb796d3c751baa56697d22c2eebb6cc0
padlichan <lili.berenyi@gmail.com>
2025-10-27 14:02:47 +0000

    Add happy path form submission cypress test

--- packages/user-service/cypress/e2e/feedback.cy.js
+++ packages/user-service/cypress/e2e/feedback.cy.js
@@ -9,3 +9,13 @@ describe("Feedback", () => {
     cy.get('input[name="rating"]').should("have.length", 5)
   })
 })
+
+describe("Form submission", () => {
+  it("should submit form and redirect to confirmation page", () => {
+    cy.visit("/feedback")
+    cy.get('input[name="rating"][value="satisfied"]').check()
+    cy.get('textarea[name="feedback"]').type("This is a test.")
+    cy.get("form").submit()
+    cy.url().should("include", "/confirmation")
+  })
+})
