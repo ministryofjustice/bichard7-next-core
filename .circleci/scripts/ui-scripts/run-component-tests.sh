7c116e2dfb7fb8c351ec224652dd269438f0ab2c
Richard Race <1273965+richrace@users.noreply.github.com>
2025-10-30 16:35:48 +0000

    Remove MS Edge related code

--- .circleci/scripts/ui-scripts/run-component-tests.sh
+++ .circleci/scripts/ui-scripts/run-component-tests.sh
@@ -3,8 +3,4 @@ cd ~/project/packages/ui
 
 options="--component --reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
-if [[ $MS_EDGE == "true" ]]; then
-  options+=" --browser edge"
-fi
-
 circleci tests glob "cypress/component/**/*.cy.tsx" | tr [:space:] "," | sed 's/,$//g'  | circleci tests run --command="xargs npx cypress run ${options} --spec"
