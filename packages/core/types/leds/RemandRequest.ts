e2e683ed07ed23beda3f7a32644117200edef981
Emad Karamad <emad.karamad@madetech.com>
2025-10-31 15:30:27 +0000

    Rename remand request types and schemas

--- packages/core/types/leds/RemandRequest.ts
+++ packages/core/types/leds/RemandRequest.ts
@@ -1,11 +1,13 @@
 import type z from "zod"
 
 import type {
-  ledsCurrentAppearanceSchema,
-  ledsNextAppearanceSchema,
+  appearanceResultSchema,
+  currentAppearanceSchema,
+  nextAppearanceSchema,
   remandRequestSchema
 } from "../../schemas/leds/remandRequest"
 
-export type ledsCurrentAppearance = z.infer<typeof ledsCurrentAppearanceSchema>
-export type ledsNextAppearance = z.infer<typeof ledsNextAppearanceSchema>
+export type AppearanceResult = z.infer<typeof appearanceResultSchema>
+export type CurrentAppearance = z.infer<typeof currentAppearanceSchema>
+export type NextAppearance = z.infer<typeof nextAppearanceSchema>
 export type RemandRequest = z.infer<typeof remandRequestSchema>
