84f825e30db629502f9951b077ae8c2e474007c6
padlichan <lili.berenyi@gmail.com>
2025-11-06 17:45:16 +0000

    Set environment variable at build time rather than run time

--- .circleci/scripts/user-service/run-cypress-e2e-tests.sh
+++ .circleci/scripts/user-service/run-cypress-e2e-tests.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd ~/project/packages/user-service
 
-export NEXT_PUBLIC_INCORRECT_DELAY=0
-
 CYPRESS_OPTS="--reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 COMMAND="xargs -I {} npx concurrently --raw --kill-others --success first \"npm run start\" \"npx cypress run $CYPRESS_OPTS --spec {}\""
