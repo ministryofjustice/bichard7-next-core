f943af5dc1c457579d6c2736e76b067ec05a85fd
emmalanigan <emma.keeley@madetech.com>
2025-11-11 15:26:15 +0000

    add e2e test

--- packages/ui/cypress/e2e/case-list/filter-cases/filterByCourtDateReceivedDateMismatch.cy.ts
+++ packages/ui/cypress/e2e/case-list/filter-cases/filterByCourtDateReceivedDateMismatch.cy.ts
@@ -1,3 +1,5 @@
+import { visitBasePath } from "../../../support/helpers"
+
 describe("Filtering cases by court date received date mismatch", () => {
   beforeEach(() => {
     cy.task("clearCourtCases")
@@ -22,6 +24,37 @@ describe("Filtering cases by court date received date mismatch", () => {
     cy.get("#court-date-received-date-mismatch").should("be.checked")
     cy.get(".govuk-heading-m").contains("Applied filters")
     cy.get(".moj-filter__tag").contains("Include cases where date received is different")
+  })
+
+  it("Should filter cases by court date received date mismatch", () => {
+    const firstDate = new Date("2001-09-26")
+    const secondDate = new Date("2008-01-26")
+    const thirdDate = new Date("2008-03-26")
+    const fourthDate = new Date("2013-10-16")
+    const orgCode = "011111"
+
+    cy.task("insertCourtCasesWithFields", [
+      { courtDate: firstDate, messageReceivedTimestamp: firstDate, orgForPoliceFilter: orgCode },
+      { courtDate: secondDate, messageReceivedTimestamp: secondDate, orgForPoliceFilter: orgCode },
+      { courtDate: secondDate, messageReceivedTimestamp: thirdDate, orgForPoliceFilter: orgCode },
+      { courtDate: thirdDate, messageReceivedTimestamp: fourthDate, orgForPoliceFilter: orgCode }
+    ])
+
+    visitBasePath()
+
+    // When checkbox isn't clicked
+    cy.get(".moj-scrollable-pane tbody tr").should("have.length", 4)
+    cy.contains("Case00000")
+    cy.contains("Case00001")
+    cy.contains("Case00002")
+    cy.contains("Case00003")
+
+    // Filter for cases with mismatch
+    cy.get("#court-date-received-date-mismatch").click()
+    cy.get("#search").click()
+    cy.get(".moj-scrollable-pane tbody tr").should("have.length", 2)
+    cy.contains("Case00002")
+    cy.contains("Case00003")
   })
 })
 
