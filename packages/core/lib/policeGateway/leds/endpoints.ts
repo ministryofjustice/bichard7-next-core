fd7273016372b955b5ede4dc0376088f9c009bb3
Emad Karamad <emad.karamad@madetech.com>
2025-11-13 15:05:11 +0000

    Update LEDS endpoints to use courtCaseId

--- packages/core/lib/policeGateway/leds/endpoints.ts
+++ packages/core/lib/policeGateway/leds/endpoints.ts
@@ -1,10 +1,10 @@
 const endpoints = {
   asnQuery: "/find-disposals-by-asn",
   remand: (personId: string, reportId: string) => `/people/${personId}/arrest-reports/${reportId}/basic-remands`,
-  addDisposal: (personId: string, disposalId: string) =>
-    `/people/${personId}/disposals/${disposalId}/court-case-disposal-result`,
-  subsequentDisposalResults: (personId: string, disposalId: string) =>
-    `/people/${personId}/disposals/${disposalId}/court-case-subsequent-disposal-results`
+  addDisposal: (personId: string, courtCaseId: string) =>
+    `/people/${personId}/disposals/${courtCaseId}/court-case-disposal-result`,
+  subsequentDisposalResults: (personId: string, courtCaseId: string) =>
+    `/people/${personId}/disposals/${courtCaseId}/court-case-subsequent-disposal-results`
 }
 
 export default endpoints
