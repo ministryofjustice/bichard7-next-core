4af3f456df9185446a896e7b2496b64d98562eb6
Tausif Patel <tausifpatel.dev@gmail.com>
2025-11-18 10:40:25 +0000

    Add remand and subsequentDisposal operations

---
+++ packages/core/lib/policeGateway/leds/processors/subsequentDisposal.ts
@@ -0,0 +1,38 @@
+import type { PncUpdateDataset } from "@moj-bichard7/common/types/PncUpdateDataset"
+
+import { PncOperation } from "@moj-bichard7/common/types/PncOperation"
+
+import type PoliceUpdateRequest from "../../../../phase3/types/PoliceUpdateRequest"
+import type { SubsequentDisposalResultsRequest } from "../../../../types/leds/SubsequentDisposalResultsRequest"
+
+import { subsequentDisposalResultsRequestSchema } from "../../../../schemas/leds/subsequentDisposalResultsRequest"
+import PoliceApiError from "../../PoliceApiError"
+import endpoints from "../endpoints"
+import { findCourtCaseId } from "../mapToAddDisposalRequest/findCourtCaseId"
+import mapToSubsequentDisposalRequest from "../mapToSubsequentDisposalRequest/mapToSubsequentDisposalRequest"
+
+export const subsequentDisposal = (
+  request: PoliceUpdateRequest,
+  personId: string,
+  pncUpdateDataset: PncUpdateDataset
+): PoliceApiError | { endpoint: string; requestBody: SubsequentDisposalResultsRequest } => {
+  if (request.operation !== PncOperation.DISPOSAL_UPDATED && request.operation !== PncOperation.SENTENCE_DEFERRED) {
+    return new PoliceApiError(["mapToRemandRequest called with a non-remand request"])
+  }
+
+  const courtCaseId = findCourtCaseId(pncUpdateDataset, request.request.courtCaseReferenceNumber)
+
+  if (!courtCaseId) {
+    return new PoliceApiError(["Failed to update LEDS due to missing data."])
+  }
+
+  const endpoint = endpoints.subsequentDisposalResults(personId, courtCaseId)
+  const requestBody = mapToSubsequentDisposalRequest(request.request, pncUpdateDataset)
+  const validation = subsequentDisposalResultsRequestSchema.safeParse(requestBody)
+
+  if (!validation.success) {
+    return new PoliceApiError(["Failed to validate LEDS request."])
+  }
+
+  return { endpoint, requestBody }
+}
