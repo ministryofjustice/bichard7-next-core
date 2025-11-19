a743a24c0b407fb33b7358ad6400227e67d57e30
Tausif Patel <tausifpatel.dev@gmail.com>
2025-11-19 16:27:40 +0000

    Add cof segment converter

---
+++ packages/e2e-test/utils/pncSegmentConverters/convertCof.ts
@@ -0,0 +1,16 @@
+export const convertCof = (cofXml: string) => {
+  const slice = (start: number, end: number) => cofXml.substring(start, end).trim()
+
+  return {
+    intfcUpdateType: slice(0, 1),
+    referenceNumber: slice(1, 4),
+    offenceQualifier1: slice(4, 6),
+    offenceQualifier2: slice(6, 8),
+    acpoOffenceCode: slice(8, 21),
+    cjsOffenceCode: slice(21, 29),
+    offenceStartDate: slice(29, 37),
+    offenceStartTime: slice(37, 41),
+    offenceEndDate: slice(41, 49),
+    offenceEndTime: slice(49, 53)
+  }
+}
