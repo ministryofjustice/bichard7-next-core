1b281e77b4afe0cbab205c31422a0bfd95dc4ec8
Tausif Patel <tausifpatel.dev@gmail.com>
2025-11-19 15:42:41 +0000

    Implement convertFsc function

--- packages/e2e-test/utils/pncSegmentConverters/convertFsc.ts
+++ packages/e2e-test/utils/pncSegmentConverters/convertFsc.ts
@@ -1,1 +1,6 @@
-const convertFsc = (fscXml: string) => {}
+export const convertFsc = (fscXml: string) => {
+  return {
+    updateTypeKey: fscXml.substring(0, 1),
+    forceStationCode: fscXml.substring(1, 5)
+  }
+}
