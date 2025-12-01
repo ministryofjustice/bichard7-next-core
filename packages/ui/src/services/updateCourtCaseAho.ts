a3a39b337e1dab226986acba03d0d881115935cf
Emad Karamad <emad.karamad@madetech.com>
2025-12-01 10:49:33 +0000

    Update core and ui to update new hearing outcome json columns
    # Conflicts:
    #	packages/core/lib/database/__snapshots__/updateErrorListRecord.integration.test.ts.snap

--- packages/ui/src/services/updateCourtCaseAho.ts
+++ packages/ui/src/services/updateCourtCaseAho.ts
@@ -1,22 +1,33 @@
+import type { AnnotatedHearingOutcome } from "@moj-bichard7/common/types/AnnotatedHearingOutcome"
+import type { PncUpdateDataset } from "@moj-bichard7/common/types/PncUpdateDataset"
+import { isPncUpdateDataset } from "@moj-bichard7/common/types/PncUpdateDataset"
+import serialiseToAhoXml from "@moj-bichard7/core/lib/serialise/ahoXml/serialiseToXml"
+import serialiseToPncUpdateDatasetXml from "@moj-bichard7/core/lib/serialise/pncUpdateDatasetXml/serialiseToXml"
 import type { DataSource, EntityManager, UpdateResult } from "typeorm"
 import CourtCase from "./entities/CourtCase"
 
 const updateCourtCaseAho = async (
   dataSource: DataSource | EntityManager,
   courtCaseId: number,
-  updatedHo: string
-): Promise<UpdateResult | Error> =>
-  await dataSource
+  updatedHo: AnnotatedHearingOutcome | PncUpdateDataset
+): Promise<UpdateResult | Error> => {
+  const updatedHoXml = isPncUpdateDataset(updatedHo)
+    ? serialiseToPncUpdateDatasetXml(updatedHo, false)
+    : serialiseToAhoXml(updatedHo, false)
+
+  return await dataSource
     .getRepository(CourtCase)
     .createQueryBuilder()
     .update(CourtCase)
     .set({
-      updatedHearingOutcome: updatedHo,
+      updatedHearingOutcome: updatedHoXml,
+      updatedHearingOutcomeJson: updatedHo,
       userUpdatedFlag: 1
     })
     .where("error_id = :id", { id: courtCaseId })
     .returning("*")
     .execute()
     .catch((error: Error) => error)
+}
 
 export default updateCourtCaseAho
