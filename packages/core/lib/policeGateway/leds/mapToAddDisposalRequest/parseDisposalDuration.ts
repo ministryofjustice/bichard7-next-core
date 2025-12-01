55592a5e566efa5880a5ca34145699a98ba28275
Tausif Patel <tausifpatel.dev@gmail.com>
2025-12-01 14:57:43 +0000

    Extract out parseDisposalDuration function so that it can be reused

---
+++ packages/core/lib/policeGateway/leds/mapToAddDisposalRequest/parseDisposalDuration.ts
@@ -0,0 +1,26 @@
+import type { DisposalDurationUnit } from "../../../../types/leds/DisposalRequest"
+
+export const parseDisposalDuration = (disposalQuantity: string) => {
+  const quantity = disposalQuantity.toLowerCase()
+  const durationCode = quantity.slice(0, 1).trim()
+
+  let count: number = 0
+  let units: DisposalDurationUnit
+
+  if (quantity.slice(0, 4) === "y999") {
+    count = 1
+    units = "life"
+  } else {
+    count = Number(quantity.slice(1, 4).trim()) || 0
+    const unitMap: Record<string, DisposalDurationUnit> = {
+      d: "days",
+      h: "hours",
+      m: "months",
+      w: "weeks",
+      y: "years"
+    }
+    units = unitMap[durationCode] ?? ""
+  }
+
+  return { units, count }
+}
