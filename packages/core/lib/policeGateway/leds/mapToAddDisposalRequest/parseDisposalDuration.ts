9c4b64fa1969123903310bebd5f655f807e23fc9
Tausif Patel <tausifpatel.dev@gmail.com>
2025-12-03 12:42:49 +0000

    Move unitMap out of the function

--- packages/core/lib/policeGateway/leds/mapToAddDisposalRequest/parseDisposalDuration.ts
+++ packages/core/lib/policeGateway/leds/mapToAddDisposalRequest/parseDisposalDuration.ts
@@ -1,16 +1,17 @@
 import type { DisposalDurationUnit } from "../../../../types/leds/DisposalRequest"
 
+const unitMap: Record<string, DisposalDurationUnit> = {
+  d: "days",
+  h: "hours",
+  m: "months",
+  w: "weeks",
+  y: "years"
+} as const
+
 export const parseDisposalDuration = (disposalQuantity: string) => {
   const quantity = disposalQuantity.toLowerCase()
   const durationCode = quantity.slice(0, 1).trim()
   const countString = quantity.slice(1, 4).trim()
-  const unitMap: Record<string, DisposalDurationUnit> = {
-    d: "days",
-    h: "hours",
-    m: "months",
-    w: "weeks",
-    y: "years"
-  } as const
 
   let count: number = 0
   let units: DisposalDurationUnit
