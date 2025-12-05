225937118d5cb7377d14cc5595124544146e0dbe
joe-fol <joefolkard@protonmail.com>
2025-12-05 11:15:24 +0000

    Dont set the reporter in run-component-test.sh

--- .circleci/scripts/ui-scripts/run-component-tests.sh
+++ .circleci/scripts/ui-scripts/run-component-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/project/packages/ui
 
-options="--component --reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
+options="--component"
 
-circleci tests glob "cypress/component/**/*.cy.tsx" | tr [:space:] "," | sed 's/,$//g'  | circleci tests run --command="xargs npx cypress run ${options} --spec"
+circleci tests glob "cypress/component/**/*.cy.tsx" | tr [:space:] "," | sed 's/,$//g' | circleci tests run --command="xargs npx cypress run ${options} --spec"
