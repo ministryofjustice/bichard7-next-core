513b4595be981b1fcbec2f71c0334875630ad9ff
joe-fol <joefolkard@protonmail.com>
2025-12-05 15:32:02 +0000

    Dont set the reporter in run-cypress-e2e-tests.sh

--- .circleci/scripts/user-service/run-cypress-e2e-tests.sh
+++ .circleci/scripts/user-service/run-cypress-e2e-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd ~/project/packages/user-service
 
-CYPRESS_OPTS="--reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
+CYPRESS_OPTS="resultsDir=./cypress/results"
 
 COMMAND="xargs -I {} npx concurrently --raw --kill-others --success first \"npm run start\" \"npx cypress run $CYPRESS_OPTS --spec {}\""
 
 circleci tests glob "cypress/e2e/**/*.cy.js" | circleci tests run --command="$COMMAND" --split-by=timings
+
