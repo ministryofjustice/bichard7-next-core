a9c94833abb1de333a916e796950f8cefdee6957
Tom Watkins <tom.watkins@justice.gov.uk>
2025-12-09 13:21:51 +0000

    Correct tests

--- packages/api/src/useCases/cases/getCase/lockAndFetchCaseDto.integration.test.ts
+++ packages/api/src/useCases/cases/getCase/lockAndFetchCaseDto.integration.test.ts
@@ -51,7 +51,7 @@ describe("lockAndFetchCaseDto", () => {
       errorReport: "HO100304||br7:ArrestSummonsNumber",
       errorStatus: "Unresolved",
       isUrgent: 1,
-      messageReceivedAt: new Date("2025-05-23"),
+      messageReceivedTimestamp: new Date("2025-05-23"),
       noteCount: undefined,
       notes: [],
       orgForPoliceFilter: "01",
