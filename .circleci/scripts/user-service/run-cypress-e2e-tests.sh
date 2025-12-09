efdaa358aa8b7c074b6be5a4dc3f96260ef97f63
joe-fol <joefolkard@protonmail.com>
2025-12-09 10:53:54 +0000

    Revert "Dont set the reporter in run-cypress-e2e-tests.sh"
    This reverts commit 169a1403497d3c4d23db362fc630c7cc3d30c3d7.

--- .circleci/scripts/user-service/run-cypress-e2e-tests.sh
+++ .circleci/scripts/user-service/run-cypress-e2e-tests.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 cd ~/project/packages/user-service
 
-CYPRESS_OPTS="resultsDir=./cypress/results"
+CYPRESS_OPTS="--reporter ../../node_modules/cypress-circleci-reporter --reporter-options resultsDir=./cypress/results"
 
 COMMAND="xargs -I {} npx concurrently --raw --kill-others --success first \"npm run start\" \"npx cypress run $CYPRESS_OPTS --spec {}\""
 
 circleci tests glob "cypress/e2e/**/*.cy.js" | circleci tests run --command="$COMMAND" --split-by=timings
-
