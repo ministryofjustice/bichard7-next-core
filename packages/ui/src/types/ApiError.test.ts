fdf86e515fe570be996eaddbd97f7de132ebbbd2
Tom Watkins <tom.watkins@justice.gov.uk>
2025-12-10 14:35:53 +0000

    New API error type

---
+++ packages/ui/src/types/ApiError.test.ts
@@ -0,0 +1,18 @@
+import { ApiError, isApiError } from "./ApiError"
+
+describe("ApiError", () => {
+  it("should return true if API error", () => {
+    const error = new ApiError(404, "Not Found")
+    expect(isApiError(error)).toBe(true)
+  })
+
+  it("should return false if for normal error", () => {
+    const error = new Error("Normal error")
+    expect(isApiError(error)).toBe(false)
+  })
+
+  it("should return false if not an error at all", () => {
+    const error = "Error message"
+    expect(isApiError(error)).toBe(false)
+  })
+})
