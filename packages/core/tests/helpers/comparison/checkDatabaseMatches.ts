926bfc5a51b8acf18823e70b72e6ce0fdb9296ac
Emad Karamad <emad.karamad@madetech.com>
2025-12-12 12:22:30 +0000

    Fix comparison tests to compare all db records

--- packages/core/tests/helpers/comparison/checkDatabaseMatches.ts
+++ packages/core/tests/helpers/comparison/checkDatabaseMatches.ts
@@ -18,43 +18,47 @@ const checkDatabaseMatches = async (expected: DbRecords): Promise<void> => {
   }
 
   expect(errorList).toHaveLength(expected.errorList.length)
-  const expectedError = expected.errorList[0]
 
-  expect(errorList[0].message_id).toEqual(expectedError.message_id)
-  expect(errorList[0].phase).toEqual(expectedError.phase)
-  expect(errorList[0].error_status).toEqual(expectedError.error_status)
-  expect(errorList[0].trigger_status).toEqual(expectedError.trigger_status)
-  expect(errorList[0].error_quality_checked).toEqual(expectedError.error_quality_checked)
-  expect(errorList[0].trigger_quality_checked).toEqual(expectedError.trigger_quality_checked)
-  expect(errorList[0].trigger_count).toEqual(expectedError.trigger_count)
-  expect(errorList[0].is_urgent).toEqual(expectedError.is_urgent)
-  expect(errorList[0].asn).toEqual(expectedError.asn)
-  expect(errorList[0].court_code).toEqual(expectedError.court_code)
-  expect(normaliseXml(errorList[0].annotated_msg)).toEqualXML(normaliseXml(expectedError.annotated_msg))
-  expect(normaliseXml(errorList[0].updated_msg)).toEqualXML(normaliseXml(expectedError.updated_msg))
-  expect(errorList[0].error_report).toEqual(expectedError.error_report)
-  expect(errorList[0].create_ts).toBeDefined()
-  expect(errorList[0].error_reason).toEqual(expectedError.error_reason)
+  for (const expectedError of expected.errorList) {
+    const actualErrorRecord = errorList.find((record) => record.message_id === expectedError.message_id)!
 
-  if (expectedError.trigger_reason) {
-    expect(expectedTriggers).toContain(errorList[0].trigger_reason)
-  } else {
-    expect(errorList[0].trigger_reason).toBeNull()
+    expect(actualErrorRecord).toBeDefined()
+    expect(actualErrorRecord.message_id).toEqual(expectedError.message_id)
+    expect(actualErrorRecord.phase).toEqual(expectedError.phase)
+    expect(actualErrorRecord.error_status).toEqual(expectedError.error_status)
+    expect(actualErrorRecord.trigger_status).toEqual(expectedError.trigger_status)
+    expect(actualErrorRecord.error_quality_checked).toEqual(expectedError.error_quality_checked)
+    expect(actualErrorRecord.trigger_quality_checked).toEqual(expectedError.trigger_quality_checked)
+    expect(actualErrorRecord.trigger_count).toEqual(expectedError.trigger_count)
+    expect(actualErrorRecord.is_urgent).toEqual(expectedError.is_urgent)
+    expect(actualErrorRecord.asn).toEqual(expectedError.asn)
+    expect(actualErrorRecord.court_code).toEqual(expectedError.court_code)
+    expect(normaliseXml(actualErrorRecord.annotated_msg)).toEqualXML(normaliseXml(expectedError.annotated_msg))
+    expect(normaliseXml(actualErrorRecord.updated_msg)).toEqualXML(normaliseXml(expectedError.updated_msg))
+    expect(actualErrorRecord.error_report).toEqual(expectedError.error_report)
+    expect(actualErrorRecord.create_ts).toBeDefined()
+    expect(actualErrorRecord.error_reason).toEqual(expectedError.error_reason)
+
+    if (expectedError.trigger_reason) {
+      expect(expectedTriggers).toContain(actualErrorRecord.trigger_reason)
+    } else {
+      expect(actualErrorRecord.trigger_reason).toBeNull()
+    }
+
+    expect(actualErrorRecord.error_count).toEqual(expectedError.error_count)
+    expect(actualErrorRecord.court_date).toEqual(expectedError.court_date)
+    expect(actualErrorRecord.ptiurn).toEqual(expectedError.ptiurn)
+    expect(actualErrorRecord.court_name).toEqual(expectedError.court_name)
+    expect(actualErrorRecord.msg_received_ts).toBeDefined()
+    expect(actualErrorRecord.trigger_resolved_ts).toEqual(expectedError.trigger_resolved_ts)
+    expect(actualErrorRecord.defendant_name).toEqual(expectedError.defendant_name)
+    expect(actualErrorRecord.org_for_police_filter).toEqual(expectedError.org_for_police_filter)
+    expect(actualErrorRecord.court_room).toEqual(expectedError.court_room)
+    expect(actualErrorRecord.court_reference).toEqual(expectedError.court_reference)
+    expect(actualErrorRecord.error_insert_ts).toBeDefined()
+    expect(actualErrorRecord.trigger_insert_ts).toBeDefined()
+    expect(actualErrorRecord.pnc_update_enabled).toEqual(expectedError.pnc_update_enabled)
   }
-
-  expect(errorList[0].error_count).toEqual(expectedError.error_count)
-  expect(errorList[0].court_date).toEqual(expectedError.court_date)
-  expect(errorList[0].ptiurn).toEqual(expectedError.ptiurn)
-  expect(errorList[0].court_name).toEqual(expectedError.court_name)
-  expect(errorList[0].msg_received_ts).toBeDefined()
-  expect(errorList[0].trigger_resolved_ts).toEqual(expectedError.trigger_resolved_ts)
-  expect(errorList[0].defendant_name).toEqual(expectedError.defendant_name)
-  expect(errorList[0].org_for_police_filter).toEqual(expectedError.org_for_police_filter)
-  expect(errorList[0].court_room).toEqual(expectedError.court_room)
-  expect(errorList[0].court_reference).toEqual(expectedError.court_reference)
-  expect(errorList[0].error_insert_ts).toBeDefined()
-  expect(errorList[0].trigger_insert_ts).toBeDefined()
-  expect(errorList[0].pnc_update_enabled).toEqual(expectedError.pnc_update_enabled)
 
   expect(errorListTriggers).toHaveLength(expected.errorListTriggers.length)
   expect(normaliseErrorListTriggers(errorListTriggers)).toStrictEqual(
