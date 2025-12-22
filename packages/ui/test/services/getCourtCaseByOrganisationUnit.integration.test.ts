897f17e8608f7bbfbdc84d9379bfd8f35fc3e05f
Tom Watkins <tom.watkins@justice.gov.uk>
2025-12-22 15:27:44 +0000

    Add tests for user roles

--- packages/ui/test/services/getCourtCaseByOrganisationUnit.integration.test.ts
+++ packages/ui/test/services/getCourtCaseByOrganisationUnit.integration.test.ts
@@ -8,6 +8,9 @@ import getDataSource from "../../src/services/getDataSource"
 import { isError } from "../../src/types/Result"
 import deleteFromEntity from "../utils/deleteFromEntity"
 import { getDummyCourtCase, insertCourtCases } from "../utils/insertCourtCases"
+import { UserGroup } from "@moj-bichard7/common/types/UserGroup"
+import { ResolutionStatus } from "@moj-bichard7/common/types/ApiCaseQuery"
+import TriggerCode from "@moj-bichard7-developers/bichard7-next-data/dist/types/TriggerCode"
 
 jest.mock("services/queries/leftJoinAndSelectTriggersQuery")
 
@@ -34,6 +37,203 @@ describe("getCourtCaseByOrganisationUnits", () => {
     if (dataSource) {
       await dataSource.destroy()
     }
+  })
+
+  describe("Test different roles", () => {
+    it("fetches case if user is a supervisor", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " ")
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        groups: [UserGroup.Supervisor]
+      } as Partial<User> as User)
+
+      expect(isError(result)).toBe(false)
+      expect(result).toStrictEqual(inputCourtCase)
+    })
+
+    it("returns null if user in no groups", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " ")
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        groups: [UserGroup.NewUI]
+      } as Partial<User> as User)
+
+      expect(result).toBeNull()
+    })
+
+    it("fetches the case if the user is an exception handler and the case has unresolved exceptions", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " "),
+        errorCount: 1
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        groups: [UserGroup.ExceptionHandler]
+      } as Partial<User> as User)
+
+      expect(isError(result)).toBe(false)
+      expect(result).toStrictEqual(inputCourtCase)
+    })
+
+    it("fetches the case if the user is an exception handler and the case has been resolved by them", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " "),
+        errorCount: 1,
+        errorResolvedBy: "ExceptionHandler",
+        errorStatus: ResolutionStatus.Resolved
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        username: "ExceptionHandler",
+        groups: [UserGroup.ExceptionHandler]
+      } as Partial<User> as User)
+
+      expect(isError(result)).toBe(false)
+      expect(result).toStrictEqual(inputCourtCase)
+    })
+
+    it("returns null if the user is an exception handler but the case has been resolved by another user", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " "),
+        errorCount: 1,
+        errorResolvedBy: "AnotherUser",
+        errorStatus: ResolutionStatus.Resolved
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        username: "ExceptionHandler",
+        groups: [UserGroup.ExceptionHandler]
+      } as Partial<User> as User)
+
+      expect(result).toBeNull()
+    })
+
+    it("returns null if the user is an exception handler but the case has no exceptions", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " "),
+        errorCount: 0
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        groups: [UserGroup.ExceptionHandler]
+      } as Partial<User> as User)
+
+      expect(result).toBeNull()
+    })
+
+    it("fetches the case if the user is a trigger handler and the case has unresolved triggers", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " "),
+        triggerStatus: ResolutionStatus.Unresolved,
+        triggers: [
+          {
+            triggerCode: TriggerCode.TRPR0001,
+            status: ResolutionStatus.Unresolved,
+            createdAt: new Date()
+          } as unknown as Trigger
+        ]
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        groups: [UserGroup.TriggerHandler]
+      } as Partial<User> as User)
+
+      expect(isError(result)).toBe(false)
+      expect((result as CourtCase).errorId).toBe(inputCourtCase.errorId)
+    })
+
+    it("fetches the case if the user is a trigger handler and the case has been resolved by them", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " "),
+        triggerStatus: ResolutionStatus.Resolved,
+        triggers: [
+          {
+            triggerCode: TriggerCode.TRPR0001,
+            status: ResolutionStatus.Resolved,
+            resolvedBy: "TriggerHandler",
+            createdAt: new Date()
+          } as unknown as Trigger
+        ]
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        username: "TriggerHandler",
+        groups: [UserGroup.TriggerHandler]
+      } as Partial<User> as User)
+
+      expect(isError(result)).toBe(false)
+      expect((result as CourtCase).errorId).toBe(inputCourtCase.errorId)
+    })
+
+    it("returns error if the user is a trigger handler but the case has been resolved by another user", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " "),
+        triggerStatus: ResolutionStatus.Resolved,
+        triggers: [
+          {
+            triggerCode: TriggerCode.TRPR0001,
+            status: ResolutionStatus.Resolved,
+            resolvedBy: "AnotherUser",
+            createdAt: new Date()
+          } as unknown as Trigger
+        ]
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        username: "TriggerHandler",
+        groups: [UserGroup.TriggerHandler]
+      } as Partial<User> as User)
+
+      expect(isError(result)).toBe(false)
+      expect(result).toBeNull()
+    })
+
+    it("returns an error if the user is an trigger handler but the case has no triggers", async () => {
+      const inputCourtCase = await getDummyCourtCase({
+        orgForPoliceFilter: courtCode.padEnd(6, " ")
+      })
+      await insertCourtCases(inputCourtCase)
+
+      const result = await getCourtCaseByOrganisationUnit(dataSource, inputCourtCase.errorId, {
+        visibleForces: [courtCode.substring(0, 2)],
+        visibleCourts: [],
+        groups: [UserGroup.TriggerHandler]
+      } as Partial<User> as User)
+
+      expect(isError(result)).toBe(false)
+      expect(result).toBeNull()
+    })
   })
 
   it("Should call leftJoinAndSelectTriggersQuery with the correct arguments", async () => {
