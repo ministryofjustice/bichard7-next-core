dc084660b6f9cafc380beaf9c3d8314d09399c9c
Tom Watkins <tom.watkins@justice.gov.uk>
2026-01-05 13:48:11 +0000

    Change from enum to const object

--- packages/common/types/ResolutionStatus.ts
+++ packages/common/types/ResolutionStatus.ts
@@ -4,10 +4,10 @@ export const ResolutionStatus = {
   Unresolved: "Unresolved"
 } as const
 
-export enum ResolutionStatusNumber {
-  Resolved = 2,
-  Submitted = 3,
-  Unresolved = 1
-}
+export const ResolutionStatusNumber = {
+  Resolved: 2,
+  Submitted: 3,
+  Unresolved: 1
+} as const
 
 export type ResolutionStatus = (typeof ResolutionStatus)[keyof typeof ResolutionStatus]
