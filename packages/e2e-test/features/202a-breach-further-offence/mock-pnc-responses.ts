68a570b5d20b029221f5ddc47830cadc94b7087b
Emad Karamad <emad.karamad@madetech.com>
2026-01-09 10:47:47 +0000

    Update mock-pnc-response files to use mockAsnQuery

--- packages/e2e-test/features/202a-breach-further-offence/mock-pnc-responses.ts
+++ packages/e2e-test/features/202a-breach-further-offence/mock-pnc-responses.ts
@@ -1,7 +1,8 @@
+import extractAsnFromInputXml from "../../utils/extractAsnFromInputXml"
 import type Bichard from "../../utils/world"
 
 export default (_: string, { policeApi }: Bichard) => [
-  {
+  policeApi.mockAsnQuery({
     matchRegex: "CXE01",
     response: `<?xml version="1.0" standalone="yes"?>
     <CXE01>
@@ -14,9 +15,10 @@ export default (_: string, { policeApi }: Bichard) => [
       </ASI>
       <GMT>000008073ENQR000710R</GMT>
     </CXE01>`,
+    asn: extractAsnFromInputXml(`${__dirname}/input-message-1.xml`),
     expectedRequest: "",
     count: 1
-  },
+  }),
   policeApi.mockUpdate("CXU02", {
     expectedRequest:
       "<FSC>K01YZ</FSC><IDS>K21/15K     COMMUNITYORD            </IDS><CCR>K21/2812/4H                    </CCR><COU>I2576                                                                       COMMUNITYORDER/BREACH                                 260920080000</COU><CCH>K001              CJ88116 </CCH><ADJ>INOT GUILTY   GUILTY        260920080000 </ADJ><DIS>I1116                      00                                                                            </DIS><DIS>I3101H100                  00                                                                            </DIS>",
--- packages/e2e-test/features/439-HO100332-two-court-offences-two-pnc-ccrs/mock-pnc-responses.ts
+++ packages/e2e-test/features/439-HO100332-two-court-offences-two-pnc-ccrs/mock-pnc-responses.ts
@@ -1,7 +1,8 @@
+import extractAsnFromInputXml from "../../utils/extractAsnFromInputXml"
 import type Bichard from "../../utils/world"
 
 export default (_: string, { policeApi }: Bichard) => [
-  {
+  policeApi.mockAsnQuery({
     matchRegex: "CXE01",
     response: `<?XML VERSION="1.0" STANDALONE="YES"?>
     <CXE01>
@@ -17,8 +18,10 @@ export default (_: string, { policeApi }: Bichard) => [
       </ASI>
       <GMT>000008073ENQR004540S</GMT>
     </CXE01>`,
-    expectedRequest: ""
-  },
+    asn: extractAsnFromInputXml(`${__dirname}/input-message-1.xml`),
+    expectedRequest: "",
+    count: 1
+  }),
   policeApi.mockUpdate("CXU02", {
     matchRegex: "CXU02.*SX03001A",
     count: 1,
