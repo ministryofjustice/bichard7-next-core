7c96b1edaf6bd8d33eac6af55469db231a1a00bc
Tausif Patel <tausifpatel.dev@gmail.com>
2026-01-26 15:27:36 +0000

    Fix disposalQualifiers mapping

--- packages/core/lib/policeGateway/leds/mapToAddDisposalRequest/mapOffences.ts
+++ packages/core/lib/policeGateway/leds/mapToAddDisposalRequest/mapOffences.ts
@@ -36,10 +36,14 @@ const mapOffences = (
 
     const disposalResults = disposals.map((disposal) => {
       const { count, units, disposalEffectiveDate, amount } = parseDisposalQuantity(disposal.disposalQuantity)
+      const disposalQualifiers = disposal.disposalQualifiers
+        ?.match(/.{1,2}/g)
+        ?.map((q) => q.trim())
+        .filter(Boolean)
 
       return {
         disposalCode: Number(disposal.disposalType),
-        disposalQualifiers: disposal.disposalQualifiers ? [disposal.disposalQualifiers] : [],
+        disposalQualifiers,
         disposalText: disposal.disposalText ?? undefined,
         ...(units && { disposalDuration: { count, units } }),
         disposalEffectiveDate,
