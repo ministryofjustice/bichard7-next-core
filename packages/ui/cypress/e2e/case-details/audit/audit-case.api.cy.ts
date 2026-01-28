0a8ff96005f2b54cc6573069397c8be948b3ad8e
Tom Watkins <tom.watkins@justice.gov.uk>
2026-01-28 13:36:44 +0000

    Added e2e tests for partial auditing

--- packages/ui/cypress/e2e/case-details/audit/audit-case.api.cy.ts
+++ packages/ui/cypress/e2e/case-details/audit/audit-case.api.cy.ts
@@ -38,4 +38,68 @@ describe("Audit case", () => {
     cy.contains("Trigger Quality").should("exist")
     cy.contains("Exception Quality").should("exist")
   })
+
+  it("saves trigger only audit results and show QualityStatusForm with partially read only fields", () => {
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: AsnExceptionHO100206.hearingOutcomeXml,
+        updatedHearingOutcome: AsnExceptionHO100206.hearingOutcomeXml,
+        errorCount: 1,
+        errorStatus: "Resolved",
+        triggerCount: 1
+      }
+    ])
+
+    loginAndVisit("Supervisor", "/bichard/court-cases/0")
+
+    cy.contains("Set quality status").should("be.visible")
+    cy.get("select[name='trigger-quality']").should("be.visible")
+    cy.get("select[name='trigger-quality']").select(1)
+    cy.get("select[name='exception-quality']").should("be.visible")
+    cy.get("textarea[name='quality-status-note']").type("test note")
+
+    cy.get("#quality-status-submit").click()
+
+    cy.get("select[name='trigger-quality']").should("not.exist")
+    cy.get("select[name='exception-quality']").should("exist")
+    cy.get("textarea[name='quality-status-note']").should("exist")
+    cy.get("#quality-status-submit").should("exist")
+
+    cy.contains("Quality status").should("be.visible")
+    cy.contains("Trigger Quality").should("exist")
+    cy.contains("Exception Quality").should("exist")
+  })
+
+  it("saves exception only audit results and show QualityStatusForm with partially read only fields", () => {
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: AsnExceptionHO100206.hearingOutcomeXml,
+        updatedHearingOutcome: AsnExceptionHO100206.hearingOutcomeXml,
+        errorCount: 1,
+        errorStatus: "Resolved",
+        triggerCount: 1
+      }
+    ])
+
+    loginAndVisit("Supervisor", "/bichard/court-cases/0")
+
+    cy.contains("Set quality status").should("be.visible")
+    cy.get("select[name='trigger-quality']").should("be.visible")
+    cy.get("select[name='exception-quality']").should("be.visible")
+    cy.get("select[name='exception-quality']").select(1)
+    cy.get("textarea[name='quality-status-note']").type("test note")
+
+    cy.get("#quality-status-submit").click()
+
+    cy.get("select[name='trigger-quality']").should("exist")
+    cy.get("select[name='exception-quality']").should("not.exist")
+    cy.get("textarea[name='quality-status-note']").should("exist")
+    cy.get("#quality-status-submit").should("exist")
+
+    cy.contains("Quality status").should("be.visible")
+    cy.contains("Trigger Quality").should("exist")
+    cy.contains("Exception Quality").should("exist")
+  })
 })
