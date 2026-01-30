7ee9ee640120e0be17ca1c6326dde605738f9fa1
Richard Race <1273965+richrace@users.noreply.github.com>
2026-01-30 17:09:33 +0000

    Add API endpoint

--- packages/common/apiEndpoints/endpoints.ts
+++ packages/common/apiEndpoints/endpoints.ts
@@ -1,12 +1,13 @@
-export const enum Endpoints {
-  AuditLog = "/audit-logs/:correlationId",
-  AuditLogEvents = "/audit-logs/:correlationId/events",
-  AuditLogs = "/audit-logs",
-  Case = "/cases/:caseId",
-  CaseAudit = "/cases/:caseId/audit",
-  CaseResubmit = "/cases/:caseId/resubmit",
-  Cases = "/cases",
-  CasesResubmit = "/cases/resubmit",
-  Health = "/health",
-  Me = "/me"
-}
+export const Endpoints = {
+  AuditLog: "/audit-logs/:correlationId",
+  AuditLogEvents: "/audit-logs/:correlationId/events",
+  AuditLogs: "/audit-logs",
+  Case: "/cases/:caseId",
+  CaseAudit: "/cases/:caseId/audit",
+  CaseResubmit: "/cases/:caseId/resubmit",
+  Cases: "/cases",
+  CasesReportsExceptions: "/cases/reports/exceptions",
+  CasesResubmit: "/cases/resubmit",
+  Health: "/health",
+  Me: "/me"
+} as const
