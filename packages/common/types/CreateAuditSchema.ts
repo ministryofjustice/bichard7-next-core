28db7978bd190875fc22f258959491c43c92d455
Tom Watkins <tom.watkins@justice.gov.uk>
2026-02-02 15:33:35 +0000

    Add schema

---
+++ packages/common/types/CreateAuditSchema.ts
@@ -0,0 +1,19 @@
+import { isAfter, isBefore } from "date-fns"
+import z from "zod"
+
+export const CreateAuditSchema = z
+  .object({
+    dateFrom: z.date(),
+    dateTo: z.date(),
+    includedTypes: z.enum(["Triggers", "Exceptions"]),
+    resolvedByUsers: z.string().array().optional(),
+    triggerTypes: z.string().array().optional(),
+    volumeOfCases: z.number().gte(1).lte(100)
+  })
+  .refine(({ dateFrom, dateTo }) => {
+    if (isBefore(dateTo, dateFrom) || isAfter(dateTo, new Date())) {
+      return false
+    }
+  })
+
+export type CreateAudit = z.infer<typeof CreateAuditSchema>
