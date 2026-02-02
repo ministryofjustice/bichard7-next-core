c281fd09df3b42e8a992c8035b2e9ffd1f6d6784
Tausif Patel <tausifpatel.dev@gmail.com>
2026-02-02 16:30:42 +0000

    Return correct expected values

--- packages/core/lib/policeGateway/leds/mapToAddDisposalRequest/parseDisposalQuantity.test.ts
+++ packages/core/lib/policeGateway/leds/mapToAddDisposalRequest/parseDisposalQuantity.test.ts
@@ -5,9 +5,9 @@ describe("parseDisposalQuantity", () => {
     ["D123100520240012000.9900", 123, "days", "2024-05-10", 12000.99],
     ["d123100520249999999.9900", 123, "days", "2024-05-10", 9999999.99],
     ["d1  100520240000009.9900", 1, "days", "2024-05-10", 9.99],
-    ["Y99910052024          00", 1, "life", "2024-05-10", 0],
+    ["Y99910052024          00", 0, "life", "2024-05-10", 0],
     ["D123                  00", 123, "days", undefined, 0],
-    ["    10052024          00", 0, "", "2024-05-10", 0]
+    ["    10052024          00", undefined, undefined, "2024-05-10", 0]
   ])(
     "parses %s into { count: %d, unit: %s, disposalEffectiveDate: %s, amount: %d }",
     (input, expectedCount, expectedUnits, expectedDate, expectedAmount) => {
