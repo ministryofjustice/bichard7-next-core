38d948caa1f1f00ac6218d5e4d84783ca4d19257
Tom Watkins <tom.watkins@justice.gov.uk>
2026-02-02 09:56:23 +0000

    Add index

--- environment/postgres/liquibase/migrations/base/039_add_audit_tables.sql
+++ environment/postgres/liquibase/migrations/base/039_add_audit_tables.sql
@@ -1,8 +1,7 @@
 --liquibase formatted sql
 --changeset tom:039_add_audit_tables
 
-CREATE TABLE br7own.audits
-(
+CREATE TABLE br7own.audits (
     audit_id          SERIAL PRIMARY KEY,
     created_by        VARCHAR   NOT NULL,
     created_when      TIMESTAMP NOT NULL,
@@ -18,6 +17,7 @@ CREATE TABLE br7own.audits
 CREATE TABLE br7own.audit_cases (
     audit_case_id SERIAL PRIMARY KEY,
     audit_id INT REFERENCES br7own.audits(audit_id) NOT NULL,
-    error_id INT REFERENCES br7own.error_list(error_id) NOT NULL,
+    error_id INT REFERENCES br7own.error_list(error_id) NOT NULL
 );
 
+CREATE INDEX audit_cases_audit_id_idx ON br7own.audit_cases (audit_id);
