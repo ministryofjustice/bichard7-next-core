dd091bbdf7abbadb9e677c116081bbdb7a6c8fcb
Kate Reidy <kate.reidy@madetech.com>
2026-02-04 09:27:24 +0000

    Reducing branching when checking path

--- packages/ui/src/utils/nav/isNavLinkForPath.ts
+++ packages/ui/src/utils/nav/isNavLinkForPath.ts
@@ -1,16 +1,11 @@
 import { NavLink } from "types/NavLinks"
 
 export function isNavLinkForPath(path: string, navLink: string): boolean {
-  // court-cases linked back to by /audit should be shown as part of the Audit section
-  if (path.includes("audit") && navLink !== NavLink.Audit) {
-    return false
-  }
-
   switch (navLink) {
     case NavLink.Audit:
       return path.includes("audit")
     case NavLink.CaseList:
-      return path.startsWith("/?") || path === "/" || path.includes("/court-cases")
+      return !path.includes("audit") && (path.includes("/court-cases") || path.startsWith("/?") || path === "/")
     default:
       return false
   }
