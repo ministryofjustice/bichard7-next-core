90e5230bd9786fa341df96b5b9aeda9f3cc5c2c9
Kate Reidy <kate.reidy@madetech.com>
2026-02-06 16:52:52 +0000

    Arrange search fields into 4 grid columns

--- packages/ui/src/features/AuditSearch/AuditSearch.tsx
+++ packages/ui/src/features/AuditSearch/AuditSearch.tsx
@@ -26,126 +26,140 @@ const AuditSearch: React.FC<Props> = (props) => {
       </div>
       <div className="moj-filter__content">
         <div className="moj-filter__options">
-          <FormGroup>
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend--m">{"Dates"}</legend>
+          <div className="govuk-grid-row">
+            <div className="govuk-grid-column-one-quarter">
               <FormGroup>
-                <label className="govuk-body" htmlFor="date-search-from">
-                  {"From date"}
-                </label>
-                <input className="govuk-input" id="date-search-from" type="date" />
+                <fieldset className="govuk-fieldset">
+                  <legend className="govuk-fieldset__legend--m">{"Dates"}</legend>
+                  <FormGroup>
+                    <label className="govuk-body" htmlFor="date-search-from">
+                      {"From date"}
+                    </label>
+                    <input className="govuk-input" id="date-search-from" type="date" />
+                  </FormGroup>
+                  <FormGroup>
+                    <label className="govuk-body" htmlFor="date-search-to">
+                      {"To date"}
+                    </label>
+                    <input className="govuk-input" id="date-search-to" type="date" />
+                  </FormGroup>
+                </fieldset>
               </FormGroup>
               <FormGroup>
-                <label className="govuk-body" htmlFor="date-search-to">
-                  {"To date"}
-                </label>
-                <input className="govuk-input" id="date-search-to" type="date" />
-              </FormGroup>
-            </fieldset>
-          </FormGroup>
-          <FormGroup>
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend--m">{"Include"}</legend>
-              <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
-                <div className="govuk-checkboxes__item">
-                  <input
-                    className="govuk-checkboxes__input"
-                    name="exceptions"
-                    id="date-search-exceptions"
-                    type="checkbox"
-                    value="exceptions"
-                  />
-                  <label className="govuk-label govuk-checkboxes__label" htmlFor="date-search-exceptions">
-                    {"Exceptions"}
-                  </label>
-                </div>
-                <div className="govuk-checkboxes__item">
-                  <input
-                    className="govuk-checkboxes__input"
-                    name="triggers"
-                    id="date-search-triggers"
-                    type="checkbox"
-                    value="triggers"
-                  />
-                  <label className="govuk-label govuk-checkboxes__label" htmlFor="date-search-triggers">
-                    {"Triggers"}
-                  </label>
-                </div>
-              </div>
-            </fieldset>
-          </FormGroup>
-          <FormGroup>
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend--m">{"Resolved by"}</legend>
-              <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
-                {resolvedBy.map((resolver, index) => {
-                  return (
-                    <div key={resolver.name} className="govuk-checkboxes__item">
+                <fieldset className="govuk-fieldset">
+                  <legend className="govuk-fieldset__legend--m">{"Include"}</legend>
+                  <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
+                    <div className="govuk-checkboxes__item">
                       <input
                         className="govuk-checkboxes__input"
                         name="exceptions"
-                        id={`audit-resolved-by-${index}`}
+                        id="date-search-exceptions"
                         type="checkbox"
                         value="exceptions"
                       />
-                      <label className="govuk-label govuk-checkboxes__label" htmlFor={`audit-resolved-by-${index}`}>
-                        {resolver.name}
+                      <label className="govuk-label govuk-checkboxes__label" htmlFor="date-search-exceptions">
+                        {"Exceptions"}
                       </label>
                     </div>
-                  )
-                })}
-              </div>
-            </fieldset>
-          </FormGroup>
-          <FormGroup>
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend--m">{"Trigger type"}</legend>
-              <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
-                {triggerTypes.map((triggerType, index) => {
-                  return (
-                    <div key={triggerType} className="govuk-checkboxes__item">
+                    <div className="govuk-checkboxes__item">
                       <input
                         className="govuk-checkboxes__input"
-                        name="exceptions"
-                        id={`audit-trigger-type-${index}`}
+                        name="triggers"
+                        id="date-search-triggers"
                         type="checkbox"
-                        value="exceptions"
+                        value="triggers"
                       />
-                      <label className="govuk-label govuk-checkboxes__label" htmlFor={`audit-trigger-type-${index}`}>
-                        {triggerType}
+                      <label className="govuk-label govuk-checkboxes__label" htmlFor="date-search-triggers">
+                        {"Triggers"}
                       </label>
                     </div>
-                  )
-                })}
-              </div>
-            </fieldset>
-          </FormGroup>
-          <FormGroup>
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend--m">{"Volume of cases"}</legend>
-              <div className="govuk-radios govuk-radios--small" data-module="govuk-radios">
-                {volumes.map((v) => {
-                  return (
-                    <div key={v} className="govuk-radios__item">
-                      <input
-                        className="govuk-radios__input"
-                        type="radio"
-                        name="volume"
-                        checked={v == volume}
-                        id={`audit-volume-${v}`}
-                        value={v}
-                        onChange={(e) => setVolume(e.target.value)}
-                      />
-                      <label className="govuk-label govuk-radios__label" htmlFor={`audit-volume-${v}`}>
-                        {v}
-                        {"% of cases"}
-                      </label>
-                    </div>
-                  )
-                })}
-              </div>
-            </fieldset>
-          </FormGroup>
+                  </div>
+                </fieldset>
+              </FormGroup>
+            </div>
+            <div className="govuk-grid-column-one-quarter">
+              <FormGroup>
+                <fieldset className="govuk-fieldset">
+                  <legend className="govuk-fieldset__legend--m">{"Resolved by"}</legend>
+                  <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
+                    {resolvedBy.map((resolver, index) => {
+                      return (
+                        <div key={resolver.name} className="govuk-checkboxes__item">
+                          <input
+                            className="govuk-checkboxes__input"
+                            name="exceptions"
+                            id={`audit-resolved-by-${index}`}
+                            type="checkbox"
+                            value="exceptions"
+                          />
+                          <label className="govuk-label govuk-checkboxes__label" htmlFor={`audit-resolved-by-${index}`}>
+                            {resolver.name}
+                          </label>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </fieldset>
+              </FormGroup>
+            </div>
+            <div className="govuk-grid-column-one-quarter">
+              <FormGroup>
+                <fieldset className="govuk-fieldset">
+                  <legend className="govuk-fieldset__legend--m">{"Trigger type"}</legend>
+                  <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
+                    {triggerTypes.map((triggerType, index) => {
+                      return (
+                        <div key={triggerType} className="govuk-checkboxes__item">
+                          <input
+                            className="govuk-checkboxes__input"
+                            name="exceptions"
+                            id={`audit-trigger-type-${index}`}
+                            type="checkbox"
+                            value="exceptions"
+                          />
+                          <label
+                            className="govuk-label govuk-checkboxes__label"
+                            htmlFor={`audit-trigger-type-${index}`}
+                          >
+                            {triggerType}
+                          </label>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </fieldset>
+              </FormGroup>
+            </div>
+            <div className="govuk-grid-column-one-quarter">
+              <FormGroup>
+                <fieldset className="govuk-fieldset">
+                  <legend className="govuk-fieldset__legend--m">{"Volume of cases"}</legend>
+                  <div className="govuk-radios govuk-radios--small" data-module="govuk-radios">
+                    {volumes.map((v) => {
+                      return (
+                        <div key={v} className="govuk-radios__item">
+                          <input
+                            className="govuk-radios__input"
+                            type="radio"
+                            name="volume"
+                            checked={v == volume}
+                            id={`audit-volume-${v}`}
+                            value={v}
+                            onChange={(e) => setVolume(e.target.value)}
+                          />
+                          <label className="govuk-label govuk-radios__label" htmlFor={`audit-volume-${v}`}>
+                            {v}
+                            {"% of cases"}
+                          </label>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </fieldset>
+              </FormGroup>
+            </div>
+          </div>
+
           <div>
             <button className="govuk-button">{"Search cases"}</button>
             <p className="govuk-body">
