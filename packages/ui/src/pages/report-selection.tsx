2260a5537e8f8012d927cfa9d2e24fbab3edf3df
irenegrandeh <irenegrandeh@gmail.com>
2026-02-06 12:24:13 +0000

    Create empty page for the Report Search

---
+++ packages/ui/src/pages/report-selection.tsx
@@ -0,0 +1,66 @@
+import Head from "next/head"
+import Layout from "../components/Layout"
+import { CurrentUserContext, CurrentUserContextType } from "context/CurrentUserContext"
+import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from "next"
+import { withAuthentication, withMultipleServerSideProps } from "../middleware"
+import withCsrf from "../middleware/withCsrf/withCsrf"
+import CsrfServerSidePropsContext from "../types/CsrfServerSidePropsContext"
+import AuthenticationServerSidePropsContext from "../types/AuthenticationServerSidePropsContext"
+import { userToDisplayFullUserDto } from "../services/dto/userDto"
+import { DisplayFullUser } from "types/display/Users"
+import { ParsedUrlQuery } from "querystring"
+import { useRouter } from "next/router"
+import { useState } from "react"
+
+export const getServerSideProps = withMultipleServerSideProps(
+  withAuthentication,
+  withCsrf,
+  async (context: GetServerSidePropsContext<ParsedUrlQuery>): Promise<GetServerSidePropsResult<Props>> => {
+    const { currentUser, query /*csrfToken*/ } = context as AuthenticationServerSidePropsContext &
+      CsrfServerSidePropsContext
+    const { previousPath } = query as { previousPath: string }
+
+    const props = {
+      //csrfToken,
+      user: userToDisplayFullUserDto(currentUser),
+      previousPath: previousPath ?? null
+    }
+
+    return { props }
+  }
+)
+
+interface Props {
+  //csrfToken: string
+  user: DisplayFullUser
+  previousPath: string
+}
+
+const ReportSelectionPage: NextPage<Props> = ({ user, previousPath /*csrfToken*/ }: Props) => {
+  const [currentUserContext] = useState<CurrentUserContextType>({ currentUser: user })
+  //const [remainingFeedbackLength, setRemainingFeedbackLength] = useState(100)
+  const router = useRouter()
+
+  return (
+    <CurrentUserContext.Provider value={currentUserContext}>
+      <Head>
+        <title>{"Bichard7 | Select a report"}</title>
+        <meta name="description" content="Bichard7 | Report selection" />
+      </Head>
+      <a className="govuk-back-link" href={`${router.basePath}` + previousPath} onClick={function noRefCheck() {}}>
+        {"Back"}
+      </a>
+      <Layout
+        bichardSwitch={{
+          display: true,
+          href: `/bichard-ui/ReturnToReportIndex`,
+          displaySwitchingSurveyFeedback: true
+        }}
+      >
+        <h1>{"Search reports"}</h1>
+      </Layout>
+    </CurrentUserContext.Provider>
+  )
+}
+
+export default ReportSelectionPage
--- packages/ui/src/types/NavLinks.ts
+++ packages/ui/src/types/NavLinks.ts
@@ -1,7 +1,7 @@
 export enum NavLink {
   CaseList = "/bichard/",
   //Reports = "/bichard-ui/ReturnToReportIndex",
-  Reports = "/bichard-ui/report-selection",
+  Reports = "/bichard/report-selection",
   Audit = "/bichard/audit/search",
   UserManagement = "/users/users/",
   Help = "/help/",
