d9df4e39fd372d1393d3dcc511062a6963c45ae8
irenegrandeh <irenegrandeh@gmail.com>
2026-02-06 12:12:15 +0000

    Test push. Change link to Reports.

--- packages/ui/src/types/NavLinks.ts
+++ packages/ui/src/types/NavLinks.ts
@@ -1,6 +1,7 @@
 export enum NavLink {
   CaseList = "/bichard/",
-  Reports = "/bichard-ui/ReturnToReportIndex",
+  //Reports = "/bichard-ui/ReturnToReportIndex",
+  Reports = "/bichard-ui/report-selection",
   Audit = "/bichard/audit/search",
   UserManagement = "/users/users/",
   Help = "/help/",
