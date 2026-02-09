716eee766936f88d88603dcbb6a82f9c03275ec2
Emad Karamad <emad.karamad@madetech.com>
2026-02-09 15:11:50 +0000

    Fix zap in CI

--- packages/ui/scripts/zap/zap-scan.sh
+++ packages/ui/scripts/zap/zap-scan.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+echo "IS CI? ${IS_CI}"
+
+if [[ "${IS_CI}" == "true" ]]; then
+  echo "Preparing ZAP workspace"
+  chmod -R 777 $(pwd)/scripts/zap
+fi
+
+echo "Running ZAP"
+
 docker run \
   --rm \
   --network="bichard_default" \
