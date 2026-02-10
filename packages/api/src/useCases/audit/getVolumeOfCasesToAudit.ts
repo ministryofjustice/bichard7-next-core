00ce1f4594c8f33c5ec0547e69dfdb2005131e5a
Tom Watkins <tom.watkins@justice.gov.uk>
2026-02-10 19:21:51 +0000

    Sort the cases by audited first

--- packages/api/src/useCases/audit/getVolumeOfCasesToAudit.ts
+++ packages/api/src/useCases/audit/getVolumeOfCasesToAudit.ts
@@ -3,7 +3,16 @@ import type { CasesToAuditByUser } from "../../types/CasesToAuditByUser"
 export function getVolumeOfCasesToAudit(casesToAuditByUser: CasesToAuditByUser[], volume: number): number[] {
   const caseIds = casesToAuditByUser.flatMap(({ cases }) => {
     const numberOfCases = Math.ceil(cases.length * (volume / 100))
-    return cases.slice(0, numberOfCases).map((row) => row.id)
+    return cases
+      .sort((a, b) => {
+        if (a.audited === b.audited) {
+          return a.id - b.id
+        }
+
+        return a.audited ? -1 : 1
+      })
+      .slice(0, numberOfCases)
+      .map((row) => row.id)
   })
 
   return [...new Set(caseIds)]
--- packages/api/src/useCases/audit/getVolumeOfCasesToAudit.unit.test.ts
+++ packages/api/src/useCases/audit/getVolumeOfCasesToAudit.unit.test.ts
@@ -61,4 +61,31 @@ describe("getVolumeOfCasesToAudit", () => {
 
     expect(caseIds).toStrictEqual([1, 2, 3, 4])
   })
+
+  it("should use cases already audited first", () => {
+    const casesToAuditByUser: CasesToAuditByUser[] = [
+      {
+        cases: [
+          { audited: false, id: 1 },
+          { audited: false, id: 2 },
+          { audited: true, id: 3 },
+          { audited: false, id: 4 }
+        ],
+        username: "user@example.com"
+      },
+      {
+        cases: [
+          { audited: false, id: 5 },
+          { audited: false, id: 6 },
+          { audited: true, id: 7 },
+          { audited: false, id: 8 }
+        ],
+        username: "user2@example.com"
+      }
+    ]
+
+    const caseIds = getVolumeOfCasesToAudit(casesToAuditByUser, 50)
+
+    expect(caseIds).toStrictEqual([3, 1, 7, 5])
+  })
 })
