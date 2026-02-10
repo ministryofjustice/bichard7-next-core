9c37be31b8271956951f72386497588788d2fc74
Tausif Patel <tausifpatel.dev@gmail.com>
2026-02-10 14:13:00 +0000

    Convert pnc remand-request json to leds json (#1790)
    * Add convertPncJsonToLedsRemandRequest function
    
    * Refactor
    
    * Utilise mapToRemandRequest function instead of rewriting identical function

---
+++ packages/e2e-test/utils/convertPncJsonToLeds/convertPncJsonToLedsRemandRequest.ts
@@ -0,0 +1,13 @@
+import mapToRemandRequest from "@moj-bichard7/core/lib/policeGateway/leds/mapToRemandRequest"
+import type { RemandRequest } from "@moj-bichard7/core/types/leds/RemandRequest"
+import type { PncRemandJson } from "../convertPncXmlToJson/convertPncXmlToJson"
+
+export const convertPncJsonToLedsRemandRequest = (pncJson: PncRemandJson): RemandRequest =>
+  mapToRemandRequest({
+    ...pncJson,
+    hearingDate: pncJson.remandDate,
+    pncRemandStatus: pncJson.remandResult,
+    bailConditions: pncJson.bailConditions.split(" ").filter(Boolean) ?? [],
+    nextHearingDate: pncJson.nextAppearanceDate,
+    psaCourtCode: pncJson.institutionCode
+  })
