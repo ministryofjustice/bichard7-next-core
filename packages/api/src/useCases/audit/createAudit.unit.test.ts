8001b7d34d32d2e44c1fd368cc5b21918f947773
Tom Watkins <tom.watkins@justice.gov.uk>
2026-02-13 15:47:25 +0000

    Fix unit tests

--- packages/api/src/useCases/audit/createAudit.unit.test.ts
+++ packages/api/src/useCases/audit/createAudit.unit.test.ts
@@ -1,6 +1,8 @@
 import type { CreateAudit } from "@moj-bichard7/common/contracts/CreateAudit"
+import type { AuditDto } from "@moj-bichard7/common/types/Audit"
 import type { User } from "@moj-bichard7/common/types/User"
 
+import { isError } from "@moj-bichard7/common/types/Result"
 import { UserGroup } from "@moj-bichard7/common/types/UserGroup"
 import { format, subWeeks } from "date-fns"
 
@@ -10,7 +12,9 @@ import { NotAllowedError } from "../../types/errors/NotAllowedError"
 import { createAudit } from "./createAudit"
 
 describe("createAudit", () => {
-  let mockDatabase: jest.Mocked<Partial<WritableDatabaseConnection>>
+  const mockDatabase = {
+    transaction: jest.fn()
+  } as unknown as jest.Mocked<WritableDatabaseConnection>
 
   const createAuditBody: CreateAudit = {
     fromDate: format(subWeeks(new Date(), 1), "yyyy-MM-dd"),
@@ -22,25 +26,36 @@ describe("createAudit", () => {
 
   beforeEach(() => {
     jest.clearAllMocks()
-
-    mockDatabase = {
-      transaction: jest.fn()
-    }
   })
 
   it("runs the transaction if the user is a supervisor", async () => {
-    await createAudit(mockDatabase as WritableDatabaseConnection, createAuditBody, {
+    mockDatabase.transaction.mockResolvedValue({ auditId: 1 })
+
+    const result = await createAudit(mockDatabase, createAuditBody, {
       groups: [UserGroup.Supervisor]
     } as User)
 
-    expect(mockDatabase?.transaction).toHaveBeenCalled()
+    expect(mockDatabase.transaction).toHaveBeenCalled()
+    expect(isError(result)).toBe(false)
+    expect((result as AuditDto).auditId).toBe(1)
   })
 
   it("returns an error due to lack of user permissions", async () => {
-    const audit = await createAudit(mockDatabase as WritableDatabaseConnection, createAuditBody, {
+    const audit = await createAudit(mockDatabase, createAuditBody, {
       groups: [UserGroup.GeneralHandler]
     } as User)
 
     expect(audit).toBeInstanceOf(NotAllowedError)
+  })
+
+  it("returns an error if the transaction returns an error", async () => {
+    mockDatabase.transaction.mockRejectedValue(new Error("Test error message"))
+
+    const result = await createAudit(mockDatabase, createAuditBody, {
+      groups: [UserGroup.Supervisor]
+    } as User)
+
+    expect(result).toBeInstanceOf(Error)
+    expect((result as Error).message).toBe("Test error message")
   })
 })
