94baac8c13edbe07c324415feb9fe5b5c0f05c1e
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-02-16 12:08:57 +0000

    add typeorm query for fetching users by visible force

--- packages/ui/src/services/getSupervisedUsers.ts
+++ packages/ui/src/services/getSupervisedUsers.ts
@@ -1,13 +1,24 @@
 import type { DataSource, EntityManager } from "typeorm"
+import { Raw, Brackets } from "typeorm"
 import type PromiseResult from "../types/PromiseResult"
 import User from "./entities/User"
 
-export default async (dataSource: DataSource | EntityManager, currentUser: User): PromiseResult<User[]> => {
-  const user = await dataSource
-    .getRepository(User)
-    .createQueryBuilder("user")
-    .where("user.visible_forces = :visibleForces", { visibleForces: currentUser.visibleForces[0] })
-    .getMany()
-
-  return user
+function getVisibleForce(index: number, force: string) {
+  return Raw((alias) => `${alias} ~ :force${index}`, { [`force${index}`]: String.raw`'\y${force}\y'` })
 }
+
+const getSupervisedUsers = async (dataSource: DataSource | EntityManager, currentUser: User): PromiseResult<User[]> => {
+  const queryBuilder = dataSource.getRepository(User).createQueryBuilder("user")
+
+  const userForceQuery = queryBuilder.where("user.deleted_at IS NULL").andWhere(
+    new Brackets((qb) => {
+      currentUser.visibleForces.forEach((force, index) => {
+        qb.orWhere({ visibleForces: getVisibleForce(index, force) })
+      })
+    })
+  )
+
+  return userForceQuery.getMany()
+}
+
+export default getSupervisedUsers
