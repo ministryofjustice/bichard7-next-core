66f1057b92af747a83c869da20a10d524353f528
Tom Watkins <tom.watkins@justice.gov.uk>
2026-02-16 14:11:00 +0000

    Check cases in integration

--- packages/api/src/routes/v1/audit/index.integration.test.ts
+++ packages/api/src/routes/v1/audit/index.integration.test.ts
@@ -3,12 +3,13 @@ import type { AuditDto } from "@moj-bichard7/common/types/Audit"
 
 import { V1 } from "@moj-bichard7/common/apiEndpoints/versionedEndpoints"
 import { UserGroup } from "@moj-bichard7/common/types/UserGroup"
-import { addDays, format, subWeeks } from "date-fns"
+import { addDays, format, subDays, subWeeks } from "date-fns"
 import { type FastifyInstance } from "fastify"
 import { BAD_REQUEST, CREATED, FORBIDDEN } from "http-status"
 
 import build from "../../../app"
 import AuditLogDynamoGateway from "../../../services/gateways/dynamo/AuditLogDynamoGateway/AuditLogDynamoGateway"
+import { createCase } from "../../../tests/helpers/caseHelper"
 import auditLogDynamoConfig from "../../../tests/helpers/dynamoDbConfig"
 import { createUserAndJwtToken } from "../../../tests/helpers/userHelper"
 import End2EndPostgres from "../../../tests/testGateways/e2ePostgres"
@@ -35,6 +36,22 @@ describe("POST /v1/audit", () => {
   it("returns 201 CREATED when audit created successfully", async () => {
     const testUsername = "user1"
     const [encodedJwt, user] = await createUserAndJwtToken(testDatabaseGateway, [UserGroup.Supervisor])
+    const cases = await Promise.all([
+      createCase(testDatabaseGateway, {
+        courtCode: user.visibleCourts[0],
+        errorId: 1,
+        orgForPoliceFilter: user.visibleForces[0],
+        triggerResolvedAt: subDays(new Date(), 1),
+        triggerResolvedBy: testUsername
+      }),
+      createCase(testDatabaseGateway, {
+        courtCode: user.visibleCourts[0],
+        errorId: 2,
+        errorResolvedAt: subDays(new Date(), 1),
+        errorResolvedBy: testUsername,
+        orgForPoliceFilter: user.visibleForces[0]
+      })
+    ])
 
     const payload = {
       fromDate: format(subWeeks(new Date(), 1), "yyyy-MM-dd"),
@@ -57,6 +74,12 @@ describe("POST /v1/audit", () => {
     expect(body.fromDate).toBe(payload.fromDate)
     expect(body.toDate).toBe(payload.toDate)
     expect(body.createdBy).toBe(user.username)
+
+    const auditCases = await testDatabaseGateway.getAuditCases(body.auditId)
+    expect(auditCases).toHaveLength(1) // 50% of 2 courses
+    expect(auditCases).toEqual([
+      { audit_case_id: expect.any(Number), audit_id: body.auditId, error_id: cases[0].errorId }
+    ])
   })
 
   it("returns 400 Bad Request when date range is in the past", async () => {
