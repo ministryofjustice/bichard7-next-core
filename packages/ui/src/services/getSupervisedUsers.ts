ee60ea8a7b8572625db4c670b8d0e6ee70bfdcde
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-02-16 15:04:49 +0000

    refactor supervised user query

--- packages/ui/src/services/getSupervisedUsers.ts
+++ packages/ui/src/services/getSupervisedUsers.ts
@@ -1,19 +1,15 @@
 import type { DataSource, EntityManager } from "typeorm"
-import { Raw, Brackets } from "typeorm"
+import { Brackets } from "typeorm"
 import type PromiseResult from "../types/PromiseResult"
 import User from "./entities/User"
-
-function getVisibleForce(index: number, force: string) {
-  return Raw((alias) => `${alias} ~ :force${index}`, { [`force${index}`]: String.raw`'\y${force}\y'` })
-}
 
 const getSupervisedUsers = async (dataSource: DataSource | EntityManager, currentUser: User): PromiseResult<User[]> => {
   const queryBuilder = dataSource.getRepository(User).createQueryBuilder("user")
 
-  const userForceQuery = queryBuilder.where("user.deleted_at IS NULL").andWhere(
+  const userForceQuery = queryBuilder.where('"user"."deleted_at" IS NULL').andWhere(
     new Brackets((qb) => {
       currentUser.visibleForces.forEach((force, index) => {
-        qb.orWhere({ visibleForces: getVisibleForce(index, force) })
+        qb.orWhere(`"user"."visible_forces" ~ :force${index}`, { [`force${index}`]: String.raw`\y${force}\y` })
       })
     })
   )
