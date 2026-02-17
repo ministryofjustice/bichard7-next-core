eaa7474247778be73fe0cf6a786b7f42fc2928fa
Richard Race <1273965+richrace@users.noreply.github.com>
2026-02-17 18:03:42 +0000

    Use GOVUK pagination

--- packages/ui/src/components/Pagination/PaginationNavigation.styles.tsx
+++ packages/ui/src/components/Pagination/PaginationNavigation.styles.tsx
@@ -1,11 +1,38 @@
 import styled from "styled-components"
+import { lightGrey, blue, white } from "utils/colours"
 
 export const PaginationNav = styled.nav`
   justify-self: flex-end;
   align-self: flex-end;
   min-height: 50px;
 
-  li.moj-pagination__item {
-    margin-top: 8px;
+  li {
+    &.govuk-pagination {
+      &__item {
+        margin-top: 8px;
+
+        &:hover {
+          background-color: ${lightGrey};
+        }
+
+        &--current {
+          color: ${white};
+
+          &,
+          &:hover {
+            background-color: ${blue};
+          }
+        }
+
+        &--ellipsis:hover {
+          background-color: transparent;
+        }
+      }
+
+      &__next:hover,
+      &__prev:hover {
+        background-color: ${lightGrey};
+      }
+    }
   }
 `
