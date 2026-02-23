65c57cc96e30e28c57251cda67addd7ada280b40
Tom Watkins <tom.watkins@justice.gov.uk>
2026-02-23 23:57:59 +0000

    Tidy up tests

--- packages/api/src/services/db/audit/fetchAuditCases.integration.test.ts
+++ packages/api/src/services/db/audit/fetchAuditCases.integration.test.ts
@@ -148,112 +148,21 @@ describe("fetchAuditCases", () => {
     )
   })
 
-  it("Handles pagination", async () => {
-    const user = await createUser(testDatabaseGateway)
-    const cases = await Promise.all([
-      createCase(testDatabaseGateway, {
-        courtCode: user.visibleCourts[0],
-        errorId: 1,
-        orgForPoliceFilter: user.visibleForces[0]
-      }),
-      createCase(testDatabaseGateway, {
-        courtCode: user.visibleCourts[0],
-        errorId: 2,
-        orgForPoliceFilter: user.visibleForces[0]
-      })
-    ])
-    const audit = await insertAudit(
-      testDatabaseGateway.writable,
-      {
-        fromDate: format(subWeeks(new Date(), 1), "yyyy-MM-dd"),
-        includedTypes: ["Triggers", "Exceptions"],
-        resolvedByUsers: ["user1"],
-        toDate: format(new Date(), "yyyy-MM-dd"),
-        volumeOfCases: 20
-      },
-      user
-    )
-    expect(isError(audit)).toBe(false)
-
-    const auditCases = await insertAuditCases(
-      testDatabaseGateway.writable,
-      (audit as AuditDto).auditId,
-      cases.map((c) => c.errorId)
-    )
-    expect(isError(auditCases)).toBe(false)
-
-    const retrievedAuditCasesP1 = await fetchAuditCases(
-      testDatabaseGateway.readonly,
-      (audit as AuditDto).auditId,
-      { maxPerPage: 1, order: "asc", pageNum: 1 },
-      user
-    )
-
-    expect(isError(retrievedAuditCasesP1)).toBe(false)
-    expect(retrievedAuditCasesP1 as AuditCasesMetadata).toEqual(
-      expect.objectContaining({
-        cases: expect.arrayContaining([
-          expect.objectContaining({
-            errorId: 1
-          })
-        ]),
-        maxPerPage: 1,
-        pageNum: 1,
-        returnCases: 1,
-        totalCases: 2
-      })
-    )
-
-    const retrievedAuditCasesP2 = await fetchAuditCases(
-      testDatabaseGateway.readonly,
-      (audit as AuditDto).auditId,
-      { maxPerPage: 1, order: "asc", pageNum: 2 },
-      user
-    )
-
-    expect(isError(retrievedAuditCasesP2)).toBe(false)
-    expect(retrievedAuditCasesP2 as AuditCasesMetadata).toEqual(
-      expect.objectContaining({
-        cases: expect.arrayContaining([
-          expect.objectContaining({
-            errorId: 2
-          })
-        ]),
-        maxPerPage: 1,
-        pageNum: 2,
-        returnCases: 1,
-        totalCases: 2
-      })
-    )
-  })
-
-  it.each<CaseOrderBy>(["courtDate", "courtName", "ptiurn", "defendantName", "messageReceivedTimestamp"])(
-    "Get audit cases in %s order",
-    async (orderBy) => {
+  describe("Pagination", () => {
+    it.each([1, 2])("Page %s", async (pageNum) => {
       const user = await createUser(testDatabaseGateway)
       const cases = await Promise.all([
         createCase(testDatabaseGateway, {
           courtCode: user.visibleCourts[0],
-          courtDate: new Date(),
-          courtName: "XYZ",
-          defendantName: "XYZ",
           errorId: 1,
-          messageReceivedAt: new Date(),
-          orgForPoliceFilter: user.visibleForces[0],
-          ptiurn: "XYZ"
+          orgForPoliceFilter: user.visibleForces[0]
         }),
         createCase(testDatabaseGateway, {
           courtCode: user.visibleCourts[0],
-          courtDate: subDays(new Date(), 1),
-          courtName: "ABC",
-          defendantName: "ABC",
           errorId: 2,
-          messageReceivedAt: subDays(new Date(), 1),
-          orgForPoliceFilter: user.visibleForces[0],
-          ptiurn: "ABC"
+          orgForPoliceFilter: user.visibleForces[0]
         })
       ])
-
       const audit = await insertAudit(
         testDatabaseGateway.writable,
         {
@@ -274,31 +183,104 @@ describe("fetchAuditCases", () => {
       )
       expect(isError(auditCases)).toBe(false)
 
-      const retrievedAuditCasesAsc = await fetchAuditCases(
+      const retrievedAuditCases = await fetchAuditCases(
         testDatabaseGateway.readonly,
         (audit as AuditDto).auditId,
-        { maxPerPage: 50, order: "asc", orderBy, pageNum: 1 },
+        { maxPerPage: 1, order: "asc", pageNum },
         user
       )
 
-      expect(isError(retrievedAuditCasesAsc)).toBe(false)
-      expect((retrievedAuditCasesAsc as AuditCasesMetadata).cases).toMatchObject([
-        expect.objectContaining({ errorId: 2 }),
-        expect.objectContaining({ errorId: 1 })
-      ])
-
-      const retrievedAuditCasesDesc = await fetchAuditCases(
-        testDatabaseGateway.readonly,
-        (audit as AuditDto).auditId,
-        { maxPerPage: 50, order: "desc", orderBy, pageNum: 1 },
-        user
+      expect(isError(retrievedAuditCases)).toBe(false)
+      expect(retrievedAuditCases as AuditCasesMetadata).toEqual(
+        expect.objectContaining({
+          cases: expect.arrayContaining([
+            expect.objectContaining({
+              errorId: pageNum
+            })
+          ]),
+          maxPerPage: 1,
+          pageNum,
+          returnCases: 1,
+          totalCases: 2
+        })
       )
+    })
+  })
 
-      expect(isError(retrievedAuditCasesDesc)).toBe(false)
-      expect((retrievedAuditCasesDesc as AuditCasesMetadata).cases).toMatchObject([
-        expect.objectContaining({ errorId: 1 }),
-        expect.objectContaining({ errorId: 2 })
-      ])
-    }
-  )
+  describe("Ordering", () => {
+    it.each<CaseOrderBy>(["courtDate", "courtName", "ptiurn", "defendantName", "messageReceivedTimestamp"])(
+      "Get audit cases in %s order",
+      async (orderBy) => {
+        const user = await createUser(testDatabaseGateway)
+        const cases = await Promise.all([
+          createCase(testDatabaseGateway, {
+            courtCode: user.visibleCourts[0],
+            courtDate: new Date(),
+            courtName: "XYZ",
+            defendantName: "XYZ",
+            errorId: 1,
+            messageReceivedAt: new Date(),
+            orgForPoliceFilter: user.visibleForces[0],
+            ptiurn: "XYZ"
+          }),
+          createCase(testDatabaseGateway, {
+            courtCode: user.visibleCourts[0],
+            courtDate: subDays(new Date(), 1),
+            courtName: "ABC",
+            defendantName: "ABC",
+            errorId: 2,
+            messageReceivedAt: subDays(new Date(), 1),
+            orgForPoliceFilter: user.visibleForces[0],
+            ptiurn: "ABC"
+          })
+        ])
+
+        const audit = await insertAudit(
+          testDatabaseGateway.writable,
+          {
+            fromDate: format(subWeeks(new Date(), 1), "yyyy-MM-dd"),
+            includedTypes: ["Triggers", "Exceptions"],
+            resolvedByUsers: ["user1"],
+            toDate: format(new Date(), "yyyy-MM-dd"),
+            volumeOfCases: 20
+          },
+          user
+        )
+        expect(isError(audit)).toBe(false)
+
+        const auditCases = await insertAuditCases(
+          testDatabaseGateway.writable,
+          (audit as AuditDto).auditId,
+          cases.map((c) => c.errorId)
+        )
+        expect(isError(auditCases)).toBe(false)
+
+        const retrievedAuditCasesAsc = await fetchAuditCases(
+          testDatabaseGateway.readonly,
+          (audit as AuditDto).auditId,
+          { maxPerPage: 50, order: "asc", orderBy, pageNum: 1 },
+          user
+        )
+
+        expect(isError(retrievedAuditCasesAsc)).toBe(false)
+        expect((retrievedAuditCasesAsc as AuditCasesMetadata).cases).toMatchObject([
+          expect.objectContaining({ errorId: 2 }),
+          expect.objectContaining({ errorId: 1 })
+        ])
+
+        const retrievedAuditCasesDesc = await fetchAuditCases(
+          testDatabaseGateway.readonly,
+          (audit as AuditDto).auditId,
+          { maxPerPage: 50, order: "desc", orderBy, pageNum: 1 },
+          user
+        )
+
+        expect(isError(retrievedAuditCasesDesc)).toBe(false)
+        expect((retrievedAuditCasesDesc as AuditCasesMetadata).cases).toMatchObject([
+          expect.objectContaining({ errorId: 1 }),
+          expect.objectContaining({ errorId: 2 })
+        ])
+      }
+    )
+  })
 })
