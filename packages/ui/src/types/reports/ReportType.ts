0177e42564a8a25116ae3d5234406b19c52828ac
Richard Race <1273965+richrace@users.noreply.github.com>
2026-02-25 13:49:24 +0000

    Add types to help dynamically create tables/CSVs

--- packages/ui/src/types/reports/ReportType.ts
+++ packages/ui/src/types/reports/ReportType.ts
@@ -1,1 +1,14 @@
 export type ReportType = "exceptions" | "bails" | "warrants" | "domestic violence"
+
+export type PrettyReportType =
+  | "Bail Conditions"
+  | "Domestic Violence & Vulnerable Victims"
+  | "Resolved Exceptions/Triggers"
+  | "Warrants"
+
+export const REPORT_TYPE_MAP: Record<ReportType, PrettyReportType> = {
+  bails: "Bail Conditions",
+  "domestic violence": "Domestic Violence & Vulnerable Victims",
+  exceptions: "Resolved Exceptions/Triggers",
+  warrants: "Warrants"
+} as const
