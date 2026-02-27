152bd61d59321a32eb2f73b8a3c4ed7fd99fcd5d
Emad Karamad <emad.karamad@madetech.com>
2026-02-27 12:17:55 +0000

    Update types

--- packages/e2e-test/utils/LedsApi/helpers/LedsTestApiHelper/fetchArrestSummaries.ts
+++ packages/e2e-test/utils/LedsApi/helpers/LedsTestApiHelper/fetchArrestSummaries.ts
@@ -1,47 +1,13 @@
-import type { CurrentAppearance, NextAppearance } from "@moj-bichard7/core/types/leds/RemandRequest"
 import type { AxiosError } from "axios"
 import axios from "axios"
 import https from "https"
+import type ArrestSummariesResponse from "../../../../types/LedsTestApiHelper/ArrestSummariesResponse"
+import type { ArrestSummaries } from "../../../../types/LedsTestApiHelper/ArrestSummariesResponse"
 import type PersonDetails from "../../../../types/LedsTestApiHelper/PersonDetails"
 import type RequestOptions from "../../../../types/LedsTestApiHelper/RequestOptions"
 import { isError } from "../../../isError"
 import ApiError from "./ApiError"
 import generateHeaders, { ENDPOINT_HEADERS } from "./generateHeaders"
-
-export type RemandHeadline = {
-  remandId: string
-  appearanceResult: string
-  remandDate: string
-  currentAppearance: CurrentAppearance
-  nextAppearance: Omit<NextAppearance, "forceStationCode">
-}
-
-type OffenceHeadline = {
-  offenceId: string
-  additionalOffenceMarker: boolean
-  npccOffenceCode: string
-  cjsOffenceCode: string
-  qualifiedCjsCode: string
-  startDate: string
-  roleQualifier: string[]
-  legislationQualifier: string[]
-}
-
-export type ArrestSummaries = {
-  arrestReportHeadlines: {
-    reportId: string
-    asn: string
-    processStage: string
-    processStageDate: string
-  }
-  fingerprintStatusCode1: string
-  offencesHeadlines: OffenceHeadline[]
-  remandHeadlines: RemandHeadline[]
-}
-
-type ArrestSummariesResult = {
-  arrestSummaries: ArrestSummaries[]
-}
 
 const fetchArrestSummaries = async (
   requestOptions: RequestOptions,
@@ -53,7 +19,7 @@ const fetchArrestSummaries = async (
 
   const headers = generateHeaders(ENDPOINT_HEADERS.arrestSummaries, requestOptions.authToken)
   const response = await axios
-    .get<ArrestSummariesResult>(
+    .get<ArrestSummariesResponse>(
       `${requestOptions.baseUrl}/person-services/v1/people/${person.personId}/arrest-reports?offset=0&limit=999`,
       {
         headers,
