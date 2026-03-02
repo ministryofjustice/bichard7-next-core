6fb997b4d68b34771197b98c18d71726b5145a3a
Tom Watkins <tom.watkins@justice.gov.uk>
2026-03-02 17:45:54 +0000

    e2e tests

---
+++ packages/ui/cypress/e2e/audit/auditId.api.cy.ts
@@ -0,0 +1,58 @@
+import { loginAndVisit } from "../../support/helpers"
+import AsnExceptionHO100206 from "../../../test/test-data/HO100206.json"
+
+describe("/audit/:auditId", () => {
+  beforeEach(() => {
+    cy.task("clearAudits")
+    cy.task("clearCourtCases")
+
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: AsnExceptionHO100206.hearingOutcomeXml,
+        updatedHearingOutcome: AsnExceptionHO100206.hearingOutcomeXml,
+        errorCount: 1,
+        errorStatus: "Resolved",
+        triggerCount: 1
+      }
+    ])
+    cy.task("insertAudit", {
+      overrides: {},
+      caseIds: [0],
+      username: "Supervisor"
+    })
+  })
+
+  it("Should open the audit page", () => {
+    loginAndVisit("Supervisor", `/bichard/audit/1`)
+
+    cy.location("pathname").should("eq", "/bichard/audit/1")
+  })
+
+  it("Should be able to navigate to the case details page", () => {
+    loginAndVisit("Supervisor", `/bichard/audit/1`)
+
+    cy.location("pathname").should("eq", "/bichard/audit/1")
+
+    cy.get("a#defendant-name-link").click()
+    cy.location("pathname").should("eq", "/bichard/court-cases/0")
+    cy.location("search").should("eq", "?prev=/audit/1")
+  })
+
+  it("Should redirect to the case list if user is not a supervisor", () => {
+    loginAndVisit("GeneralHandler", `/bichard/audit/1`)
+
+    cy.location("pathname").should("eq", "/bichard")
+  })
+
+  it("Should go to a not found page", () => {
+    cy.loginAs("Supervisor")
+
+    cy.request({
+      failOnStatusCode: false,
+      url: "/bichard/audit/0"
+    }).then((response) => {
+      expect(response.status).to.eq(404)
+    })
+  })
+})
