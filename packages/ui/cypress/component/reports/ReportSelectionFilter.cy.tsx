773d6e947b92bb3207f9f2b619a5b198d3cb2f37
timhowes-lab <tim.howes@madetech.com>
2026-03-03 16:36:13 +0000

    Fixed component test, added checkboxes hidden test

--- packages/ui/cypress/component/reports/ReportSelectionFilter.cy.tsx
+++ packages/ui/cypress/component/reports/ReportSelectionFilter.cy.tsx
@@ -6,7 +6,7 @@ describe("ReportSelectionFilter", () => {
 
     cy.get("div#report-section").should("exist")
     cy.get("div#report-section").find("h2").should("have.text", "Reports")
-    cy.get("div#report-section").find("label").should("have.text", "Sort by")
+    cy.get("div#report-section").find("label").should("have.text", "Select Report")
     cy.get('select[name="select-case-type"]').should("exist")
   })
 
@@ -19,8 +19,10 @@ describe("ReportSelectionFilter", () => {
     cy.get("div#date-range-section").find("div#report-selection-date-to").should("exist")
   })
 
-  it("renders the correct fields for include/checkboxes section", () => {
+  it("renders the correct fields for include/checkboxes section when triggers/exceptions is selected", () => {
     cy.mount(<ReportSelectionFilter />)
+
+    cy.get('select[name="select-case-type"]').select("Resolved Exceptions/Triggers")
 
     cy.get("div#include-section").should("exist")
     cy.get("div#include-section").find("h2").should("have.text", "Include")
@@ -39,6 +41,26 @@ describe("ReportSelectionFilter", () => {
 
     cy.get("div#include-section").find("label").eq(1).should("have.attr", "for", "triggers")
     cy.get("div#include-section").find("label").eq(2).should("have.attr", "for", "exceptions")
+  })
+
+  it("hides the checkboxes when something other than trigger/exceptions is selected in the reports dropdown", () => {
+    const checkboxesShouldNotExist = () => {
+      cy.get("div#include-section").should("exist")
+      cy.get("div#include-section").find("h2").should("not.exist")
+      cy.get("div#include-section").find("label").should("not.exist")
+    }
+
+    cy.mount(<ReportSelectionFilter />)
+    checkboxesShouldNotExist()
+
+    cy.get('select[name="select-case-type"]').select("Bail Conditions")
+    checkboxesShouldNotExist()
+
+    cy.get('select[name="select-case-type"]').select("Domestic Violence & Vulnerable Victims")
+    checkboxesShouldNotExist()
+
+    cy.get('select[name="select-case-type"]').select("Warrants")
+    checkboxesShouldNotExist()
   })
 
   it("renders the run reports button and clear filters link", () => {
