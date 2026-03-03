7d2c41fc030cf125964d53f7709a035f4cb23b99
Tom Watkins <tom.watkins@justice.gov.uk>
2026-03-03 11:57:23 +0000

    Output dates as strings

--- packages/api/src/useCases/dto/convertAuditCaseToDto.ts
+++ packages/api/src/useCases/dto/convertAuditCaseToDto.ts
@@ -1,22 +1,24 @@
 import type { AuditCase, AuditCaseDto } from "@moj-bichard7/common/types/AuditCase"
 
+import { format } from "date-fns"
+
 import { convertNoteToDto } from "./convertNoteToDto"
 import { resolutionStatusFromDb } from "./convertResolutionStatus"
 
 export const convertAuditCaseToDto = (auditCase: AuditCase): AuditCaseDto => ({
   asn: auditCase.asn,
-  courtDate: auditCase.court_date,
+  courtDate: auditCase.court_date ? format(auditCase.court_date, "yyyy-MM-dd") : null,
   courtName: auditCase.court_name,
   courtReference: auditCase.court_reference,
   defendantName: auditCase.defendant_name,
   errorId: auditCase.error_id,
   errorQualityChecked: auditCase.error_quality_checked,
   errorStatus: resolutionStatusFromDb(auditCase.error_status),
-  messageReceivedTimestamp: auditCase.msg_received_ts,
+  messageReceivedTimestamp: auditCase.msg_received_ts?.toISOString() ?? null,
   noteCount: auditCase.notes?.length,
   notes: auditCase.notes ? auditCase.notes.map(convertNoteToDto) : [],
   ptiurn: auditCase.ptiurn,
-  resolutionTimestamp: auditCase.resolution_ts,
+  resolutionTimestamp: auditCase.resolution_ts?.toISOString() ?? null,
   triggerQualityChecked: auditCase.trigger_quality_checked,
   triggerStatus: resolutionStatusFromDb(auditCase.trigger_status)
 })
