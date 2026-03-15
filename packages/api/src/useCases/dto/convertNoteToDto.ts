29aa2592ab2b60adf5557a5f4ff20f2899d3d022
Tom Watkins <tom.watkins@justice.gov.uk>
2026-03-15 12:14:40 +0000

    Move parsing to service instead of dto function

--- packages/api/src/useCases/dto/convertNoteToDto.ts
+++ packages/api/src/useCases/dto/convertNoteToDto.ts
@@ -1,24 +1,21 @@
 import type { NoteDto, NoteRow } from "@moj-bichard7/common/types/Note"
 
-import { NoteRowSchema } from "@moj-bichard7/common/types/Note"
-
 import { convertNoteUserRowToNoteUserDto } from "./convertUserToDto"
 
 export const convertNoteToDto = (noteRow: NoteRow): NoteDto => {
-  const parsedNoteRow = NoteRowSchema.parse(noteRow)
   let fullname: string | undefined
 
-  const noteUser = parsedNoteRow.user?.username ? convertNoteUserRowToNoteUserDto(parsedNoteRow.user) : undefined
+  const noteUser = noteRow.user?.username ? convertNoteUserRowToNoteUserDto(noteRow.user) : undefined
 
   if (noteUser) {
     fullname = `${noteUser.forenames} ${noteUser.surname}`
   }
 
   return {
-    createdAt: parsedNoteRow.create_ts,
-    noteText: parsedNoteRow.note_text,
+    createdAt: noteRow.create_ts,
+    noteText: noteRow.note_text,
     user: noteUser,
     userFullName: fullname,
-    userId: parsedNoteRow.user_id
+    userId: noteRow.user_id
   }
 }
