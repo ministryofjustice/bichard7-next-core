a1d6925cd5a223ce664b8f5bf36ce555b9836da1
Richard Race <1273965+richrace@users.noreply.github.com>
2026-03-17 17:09:30 +0000

    Refactor Report Stragegy (#1856)

--- packages/ui/src/types/reports/Config.ts
+++ packages/ui/src/types/reports/Config.ts
@@ -5,39 +5,48 @@ import type { CaseForBailsReportDto } from "@moj-bichard7/common/types/reports/B
 import type { CaseForDomesticViolenceReportDto } from "@moj-bichard7/common/types/reports/DomesticViolence"
 import type { ExceptionReportDto, CaseForExceptionReportDto } from "@moj-bichard7/common/types/reports/Exceptions"
 import type { CaseForWarrantsReportDto } from "@moj-bichard7/common/types/reports/Warrants"
+import { V1 } from "@moj-bichard7/common/apiEndpoints/versionedEndpoints"
+
+interface BaseConfig {
+  endpoint: string
+}
 
 export type FlatReportConfig<TRow> = {
   isGrouped: false
   columns: ReportColumn<TRow>[]
-}
+} & BaseConfig
 
 export type GroupedReportConfig<TGroup, TRow> = {
   isGrouped: true
   groupNameKey: Extract<keyof TGroup, string>
   dataListKey: Extract<keyof TGroup, string>
   columns: ReportColumn<TRow>[]
-}
+} & BaseConfig
 
 export type ReportConfig =
-  | { isGrouped: false; columns: BaseReportColumn[] }
-  | { isGrouped: true; groupNameKey: string; dataListKey: string; columns: BaseReportColumn[] }
+  | ({ isGrouped: false; columns: BaseReportColumn[] } & BaseConfig)
+  | ({ isGrouped: true; groupNameKey: string; dataListKey: string; columns: BaseReportColumn[] } & BaseConfig)
 
 export const ReportConfigs: Record<ReportType, ReportConfig> = {
   bails: {
+    endpoint: V1.CasesReportsBails,
     isGrouped: false,
     columns: bailsColumns
   } satisfies FlatReportConfig<CaseForBailsReportDto>,
   "domestic violence": {
+    endpoint: V1.CasesReportsDomesticViolence,
     isGrouped: false,
     columns: domesticViolenceColumns
   } satisfies FlatReportConfig<CaseForDomesticViolenceReportDto>,
   exceptions: {
+    endpoint: V1.CasesReportsExceptions,
     isGrouped: true,
     groupNameKey: "username",
     dataListKey: "cases",
     columns: exceptionsColumns
   } satisfies GroupedReportConfig<ExceptionReportDto, CaseForExceptionReportDto>,
   warrants: {
+    endpoint: V1.CasesReportsWarrants,
     isGrouped: false,
     columns: warrantsColumns
   } satisfies FlatReportConfig<CaseForWarrantsReportDto>
