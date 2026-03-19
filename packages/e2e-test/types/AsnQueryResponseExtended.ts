98cc2abc7bf1c7818f57aaf537cea501abf1f6f2
Tausif Patel <tausifpatel.dev@gmail.com>
2026-03-19 12:16:58 +0000

    Add AsnQueryResponseExtended type

---
+++ packages/e2e-test/types/AsnQueryResponseExtended.ts
@@ -0,0 +1,20 @@
+import { asnQueryResponseSchema, disposalSchema, offenceSchema } from "@moj-bichard7/core/schemas/leds/asnQueryResponse"
+import { z } from "zod"
+
+const offenceExtendedSchema = offenceSchema.extend({
+  acpoOffenceCode: z.string().optional()
+})
+
+const disposalExtendedSchema = disposalSchema.extend({
+  crimeOffenceReferenceNumber: z.string().optional(),
+  offences: z.array(offenceExtendedSchema)
+})
+
+export const asnQueryResponseExtendedSchema = asnQueryResponseSchema.extend({
+  updateType: z.string().optional(),
+  pncCheckName: z.string().optional(),
+  croNumber: z.string().optional(),
+  disposals: z.array(disposalExtendedSchema)
+})
+
+export type AsnQueryResponseExtended = z.infer<typeof asnQueryResponseExtendedSchema>
