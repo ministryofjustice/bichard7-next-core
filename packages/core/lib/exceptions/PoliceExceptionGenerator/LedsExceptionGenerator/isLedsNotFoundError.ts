86034eca8154d99aed720d6e38cd61d0f5d644f9
Emad Karamad <emad.karamad@madetech.com>
2026-03-23 11:23:22 +0000

    Refactor not found error functions

--- packages/core/lib/exceptions/PoliceExceptionGenerator/LedsExceptionGenerator/isLedsNotFoundError.ts
+++ packages/core/lib/exceptions/PoliceExceptionGenerator/LedsExceptionGenerator/isLedsNotFoundError.ts
@@ -1,3 +1,3 @@
-const isLedsNotFoundError = (message: string): boolean => !!message.match(/No matching arrest reports found for asn/i)
+const isLedsNotFoundError = (message: string): boolean => /No matching arrest reports found for asn/i.test(message)
 
 export default isLedsNotFoundError
--- packages/core/lib/exceptions/PoliceExceptionGenerator/PncExceptionGenerator/isPncNotFoundError.ts
+++ packages/core/lib/exceptions/PoliceExceptionGenerator/PncExceptionGenerator/isPncNotFoundError.ts
@@ -1,3 +1,3 @@
-const isPncNotFoundError = (message: string): boolean => !!message.match(/^I1008.*ARREST\/SUMMONS REF .* NOT FOUND/)
+const isPncNotFoundError = (message: string): boolean => /^I1008.*ARREST\/SUMMONS REF .* NOT FOUND/.test(message)
 
 export default isPncNotFoundError
