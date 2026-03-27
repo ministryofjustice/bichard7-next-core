349ae3528c5a0e89b89d8f5014e954c30c5de783
Emad Karamad <emad.karamad@madetech.com>
2026-03-27 12:37:29 +0000

    Tidy up LedsGateway

--- packages/core/lib/policeGateway/leds/LedsGateway.ts
+++ packages/core/lib/policeGateway/leds/LedsGateway.ts
@@ -147,9 +147,11 @@ export default class LedsGateway implements PoliceGateway {
     }
 
     const updateUrl = this.generateUrl(endpoint)
+    const body = cleanObjectStrings(requestBody)
+    const headers = generateRequestHeaders(correlationId, actionCode, authToken)
     const apiResponse = await axios
-      .post(updateUrl, cleanObjectStrings(requestBody), {
-        headers: generateRequestHeaders(correlationId, actionCode, authToken),
+      .post(updateUrl, body, {
+        headers,
         httpsAgent: new https.Agent({
           rejectUnauthorized: false
         }),
