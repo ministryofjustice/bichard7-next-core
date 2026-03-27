0cfbdfc93aaefaf9b81a6771b69e38a059824bf5
Tausif Patel <tausifpatel.dev@gmail.com>
2026-03-27 09:42:24 +0000

    Refactor

--- packages/e2e-test/utils/converters/convertJsonToXml/generators/remSegmentGenerator.ts
+++ packages/e2e-test/utils/converters/convertJsonToXml/generators/remSegmentGenerator.ts
@@ -1,29 +1,14 @@
+import type { Court } from "@moj-bichard7/core/types/leds/DisposalRequest"
 import type { MockRemandRequest } from "../../../../types/MockRemandRequest"
-import {
-  BAIL_ADDRESS_FIELD_LENGTH,
-  BAIL_CONDITIONS_FIELD_LENGTH,
-  BREACH_OF_BAIL_CONDITIONS_FIELD_LENGTH,
-  COURT_NAME_TYPE1_FIELD_LENGTH,
-  COURT_NAME_TYPE2_FIELD_LENGTH,
-  CUSTODY_FORCE_STATION_CODE_FIELD_LENGTH,
-  CUSTODY_TEXT_FIELD_LENGTH,
-  INSTITUTION_CODE_FIELD_LENGTH,
-  LOCAL_AUTHORITY_CODE_FIELD_LENGTH,
-  LOCAL_AUTHORITY_NAME_FIELD_LENGTH,
-  LOCAL_AUTHORITY_SECURE_UNIT_MARKER_FIELD_LENGTH,
-  NEXT_APPEARANCE_DATE_FIELD_LENGTH,
-  NEXT_APPEARANCE_LOCATION_FFSS_FIELD_LENGTH,
-  NEXT_APPEARANCE_LOCATION_FIELD_LENGTH,
-  PRISONER_NUMBER_FIELD_LENGTH,
-  REMAND_DATE_FIELD_LENGTH,
-  REMAND_LOCATION_COURT_FIELD_LENGTH,
-  REMAND_LOCATION_FFSS_FIELD_LENGTH,
-  REMAND_RESULT_FIELD_LENGTH,
-  SOCIAL_WORKER_NAME_FIELD_LENGTH,
-  SOCIAL_WORKER_TELEPHONE_FIELD_LENGTH
-} from "../../../constants"
+import * as C from "../../../constants"
 import { convertToPncDate } from "../helpers/convertToPncDate"
 import generateRow from "../helpers/generateRow"
+
+const extractCourtCode = (court: Court | undefined): string | undefined =>
+  court?.courtIdentityType === "code" ? court.courtCode : undefined
+
+const extractCourtName = (court: Court | undefined): string | undefined =>
+  court?.courtIdentityType === "name" ? court.courtName : undefined
 
 const remSegmentGenerator = (ledsJson: MockRemandRequest): string => {
   const currentAppearance = ledsJson.currentAppearance?.court
@@ -32,47 +17,41 @@ const remSegmentGenerator = (ledsJson: MockRemandRequest): string => {
   const remandDate = convertToPncDate(ledsJson.remandDate)
   const remandResult = ledsJson.remandResult
   const remandLocationFfss = ledsJson.remandLocationFfss
-  const remandLocationCourt = currentAppearance?.courtIdentityType === "code" ? currentAppearance.courtCode : undefined
-  const courtNameType1 = currentAppearance?.courtIdentityType === "name" ? currentAppearance.courtName : undefined
+  const remandLocationCourt = extractCourtCode(currentAppearance)
+  const courtNameType1 = extractCourtName(currentAppearance)
   const nextAppearanceDate = ledsJson.nextAppearance?.date && convertToPncDate(ledsJson.nextAppearance?.date)
-  const nextAppearanceLocation = nextAppearance?.courtIdentityType === "code" ? nextAppearance?.courtCode : undefined
-  const courtNameType2 = nextAppearance?.courtIdentityType === "name" ? nextAppearance.courtName : undefined
+  const nextAppearanceLocation = extractCourtCode(nextAppearance)
+  const courtNameType2 = extractCourtName(nextAppearance)
   const bailConditions = ledsJson.bailConditions.join("")
-  const bailAddress = ""
-  const breachOfBailConditions = ""
-  const custodyForceStationCode = ""
-  const institutionCode = ""
-  const prisonerNumber = ""
-  const custodyText = ""
-  const nextAppearanceLocationFfss = ""
-  const localAuthorityCode = ""
-  const localAuthorityName = ""
-  const localAuthoritySecureUnitMarker = ""
-  const socialWorkerName = ""
-  const socialWorkerTelephone = ""
+  const localAuthorityCode = "0000"
+
+  const unimplementedFields: [string, number][] = [
+    ["", C.BAIL_ADDRESS_FIELD_LENGTH],
+    ["", C.BREACH_OF_BAIL_CONDITIONS_FIELD_LENGTH],
+    ["", C.CUSTODY_FORCE_STATION_CODE_FIELD_LENGTH],
+    ["", C.INSTITUTION_CODE_FIELD_LENGTH],
+    ["", C.PRISONER_NUMBER_FIELD_LENGTH],
+    ["", C.CUSTODY_TEXT_FIELD_LENGTH],
+    ["", C.NEXT_APPEARANCE_LOCATION_FFSS_FIELD_LENGTH],
+    [localAuthorityCode, C.LOCAL_AUTHORITY_CODE_FIELD_LENGTH],
+    ["", C.LOCAL_AUTHORITY_NAME_FIELD_LENGTH],
+    ["", C.LOCAL_AUTHORITY_SECURE_UNIT_MARKER_FIELD_LENGTH],
+    ["", C.SOCIAL_WORKER_NAME_FIELD_LENGTH],
+    ["", C.SOCIAL_WORKER_TELEPHONE_FIELD_LENGTH]
+  ]
 
   const remSegment = generateRow("REM", [
-    [remandDate, REMAND_DATE_FIELD_LENGTH],
-    [remandResult, REMAND_RESULT_FIELD_LENGTH],
-    [remandLocationFfss, REMAND_LOCATION_FFSS_FIELD_LENGTH],
-    [remandLocationCourt, REMAND_LOCATION_COURT_FIELD_LENGTH],
-    [courtNameType1, COURT_NAME_TYPE1_FIELD_LENGTH],
-    [nextAppearanceDate, NEXT_APPEARANCE_DATE_FIELD_LENGTH],
-    [nextAppearanceLocation, NEXT_APPEARANCE_LOCATION_FIELD_LENGTH],
-    [courtNameType2, COURT_NAME_TYPE2_FIELD_LENGTH],
-    [bailConditions, BAIL_CONDITIONS_FIELD_LENGTH],
-    [bailAddress, BAIL_ADDRESS_FIELD_LENGTH],
-    [breachOfBailConditions, BREACH_OF_BAIL_CONDITIONS_FIELD_LENGTH],
-    [custodyForceStationCode, CUSTODY_FORCE_STATION_CODE_FIELD_LENGTH],
-    [institutionCode, INSTITUTION_CODE_FIELD_LENGTH],
-    [prisonerNumber, PRISONER_NUMBER_FIELD_LENGTH],
-    [custodyText, CUSTODY_TEXT_FIELD_LENGTH],
-    [nextAppearanceLocationFfss, NEXT_APPEARANCE_LOCATION_FFSS_FIELD_LENGTH],
-    [localAuthorityCode, LOCAL_AUTHORITY_CODE_FIELD_LENGTH],
-    [localAuthorityName, LOCAL_AUTHORITY_NAME_FIELD_LENGTH],
-    [localAuthoritySecureUnitMarker, LOCAL_AUTHORITY_SECURE_UNIT_MARKER_FIELD_LENGTH],
-    [socialWorkerName, SOCIAL_WORKER_NAME_FIELD_LENGTH],
-    [socialWorkerTelephone, SOCIAL_WORKER_TELEPHONE_FIELD_LENGTH]
+    [C.OFFENCE_UPDATE_TYPE, C.UPDATE_TYPE_FIELD_LENGTH],
+    [remandDate, C.REMAND_DATE_FIELD_LENGTH],
+    [remandResult, C.REMAND_RESULT_FIELD_LENGTH],
+    [remandLocationFfss, C.REMAND_LOCATION_FFSS_FIELD_LENGTH],
+    [remandLocationCourt, C.REMAND_LOCATION_COURT_FIELD_LENGTH],
+    [courtNameType1, C.COURT_NAME_TYPE1_FIELD_LENGTH],
+    [nextAppearanceDate, C.NEXT_APPEARANCE_DATE_FIELD_LENGTH],
+    [nextAppearanceLocation, C.NEXT_APPEARANCE_LOCATION_FIELD_LENGTH],
+    [courtNameType2, C.COURT_NAME_TYPE2_FIELD_LENGTH],
+    [bailConditions, C.BAIL_CONDITIONS_FIELD_LENGTH],
+    ...unimplementedFields
   ])
 
   return remSegment
