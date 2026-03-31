b0b3030549f84f88623481dc1bb9dc849db7c034
Tausif Patel <tausifpatel.dev@gmail.com>
2026-03-31 12:29:45 +0100

    Add updateType to CRT segment

--- packages/e2e-test/utils/converters/convertJsonToXml/generators/crtSegmentGenerator.ts
+++ packages/e2e-test/utils/converters/convertJsonToXml/generators/crtSegmentGenerator.ts
@@ -1,14 +1,25 @@
 import type { AddDisposalRequest } from "@moj-bichard7/core/types/leds/AddDisposalRequest"
-import { COURT_CODE_FIELD_LENGTH, COURT_NAME_FIELD_LENGTH, DATE_OF_HEARING_FIELD_LENGTH } from "../../../constants"
+import {
+  COURT_CODE_FIELD_LENGTH,
+  COURT_NAME_FIELD_LENGTH,
+  DATE_OF_HEARING_FIELD_LENGTH,
+  OFFENCE_UPDATE_TYPE,
+  UPDATE_TYPE_FIELD_LENGTH
+} from "../../../constants"
 
 import { convertToPncDate } from "../helpers/convertToPncDateTime"
 import { extractCourtCode, extractCourtName } from "../helpers/formatters"
 import generateRow from "../helpers/generateRow"
 
-const crtSegmentGenerator = (ledsJson: AddDisposalRequest): string => {
+const crtSegmentGenerator = (ledsJson: AddDisposalRequest): string | undefined => {
   const courtDate = ledsJson.carryForward?.appearanceDate && convertToPncDate(ledsJson.carryForward.appearanceDate)
 
+  if (!courtDate) {
+    return undefined
+  }
+
   return generateRow("CRT", [
+    [OFFENCE_UPDATE_TYPE, UPDATE_TYPE_FIELD_LENGTH],
     [extractCourtCode(ledsJson.court), COURT_CODE_FIELD_LENGTH],
     [extractCourtName(ledsJson.court), COURT_NAME_FIELD_LENGTH],
     [courtDate, DATE_OF_HEARING_FIELD_LENGTH]
