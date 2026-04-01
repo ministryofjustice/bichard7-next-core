bac7527d8865c4edbd8466b343438f7a7168aadc
timhowes-lab <tim.howes@madetech.com>
2026-04-01 14:56:31 +0100

    Added /bichard redirect if new bichard flag is true

--- packages/user-service/src/lib/getRedirectPath.ts
+++ packages/user-service/src/lib/getRedirectPath.ts
@@ -6,8 +6,12 @@ export const isValidRedirectPath = (redirect: string): boolean => {
   return containsOnlyStandardChars && isAbsoluteNonRootPath
 }
 
-const getRedirectPath = ({ redirect }: ParsedUrlQuery): string | undefined => {
+const getRedirectPath = ({ redirect }: ParsedUrlQuery, onlyAccessNewBichard: boolean): string | undefined => {
   if (!!redirect && !Array.isArray(redirect) && isValidRedirectPath(redirect)) {
+    if (onlyAccessNewBichard && redirect.startsWith("/bichard-ui")) {
+      return "/bichard"
+    }
+
     return redirect
   }
 
