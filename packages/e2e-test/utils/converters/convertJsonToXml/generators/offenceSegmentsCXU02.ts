f2e11da1a4b99513f893e10c451f083441dcd0a2
Tausif Patel <tausifpatel.dev@gmail.com>
2026-04-08 12:34:08 +0100

    Refactor offenceSegmentsCXU02

--- packages/e2e-test/utils/converters/convertJsonToXml/generators/offenceSegmentsCXU02.ts
+++ packages/e2e-test/utils/converters/convertJsonToXml/generators/offenceSegmentsCXU02.ts
@@ -1,3 +1,5 @@
+import type { ArrestOffence, Offence } from "@moj-bichard7/core/types/leds/AddDisposalRequest"
+import type { AdditionalArrestOffences } from "@moj-bichard7/core/types/leds/DisposalRequest"
 import type { MockAddDisposalRequest } from "../../../../types/MockAddDisposalRequest"
 import achSegmentGenerator from "./achSegmentGenerator"
 import { adjSegmentFromAddDisposalRequest } from "./adjSegment"
@@ -8,32 +10,47 @@ import couSegmentGenerator from "./couSegmentGenerator"
 import crtSegmentGenerator from "./crtSegmentGenerator"
 import disSegmentGenerator from "./disSegmentGenerator"
 
+const buildOffenceSegments = (offence: Offence): string[] => {
+  const cch = cchSegmentGenerator(offence)
+  const adj = adjSegmentFromAddDisposalRequest(offence) ?? ""
+  const dis =
+    offence.disposalResults?.flatMap((result) => {
+      const segment = disSegmentGenerator(result)
+      return segment ? [segment] : []
+    }) ?? []
+
+  return [cch, adj, ...dis]
+}
+
+const buildAdditionalArrestOffenceSegments = (arrestOffence: ArrestOffence): string[] => {
+  const ach = achSegmentGenerator(arrestOffence)
+  const adj = adjSegmentFromAddDisposalRequest(arrestOffence) ?? ""
+  const dis =
+    arrestOffence.disposalResults?.flatMap((disposalResult) => {
+      const segment = disSegmentGenerator(disposalResult)
+      return segment ? [segment] : []
+    }) ?? []
+
+  return [ach, adj, ...dis]
+}
+
+const buildAdditionalOffenceSegments = (
+  additionalArrestOffence: AdditionalArrestOffences,
+  crimeOffenceReferenceNumber: string
+): string[] => {
+  const asr = asrSegmentGenerator(additionalArrestOffence.asn, crimeOffenceReferenceNumber)
+  const additionalOffences = additionalArrestOffence.additionalOffences.flatMap(buildAdditionalArrestOffenceSegments)
+
+  return [asr, ...additionalOffences]
+}
+
 export const offenceSegmentsCXU02 = (ledsJson: MockAddDisposalRequest): string => {
-  const ccr = ccrSegmentGenerator(ledsJson)
-  const cou = couSegmentGenerator(ledsJson)
-  const crt = crtSegmentGenerator(ledsJson)
+  const headerSegments = [ccrSegmentGenerator(ledsJson), couSegmentGenerator(ledsJson), crtSegmentGenerator(ledsJson)]
+  const offences = ledsJson.offences?.flatMap(buildOffenceSegments) ?? []
+  const additionalOffences =
+    ledsJson.additionalArrestOffences?.flatMap((additionalArrestOffence) =>
+      buildAdditionalOffenceSegments(additionalArrestOffence, ledsJson.crimeOffenceReferenceNumber)
+    ) ?? []
 
-  const offences = ledsJson.offences?.flatMap((offence) => {
-    const cch = cchSegmentGenerator(offence)
-    const adj = adjSegmentFromAddDisposalRequest(offence)
-    const dis = offence.disposalResults?.map((disposalResult) => disSegmentGenerator(disposalResult)) ?? []
-
-    return [cch, adj, ...dis]
-  })
-
-  const additionalOffences = ledsJson.additionalArrestOffences?.flatMap((arrestOffences) => {
-    const asr = asrSegmentGenerator(arrestOffences.asn, ledsJson.crimeOffenceReferenceNumber)
-
-    const additionalArrestOffences = arrestOffences.additionalOffences.flatMap((offence) => {
-      const ach = achSegmentGenerator(offence)
-      const adj = adjSegmentFromAddDisposalRequest(offence)
-      const dis = offence.disposalResults?.map((disposalResult) => disSegmentGenerator(disposalResult)) ?? []
-
-      return [ach, adj, ...dis]
-    })
-
-    return [asr, ...additionalArrestOffences]
-  })
-
-  return [ccr, cou, crt, ...(offences ?? []), ...(additionalOffences ?? [])].join("")
+  return [...headerSegments, ...offences, ...additionalOffences].join("")
 }
