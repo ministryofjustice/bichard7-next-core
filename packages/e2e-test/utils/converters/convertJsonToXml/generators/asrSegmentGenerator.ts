529691e5390f3ea01c9e945f6b07af56273d4133
Tausif Patel <tausifpatel.dev@gmail.com>
2026-04-08 11:46:28 +0100

    Amen ASR segment generator

--- packages/e2e-test/utils/converters/convertJsonToXml/generators/asrSegmentGenerator.ts
+++ packages/e2e-test/utils/converters/convertJsonToXml/generators/asrSegmentGenerator.ts
@@ -1,4 +1,3 @@
-import type { MockRemandRequest } from "../../../../types/MockRemandRequest"
 import {
   ARREST_SUMMONS_NUMBER_FIELD_LENGTH,
   CRIME_OFFENCE_REFERENCE_FIELD_LENGTH,
@@ -7,11 +6,17 @@ import {
 } from "../../../constants"
 import generateRow from "../helpers/generateRow"
 
-const asrSegmentGenerator = (ledsJson: MockRemandRequest): string =>
+const convertToPncAsn = (asn: string): string =>
+  asn
+    .split("/")
+    .map((part, i) => (i === 3 ? part.replace(/^0*/g, "") : part))
+    .join("/")
+
+const asrSegmentGenerator = (asn: string, crimeOffenceReferenceNo: string): string =>
   generateRow("ASR", [
     [UPDATE_TYPE, UPDATE_TYPE_FIELD_LENGTH],
-    [ledsJson.arrestSummonsNumber, ARREST_SUMMONS_NUMBER_FIELD_LENGTH],
-    [ledsJson.crimeOffenceReferenceNo, CRIME_OFFENCE_REFERENCE_FIELD_LENGTH]
+    [convertToPncAsn(asn), ARREST_SUMMONS_NUMBER_FIELD_LENGTH],
+    [crimeOffenceReferenceNo, CRIME_OFFENCE_REFERENCE_FIELD_LENGTH]
   ])
 
 export default asrSegmentGenerator
