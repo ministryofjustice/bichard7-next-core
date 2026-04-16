c10cbef0e9035249dfd4e08d6bdb2fab3e75f134
Tom Watkins <tom.watkins@justice.gov.uk>
2026-04-16 12:28:45 +0100

    Add db check

---
+++ packages/api/src/services/db/checkConnectivity.unit.test.ts
@@ -0,0 +1,43 @@
+import { isError } from "@moj-bichard7/common/types/Result"
+
+import checkConnectivity from "./checkConnectivity"
+
+describe("checkConnectivity", () => {
+  const mockDatabase = {
+    connection: jest.fn()
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it("should return true when the database returns a value", async () => {
+    mockDatabase.connection.mockResolvedValueOnce([{ connected: 1 }])
+
+    const result = await checkConnectivity(mockDatabase as any)
+
+    expect(isError(result)).toBe(false)
+    expect(result).toBe(true)
+  })
+
+  it("should return false when the database throws an error", async () => {
+    mockDatabase.connection.mockRejectedValueOnce(new Error("Connection Refused"))
+
+    const result = await checkConnectivity(mockDatabase as any)
+
+    expect(result).toBe(false)
+  })
+
+  it("should return false when db times out", async () => {
+    jest.useFakeTimers()
+    mockDatabase.connection.mockResolvedValueOnce(new Promise(() => {}))
+
+    const resultPromise = checkConnectivity(mockDatabase as any)
+    jest.advanceTimersByTime(2100)
+
+    const result = await resultPromise
+    expect(result).toBe(false)
+
+    jest.useRealTimers()
+  })
+})
