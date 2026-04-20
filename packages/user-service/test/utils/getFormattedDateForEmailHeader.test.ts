d5086122111a3a3181be8c2d8352a3ac7c0ddb36
timhowes-lab <tim.howes@madetech.com>
2026-04-20 11:04:20 +0100

    Bicaws7 4152 email with mfa code doesnt show in bst (#1930)
    * Force uk timezone
    
    * include date property when sending email
    
    * updated date formatting
    
    * added date-fns timezone import
    
    * Removed timezone offset initials
    
    * formatting fix
    
    * Separated email date formatter in to utils, added a BST and GMT test
    
    * fixed import
    
    * import fix
    
    * test fix

---
+++ packages/user-service/test/utils/getFormattedDateForEmailHeader.test.ts
@@ -0,0 +1,17 @@
+import getFormattedDateForEmailHeader from "utils/getFormattedDateForEmailHeader"
+
+describe("getFormattedDateForEmailHeader", () => {
+  it("formats correctly for BST (Summer)", () => {
+    const summerDate = new Date("2026-04-17T15:40:00Z")
+    const result = getFormattedDateForEmailHeader(summerDate)
+
+    expect(result).toBe("Fri, 17 Apr 2026 16:40:00 +0100")
+  })
+
+  it("formats correctly for GMT (Winter)", () => {
+    const winterDate = new Date("2026-03-01T12:00:00Z")
+    const result = getFormattedDateForEmailHeader(winterDate)
+
+    expect(result).toBe("Sun, 01 Mar 2026 12:00:00 +0000")
+  })
+})
