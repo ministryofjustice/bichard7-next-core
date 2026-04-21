269605ffb1c967abd35865ec4bccd3cb774f5921
Tausif Patel <tausifpatel.dev@gmail.com>
2026-04-21 10:46:05 +0100

    Expand the script to include CXU01, CXU02, CXU03 and CXU04 operations

--- migrateXml.ts
+++ migrateXml.ts
@@ -1,28 +1,49 @@
 import fs from "fs"
 import { convertPncJsonToLedsAddDisposalRequest } from "./packages/e2e-test/utils/converters/convertPncJsonToLeds/convertPncJsonToLedsAddDisposalRequest"
+import { convertPncJsonToLedsRemandRequest } from "./packages/e2e-test/utils/converters/convertPncJsonToLeds/convertPncJsonToLedsRemandRequest"
+import { convertPncJsonToLedsSubsequentDisposalRequest } from "./packages/e2e-test/utils/converters/convertPncJsonToLeds/convertPncJsonToLedsSubsequentDisposalRequest"
+
 import { convertPncXmlToJson } from "./packages/e2e-test/utils/converters/convertPncXmlToJson"
-import type { PncNormalDisposalJson } from "./packages/e2e-test/utils/converters/convertPncXmlToJson/convertPncXmlToJson"
+import type {
+  PncNormalDisposalJson,
+  PncRemandJson,
+  PncSubsequentDisposalJson
+} from "./packages/e2e-test/utils/converters/convertPncXmlToJson/convertPncXmlToJson"
 
 const run = () => {
-  const file = fs
+  const fileContent = fs
     .readFileSync("./packages/e2e-test/features/028-driver-disqualification/mock-pnc-responses.ts")
     .toString()
   const regex = /policeApi\.mockUpdate\s*\(\s*"(CXU0\d)"\s*,\s*({[\w\W]*?})\s*\)/g
-  const allMatches = [...file.matchAll(regex)]
 
-  allMatches.forEach((match) => {
-    let mockJson
-    const code = match[1]
-    const contentObj = eval(`(${match[2]})`)
+  const updatedContent = fileContent.replace(regex, (_: string, code: string , content: string ) => {
+    const contentObj = eval(`(${content})`)
     const pncXml = contentObj.expectedRequest
     const pncJson = convertPncXmlToJson(pncXml)
 
-    if (code === "CXU02") {
-      mockJson = convertPncJsonToLedsAddDisposalRequest(pncJson as PncNormalDisposalJson)
+    let mockJson
+    switch (code) {
+      case "CXU01":
+        mockJson = convertPncJsonToLedsRemandRequest(pncJson as PncRemandJson)
+        break
+      case "CXU02":
+        mockJson = convertPncJsonToLedsAddDisposalRequest(pncJson as PncNormalDisposalJson)
+        break
+      case "CXU04":
+      case "CXU03":
+        mockJson = convertPncJsonToLedsSubsequentDisposalRequest(pncJson as PncSubsequentDisposalJson)
+        break
     }
 
-    console.log("======>", JSON.stringify(mockJson, null, 2))
+    const newObject = {
+      ...contentObj,
+      expectedRequest: mockJson
+    }
+
+    return `policeApi.mockUpdate("${code}", ${JSON.stringify(newObject, null, 2)})`
   })
+
+  console.log(updatedContent)
 }
 
 run()
