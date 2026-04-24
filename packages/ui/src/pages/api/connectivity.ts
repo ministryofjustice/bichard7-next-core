975118e4fa3d98a58b441dc9fa7a068fe587e958
Tom Watkins <tom.watkins@justice.gov.uk>
2026-04-24 10:59:28 +0100

    Do not return error error if API error, instead return false

--- packages/ui/src/pages/api/connectivity.ts
+++ packages/ui/src/pages/api/connectivity.ts
@@ -1,7 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import type { ApiConnectivityDto } from "@moj-bichard7/common/types/ApiConnectivity"
+
 import ApiClient from "services/api/ApiClient"
 import BichardApiV1 from "services/api/BichardApiV1"
 import { isError } from "types/Result"
+import logger from "utils/logger"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const apiKey = req.headers["x-connectivity-check-key"] as string
@@ -15,10 +18,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const apiClient = new ApiClient("") // Does not require auth via JWT
   const apiGateway = new BichardApiV1(apiClient)
 
-  const apiConnectivity = await apiGateway.connectivity(apiKey)
-  if (isError(apiConnectivity)) {
-    res.status(400).send(apiConnectivity.message)
-    return
+  const apiConnectivityResult = await apiGateway.connectivity(apiKey)
+  let apiConnectivity: false | ApiConnectivityDto
+  if (isError(apiConnectivityResult)) {
+    logger.error(`Connectivity endpoint failed to connect to the API: ${apiConnectivityResult.message}`)
+    apiConnectivity = false
+  } else {
+    apiConnectivity = apiConnectivityResult
   }
 
   res.status(200).json({
