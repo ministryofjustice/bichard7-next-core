06cb25d8c0ce254fc1bab3079e1e8d2b8d226df2
Tausif Patel <tausifpatel.dev@gmail.com>
2026-05-01 10:42:37 +0100

    Add a function to find running conductor workflows

--- packages/e2e-test/helpers/ConductorHelper.ts
+++ packages/e2e-test/helpers/ConductorHelper.ts
@@ -1,5 +1,21 @@
-export default {
-  CONDUCTOR_URL: process.env.CONDUCTOR_URL ?? "http://localhost:5002/api",
-  CONDUCTOR_USER: process.env.CONDUCTOR_USER ?? "bichard",
-  CONDUCTOR_PASSWORD: process.env.CONDUCTOR_PASSWORD ?? "password"
+import createConductorClient from "@moj-bichard7/common/conductor/createConductorClient"
+
+const client = createConductorClient()
+
+export const findRunningConductorWorkflowIds = async (): Promise<string[]> => {
+  const searchResult = await client.workflowResource.search1(
+    undefined,
+    0,
+    100,
+    "startTime:DESC",
+    undefined,
+    "status='RUNNING'"
+  )
+
+  return searchResult.results?.map((workflow) => workflow.workflowId!) || []
+}
+
+export const terminateConductorWorkflows = async () => {
+  const idsToTerminate = await findRunningConductorWorkflowIds()
+  await Promise.all(idsToTerminate.map((id) => client.workflowResource.terminate1(id, "Termination by test script")))
 }
