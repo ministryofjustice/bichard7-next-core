ab5079cb1f09a97ce80557dba5cccfec990925b5
Tausif Patel <tausifpatel.dev@gmail.com>
2026-05-05 17:02:46 +0100

    Handle txt field for leds

--- packages/e2e-test/utils/converters/convertJsonToXml/convertLedsAsnQueryResponseToXml.ts
+++ packages/e2e-test/utils/converters/convertJsonToXml/convertLedsAsnQueryResponseToXml.ts
@@ -1,3 +1,4 @@
+import type { MockAsnQueryErrorResponse } from "../../../types/MockAsnQueryErrorResponse"
 import type { MockAsnQueryResponse } from "../../../types/MockAsnQueryResponse"
 import { XML_DECLARATION } from "../../constants"
 import commonSegmentGenerator from "./generators/commonSegmentGenerator"
@@ -5,9 +6,16 @@ import fscSegmentGenerator from "./generators/fscSegmentGenerator"
 import idsSegmentGenerator from "./generators/idsSegmentGenerator"
 import offenceSegmentsCXE01 from "./generators/offenceSegmentsCXE01"
 
-const convertLedsAsnQueryResponseToXml = (mockJson: MockAsnQueryResponse) => {
+const convertLedsAsnQueryResponseToXml = (mockJson: MockAsnQueryResponse | MockAsnQueryErrorResponse) => {
   const gmh = commonSegmentGenerator("GMH", mockJson.gmh)
   const gmt = commonSegmentGenerator("GMT", mockJson.gmt)
+
+  if ("txt" in mockJson) {
+    const txt = commonSegmentGenerator("TXT", mockJson.txt)
+
+    return `${XML_DECLARATION}<CXE01>${gmh}${txt}${gmt}</CXE01>`
+  }
+
   const segments = [fscSegmentGenerator(mockJson), idsSegmentGenerator(mockJson), offenceSegmentsCXE01(mockJson)]
   const innerContent = segments.join("")
 
--- packages/e2e-test/utils/converters/convertJsonToXml/generators/commonSegmentGenerator.ts
+++ packages/e2e-test/utils/converters/convertJsonToXml/generators/commonSegmentGenerator.ts
@@ -1,8 +1,14 @@
-import { GMH_FIELD_LENGTH, GMT_FIELD_LENGTH } from "../../../constants"
+import { GMH_FIELD_LENGTH, GMT_FIELD_LENGTH, TXT_FIELD_LENGTH } from "../../../constants"
 import generateRow from "../helpers/generateRow"
 
-const commonSegmentGenerator = (tag: "GMH" | "GMT", data: string | undefined): string => {
-  const fieldLength = tag === "GMH" ? GMH_FIELD_LENGTH : GMT_FIELD_LENGTH
+const fieldLengthMapper = {
+  GMH: GMH_FIELD_LENGTH,
+  GMT: GMT_FIELD_LENGTH,
+  TXT: TXT_FIELD_LENGTH
+}
+
+const commonSegmentGenerator = (tag: "GMH" | "GMT" | "TXT", data: string | undefined): string => {
+  const fieldLength = fieldLengthMapper[tag]
   return generateRow(tag, [[data, fieldLength]])
 }
 
