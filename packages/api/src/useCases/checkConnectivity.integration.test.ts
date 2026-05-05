463d29ed9a331ac50b024c2510d71e9b36ac1697
Richard Race <1273965+richrace@users.noreply.github.com>
2026-05-05 16:21:24 +0100

    Fix open handles

--- packages/api/src/useCases/checkConnectivity.integration.test.ts
+++ packages/api/src/useCases/checkConnectivity.integration.test.ts
@@ -1,9 +1,13 @@
 import End2EndPostgres from "../tests/testGateways/e2ePostgres"
 import checkConnectivity from "./checkConnectivity"
 
-const testDatabaseGateway = new End2EndPostgres()
-
 describe("checkConnectivity", () => {
+  const testDatabaseGateway = new End2EndPostgres()
+
+  afterAll(async () => {
+    await testDatabaseGateway.close()
+  })
+
   it("should return true for all services", async () => {
     const connectivity = await checkConnectivity(testDatabaseGateway.readonly)
 
