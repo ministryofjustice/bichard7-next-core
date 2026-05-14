8caf98d30efb75df112c8c163e1aa168c6e1b4b4
timhowes-lab <tim.howes@madetech.com>
2026-05-14 12:11:55 +0100

    Removed unused columns obj

--- packages/ui/src/types/reports/Columns.ts
+++ packages/ui/src/types/reports/Columns.ts
@@ -94,19 +94,6 @@ export const userPerformanceSummaryColumns: ReportColumn<UserForPerformanceSumma
   { header: "Total Exceptions/Triggers Still Locked", key: "totalNumberStillLocked" }
 ] as const
 
-export const userPerformanceDetailColumns: ReportColumn<CodeDetailUserDto>[][] = [
-  [
-    { header: "User ID", key: "username" },
-    { header: "Number of exceptions resolved today", key: "resolved" },
-    { header: "Total number of exceptions still locked", key: "totalLocked" }
-  ],
-  [
-    { header: "User ID", key: "username" },
-    { header: "Number of triggers resolved today", key: "resolved" },
-    { header: "Total number of triggers still locked", key: "totalLocked" }
-  ]
-] as const
-
 export const codeDetailUserColumns: Record<string, ReportColumn<CodeDetailUserDto>[]> = {
   exception: [
     { header: "User ID", key: "username" },
