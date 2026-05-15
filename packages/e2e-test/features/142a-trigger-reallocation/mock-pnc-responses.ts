fd24121563ecd514dd2d5641c833ca40f0f69bbf
Tausif Patel <tausifpatel.dev@gmail.com>
2026-05-15 21:12:20 +0100

    Replace xml with JSON and remove optional expectedRequest field from mockUpdate

--- packages/e2e-test/features/142a-trigger-reallocation/mock-pnc-responses.ts
+++ packages/e2e-test/features/142a-trigger-reallocation/mock-pnc-responses.ts
@@ -3,7 +3,97 @@ import type Bichard from "../../utils/world"
 export default (ncm: string, { policeApi }: Bichard) => [
   policeApi.mockEnquiryFromNcm(ncm),
   policeApi.mockUpdate("CXU02", {
-    expectedRequest:
-      "<FSC>K01ZD</FSC><IDS>K00/377244A ALLOCATION              </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I1971                                                                       ALLOCATION/TRIGGER                                    260920080000</COU><CCH>K001              TH68006 </CCH><ADJ>INOT GUILTY   GUILTY        260920080000 </ADJ><DIS>I3095M14                   00                                                                            </DIS><CCH>K002              TH68151 </CCH><ADJ>INOT GUILTY   GUILTY        260920080000 </ADJ><DIS>I3052M11                   00                                                                            </DIS><CCH>K003              RT88191 </CCH><ADJ>INOT GUILTY   GUILTY        260920080000 </ADJ><DIS>I3088M12                   00                                                                            </DIS><CCH>K004              TH68006 </CCH><ADJ>INOT GUILTY   GUILTY        260920080000 </ADJ><DIS>I1002M12                   00                                                                            </DIS>"
+    expectedRequest: {
+      pncCheckName: "ALLOCATION",
+      croNumber: "",
+      crimeOffenceReferenceNumber: "",
+      ownerCode: "01YZ",
+      personUrn: "00/377244A",
+      courtCaseReference: "97/1626/008395Q",
+      court: {
+        courtIdentityType: "code",
+        courtCode: "1971"
+      },
+      dateOfConviction: "2008-09-26",
+      defendant: {
+        defendantType: "individual",
+        defendantFirstNames: ["TRIGGER"],
+        defendantLastName: "ALLOCATION"
+      },
+      offences: [
+        {
+          courtOffenceSequenceNumber: 1,
+          cjsOffenceCode: "TH68006",
+          plea: "Not Guilty",
+          adjudication: "Guilty",
+          dateOfSentence: "2008-09-26",
+          offenceTic: 0,
+          disposalResults: [
+            {
+              disposalCode: 3095,
+              disposalDuration: {
+                units: "months",
+                count: 14
+              }
+            }
+          ],
+          offenceId: "7afeddf7-43e9-4e1a-b299-d267e1aa257b"
+        },
+        {
+          courtOffenceSequenceNumber: 2,
+          cjsOffenceCode: "TH68151",
+          plea: "Not Guilty",
+          adjudication: "Guilty",
+          dateOfSentence: "2008-09-26",
+          offenceTic: 0,
+          disposalResults: [
+            {
+              disposalCode: 3052,
+              disposalDuration: {
+                units: "months",
+                count: 11
+              }
+            }
+          ],
+          offenceId: "11c81a5f-4a4c-4506-a353-b5a599990eee"
+        },
+        {
+          courtOffenceSequenceNumber: 3,
+          cjsOffenceCode: "RT88191",
+          plea: "Not Guilty",
+          adjudication: "Guilty",
+          dateOfSentence: "2008-09-26",
+          offenceTic: 0,
+          disposalResults: [
+            {
+              disposalCode: 3088,
+              disposalDuration: {
+                units: "months",
+                count: 12
+              }
+            }
+          ],
+          offenceId: "f3576738-0967-4807-b068-ab88ee59ed6a"
+        },
+        {
+          courtOffenceSequenceNumber: 4,
+          cjsOffenceCode: "TH68006",
+          plea: "Not Guilty",
+          adjudication: "Guilty",
+          dateOfSentence: "2008-09-26",
+          offenceTic: 0,
+          disposalResults: [
+            {
+              disposalCode: 1002,
+              disposalDuration: {
+                units: "months",
+                count: 12
+              }
+            }
+          ],
+          offenceId: "dc9381b9-9104-420c-9606-c63e4ae02b02"
+        }
+      ]
+    }
   })
 ]
