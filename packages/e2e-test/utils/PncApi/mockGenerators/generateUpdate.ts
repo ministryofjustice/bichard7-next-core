7427f91ae49e3c188d5b72c3aa1a209191e0e029
Tausif Patel <tausifpatel.dev@gmail.com>
2026-05-20 12:57:37 +0100

    Extract out generating response in a separate function

--- packages/e2e-test/utils/PncApi/mockGenerators/generateUpdate.ts
+++ packages/e2e-test/utils/PncApi/mockGenerators/generateUpdate.ts
@@ -1,32 +1,9 @@
 import type { PartialPncMock, PncMockOptions } from "../../../types/PncMock"
 import convertToXml from "../../converters/convertJsonToXml"
-
-const generateRandomGmtSuffix = Math.floor(Math.random() * 8999) + 1000
-
-const DEFAULT_MESSAGE = "A0031-REMAND REPORT HAS BEEN PROCESSED SUCCESSFULLY - ID: 00/263503N "
-const DEFAULT_GMH = `073GENL000001RNEWREMPNCA05A73000017300000120210415154673000001                                             09000${
-  generateRandomGmtSuffix
-}`
-const DEFAULT_GMT = "000003073GENL000001S"
+import generateResponse from "../generateResponse"
 
 export const generateUpdate = (code: string, options?: PncMockOptions): PartialPncMock => {
-  let txt = DEFAULT_MESSAGE
-  let gmh = DEFAULT_GMH
-  let gmt = DEFAULT_GMT
-
-  if (options?.response && typeof options?.response === "object" && "status" in options?.response) {
-    txt = `${options.response.leds.errors[0].message}`
-    gmh = options.response.gmh
-    gmt = options.response.gmt
-  }
-
-  const response = `<?XML VERSION="1.0" STANDALONE="YES"?>
-    <${code}>
-      <GMH>${gmh}</GMH>
-      <TXT>${txt}</TXT>
-      <GMT>${gmt}</GMT>
-    </${code}>`
-
+  const response = generateResponse(code, options?.response)
   const expectedRequest =
     typeof options?.expectedRequest === "object"
       ? convertToXml(code, options?.expectedRequest)
