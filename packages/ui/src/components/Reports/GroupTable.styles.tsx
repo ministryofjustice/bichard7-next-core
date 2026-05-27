8bc43a8990768ac98faf9f037294669742c92ae0
timhowes-lab <tim.howes@madetech.com>
2026-05-27 09:02:54 +0100

    Removed padding and background colour from group table styles

--- packages/ui/src/components/Reports/GroupTable.styles.tsx
+++ packages/ui/src/components/Reports/GroupTable.styles.tsx
@@ -1,16 +1,11 @@
 import styled from "styled-components"
-import { tagBlue, gdsTagBlue } from "utils/colours"
 
 export const ReportContainer = styled.div`
   h3 {
     margin: 0;
-    padding: 1rem;
-    background-color: ${tagBlue};
   }
 
   h4 {
     margin: 0;
-    padding: 1rem;
-    background-color: ${gdsTagBlue};
   }
 `
