fb27c4cbb97c94a3fe7615413c1e1285a46ba8d9
timhowes-lab <tim.howes@madetech.com>
2026-05-27 08:59:32 +0100

    triggers and exceptions test fix

--- packages/ui/cypress/e2e/reports/triggers-and-exceptions.api.cy.ts
+++ packages/ui/cypress/e2e/reports/triggers-and-exceptions.api.cy.ts
@@ -31,8 +31,8 @@ describe("exceptions/triggers report type filter", () => {
       "Resolution action"
     ]
 
-    cy.get('section[aria-labelledby="report-group-user1"]').within(() => {
-      cy.get("h3#report-group-user1").should("exist")
+    cy.get("section#table-user1-1-section").within(() => {
+      cy.get("h3#table-user1-1-header").should("exist")
 
       headers.forEach((text, index) => {
         cy.get("table thead tr th").eq(index).should("have.text", text)
@@ -44,8 +44,8 @@ describe("exceptions/triggers report type filter", () => {
       cy.get("table tbody tr td:nth(2)").should("have.text", "Case00003")
     })
 
-    cy.get('section[aria-labelledby="report-group-GeneralHandler"]').within(() => {
-      cy.get("h3#report-group-GeneralHandler").should("exist")
+    cy.get("section#table-generalhandler-0-section").within(() => {
+      cy.get("h3#table-generalhandler-0-header").should("exist")
 
       cy.get("table tbody tr").should("have.length", 1)
 
