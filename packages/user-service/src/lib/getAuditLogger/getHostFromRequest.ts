d5cf11af3ad91b5c330acd782e973003139723c3
Kate Reidy <kate.reidy@madetech.com>
2026-06-08 15:26:27 +0100

    Use x-origin header for determining domain used by user.

--- packages/user-service/src/lib/getAuditLogger/getHostFromRequest.ts
+++ packages/user-service/src/lib/getAuditLogger/getHostFromRequest.ts
@@ -1,39 +1,13 @@
 import type { IncomingMessage } from "http"
 
-function extractHost(host: string) {
-  // Host string can also contain port (which we're not interested in)
-  return host.split(":")[0]
-}
-
 export function getHostFromRequest(req: IncomingMessage): string {
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Forwarded
-  // Forwarded: by=<identifier>;for=<identifier>;host=<host>;proto=<http|https>
+  // x-origin header is inserted by nginx auth proxy
+  const xOrigin = req.headers["x-origin"]?.toString() ?? ""
 
-  if (req.headers.forwarded) {
-    const forwardHeader: Record<string, string> = Object.fromEntries(
-      req.headers.forwarded
-        .split(";")
-        .filter((elem) => !!elem)
-        .map((kv) => kv.split("="))
-        .map((arr) => [arr[0].trim().toLowerCase(), arr[1].trim()])
-    )
-
-    const host = forwardHeader["host"]
-    if (host) {
-      return extractHost(host)
-    }
+  if (URL.canParse(xOrigin)) {
+    const xOriginUrl = new URL(xOrigin)
+    return xOriginUrl.host
   }
 
-  const headerKey = Object.keys(req.headers).find((h) => h.toLowerCase() == "x-forwarded-host")
-
-  if (headerKey) {
-    const headerValue = req.headers[headerKey]
-    if (typeof headerValue === "string") {
-      return extractHost(headerValue)
-    } else if (Array.isArray(headerValue)) {
-      return extractHost(headerValue[0])
-    }
-  }
-
-  return extractHost(req.headers.host ?? "")
+  return req.headers.origin ?? req.headers.host ?? ""
 }
--- packages/user-service/test/lib/getAuditLogger/getHostFromRequest.test.ts
+++ packages/user-service/test/lib/getAuditLogger/getHostFromRequest.test.ts
@@ -1,14 +1,6 @@
 import { getHostFromRequest } from "lib/getAuditLogger/getHostFromRequest"
 import type { IncomingMessage } from "http"
 
-it("should return an empty string if no host or forwarded header is provided", () => {
-  const request = { headers: {} } as unknown as IncomingMessage
-
-  const host = getHostFromRequest(request)
-
-  expect(host).toBe("")
-})
-
 it("should return the request host if one is specified", () => {
   const request = { headers: { host: "example.com" } } as unknown as IncomingMessage
 
@@ -17,40 +9,25 @@ it("should return the request host if one is specified", () => {
   expect(host).toBe("example.com")
 })
 
-it("should strip any port from the host header", () => {
-  const request = { headers: { host: "example.com:8080" } } as unknown as IncomingMessage
+it("should return the request origin if one is specified", () => {
+  const request = { headers: { origin: "example.com" } } as unknown as IncomingMessage
 
   const host = getHostFromRequest(request)
 
   expect(host).toBe("example.com")
 })
 
-it.each([
-  "by=nginx; proto=http; for=127.0.0.1; host=bichard.example.gov.uk",
-  "Host=bichard.example.gov.uk;By=nginx;Proto=http;For=127.0.0.1;"
-])("should return the forwarded host from %s", (forwardedHeader) => {
-  const request = {
-    headers: { host: "localhost", forwarded: forwardedHeader }
-  } as unknown as IncomingMessage
+it("should return host if x-origin is invalid", () => {
+  const request = { headers: { "x-origin": "invalid", host: "example.com" } } as unknown as IncomingMessage
 
   const host = getHostFromRequest(request)
 
-  expect(host).toBe("bichard.example.gov.uk")
+  expect(host).toBe("example.com")
 })
 
-it("should use the host header if the forwarded header does not contain host", () => {
+it.each(["http://example.com", "https://example.com"])("should parse host from x-origin '%s'", (originUrl) => {
   const request = {
-    headers: { host: "localhost", forwarded: "for=192.168.0.1" }
-  } as unknown as IncomingMessage
-
-  const host = getHostFromRequest(request)
-
-  expect(host).toBe("localhost")
-})
-
-it.each(["x-forwarded-host", "X-Forwarded-Host"])("should use host from '%s' if set", (forwardedHostHeader) => {
-  const request = {
-    headers: { host: "localhost", [forwardedHostHeader]: "example.com:8080" }
+    headers: { host: "localhost", "x-origin": originUrl }
   } as unknown as IncomingMessage
 
   const host = getHostFromRequest(request)
