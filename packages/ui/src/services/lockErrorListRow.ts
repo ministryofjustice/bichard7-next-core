758353c62b2f55390ecdda324fcb29655796a282
Denise <denise.turkova@justice.gov.uk>
2026-06-16 10:00:08 +0100

    refactor: adjusted to Typeorm syntax

--- packages/ui/src/services/lockErrorListRow.ts
+++ packages/ui/src/services/lockErrorListRow.ts
@@ -1,6 +1,10 @@
 import type { EntityManager } from "typeorm"
+import CourtCase from "./entities/CourtCase"
 
 export const lockErrorListRow = async (db: EntityManager, caseId: number) => {
-  //  query used instead connection in entityManager
-  await db.query("SELECT * from br7own.error_list el WHERE error_id = $1 FOR UPDATE", [caseId])
+  //   //  query used instead connection in entityManager
+  // /await db.query("SELECT * from br7own.error_list el WHERE error_id = $1 FOR UPDATE", [caseId])
+  // }
+
+  await db.createQueryBuilder(CourtCase, "el").where("el.errorId = :caseId", { caseId }).getOne()
 }
