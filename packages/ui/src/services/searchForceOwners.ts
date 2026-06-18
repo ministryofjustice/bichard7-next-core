756071e8022857ef368fab27c9322cc2ffd2c31f
timhowes-lab <tim.howes@madetech.com>
2026-06-18 16:35:57 +0100

    Added force acronym - code mappings

--- packages/ui/src/services/searchForceOwners.ts
+++ packages/ui/src/services/searchForceOwners.ts
@@ -1,12 +1,30 @@
-import getForcesForReallocation from "./getForcesForReallocation"
-import { sortBy } from "lodash"
 import type { Force } from "@moj-bichard7-developers/bichard7-next-data/dist/types/types"
+import { sortBy } from "lodash"
+import getForcesForReallocation from "./getForcesForReallocation"
 
 export const getForceCode = (force: Force) => force.code
 
 export const getForceName = (force: Force) => [force.name].filter((part) => !!part).join(" ")
 
-export const getForceOwnerCodeAndName = (force: Force) => `${getForceCode(force)} ${getForceName(force)}`
+const forceNameAcronymMappings: Record<string, string> = {
+  "06": "GMP",
+  67: "SOCA",
+  89: "ACRO",
+  91: "NCA",
+  93: "British Transport Police"
+}
+
+export const getForceAcronym = (force: Force) => {
+  const acronym = forceNameAcronymMappings[force.code]
+  if (acronym) {
+    return acronym
+  } else {
+    return ""
+  }
+}
+
+export const getForceOwnerCodeAndName = (force: Force) =>
+  `${getForceCode(force)} ${getForceName(force)} ${getForceAcronym(force)}`
 
 const searchForceOwners = (currentForceOwner: string, keyword: string) => {
   if (currentForceOwner === "" || keyword === "") {
