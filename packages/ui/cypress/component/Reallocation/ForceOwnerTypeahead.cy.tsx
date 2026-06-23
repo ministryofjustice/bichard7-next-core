56fb78181258e6dade25b349a949af314d9852b3
timhowes-lab <tim.howes@madetech.com>
2026-06-23 11:03:44 +0100

    Bicaws7 4249 enable users to search using common acronyms in the new force owner field in the case reallocation journey (#2062)
    * Added force acronym - code mappings
    
    * Added tests
    
    * Force acronym now shown in brackets
    
    * test name change
    
    * console logs
    
    * Added sample force with acronym
    
    * Changed function name

--- packages/ui/cypress/component/Reallocation/ForceOwnerTypeahead.cy.tsx
+++ packages/ui/cypress/component/Reallocation/ForceOwnerTypeahead.cy.tsx
@@ -4,7 +4,8 @@ import type ForceOwnerApiResponse from "types/ForceOwnerApiResponse"
 const mockForces: ForceOwnerApiResponse = [
   { forceCode: "01", forceName: "Metropolitan Police" },
   { forceCode: "02", forceName: "Suffolk Constabulary" },
-  { forceCode: "03", forceName: "Cumbria Constabulary" }
+  { forceCode: "03", forceName: "Cumbria Constabulary" },
+  { forceCode: "06", forceName: "Greater Manchester" }
 ]
 
 describe("ForceOwnerTypeahead Component", () => {
@@ -112,5 +113,21 @@ describe("ForceOwnerTypeahead Component", () => {
     cy.get("input#force").clear()
 
     cy.get("@onSelectSpy").its("lastCall.args").should("deep.equal", [null])
+  })
+
+  it("includes the force acronym in brackets", () => {
+    const onSelectSpy = cy.spy().as("onSelectSpy")
+    cy.mount(<ForceOwnerTypeahead onSelect={onSelectSpy} />)
+
+    cy.get("input#force").type("06")
+
+    cy.wait("@getForces")
+
+    cy.get("ul li").should("have.length", 1)
+    cy.get("ul li").first().should("contain.text", "06 - Greater Manchester (GMP)")
+
+    cy.get("input#force").type("{enter}")
+
+    cy.get("input#force").should("have.value", "06 - Greater Manchester (GMP)")
   })
 })
--- packages/ui/src/services/searchForceOwners.ts
+++ packages/ui/src/services/searchForceOwners.ts
@@ -1,12 +1,37 @@
-import getForcesForReallocation from "./getForcesForReallocation"
-import { sortBy } from "lodash"
 import type { Force } from "@moj-bichard7-developers/bichard7-next-data/dist/types/types"
+import { sortBy } from "lodash"
+import getForcesForReallocation from "./getForcesForReallocation"
 
 export const getForceCode = (force: Force) => force.code
 
 export const getForceName = (force: Force) => [force.name].filter((part) => !!part).join(" ")
 
-export const getForceOwnerCodeAndName = (force: Force) => `${getForceCode(force)} ${getForceName(force)}`
+const forceCodeAcronymMappings: Record<string, string> = {
+  "06": "GMP",
+  67: "SOCA",
+  89: "ACRO",
+  91: "NCA",
+  93: "British Transport Police"
+}
+
+export const getForceAcronym = (forceCode: string) => {
+  const acronym = forceCodeAcronymMappings[forceCode]
+  if (acronym) {
+    return acronym
+  } else {
+    return ""
+  }
+}
+
+export const getForceOwnerCodeNameAcronym = (force: Force) => {
+  const acronym = getForceAcronym(force.code)
+
+  if (acronym) {
+    return `${getForceCode(force)} ${getForceName(force)} ${acronym}`
+  } else {
+    return `${getForceCode(force)} ${getForceName(force)}`
+  }
+}
 
 const searchForceOwners = (currentForceOwner: string, keyword: string) => {
   if (currentForceOwner === "" || keyword === "") {
@@ -15,9 +40,11 @@ const searchForceOwners = (currentForceOwner: string, keyword: string) => {
 
   const sortedForceOwners = sortBy(getForcesForReallocation(currentForceOwner), (force) => force.code)
 
-  return sortedForceOwners.filter((force) =>
-    getForceOwnerCodeAndName(force).toLowerCase().includes(keyword.replace(" - ", " ").toLowerCase())
-  )
+  return sortedForceOwners.filter((force) => {
+    return getForceOwnerCodeNameAcronym(force)
+      .toLowerCase()
+      .includes(keyword.replace(" - ", " ").replace("(", "").replace(")", "").toLowerCase())
+  })
 }
 
 export default searchForceOwners
