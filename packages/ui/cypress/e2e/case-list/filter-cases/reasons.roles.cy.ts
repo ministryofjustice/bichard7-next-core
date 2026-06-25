e415fd98a1559dd76c043f386285ce8c35785223
Denise <denise.turkova@justice.gov.uk>
2026-06-25 09:24:27 +0100

    chore: reverting

--- packages/ui/cypress/e2e/case-list/filter-cases/reasons.roles.cy.ts
+++ packages/ui/cypress/e2e/case-list/filter-cases/reasons.roles.cy.ts
@@ -1,9 +1,6 @@
 import { loginAndVisit } from "../../../support/helpers"
 
 describe("Reasons filters", () => {
-  beforeEach(() => {
-  cy.task("clearCourtCases")
-  })
   it("should not render the reasons component if no user group is specified", () => {
     loginAndVisit("NoGroups")
 
