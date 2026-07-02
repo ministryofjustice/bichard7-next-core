616360b98100c78fb4f0c3f91682f46d976dffea
Denise T <denise.turkova@justice.gov.uk>
2026-07-02 14:21:31 +0100

    chore: renamed convertUserLookupToUserLookupDto and implemented parsing in fetchUserLookupList (#2080)

--- packages/api/src/useCases/users/fetchUserLookupList.ts
+++ packages/api/src/useCases/users/fetchUserLookupList.ts
@@ -1,17 +1,18 @@
 import type { ApiUserLookupQuery } from "@moj-bichard7/common/contracts/ApiUserLookupQuery"
 import type { PromiseResult } from "@moj-bichard7/common/types/Result"
-import type { User, UserLookupList } from "@moj-bichard7/common/types/User"
 import type { FastifyBaseLogger } from "fastify"
 
 import Permission from "@moj-bichard7/common/types/Permission"
 import { isError } from "@moj-bichard7/common/types/Result"
+import { type User, type UserLookupList, UserLookupRowSchema } from "@moj-bichard7/common/types/User"
 import { userAccess } from "@moj-bichard7/common/utils/userPermissions"
+import z from "zod"
 
 import type { DatabaseConnection } from "../../types/DatabaseGateway"
 
 import fetchUserLookups from "../../services/db/users/fetchUserLookups"
 import { NotAllowedError } from "../../types/errors/NotAllowedError"
-import { convertUserToLookupDto } from "../dto/convertUserToDto"
+import { convertUserLookupToUserLookupDto } from "../dto/convertUserToDto"
 
 const fetchUserLookupList = async (
   database: DatabaseConnection,
@@ -30,7 +31,12 @@ const fetchUserLookupList = async (
     return users
   }
 
-  const usersDto = users.users.map((u) => convertUserToLookupDto(u))
+  const parsedResults = z.array(UserLookupRowSchema).safeParse(users.users)
+  if (!parsedResults.success) {
+    return parsedResults.error
+  }
+
+  const usersDto = parsedResults.data.map((u) => convertUserLookupToUserLookupDto(u))
 
   return { users: usersDto }
 }
