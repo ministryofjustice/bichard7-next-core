36af637236dbeed0a8bb70ab72b3b2110622bbd1
Richard Race <1273965+richrace@users.noreply.github.com>
2026-07-10 16:07:17 +0100

    Send trace ID from the UI Client

--- packages/ui/src/services/api/ReportsApiClient.ts
+++ packages/ui/src/services/api/ReportsApiClient.ts
@@ -1,5 +1,7 @@
 import { fetch, Agent } from "undici"
 import { API_LOCATION } from "config"
+import { randomUUID } from "crypto"
+import apiLogger from "./apiLogger"
 
 export default class ReportsApiClient {
   private readonly jwt: string
@@ -16,19 +18,29 @@ export default class ReportsApiClient {
   }
 
   async *fetchReport<T>(url: string): AsyncIterable<T | Error> {
+    const traceId = randomUUID()
+    const logger = apiLogger(traceId, url)
+
     try {
+      const startTime = new Date().getTime()
+
+      logger.info("Requesting reports")
+
       const requestUrl = `${API_LOCATION}${url}`
 
       const response = await fetch(requestUrl, {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${this.jwt}`
+          Authorization: `Bearer ${this.jwt}`,
+          "x-trace-id": traceId
         },
         dispatcher: this.dispatcher
       })
 
       if (!response.ok) {
-        yield new Error(`Stream failed: Request failed with status code ${response.status}`)
+        logger.error("Error: stream error")
+
+        yield new Error(`Stream failed: Request failed with status code ${response.status}, trace ID ${traceId}`)
         return
       }
 
@@ -36,9 +48,16 @@ export default class ReportsApiClient {
         for await (const chunk of response.body) {
           yield chunk as T
         }
+
+        const duration = new Date().getTime() - startTime
+        logger.info(`Success - Took ${duration}ms`)
       }
-    } catch (error) {
-      yield error as Error
+    } catch (err) {
+      const error = err as Error
+
+      logger.error(`Error: ${error.name}  ${error.message}`)
+
+      yield error
     }
   }
 }
