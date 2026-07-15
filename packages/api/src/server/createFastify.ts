11e82e1ef4f7598e4b53d54f648b3a77786b8ec9
Richard Race <1273965+richrace@users.noreply.github.com>
2026-07-15 14:06:41 +0100

    Remove depreciated option

--- packages/api/src/server/createFastify.ts
+++ packages/api/src/server/createFastify.ts
@@ -8,7 +8,6 @@ import logger from "./logging/logger"
 
 export default function (): FastifyInstance<Server, IncomingMessage, ServerResponse> {
   const options = {
-    disableRequestLogging: true,
     genReqId: () => randomUUID(),
     logger: logger(process.env.NODE_ENV)
   }
