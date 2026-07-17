2e7ae947bcdd69c96df7fb4b016ef05f3c1fbc8e
Caitlin Barnard <caitlin@madetech.com>
2026-07-17 12:26:58 +0100

    Refactor ResolverDetails into its own file

--- packages/ui/src/components/SearchFilters/ResolvedByFilter.tsx
+++ packages/ui/src/components/SearchFilters/ResolvedByFilter.tsx
@@ -1,8 +1,7 @@
 import AuditResolvedBy from "@/types/AuditResolvedBy"
-import { formatUserFullName } from "@/utils/formatUserFullName"
 import { useRef, useState } from "react"
 import Checkbox from "../Checkbox/Checkbox"
-import Details from "../Details/Details"
+import ResolverDetails from "../Details/ResolverDetails"
 
 interface ResolveByFilterProps {
   resolvedBy: string[]
@@ -82,53 +81,6 @@ function ResolveByFilter({ resolvedBy, resolvers, onChange }: Readonly<ResolveBy
         />
       )}
     </div>
-  )
-}
-
-interface ResolverDetailsProps {
-  summary: string
-  detailsId: string
-  resolvers: AuditResolvedBy[]
-  refs: React.RefObject<HTMLInputElement[]>
-  resolvedBy: string[]
-  keyPrefix?: string
-  idPrefix: string
-  onCheckboxChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-}
-
-const ResolverDetails = ({
-  summary,
-  detailsId,
-  resolvers,
-  refs,
-  resolvedBy,
-  keyPrefix = "",
-  idPrefix,
-  onCheckboxChange
-}: ResolverDetailsProps) => {
-  if (!resolvers || resolvers.length === 0) {
-    return null
-  }
-  return (
-    <Details summary={summary} id={detailsId}>
-      {resolvers.map((resolver, index) => (
-        <Checkbox
-          key={`${keyPrefix}${resolver.username}-${index}`}
-          id={`${idPrefix}${index}`}
-          name="resolvedBy"
-          value={resolver.username}
-          defaultChecked={resolvedBy.includes(resolver.username)}
-          label={formatUserFullName(resolver.forenames, resolver.surname)}
-          data-testid={`audit-resolved-by-${keyPrefix}${index}`}
-          onChange={onCheckboxChange}
-          ref={(elem) => {
-            if (elem) {
-              refs.current[index] = elem
-            }
-          }}
-        />
-      ))}
-    </Details>
   )
 }
 
