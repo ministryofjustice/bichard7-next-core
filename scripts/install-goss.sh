74c2751dc5cb09ccf38ec68ff8b63d90af3c0efb
joe folkard <joe.folkard@madetech.com>
2026-07-27 14:58:18 +0100

    Add flag to force HTTPS in curl command

--- scripts/install-goss.sh
+++ scripts/install-goss.sh
@@ -5,9 +5,9 @@ set -e
 GOSS_VERSION="v0.4.9"
 
 ## Install goss
-curl -L https://github.com/aelsabbahy/goss/releases/download/$GOSS_VERSION/goss-linux-amd64 -o /usr/local/bin/goss
+curl -L --proto "=https" https://github.com/aelsabbahy/goss/releases/download/$GOSS_VERSION/goss-linux-amd64 -o /usr/local/bin/goss
 chmod +rx /usr/local/bin/goss
-curl -L https://github.com/aelsabbahy/goss/releases/download/$GOSS_VERSION/dgoss -o /usr/local/bin/dgoss
+curl -L --proto "=https" https://github.com/aelsabbahy/goss/releases/download/$GOSS_VERSION/dgoss -o /usr/local/bin/dgoss
 chmod +rx /usr/local/bin/dgoss
 
 export GOSS_PATH="/usr/local/bin/goss"
