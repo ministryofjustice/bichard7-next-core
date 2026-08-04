90f3e6c22d757c7c2507f87cb96af28c0c5df73d
timhowes-lab <tim.howes@madetech.com>
2026-08-04 13:08:05 +0100

    Supervisor switch-off scripts

---
+++ scripts/rollout/prep/20260804/1.sql
@@ -0,0 +1,24 @@
+-- set onlyAccessToNewBichard flag to true
+BEGIN;
+UPDATE
+  br7own.users
+SET
+  feature_flags = jsonb_set(feature_flags, '{onlyAccessToNewBichard}', 'true', true)
+WHERE
+  deleted_at ISNULL AND
+  id IN (1311,702)
+RETURNING id, username, email;
+COMMIT;
+
+
+-- rollback
+BEGIN;
+UPDATE
+  br7own.users
+SET
+  feature_flags = jsonb_set(feature_flags, '{onlyAccessToNewBichard}', 'false', true)
+WHERE
+  deleted_at ISNULL AND
+  id IN (1311,702)
+RETURNING id, username, email;
+COMMIT;
---
+++ scripts/rollout/prep/20260804/3.sql
@@ -0,0 +1,24 @@
+-- set onlyAccessToNewBichard flag to true
+BEGIN;
+UPDATE
+  br7own.users
+SET
+  feature_flags = jsonb_set(feature_flags, '{onlyAccessToNewBichard}', 'true', true)
+WHERE
+  deleted_at ISNULL AND
+  id IN (1444,788,168,780,283,8845,454)
+RETURNING id, username, email;
+COMMIT;
+
+
+-- rollback
+BEGIN;
+UPDATE
+  br7own.users
+SET
+  feature_flags = jsonb_set(feature_flags, '{onlyAccessToNewBichard}', 'false', true)
+WHERE
+  deleted_at ISNULL AND
+  id IN (1444,788,168,780,283,8845,454)
+RETURNING id, username, email;
+COMMIT;
