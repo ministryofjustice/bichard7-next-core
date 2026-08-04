3b2342c927c4441ef5f37bc0549448931bf1d16e
Caitlin Barnard <caitlin@madetech.com>
2026-08-04 10:12:31 +0100

    Refactor buttons that use formStatus to use shared component

---
+++ packages/ui/src/components/Buttons/FormSubmitButton.tsx
@@ -0,0 +1,12 @@
+import React, { ComponentProps } from "react"
+import { useFormStatus } from "react-dom"
+import { Button } from "../../components/Buttons/Button"
+
+type FormSubmitButtonProps = ComponentProps<typeof Button>
+
+const FormSubmitButton: React.FC<FormSubmitButtonProps> = ({ disabled, ...props }) => {
+  const formStatus = useFormStatus()
+  return <Button {...props} type="submit" disabled={disabled || formStatus.pending} />
+}
+
+export default FormSubmitButton
