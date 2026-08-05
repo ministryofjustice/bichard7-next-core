ecee9926aff7d8cefdab1ceec36f1dca57ab2e23
timhowes-lab <tim.howes@madetech.com>
2026-08-05 13:17:31 +0100

    Added formatter and test

---
+++ packages/ui/src/utils/formatUnitOrganisationNameAndCode/formatUnitOrganisationNameAndCode.test.ts
@@ -0,0 +1,48 @@
+import type { OrganisationUnit } from "@moj-bichard7-developers/bichard7-next-data/dist/types/types"
+import { formatUnitOrganisationNameAndCode } from "./formatUnitOrganisationNameAndCode"
+
+describe("formatUnitOrganisationNameAndCode", () => {
+  it("should return an empty array when given an empty array", () => {
+    const result = formatUnitOrganisationNameAndCode([])
+    expect(result).toEqual([])
+  })
+
+  it("should format organisation units into combined codes and filtered names", () => {
+    const mockOrganisationUnits: OrganisationUnit[] = [
+      {
+        topLevelCode: "C",
+        topLevelName: "Magistrates Courts",
+        secondLevelCode: "00",
+        secondLevelName: "London",
+        thirdLevelCode: "01",
+        thirdLevelName: "Westminster",
+        thirdLevelPsaCode: "1234",
+        bottomLevelCode: "00",
+        bottomLevelName: "Court 1"
+      },
+      {
+        topLevelCode: "B",
+        topLevelName: "Crown Court",
+        secondLevelCode: "02",
+        secondLevelName: "",
+        thirdLevelCode: "03",
+        thirdLevelName: "Snaresbrook",
+        thirdLevelPsaCode: "5678",
+        bottomLevelCode: "01"
+      }
+    ]
+
+    const result = formatUnitOrganisationNameAndCode(mockOrganisationUnits)
+
+    expect(result).toEqual([
+      {
+        fullOrganisationCode: "C000100",
+        fullOrganisationName: "Magistrates Courts London Westminster Court 1"
+      },
+      {
+        fullOrganisationCode: "B020301",
+        fullOrganisationName: "Crown Court Snaresbrook"
+      }
+    ])
+  })
+})
---
+++ packages/ui/src/utils/formatUnitOrganisationNameAndCode/formatUnitOrganisationNameAndCode.ts
@@ -0,0 +1,15 @@
+import type OrganisationUnitNameAndCode from "@/types/OrganisationUnitNameAndCode"
+import type { OrganisationUnit } from "@moj-bichard7-developers/bichard7-next-data/dist/types/types"
+import {
+  getFullOrganisationCode,
+  getFullOrganisationName
+} from "@moj-bichard7/common/utils/searchCourtOrganisationUnits"
+
+export const formatUnitOrganisationNameAndCode = (
+  organisationUnits: OrganisationUnit[]
+): OrganisationUnitNameAndCode[] => {
+  return organisationUnits.map((ou) => ({
+    fullOrganisationCode: getFullOrganisationCode(ou),
+    fullOrganisationName: getFullOrganisationName(ou)
+  }))
+}
