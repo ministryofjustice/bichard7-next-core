0f37b6d8bd584c797f5c29cc485a1959da5b0b1f
Tom Watkins <tom.watkins@justice.gov.uk>
2026-08-13 15:04:57 +0100

    Correct variable

--- environment/conductor/scripts/build_docker.sh
+++ environment/conductor/scripts/build_docker.sh
@@ -4,7 +4,7 @@ set -e
 
 export readonly REPOSITORY_NAME="conductor"
 export readonly SOURCE_REPOSITORY_NAME="amazon-linux2023-base"
-export readonly $DOCKERFILE="environment/conductor/Dockerfile"
+export readonly DOCKERFILE="environment/conductor/Dockerfile"
 
 bash ../../../scripts/install_goss.sh
 bash ../../../scripts/build_and_push_image.sh
