0cb5f18f832945711683df32a491c36db98620ab
Tom Watkins <tom.watkins@justice.gov.uk>
2026-08-13 15:28:23 +0100

    Adjust buildspec

--- environment/conductor/scripts/build_docker.sh
+++ environment/conductor/scripts/build_docker.sh
@@ -6,5 +6,5 @@ export readonly REPOSITORY_NAME="conductor"
 export readonly SOURCE_REPOSITORY_NAME="amazon-linux2023-base"
 export readonly DOCKERFILE="environment/conductor/Dockerfile"
 
-bash ../../../scripts/install_goss.sh
-bash ../../../scripts/build_and_push_image.sh
+bash ./scripts/install_goss.sh
+bash ./scripts/build_and_push_image.sh
