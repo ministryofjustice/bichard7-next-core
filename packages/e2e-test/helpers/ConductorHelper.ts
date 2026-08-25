4f2855116d070b25009665794bbb5e0afa56d034
Tom Watkins <tom.watkins@justice.gov.uk>
2026-08-25 22:47:57 +0100

    Remove unneeded sort

--- packages/e2e-test/helpers/ConductorHelper.ts
+++ packages/e2e-test/helpers/ConductorHelper.ts
@@ -5,7 +5,7 @@ import { delay } from "../utils/puppeteer-utils"
 const findRunningConductorWorkflowIds = async (): Promise<string[]> => {
   const client = await createConductorClient()
   const executor = new WorkflowExecutor(client)
-  const searchResult = await executor.search(0, 100, "status='RUNNING'", "*", "startTime:DESC")
+  const searchResult = await executor.search(0, 100, "status='RUNNING'", "*")
 
   return searchResult.results?.map((workflow) => workflow.workflowId!) || []
 }
