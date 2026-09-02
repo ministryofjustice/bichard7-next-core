26ba5d3f36e62a6058916f6b3eeb8f75c1c75a0d
Caitlin Barnard <caitlin@madetech.com>
2026-09-02 16:26:32 +0100

    Add tests for note api route

---
+++ packages/api/src/routes/v1/cases/[caseId]/note.e2e.test.ts
@@ -0,0 +1,64 @@
+import type { FastifyInstance } from "fastify"
+
+import { expect } from "@jest/globals"
+import { V1 } from "@moj-bichard7/common/apiEndpoints/versionedEndpoints"
+import { UserGroup } from "@moj-bichard7/common/types/UserGroup"
+import { BAD_REQUEST, NOT_FOUND, OK } from "http-status"
+
+import { createCase } from "../../../../tests/helpers/caseHelper"
+import { SetupAppEnd2EndHelper } from "../../../../tests/helpers/setupAppEnd2EndHelper"
+import { createUserAndJwtToken } from "../../../../tests/helpers/userHelper"
+
+const defaultRequest = (jwt: string, body: Record<string, any> = { noteText: "This is a note" }) => ({
+  body: JSON.stringify(body),
+  headers: { Authorization: `Bearer ${jwt}`, "Content-Type": "application/json" },
+  method: "POST"
+})
+
+describe("/V1/cases/:caseId/note", () => {
+  const endpoint = V1.Note
+  let helper: SetupAppEnd2EndHelper
+  let app: FastifyInstance
+
+  beforeAll(async () => {
+    helper = await SetupAppEnd2EndHelper.setup()
+    app = helper.app
+  })
+
+  beforeEach(async () => {
+    await helper.postgres.clearDb()
+  })
+
+  afterAll(async () => {
+    await app.close()
+    await helper.postgres.close()
+  })
+
+  it("receives 200 OK when note saved successfully", async () => {
+    const [encodedJwt] = await createUserAndJwtToken(helper.postgres, [UserGroup.Supervisor])
+    await createCase(helper.postgres)
+
+    const response = await fetch(`${helper.address}${endpoint.replace(":caseId", "1")}`, defaultRequest(encodedJwt))
+
+    expect(response.status).toBe(OK)
+  })
+
+  it("received 400 Bad Request when request body is invalid", async () => {
+    const [encodedJwt] = await createUserAndJwtToken(helper.postgres, [UserGroup.Supervisor])
+    await createCase(helper.postgres)
+    const invalidRequest = defaultRequest(encodedJwt, {})
+
+    const response = await fetch(`${helper.address}${endpoint.replace(":caseId", "1")}`, invalidRequest)
+
+    expect(response.status).toBe(BAD_REQUEST)
+  })
+
+  it("receives 404 Not Found when there is no case found", async () => {
+    const [encodedJwt] = await createUserAndJwtToken(helper.postgres, [UserGroup.Supervisor])
+    await createCase(helper.postgres)
+
+    const response = await fetch(`${helper.address}${endpoint.replace(":caseId", "2")}`, defaultRequest(encodedJwt))
+
+    expect(response.status).toBe(NOT_FOUND)
+  })
+})
---
+++ packages/api/src/routes/v1/cases/[caseId]/note.integration.test.ts
@@ -0,0 +1,72 @@
+import { expect } from "@jest/globals"
+import { V1 } from "@moj-bichard7/common/apiEndpoints/versionedEndpoints"
+import { UserGroup } from "@moj-bichard7/common/types/UserGroup"
+import { type FastifyInstance } from "fastify"
+import { BAD_REQUEST, NOT_FOUND, OK } from "http-status"
+
+import build from "../../../../app"
+import AuditLogDynamoGateway from "../../../../services/gateways/dynamo/AuditLogDynamoGateway/AuditLogDynamoGateway"
+import { createCase } from "../../../../tests/helpers/caseHelper"
+import auditLogDynamoConfig from "../../../../tests/helpers/dynamoDbConfig"
+import { createUserAndJwtToken } from "../../../../tests/helpers/userHelper"
+import End2EndPostgres from "../../../../tests/testGateways/e2ePostgres"
+
+describe("createNote", () => {
+  let app: FastifyInstance
+  const testDatabaseGateway = new End2EndPostgres()
+  const auditLogGateway = new AuditLogDynamoGateway(auditLogDynamoConfig)
+
+  beforeAll(async () => {
+    app = await build({ auditLogGateway, database: testDatabaseGateway })
+    await app.ready()
+  })
+
+  beforeEach(async () => {
+    await testDatabaseGateway.clearDb()
+    await createCase(testDatabaseGateway)
+  })
+
+  afterAll(async () => {
+    await testDatabaseGateway.close()
+    await app.close()
+  })
+
+  it("returns 200 OK when note saved successfully", async () => {
+    const [encodedJwt] = await createUserAndJwtToken(testDatabaseGateway, [UserGroup.Supervisor])
+
+    const response = await app.inject({
+      headers: { Authorization: `Bearer ${encodedJwt}`, "Content-Type": "application/json" },
+      method: "POST",
+      payload: { noteText: "This is a note" },
+      url: V1.Note.replace(":caseId", "1")
+    })
+
+    expect(response.statusCode).toBe(OK)
+  })
+
+  it("returns 400 Bad Request when request body is invalid", async () => {
+    const [encodedJwt] = await createUserAndJwtToken(testDatabaseGateway, [UserGroup.Supervisor])
+
+    const response = await app.inject({
+      headers: { Authorization: `Bearer ${encodedJwt}`, "Content-Type": "application/json" },
+      method: "POST",
+      payload: {},
+      url: V1.Note.replace(":caseId", "1")
+    })
+
+    expect(response.statusCode).toBe(BAD_REQUEST)
+  })
+
+  it("returns 404 Not Found when there's no case found", async () => {
+    const [encodedJwt] = await createUserAndJwtToken(testDatabaseGateway, [UserGroup.Supervisor])
+
+    const response = await app.inject({
+      headers: { Authorization: `Bearer ${encodedJwt}`, "Content-Type": "application/json" },
+      method: "POST",
+      payload: { noteText: "This is a note on a non-existent case" },
+      url: V1.Note.replace(":caseId", "2")
+    })
+
+    expect(response.statusCode).toBe(NOT_FOUND)
+  })
+})
