a7e1ca5f390999368ede293cb67b94c8baf06109
Richard Race <1273965+richrace@users.noreply.github.com>
2026-09-07 11:21:12 +0100

    Force install

--- .circleci/scripts/install-cypress.sh
+++ .circleci/scripts/install-cypress.sh
@@ -9,6 +9,6 @@ sudo apt-get update
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 
 # Install Cypress
-npx cypress install
+npx cypress install --force
 
 touch cypressInstalled
