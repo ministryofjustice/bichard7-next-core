f4dfdb8488d9ef6c1efcef0526d081aefa8b9154
Richard Race <1273965+richrace@users.noreply.github.com>
2026-09-07 11:35:11 +0100

    Update Cypress

--- .circleci/scripts/install-cypress.sh
+++ .circleci/scripts/install-cypress.sh
@@ -9,6 +9,4 @@ sudo apt-get update
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 
 # Install Cypress
-npx cypress install --force
-
-touch cypressInstalled
+npx cypress install && touch cypressInstalled
