220105bb4c71aba95833ce9fce0eeef3d6d48cc7
Tausif Patel <tausifpatel.dev@gmail.com>
2026-09-07 10:36:55 +0000

    Fix disposal mapping (#2213)

--- packages/core/lib/policeGateway/leds/mapToPoliceQueryResult.test.ts
+++ packages/core/lib/policeGateway/leds/mapToPoliceQueryResult.test.ts
@@ -1,10 +1,11 @@
 import type { AsnQueryResponse } from "../../../types/leds/AsnQueryResponse"
 
 import ledsAsnQueryResponse from "../../../tests/fixtures/leds-asn-query-response-001.json"
+import ledsAsnQueryResponseWithEmptyDisposals from "../../../tests/fixtures/leds-asn-query-response-002.json"
 import mapToPoliceQueryResult from "./mapToPoliceQueryResult"
 
 describe("mapToPoliceQueryResult", () => {
-  it("should map LEDS ASN query response to police query result", () => {
+  it("maps LEDS ASN query response to police query result", () => {
     const policeQueryResult = mapToPoliceQueryResult(ledsAsnQueryResponse as AsnQueryResponse, "dummy checkname")
 
     expect(policeQueryResult).toEqual({
@@ -127,5 +128,14 @@ describe("mapToPoliceQueryResult", () => {
       croNumber: undefined,
       penaltyCases: undefined
     })
+  })
+
+  it("sets offence disposals to undefined when disposalResults is empty", () => {
+    const policeQueryResult = mapToPoliceQueryResult(
+      ledsAsnQueryResponseWithEmptyDisposals as AsnQueryResponse,
+      "dummy checkname"
+    )
+
+    expect(policeQueryResult.courtCases?.[0].offences[0].disposals).toBeUndefined()
   })
 })
