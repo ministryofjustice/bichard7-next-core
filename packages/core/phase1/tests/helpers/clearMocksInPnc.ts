667a81f99fa3d893ee0adb5c691671784b216de1
Denise <denise.turkova@justice.gov.uk>
2026-09-08 12:04:51 +0100

    chore: replaced axios with fetch

--- packages/core/phase1/tests/helpers/clearMocksInPnc.ts
+++ packages/core/phase1/tests/helpers/clearMocksInPnc.ts
@@ -1,9 +1,7 @@
-import axios from "axios"
-
 import defaults from "./defaults"
 
 const clearMocksInPnc = async (): Promise<void> => {
-  const response = await axios.delete(`http://${defaults.pncHost}:${defaults.pncPort}/mocks`)
+  const response = await fetch(`http://${defaults.pncHost}:${defaults.pncPort}/mocks`)
   if (response.status !== 204) {
     throw new Error("Error clearing mocks in PNC Emulator")
   }
