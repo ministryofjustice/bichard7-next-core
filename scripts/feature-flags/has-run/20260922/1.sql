16666bf05e0ee88c25426f44ba2515f89f458287
timhowes-lab <tim.howes@madetech.com>
2026-09-22 15:18:26 +0100

    Auditing feature flag switch on

---
+++ scripts/feature-flags/has-run/20260922/1.sql
@@ -0,0 +1,10 @@
+BEGIN;
+UPDATE
+  br7own.users
+SET
+  feature_flags = jsonb_set(feature_flags, '{useTriggerAndExceptionQualityAuditingEnabled}', 'true', true)
+WHERE
+  deleted_at ISNULL AND
+  id IN (8416)
+RETURNING id, username, email;
+COMMIT;
